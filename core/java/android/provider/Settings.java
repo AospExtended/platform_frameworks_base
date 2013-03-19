d58a82ef27d495c201ee927adf2d32da65ff94d0
Sam Mortimer <sam@mortimer.me.uk>
2013-03-18 18:24:07 -0700

    [1/2] Forward port power connect/disconnect notification support
    Original commit info:
    
    [1/2] Power connect/disconnect notification support
    
    part 1/2: frameworks/base PowerUI and Settings
    http://review.cyanogenmod.org/#/c/35241/
    
    part 2/2: packages/apps/Settings Sound settings
    http://review.cyanogenmod.org/#/c/35242/
    
    V-Old_Change-Id: I36a6b9f924d2cd52191a8e83a744745b37c5b068
    
    OldChange-Id: Ib865e1c296cb8e1d957b2a48616c88ceaba5c008
    
    Change-Id: I5db20ef23bdb644e2af06e8dcbc8fbd0cf7ce321
    Signed-off-by: STELIX <ssspinni@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/power/PowerUI.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10901,6 +10901,24 @@ public final class Settings {
         public static final String WIRED_RINGTONE_FOCUS_MODE = "wired_ringtone_focus_mode";
 
         /**
+         * Whether to sound when charger power is connected/disconnected
+         * @hide
+         */
+        public static final String POWER_NOTIFICATIONS_ENABLED = "power_notifications_enabled";
+
+        /**
+         * Whether to vibrate when charger power is connected/disconnected
+         * @hide
+         */
+        public static final String POWER_NOTIFICATIONS_VIBRATE = "power_notifications_vibrate";
+
+        /**
+         * URI for power notification sounds
+         * @hide
+         */
+        public static final String POWER_NOTIFICATIONS_RINGTONE = "power_notifications_ringtone";
+
+        /**
          * Whether to wake the display when plugging or unplugging the charger
          * @hide
          */
