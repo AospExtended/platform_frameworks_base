6ffe2c91ecdf9337c22eec1de16275b8e5ade805
Danny Baumann <dannybaumann@web.de>
2013-08-22 08:53:24 +0200

    PowerManager: Re-integrate button brightness
    This is a squash of the following commits:
    
    Author: Danny Baumann <dannybaumann@web.de>
    Date:   Thu Aug 22 08:53:24 2013 +0200
    
        Add PowerManager integration for button and keyboard backlight.
    
        Allows setting button and keyboard backlight brightness as well as
        button timeout.
    
        Change-Id: I550cccafc0a8f90d6347de9261adb26b75955cc4
    
    Author: nadlabak <pavel@doshaska.net>
    Date:   Sun Jun 7 02:01:05 2015 +0200
    
        PowerManagerService: Fix updating of mUserActivitySummary
    
        I7f1fc35a1573717d1ea101a07c4171d6f66d1553 missed the fact that the primary
        purpose of the affected condition block was to update mUserActivitySummary
        and the button/keyboard light handling was just appended to it later.
    
        This fixes the waking from dream/screensaver by user activity.
    
        I30c5c8c9c09e3d57ace18cac72b783510b9b3bf3 is removed here as well as it was
        just a band aid.
    
        jira: NIGHTLIES-1285
    
        Change-Id: I6b2f6c58e73110787d62e86d4d2ef538638cf491
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Tue Dec 26 17:15:05 2017 +0000
    
        Forward-port button brightness implementation to O
    
         * Reworked for the new handler interface, restoring also removed
           methods (partial revert of commit 86c39f9edee88baa912c694061010483c7da9daf).
    
         * Keyboard backlight brightness support left out for now.
    
    Change-Id: I53f031fa2da394e95a2b29a01eb3c6a8f8132507
    Signed-off-by: mhkjahromi <m.h.k.jahromi@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4603,6 +4603,34 @@ public final class Settings {
                 "double_tap_sleep_lockscreen";
 
         /**
+         * The time in ms to keep the button backlight on after pressing a button.
+         * A value of 0 will keep the buttons on for as long as the screen is on.
+         * @hide
+         */
+        public static final String BUTTON_BACKLIGHT_TIMEOUT = "button_backlight_timeout";
+
+        /** @hide */
+        public static final Validator BUTTON_BACKLIGHT_TIMEOUT_VALIDATOR = NON_NEGATIVE_INTEGER_VALIDATOR;
+
+        /**
+         * The button brightness to be used while the screen is on or after a button press,
+         * depending on the value of {@link BUTTON_BACKLIGHT_TIMEOUT}.
+         * Valid value range is between 0 and {@link PowerManager#getMaximumButtonBrightness()}
+         * @hide
+         */
+        public static final String BUTTON_BRIGHTNESS = "button_brightness";
+
+        /** @hide */
+        public static final Validator BUTTON_BRIGHTNESS_VALIDATOR = NON_NEGATIVE_INTEGER_VALIDATOR;
+
+        /**
+         * IMPORTANT: If you add a new public settings you also have to add it to
+         * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
+         * it to PRIVATE_SETTINGS below. Also add a validator that can validate
+         * the setting value. See an example above.
+         */
+
+        /**
          *  Enable statusbar double tap gesture on to put device to sleep
          * @hide
          */
@@ -4912,6 +4940,8 @@ public final class Settings {
             PRIVATE_SETTINGS.add(QS_TILE_TITLE_VISIBILITY);
             PRIVATE_SETTINGS.add(QS_QUICKBAR_COLUMNS);
             PRIVATE_SETTINGS.add(BACK_GESTURE_HEIGHT);
+            PRIVATE_SETTINGS.add(BUTTON_BACKLIGHT_TIMEOUT);
+            PRIVATE_SETTINGS.add(BUTTON_BRIGHTNESS);
         }
 
 
@@ -5020,6 +5050,8 @@ public final class Settings {
             VALIDATORS.put(QS_TILE_TITLE_VISIBILITY, QS_TILE_TITLE_VISIBILITY_VALIDATOR);
             VALIDATORS.put(QS_QUICKBAR_COLUMNS, QS_QUICKBAR_COLUMNS_VALIDATOR);
             VALIDATORS.put(BACK_GESTURE_HEIGHT, BACK_GESTURE_HEIGHT_VALIDATOR);
+            VALIDATORS.put(BUTTON_BACKLIGHT_TIMEOUT, BUTTON_BACKLIGHT_TIMEOUT_VALIDATOR);
+            VALIDATORS.put(BUTTON_BRIGHTNESS, BUTTON_BRIGHTNESS_VALIDATOR);
         }
 
         /**
