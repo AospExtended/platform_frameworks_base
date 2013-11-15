f1ba7169c804af19e5b535031eee68d5165a0911
Kyrylo Mikos <kiril.mik.os@gmail.com>
2013-11-15 18:21:50 +0000

    Add ability to use EdgeGestureService for system gestures
    Update: Default to enabled.
    
    Change-Id: Id4e2c0d82f771cd1c20e9196fb6db347494cca69
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4819,6 +4819,11 @@ public final class Settings {
          */
         public static final String FULL_GESTURE_NAVBAR_DT2S = "full_gesture_navbar_dt2s";
 
+        /**
+         * Use EdgeGesture Service for system gestures in PhoneWindowManager
+         * @hide
+         */
+        public static final String USE_EDGE_SERVICE_FOR_GESTURES = "edge_service_for_gestures";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
