5cac78bdc184e9c94ac4cbbc183dce260830c9ec
Sam Mortimer <sam@mortimer.me.uk>
2014-11-16 20:03:53 -0800

    frameworks/base: Battery and Notification Lights [1/2]
    @mydongistiny: Ported to Pie. Moved all Lineage-SDK stuff to fwb.
    
    Original commits:
    
    Author: DvTonder <david.vantonder@gmail.com>
    Author: Ricardo Cerqueira <cyanogenmod@cerqueira.org>
    Date:   Mon Nov 4 00:57:51 2013 +0000
    Framework: Port CM9 features to CM10
    Change-Id: Ibd63116df90b06f6ce6adb8a0343059bbb999bfb
    
    Author: Pawit Pornkitprasan <p.pawit@gmail.com>
    Date:   Sun Dec 8 15:24:41 2013 +0700
    BatteryService: fix FC on boot until battery stat is present
    updateLightsLocked() can be called from CM's added SettingsObserver
    when battery stat is not present, causing an FC and a loop until
    battery stat is present.
    Change-Id: Ic4438fe50e98f1aa05ae1d0d26240bf9410fd92f
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Tue Dec 31 16:22:05 2013 -0800
    [2/2] Framework: instant led test
    Adds support a new notification extra boolean
    EXTRA_FORCE_SHOW_LIGHTS.  Used by settings
    notification light picker to force lights on
    when the screen is on.
    Change-Id: If0a42d32b28fe8c02ef5f7dd148db7eb478fac17
    
    Author: Michael Bestas <mikeioannina@gmail.com>
    Date:   Mon Aug 18 04:56:28 2014 +0300
    Add support for single color notification LED (1/2)
    Change-Id: I367af77036da9e87c6dd0df552ce4c56d945a44d
    
    Author: Danesh M <daneshm90@gmail.com>
    Date:   Thu, 12 Nov 2015 10:52:11 -0800
    Framework : Move System settings to CMSettings
    Change-Id: I4e9fb06db7b9ba05e4a7bbe11916bb2271af4727
    
    Author: Adnan Begovic <adnan@cyngn.com>
    Date:   Mon, 9 Nov 2015 16:26:00 -0800
    fw: Move battery light settings to CMSettings.
    Change-Id: I28e60473356b2a9af152df82d34ad7abc9918682
    
    Author: Steve Kondik <steve@cyngn.com>
    Date:   Thu Sep 24 11:27:59 2015 -0700
    lights: Automatically generate an LED color for notifications
    Change-Id: I7288e52499819a6a6c75ed9d9ba7cfa1b13c5669
    nms: Only generate LED colors if the device has a multicolored LED
    * Check the overlay value before doing any of this stuff.
    Change-Id: Iedfceba6bfc86b2761d8af57ecab51026bfa4c19
    Change-Id: I7288e52499819a6a6c75ed9d9ba7cfa1b13c5669
    
    Author: Adrian DC <radian.dc@gmail.com>
    Date:   Sat Oct 14 23:08:47 2017 +0200
    fw: Rebrand to LineageOS and cleanup for Android Oreo
    Change-Id: I21d424433bb52a17eea7974c4ea29a3a16fe1be5
    
    Author: AdrianDC <radian.dc@gmail.com>
    Date:   Sat Jul 18 12:20:51 2015 +0200
    Lights with Screen On [1/2]: Optional allowment of lights
    Change-Id: I2071147d8ddab80ba0e1e7310e785ac3e03b3c7c
    Lights with screen on: Don't disable leds after the lockscreen
    Change-Id: If8f5b867a34be09fb061bb7ad040b16730f4e263
    Framework : Move System settings to CMSettings
    Change-Id: I4e9fb06db7b9ba05e4a7bbe11916bb2271af4727
    fw: Rebrand to LineageOS and port for Android Oreo
    Change-Id: I65cfeb659fe516ef43aa84b1c6d2eb6876df202a
    Change-Id: If8f5b867a34be09fb061bb7ad040b16730f4e263
    
    Author: Michael W <baddaemon87@gmail.com>
    Date:   Mon Oct 9 22:04:00 2017 +0200
    Core: Battery warning levels are inclusive, not exclusive
    Change-Id: Ib35b154b6117f7e26b4a3a5aee9254dda3adda12
    
    Author: Adrian DC <radian.dc@gmail.com>
    Date:   Sat Oct 14 23:08:47 2017 +0200
    fw: Rebrand to LineageOS and cleanup for Android Oreo
    Change-Id: I845f866891386aee808a4e7e80f4ab7c3ad48830
    
    Change-Id: I564fdb6ca6c5bbcf890afa4b9a2f9fa7bf8b9604
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4233,6 +4233,140 @@ public final class Settings {
         public static final Validator NOTIFICATION_LIGHT_PULSE_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /**
+         * Whether to allow notifications with the screen on or DayDreams.
+         * The value is boolean (1 or 0). Default will always be false.
+         * @hide
+         */
+        public static final String NOTIFICATION_LIGHT_SCREEN_ON =
+                "notification_light_screen_on_enable";
+
+        private static final Validator NOTIFICATION_LIGHT_SCREEN_ON_VALIDATOR = BOOLEAN_VALIDATOR;
+
+        /**
+         * What color to use for the notification LED by default
+         * @hide
+         */
+        public static final String NOTIFICATION_LIGHT_PULSE_DEFAULT_COLOR =
+                "notification_light_pulse_default_color";
+
+        /**
+         * How long to flash the notification LED by default
+         * @hide
+         */
+        public static final String NOTIFICATION_LIGHT_PULSE_DEFAULT_LED_ON =
+                "notification_light_pulse_default_led_on";
+
+        /**
+         * How long to wait between flashes for the notification LED by default
+         * @hide
+         */
+        public static final String NOTIFICATION_LIGHT_PULSE_DEFAULT_LED_OFF =
+                "notification_light_pulse_default_led_off";
+
+        /**
+         * Whether to use the custom LED values for the notification pulse LED.
+         * @hide
+         */
+        public static final String NOTIFICATION_LIGHT_PULSE_CUSTOM_ENABLE =
+                "notification_light_pulse_custom_enable";
+
+        /**
+         * Which custom LED values to use for the notification pulse LED.
+         * @hide
+         */
+        public static final String NOTIFICATION_LIGHT_PULSE_CUSTOM_VALUES =
+                "notification_light_pulse_custom_values";
+
+        /**
+         * Contains the notifications light maximum brightness to use.
+         * Values range from 1 to 255
+         */
+        public static final String NOTIFICATION_LIGHT_BRIGHTNESS_LEVEL =
+                "notification_light_brightness_level";
+
+        /**
+         * Whether we automatically generate notification LED colors or just
+         * use the boring default.
+         *
+         * @hide
+         */
+        public static final String NOTIFICATION_LIGHT_COLOR_AUTO =
+                "notification_light_color_auto";
+
+        /**
+         * What color to use for the missed call notification LED
+         * @hide
+         */
+        public static final String NOTIFICATION_LIGHT_PULSE_CALL_COLOR =
+                "notification_light_pulse_call_color";
+
+        /**
+         * How long to flash the missed call notification LED
+         * @hide
+         */
+        public static final String NOTIFICATION_LIGHT_PULSE_CALL_LED_ON =
+                "notification_light_pulse_call_led_on";
+
+        /**
+         * How long to wait between flashes for the missed call notification LED
+         * @hide
+         */
+        public static final String NOTIFICATION_LIGHT_PULSE_CALL_LED_OFF =
+                "notification_light_pulse_call_led_off";
+        /**
+         * What color to use for the voicemail notification LED
+         * @hide
+         */
+        public static final String NOTIFICATION_LIGHT_PULSE_VMAIL_COLOR =
+                "notification_light_pulse_vmail_color";
+
+        /**
+         * How long to flash the voicemail notification LED
+         * @hide
+         */
+        public static final String NOTIFICATION_LIGHT_PULSE_VMAIL_LED_ON =
+                "notification_light_pulse_vmail_led_on";
+
+        /**
+         * How long to wait between flashes for the voicemail notification LED
+         * @hide
+         */
+        public static final String NOTIFICATION_LIGHT_PULSE_VMAIL_LED_OFF =
+                "notification_light_pulse_vmail_led_off";
+
+        /**
+         * Whether the battery light should be enabled (if hardware supports it)
+         * The value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_ENABLED = "battery_light_enabled";
+
+        /**
+         * Whether the battery LED should repeatedly flash when the battery is low
+         * on charge. The value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_PULSE = "battery_light_pulse";
+
+        /**
+         * What color to use for the battery LED while charging - low
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_LOW_COLOR = "battery_light_low_color";
+
+        /**
+         * What color to use for the battery LED while charging - medium
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_MEDIUM_COLOR = "battery_light_medium_color";
+
+        /**
+         * What color to use for the battery LED while charging - full
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_FULL_COLOR = "battery_light_full_color";
+
+        /**
          * Show pointer location on screen?
          * 0 = no
          * 1 = yes
@@ -4999,6 +5133,7 @@ public final class Settings {
             VIBRATE_ON_CONNECT,
             VIBRATE_ON_CALLWAITING,
             VIBRATE_ON_DISCONNECT,
+            NOTIFICATION_LIGHT_SCREEN_ON,
         };
 
         /**
@@ -5144,6 +5279,25 @@ public final class Settings {
             PRIVATE_SETTINGS.add(BUTTON_BACKLIGHT_ENABLE);
             PRIVATE_SETTINGS.add(BUTTON_BACKLIGHT_TIMEOUT);
             PRIVATE_SETTINGS.add(BUTTON_BACKLIGHT_ON_TOUCH_ONLY);
+            PRIVATE_SETTINGS.add(NOTIFICATION_LIGHT_PULSE_DEFAULT_COLOR);
+            PRIVATE_SETTINGS.add(NOTIFICATION_LIGHT_PULSE_DEFAULT_LED_ON);
+            PRIVATE_SETTINGS.add(NOTIFICATION_LIGHT_PULSE_DEFAULT_LED_OFF);
+            PRIVATE_SETTINGS.add(NOTIFICATION_LIGHT_PULSE_CUSTOM_ENABLE);
+            PRIVATE_SETTINGS.add(NOTIFICATION_LIGHT_PULSE_CUSTOM_VALUES);
+            PRIVATE_SETTINGS.add(NOTIFICATION_LIGHT_SCREEN_ON);
+            PRIVATE_SETTINGS.add(NOTIFICATION_LIGHT_COLOR_AUTO);
+            PRIVATE_SETTINGS.add(NOTIFICATION_LIGHT_BRIGHTNESS_LEVEL);
+            PRIVATE_SETTINGS.add(NOTIFICATION_LIGHT_PULSE_CALL_COLOR);
+            PRIVATE_SETTINGS.add(NOTIFICATION_LIGHT_PULSE_CALL_LED_ON);
+            PRIVATE_SETTINGS.add(NOTIFICATION_LIGHT_PULSE_CALL_LED_OFF);
+            PRIVATE_SETTINGS.add(NOTIFICATION_LIGHT_PULSE_VMAIL_COLOR);
+            PRIVATE_SETTINGS.add(NOTIFICATION_LIGHT_PULSE_VMAIL_LED_ON);
+            PRIVATE_SETTINGS.add(NOTIFICATION_LIGHT_PULSE_VMAIL_LED_OFF);
+            PRIVATE_SETTINGS.add(BATTERY_LIGHT_ENABLED);
+            PRIVATE_SETTINGS.add(BATTERY_LIGHT_PULSE);
+            PRIVATE_SETTINGS.add(BATTERY_LIGHT_LOW_COLOR);
+            PRIVATE_SETTINGS.add(BATTERY_LIGHT_MEDIUM_COLOR);
+            PRIVATE_SETTINGS.add(BATTERY_LIGHT_FULL_COLOR);
         }
 
 
@@ -5244,7 +5398,7 @@ public final class Settings {
             VALIDATORS.put(WIFI_STATIC_DNS1, WIFI_STATIC_DNS1_VALIDATOR);
             VALIDATORS.put(WIFI_STATIC_DNS2, WIFI_STATIC_DNS2_VALIDATOR);
             VALIDATORS.put(SHOW_BATTERY_PERCENT, SHOW_BATTERY_PERCENT_VALIDATOR);
-            VALIDATORS.put(NOTIFICATION_LIGHT_PULSE, BOOLEAN_VALIDATOR);
+            VALIDATORS.put(NOTIFICATION_LIGHT_SCREEN_ON, NOTIFICATION_LIGHT_SCREEN_ON_VALIDATOR);
             VALIDATORS.put(QS_LAYOUT_COLUMNS_LANDSCAPE,
                     QS_LAYOUT_COLUMNS_LANDSCAPE_VALIDATOR);
             VALIDATORS.put(QS_LAYOUT_COLUMNS, QS_LAYOUT_COLUMNS_VALIDATOR);
