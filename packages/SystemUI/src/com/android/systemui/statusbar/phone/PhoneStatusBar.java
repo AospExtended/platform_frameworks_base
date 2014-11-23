19c844e994d78128a2448091a0c4156f6d6ef65b
Allan Hedelain <tristan202@gmail.com>
2014-11-23 19:57:49 +0100

    Statusbar clock and date customizations [1/2]
    Some things changed on kitkat + our old code needed in general a bit
    rework. All done with this patchset
    
    This commit add support for:
    
    - Clock and date enable/disable
    - Clock and date position
    - AM/PM styles multilang support
    - color
    - Date format
    - Date style
    
    Originally by kufikugel. I only ported it to 5.0
    
    PS2:
    - several fixes and optimizations and code cleanup
    
    PS3:
    - more adaptions to LP
    
    PS4:
    - cleanup
    
    PS5:
    - animate hiding correctly on center clock mode
    
    Change-Id: I456bc81c0c981c04467c9908c164e7c3b6cb75d3
    
    SystemUI: Status bar clock - fixes
    
    This commit fixes:
    
    - no overlapping more from status bar icons on center clock.
      The width depending on what the user has setuped (date and clock) is exactly meassured now
    - do not start animations if they are not needed
    - reduce settings provider calls
    - a bit cleanup
    
    Forward ported to marshmallow by liquid0624
    
    Change-Id: I08892e9cd400812eae22385b6d53277f071d0240

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -44,6 +44,7 @@ import android.app.StatusBarManager;
 import android.content.BroadcastReceiver;
 import android.content.ComponentCallbacks2;
 import android.content.ComponentName;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -117,6 +118,7 @@ import android.view.accessibility.AccessibilityEvent;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Interpolator;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.android.internal.logging.MetricsLogger;
@@ -180,6 +182,7 @@ import com.android.systemui.statusbar.policy.CastControllerImpl;
 import com.android.systemui.statusbar.policy.EncryptionHelper;
 import com.android.systemui.statusbar.policy.FlashlightController;
 import com.android.systemui.statusbar.policy.HeadsUpManager;
+import com.android.systemui.statusbar.policy.Clock;
 import com.android.systemui.statusbar.policy.HotspotControllerImpl;
 import com.android.systemui.statusbar.policy.KeyButtonView;
 import com.android.systemui.statusbar.policy.KeyguardMonitor;
@@ -355,6 +358,12 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     Object mQueueLock = new Object();
 
     protected StatusBarIconController mIconController;
+
+    // center clock
+    LinearLayout mCenterClockLayout;
+
+    private boolean mShowClock;
+    private int mClockLocation;
 
     // expanded notifications
     protected NotificationPanelView mNotificationPanel; // the sliding/resizing panel within the notification window
@@ -804,7 +813,13 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         // figure out which pixel-format to use for the status bar.
         mPixelFormat = PixelFormat.OPAQUE;
 
-        mStackScroller = (NotificationStackScrollLayout) mStatusBarWindow.findViewById(
+        mCenterClockLayout = (LinearLayout)mStatusBarView.findViewById(R.id.center_clock_layout);
+        Clock cclock = (Clock) mStatusBarView.findViewById(R.id.center_clock);
+        if (cclock != null) {
+            cclock.setPhoneStatusBar(this);
+        }
+
+        mStackScroller = (NotificationStackScrollLayout) mStatusBarWindowContent.findViewById(
                 R.id.notification_stack_scroller);
         mStackScroller.setLongPressListener(getNotificationLongClicker());
         mStackScroller.setPhoneStatusBar(this);
@@ -2377,6 +2392,24 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 && mFalsingManager.isReportingEnabled() ? View.VISIBLE : View.INVISIBLE);
     }
 
+    public void showClock(boolean show) {
+        if (mStatusBarView == null) return;
+        ContentResolver resolver = mContext.getContentResolver();
+        View clock = mStatusBarView.findViewById(R.id.clock);
+        View cclock = mStatusBarView.findViewById(R.id.center_clock);
+        if (mClockLocation == 0 && clock != null) {
+            clock.setVisibility(show ? (mShowClock ? View.VISIBLE : View.GONE) : View.GONE);
+        }
+        if (mClockLocation == 1 && cclock != null) {
+            cclock.setVisibility(show ? (mShowClock ? View.VISIBLE : View.GONE) : View.GONE);
+        }
+    }
+
+    public void setClockAndDateStatus(int width, int mode, boolean enabled) {
+        mClockLocation = mode;
+        mShowClock = enabled;
+	}
+
     protected int adjustDisableFlags(int state) {
         if (!mLaunchTransitionFadingAway && !mKeyguardFadingAway
                 && (mExpandedVisible || mBouncerShowing || mWaitingForKeyguardExit)) {
@@ -2445,6 +2478,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         Log.d(TAG, flagdbg.toString());
 
         if ((diff1 & StatusBarManager.DISABLE_SYSTEM_INFO) != 0) {
+            mCenterClockLayout.animate().cancel();
             if ((state1 & StatusBarManager.DISABLE_SYSTEM_INFO) != 0) {
                 mIconController.hideSystemIconArea(animate);
             } else {
