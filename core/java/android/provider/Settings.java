8d2133be43633f0a5f095c9ac137e543e5b223c9
Danny Baumann <dannybaumann@web.de>
2014-11-30 23:04:37 -0600

    add configurable wake keys (1/2)
    fw/base: allow home button to wake device [1/2]
    
    Change-Id: I2b79561dcfa7e569b2e24bbabfffb11517d4d313
    
    Cleanup button wake settings (2/2)
    
    Change-Id: Ie37136cbd57c4c334321abbfa4543727e940bc43
    
    Keep quiet when volume keys are used to wake up device
    
    - Userspace will make a 'beep' with it receives a key up, so
      consume that event as well.
    - Removed wake key check in music control code as it will already
      be disabled here.
    
    Change-Id: I93839acd39aec8a2ee40291f833a31f6b048c9f8
    
    Wake Keys: enforce the wake keys overlay
    
    * Keys disabled as wake keys by the overlay were
      still being allowed to wake the device. This change
      enforces the hardwareWakeKeys settings.
    
    Change-Id: Ifde0491b2de64a7f61a101cf22f5589cb5e841e2
    
    Allow disabling Search/Recents button wake (2/2)
    
    Change-Id: I6a2ac064efc4fe85413bf0b935c28aa7bde5d672
    
    Android Policy: handle volume key event as wake key when preference is set
    
    Change-Id: If9a61cd65553bf00f0efda1a75b1ab75b9129090
    
    If a wake key is disabled by the user, do not wake from doze.
    
    Currently, any wake key will wake the device from a doze, even if that
    key has not been enabled as a wake key in Settings.
    
    If the device is 'dreaming' in the Doze state, check if the user has
    explicitly disabled the wake key (or never enabled the setting in the
    first place) before waking the device.
    
    Change-Id: I7397087c143161e8e1ddb84d0e23f6027fea0aac

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4643,6 +4643,36 @@ public final class Settings {
         public static final String CUSTOM_BUTTON_USE_SCREEN_BRIGHTNESS = "custom_button_use_screen_brightness";
 
         /**
+         * Whether to wake the screen with the home key, the value is boolean.
+         * @hide
+         */
+        public static final String HOME_WAKE_SCREEN = "home_wake_screen";
+
+        /**
+         * Whether to wake the screen with the back key, the value is boolean.
+         * @hide
+         */
+        public static final String BACK_WAKE_SCREEN = "back_wake_screen";
+
+        /**
+         * Whether to wake the screen with the menu key, the value is boolean.
+         * @hide
+         */
+        public static final String MENU_WAKE_SCREEN = "menu_wake_screen";
+
+        /**
+         * Whether to wake the screen with the assist key, the value is boolean.
+         * @hide
+         */
+        public static final String ASSIST_WAKE_SCREEN = "assist_wake_screen";
+
+        /**
+         * Whether to wake the screen with the app switch key, the value is boolean.
+         * @hide
+         */
+        public static final String APP_SWITCH_WAKE_SCREEN = "app_switch_wake_screen";
+
+        /**
          * disable all button brightness (manual and auto mode)
          *
          * @hide
