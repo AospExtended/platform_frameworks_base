855b5419ce9665211e9129586a2586b3aaef4fd6
Lars Greiss <kufikugel@googlemail.com>
2014-12-03 19:56:41 +0100

    Return: Smart Pulldown [1/2]
    Rewritten by @Beanstown106 for 6.0/7.0
    
    previously the detection for ongoing/persistent notifications were not working
    im not sure how long this has actually been broken but i cleaned things up a little bit
    also put to use the isOngoing boolean and properly put to use the hasActiveVisibleNotifications boolean
    
    current options now are off, no dismissable, no ongoing, and no notifications
    
    Credits: kufikugel, HardCorePawn, acardinal
    @mydongistiny: fixed for oreo
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    Change-Id: I4db534d47452a4d3ea07984b3b63343777bec9bf
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -457,6 +457,7 @@ public class NotificationPanelViewController extends PanelViewController {
     private Runnable mExpandAfterLayoutRunnable;
 
     private int mOneFingerQuickSettingsIntercept;
+    private int mQsSmartPullDown;
 
     /**
      * Is this a collapse that started on the panel where we should allow the panel to intercept
@@ -1375,6 +1376,15 @@ public class NotificationPanelViewController extends PanelViewController {
                 break;
         }
         showQsOverride &= mBarState == StatusBarState.SHADE;
+
+        if (mQsSmartPullDown == 1 && !hasActiveClearableNotifications()
+                || mQsSmartPullDown == 2 &&
+                !mEntryManager.hasActiveOngoingNotifications()
+                || mQsSmartPullDown == 3 && !mEntryManager.hasActiveVisibleNotifications()) {
+                showQsOverride = true;
+        }
+
+
 
         return twoFingerDrag || showQsOverride || stylusButtonClickDrag || mouseButtonClickDrag;
     }
@@ -2869,6 +2879,9 @@ public class NotificationPanelViewController extends PanelViewController {
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_SMART_PULLDOWN),
+                    false, this, UserHandle.USER_ALL);
             update();
         }
 
@@ -2892,6 +2905,11 @@ public class NotificationPanelViewController extends PanelViewController {
             mStatusBarLockedOnSecureKeyguard = Settings.System.getIntForUser(
                     resolver, Settings.System.STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD, 0,
                     UserHandle.USER_CURRENT) == 1;
+            mQsSmartPullDown = Settings.System.getIntForUser(resolver,
+                    Settings.System.QS_SMART_PULLDOWN, 0,
+                    UserHandle.USER_CURRENT);
+
+
         }
     }
 
