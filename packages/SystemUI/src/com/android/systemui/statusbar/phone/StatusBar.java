0a1ef392d5c45d7cc15c9c1fcb0b3db968af2b8d
Lars Greiss <kufikugel@googlemail.com>
2014-12-03 19:56:41 +0100

    Return: Smart Pulldown [1/2]
    Rewritten by @Beanstown106 for 6.0/7.0
    
    previously the detection for ongoing/persistent notifications were not working
    im not sure how long this has actually been broken but i cleaned things up a little bit
    also put to use the isOngoing boolean and properly put to use the hasActiveVisibleNotifications boolean
    
    current options now are off, no dismissable, no ongoing, and no notifications
    
    Credits: kufikugel, HardCorePawn, acardinal
    @mydongistiny: fixed for oreo
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    Change-Id: I4db534d47452a4d3ea07984b3b63343777bec9bf

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1574,6 +1574,18 @@ public class StatusBar extends SystemUI implements DemoMode,
         mEntryManager.updateNotifications();
     }
 
+    protected boolean hasActiveVisibleNotifications() {
+        return mEntryManager.getNotificationData().hasActiveVisibleNotifications();
+    }
+
+    protected boolean hasActiveOngoingNotifications() {
+        return mEntryManager.getNotificationData().hasActiveOngoingNotifications();
+    }
+
+    protected boolean hasActiveClearableNotificationsQS() {
+        return hasActiveClearableNotifications();
+    }
+
     protected void setAreThereNotifications() {
 
         if (SPEW) {
