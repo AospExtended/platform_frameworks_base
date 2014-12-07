d24d013c3d904d1eadc9386aeb54b8256d26fe41
fusionjack <dogfight60-fusionjack@yahoo.de>
2014-12-07 20:19:52 +0100

    Turn off debugging
    Change-Id: I37c1f9da1b7fb9b812f77239cb4aeb2861c43d1d
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2574,7 +2574,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 old1, state1, diff1));
             Log.d(TAG, String.format("disable2: 0x%08x -> 0x%08x (diff2: 0x%08x)",
                 old2, state2, diff2));
-        }
 
         StringBuilder flagdbg = new StringBuilder();
         flagdbg.append("disable: < ");
@@ -2601,6 +2600,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         flagdbg.append(((diff2  & StatusBarManager.DISABLE2_QUICK_SETTINGS) != 0) ? "* " : " ");
         flagdbg.append(">");
         Log.d(TAG, flagdbg.toString());
+
+        }
 
         if ((diff1 & StatusBarManager.DISABLE_SYSTEM_INFO) != 0) {
             if ((state1 & StatusBarManager.DISABLE_SYSTEM_INFO) != 0) {
