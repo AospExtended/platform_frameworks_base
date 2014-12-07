4cece72faa5eaec10af21393786bcaf66091c8d5
Kyrylo Mikos <kiril.mik.os@gmail.com>
2014-12-07 13:51:00 +0200

    Don't report existing menu key if it was remapped
    Change-Id: Iabad06cd42cebecdff6f0440899432513681b063
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- services/core/java/com/android/server/policy/WindowManagerPolicy.java
+++ services/core/java/com/android/server/policy/WindowManagerPolicy.java
@@ -1580,6 +1580,7 @@ public interface WindowManagerPolicy extends WindowManagerPolicyConstants {
      * Specifies whether there is an on-screen navigation bar separate from the status bar.
      */
     public boolean hasNavigationBar();
+    public boolean hasPermanentMenuKey();
 
     /**
      * Send some ActionHandler commands to WindowManager.
