d2386143cf9542cc03f3007541a56f6572fa5dfc
Lars Greiss <kufikugel@googlemail.com>
2015-02-12 21:12:10 +0500

    IME feature: Force to show always enter key [1/2]
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Change-Id: I06c5b7e4cbcddfe9d09ce2de6b2cea08ea3ddee9
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3751,6 +3751,12 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
         /**
+         * Forces formal text input.  1 to replace emoticon key with enter key.
+         * @hide
+         */
+        public static final String FORMAL_TEXT_INPUT = "formal_text_input";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
