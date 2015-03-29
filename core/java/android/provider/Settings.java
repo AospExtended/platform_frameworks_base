bf97cc59890762c53124f885704631f8a94561cb
maxwen <max.weninger@gmail.com>
2015-03-29 18:39:55 +0200

    [1/2] base: allow disable of screenshot shutter sound
    Change-Id: I47d52bba21170118af87d35376d81d7569587a2f
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5191,6 +5191,11 @@ public final class Settings {
         public static final String PROXIMITY_ON_WAKE = "proximity_on_wake";
 
         /**
+         * @hide
+         */
+        public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
