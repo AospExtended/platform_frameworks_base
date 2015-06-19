d5efe71c88e2c702deada0a0d9298c613412ee2e
LorDClockaN <davor@losinj.com>
2015-06-19 11:07:35 -0600

    Add Restart SystemUI to reboot menu
    aosip edit: Include a few utils
    
    Change-Id: Ie00ffd52fec29e47ab8613d6feaf065542b40bd3
    
    Conflicts:
    	core/res/res/values/custom_arrays.xml

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -66,6 +66,8 @@ import java.io.IOException;
 
 import java.lang.reflect.Method;
 
+import com.android.internal.util.aospextended.Helpers;
+
 public final class ShutdownThread extends Thread {
     // constants
     private static final String TAG = "ShutdownThread";
@@ -82,6 +84,7 @@ public final class ShutdownThread extends Thread {
     private static final int RADIO_STOP_PERCENT = 18;
     private static final int MOUNT_SERVICE_STOP_PERCENT = 20;
     private static final String SOFT_REBOOT = "soft_reboot";
+    private static final String SYSTEMUI_REBOOT = "systemui_reboot";
 
     // length of vibration before shutting down
     private static final int SHUTDOWN_VIBRATE_MS = 500;
@@ -226,7 +229,11 @@ public final class ShutdownThread extends Thread {
                                 if (selected != ListView.INVALID_POSITION) {
                                     String actions[] = context.getResources().getStringArray(
                                             com.android.internal.R.array.shutdown_reboot_actions);
-                                    if (selected >= 0 && selected < actions.length) {
+                                    if (actions[selected].equals(SYSTEMUI_REBOOT)) {
+                                        mReason = actions[selected];
+                                        doSystemUIReboot();
+                                        return;
+                                    } else if (selected >= 0 && selected < actions.length) {
                                         mReason = actions[selected];
                                         if (actions[selected].equals(SOFT_REBOOT)) {
                                             doSoftReboot();
@@ -263,6 +270,10 @@ public final class ShutdownThread extends Thread {
         } catch (RemoteException e) {
             Log.e(TAG, "failure trying to perform soft reboot", e);
         }
+    }
+
+    private static void doSystemUIReboot() {
+        Helpers.restartSystemUI();
     }
 
     private static class CloseDialogReceiver extends BroadcastReceiver
