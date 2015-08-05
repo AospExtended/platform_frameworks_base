aeaa6e4a79134be608bb5d220b00ef535405ac72
Randall Rushing <randall.rushing@gmail.com>
2015-08-05 16:56:06 +0000

    FwBools: Enable/disable ambient notification color inversion [1/2]
    Inspired by:
    https://github.com/DirtyUnicorns/android_frameworks_base/commit/243cf8148a4117745745c410c5847883d07efb7a
    
    Let's make this runtime configurable and set enabled by default.
    There is a little I/O here, but class instances are all over
    the place making observing not practical.
    
    Change-Id: I08aaed6139a1afea11a4437731904d77767972f2

--- packages/SystemUI/src/com/android/systemui/ViewInvertHelper.java
+++ packages/SystemUI/src/com/android/systemui/ViewInvertHelper.java
@@ -24,6 +24,8 @@ import android.content.res.Resources;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.view.View;
 
 import java.util.ArrayList;
@@ -38,6 +40,7 @@ public class ViewInvertHelper {
     private final ColorMatrix mGrayscaleMatrix = new ColorMatrix();
     private final long mFadeDuration;
     private final ArrayList<View> mTargets = new ArrayList<>();
+    private final Context mContext;
 
     public ViewInvertHelper(View v, long fadeDuration) {
         this(v.getContext(), fadeDuration);
@@ -45,6 +48,7 @@ public class ViewInvertHelper {
     }
     public ViewInvertHelper(Context context, long fadeDuration) {
         mFadeDuration = fadeDuration;
+        mContext = context;
     }
 
     private static ArrayList<View> constructArray(View target) {
@@ -91,8 +95,7 @@ public class ViewInvertHelper {
     }
 
     public void update(boolean invert) {
-        if (invert && Resources.getSystem().getBoolean(
-                com.android.internal.R.bool.config_invert_colors_on_doze)) {
+        if (invert && isInvertEnabled()) {
             updateInvertPaint(1f);
             for (int i = 0; i < mTargets.size(); i++) {
                 mTargets.get(i).setLayerType(View.LAYER_TYPE_HARDWARE, mDarkPaint);
@@ -124,5 +127,10 @@ public class ViewInvertHelper {
         } else {
             update(invert);
         }
+    }
+
+    private boolean isInvertEnabled() {
+        return Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                Settings.Secure.DOZE_NOTIFICATION_INVERT_ENABLED, 1, UserHandle.USER_CURRENT) == 1;
     }
 }
