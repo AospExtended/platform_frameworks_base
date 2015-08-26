869126fe68e9e6ece8f474a78b59e524ef09e253
d34d <clark@cyngn.com>
2015-08-25 19:13:46 -0700

    Show icon of package associated with Toast
    For all those times you have some random app or background service that posts
    a Toast and you have no idea who's posting it.  This adds an icon badge to the
    top left corner of the Toast to show the app's icon the Toast belongs to
    
    Change-Id: I82bf23664eea134f3b1f89ad5a99f6be73906ba8
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/android/widget/ToastPresenter.java
+++ core/java/android/widget/ToastPresenter.java
@@ -22,8 +22,10 @@ import android.annotation.Nullable;
 import android.app.INotificationManager;
 import android.app.ITransientNotificationCallback;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.graphics.PixelFormat;
 import android.os.IBinder;
 import android.os.RemoteException;
@@ -203,6 +205,19 @@ public class ToastPresenter {
 
         adjustLayoutParams(mParams, windowToken, duration, gravity, xOffset, yOffset,
                 horizontalMargin, verticalMargin);
+
+        ImageView appIcon = (ImageView) mView.findViewById(android.R.id.icon);
+        if (appIcon != null) {
+            PackageManager pm = mContext.getPackageManager();
+            Drawable icon = null;
+            try {
+                icon = pm.getApplicationIcon(mPackageName);
+            } catch (PackageManager.NameNotFoundException e) {
+                // nothing to do
+            }
+            appIcon.setImageDrawable(icon);
+        }
+
         if (mView.getParent() != null) {
             mWindowManager.removeView(mView);
         }
