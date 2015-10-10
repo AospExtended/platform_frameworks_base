2d290c18ca738a02a130b8e45f9b1a345ba8caf4
beanstown106 <nbenis106@gmail.com>
2015-10-10 03:43:59 -0400

    Advanced Reboot + Fixs (Squashed) * SystemUI: Add global reboot option -CAF * AdvancedReboot: Add advanced reboot options -CM * AdvancedReboot: Enhance power menu reboot option -CM * ShutdownThread: Fix longpress power for safemode -EuphoriaOS * ShutdownThread: Enable advanced reboot by default -Beanstown106 * AdvancedReboot: Enable Via overlay instead of Setting -Beanstown106 * AdvancedReboot: adapted to work on android marshmallow -Beanstown106 * AdvancedReboot: Remove keyguard restrictions on advanced reboot -Beanstown106 * AdvancedReboot: Remove and cleanup duplicate strings -Beanstown106 99% of people who flash roms want advanced reboot their was no need for it to be a setting
    Edited and squashed By:@BeansTown106
    
    Edits by @B--B: updated restart action for android 7.1

--- services/core/java/com/android/server/policy/GlobalActions.java
+++ services/core/java/com/android/server/policy/GlobalActions.java
@@ -42,6 +42,7 @@ import android.net.ConnectivityManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.IPowerManager;
 import android.os.Message;
 import android.os.RemoteException;
 import android.os.ServiceManager;
@@ -406,7 +407,14 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
         @Override
         public void onPress() {
-            mWindowManagerFuncs.reboot(false /* confirm */);
+            try {
+                IPowerManager pm = IPowerManager.Stub.asInterface(ServiceManager
+                        .getService(Context.POWER_SERVICE));
+                pm.reboot(true, null, false);
+            } catch (RemoteException e) {
+                Log.e(TAG, "PowerManager service died!", e);
+                return;
+            }
         }
     }
 
