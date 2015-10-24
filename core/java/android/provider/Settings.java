c1522c2ac0b0a4aac4900d9c1b5fcbb9062d545f
beanstown106 <nbenis106@gmail.com>
2015-10-24 23:10:16 +0200

    Allow to disable lockscreen media artwork
    abc: move the setting check to the custom observer
    and adapt code to latest android release
    
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Change-Id: I08e1cb4666447787477f394004e5e66a0f8c739b
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5555,6 +5555,12 @@ public final class Settings {
         public static final String LOCKSCREEN_MEDIA_BLUR = "lockscreen_media_blur";
 
         /**
+         * media artwork wallpaper blur level on lockscreen
+         * @hide
+         */
+        public static final String SHOW_LOCKSCREEN_MEDIA_ART = "show_lockscreen_media_art";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
