9a58c80c2aab15893528878203b4e18288db154c
Jmz <jmztaylor@gmail.com>
2015-10-29 22:56:21 +0000

    Disable Immersive Messages [1/2]
    Change-Id: Ie5559d8bb50374b3a13afb6efd91fdfb9a77d179

--- services/core/java/com/android/server/policy/ImmersiveModeConfirmation.java
+++ services/core/java/com/android/server/policy/ImmersiveModeConfirmation.java
@@ -379,6 +379,8 @@ public class ImmersiveModeConfirmation {
 
         @Override
         public void handleMessage(Message msg) {
+            if (Settings.System.getInt(mContext.getContentResolver(),
+                     Settings.System.DISABLE_IMMERSIVE_MESSAGE, 0) != 1) {
             switch(msg.what) {
                 case SHOW:
                     handleShow();
@@ -386,6 +388,9 @@ public class ImmersiveModeConfirmation {
                 case HIDE:
                     handleHide();
                     break;
+                }
+            } else {
+                handleHide();
             }
         }
     }
