ba4a1b6e611237c23b349962ba2bb56cc59f257b
Patrick Lower <devvortex@gmail.com>
2015-12-03 12:43:18 -0500

    WakeKeys: add missing home button to wake enabled checks in PhoneWindowManager
    Change-Id: Ie12c16d91ce0a555c402301e2e82cd76f0b99751
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6393,6 +6393,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case KeyEvent.KEYCODE_CAMERA:
             case KeyEvent.KEYCODE_FOCUS:
                 return mCameraWakeScreen;
+            case KeyEvent.KEYCODE_HOME:
+                return mHomeWakeScreen;
         }
         return true;
     }
@@ -6438,6 +6440,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case KeyEvent.KEYCODE_CAMERA:
             case KeyEvent.KEYCODE_FOCUS:
                 return mCameraWakeScreen;
+            case KeyEvent.KEYCODE_HOME:
+                return mHomeWakeScreen;
         }
         return true;
     }
