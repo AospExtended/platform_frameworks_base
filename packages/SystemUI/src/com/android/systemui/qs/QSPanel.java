6b6d73690da202b3af60cf482bcaf011492c8c83
Varun <date.varun123@gmail.com>
2016-01-07 21:52:27 +0530

    Add option to disable auto brightness icon in brightness slider [1/2]
    - Off by default because this is not everyone's cup of tea
    - Cleaned up this commit a bit
    
    Change-Id: I73e848d523bb915fa07fd67985f7f07ff40cba9c
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -33,6 +33,7 @@ import android.database.ContentObserver;
 import android.metrics.LogMaker;
 import android.os.Bundle;
 import android.net.Uri;
+import android.os.UserHandle;
 import android.os.Handler;
 import android.os.Message;
 import android.os.UserHandle;
@@ -91,6 +92,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
     protected final Context mContext;
     protected final ArrayList<TileRecord> mRecords = new ArrayList<>();
     protected final View mBrightnessView;
+    protected final ImageView mBrightnessIcon;
     private final H mHandler = new H();
     private final MetricsLogger mMetricsLogger = Dependency.get(MetricsLogger.class);
     private final QSTileRevealController mQsTileRevealController;
@@ -99,6 +101,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
     protected boolean mListening;
 
     private SettingObserver mSettingObserver;
+    private boolean mShowBrightnessSlider = true;
 
     private QSDetail.Callback mCallback;
     private BrightnessController mBrightnessController;
@@ -137,9 +140,9 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
 
         mBrightnessView = LayoutInflater.from(mContext).inflate(
             R.layout.quick_settings_brightness_dialog, this, false);
-        ImageView brightnessIcon = (ImageView) findViewById(R.id.brightness_icon);
-        brightnessIcon.setVisibility(View.VISIBLE);
         addView(mBrightnessView);
+
+        mBrightnessIcon = (ImageView) findViewById(R.id.brightness_icon);
 
         mTileLayout = (QSTileLayout) LayoutInflater.from(mContext).inflate(
                 R.layout.qs_paged_tile_layout, this, false);
@@ -157,7 +160,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         updateResources();
 
         mBrightnessController = new BrightnessController(getContext(),
-                brightnessIcon,
+                mBrightnessIcon,
                 findViewById(R.id.brightness_slider));
         mDumpController = dumpController;
     }
@@ -260,6 +263,18 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
             }
         }
         return mHost.createTile(subPanel);
+    }
+
+    private void setBrightnessIcon() {
+        boolean brightnessIconEnabled = Settings.System.getIntForUser(
+            mContext.getContentResolver(), Settings.System.QS_SHOW_BRIGHTNESS_ICON,
+                0, UserHandle.USER_CURRENT) == 1;
+        if (mShowBrightnessSlider) {
+            mBrightnessIcon.setVisibility(brightnessIconEnabled ? View.VISIBLE : View.GONE);
+        } else {
+            mBrightnessIcon.setVisibility(View.GONE);
+        }
+        updateResources();
     }
 
     public void setBrightnessMirror(BrightnessMirrorController c) {
@@ -411,6 +426,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         } else {
             mBrightnessController.unregisterCallbacks();
         }
+        setBrightnessIcon();
     }
 
     public void refreshAllTiles() {
@@ -491,10 +507,10 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         }
 
         public void update() {
-            boolean brightnessSliderEnabled = Settings.Secure.getIntForUser(
+            mShowBrightnessSlider = Settings.Secure.getIntForUser(
             mContext.getContentResolver(), Settings.Secure.QS_SHOW_BRIGHTNESS_SLIDER,
                 1, UserHandle.USER_CURRENT) == 1;
-            mBrightnessView.setVisibility(brightnessSliderEnabled ? View.VISIBLE : View.GONE);
+            mBrightnessView.setVisibility(mShowBrightnessSlider ? View.VISIBLE : View.GONE);
         }
     }
 
