cb29f15f5cdd23728cdeeda42965fa44353e067d
d34d <clark@cyngn.com>
2016-01-19 10:02:08 -0800

    SysUI: Guard against NPE in SignalClusterView
    It is possible for mSC to be nulled out before the Runnable posted
    in onStateChanged is run, which will cause an NPE when it does run.
    This patch adds a null check to avoid this scenario.
    
    Change-Id: Ief660ad97899785f83bdb49fb50c42bbf7416852
    TICKET: CYNGNOS-1657

--- packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
@@ -251,9 +251,11 @@ public class SignalClusterView
         post(new Runnable() {
             @Override
             public void run() {
-                mVpnVisible = mSC.isVpnEnabled();
-                mVpnIconId = currentVpnIconId(mSC.isVpnBranded());
-                apply();
+                if (mSC != null) {
+                    mVpnVisible = mSC.isVpnEnabled();
+                    mVpnIconId = currentVpnIconId(mSC.isVpnBranded());
+                    apply();
+                }
             }
         });
     }
