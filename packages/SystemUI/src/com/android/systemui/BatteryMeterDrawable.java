72c1fe3325092d35477a1a317132899d99c988af
maxwen <max.weninger@gmail.com>
2016-02-07 23:49:56 +0100

    [1/2] base: battery styles are back[SQUASH]
    Change-Id: I0b457e8d93af4d63c94ce0784733331d825926fe
    
    base: some battery styles need smaller font to show 100
    
    Change-Id: I3d8105c4f5a193d962415bc030afb5a2e041b8f5
    
    base: battery styles: need to check special 100% font every time
    
    Change-Id: Iaba666722f48eaf35dc1fa050e0de12b1f03290d
    
    base: adjust battery style text
    
    use same font style as the clock
    
    Change-Id: I85ba0214b0d6eaa2c9e8c3bc934be2608dd5ff2a
    
    base: tweak battery text alignment
    
    lets face it - 100% battery is something you wont see very often
    so its makes no sense to create default layout for that and
    create useless space in 99% of the time.
    
    Instead make the font smaller in that case and use shorter
    text for the default layout.
    
    [1/2] base: add battery display config
    everything else on the statusbar can be hidden
    why not also the battery if you want
    
    Change-Id: Id105fc5f950fb31bd0a346d0bed246380991719e
    
    Conflicts:
    	core/res/res/values/custom_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
    
    base: horizontal battery style: change charging symbol positioning
    
    Change-Id: I7ce7b1deba354570debb814b6c1603d1f3820588
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    battery style: enable dark intensity when needed
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
    
    Base: Remove Battery color options [1/2]
    
    * Also remove DroidView battery icon
    
    Conflicts:
    	core/res/res/values/custom_strings.xml
    	packages/SystemUI/src/com/android/systemui/omni/AbstractBatteryView.java
    	packages/SystemUI/src/com/android/systemui/omni/BatteryDroidView.java
    
    base: battery style: fix layout changes
    
    react immediately to font size and density changes
    recalc layout when switching between font sizes
    for 99 vs. 100%
    
    Change-Id: Ieec30c3ff6d0029d9da4d5a6390d7535222c4db7
    Signed-off-by: ishubhamsingh <coolsks94@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/omni/AbstractBatteryView.java
    	packages/SystemUI/src/com/android/systemui/omni/BatteryDroidView.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java

--- packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
@@ -68,7 +68,6 @@ public class BatteryMeterDrawable extends Drawable implements
     private int mWidth;
     private String mWarningString;
     private final int mCriticalLevel;
-    private int mChargeColor;
     private final float[] mBoltPoints;
     private final Path mBoltPath = new Path();
     private final float[] mPlusPoints;
@@ -159,8 +158,6 @@ public class BatteryMeterDrawable extends Drawable implements
         font = Typeface.create("sans-serif", Typeface.BOLD);
         mWarningTextPaint.setTypeface(font);
         mWarningTextPaint.setTextAlign(Paint.Align.CENTER);
-
-        mChargeColor = context.getColor(R.color.batterymeter_charge_color);
 
         mBoltPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mBoltPaint.setColor(context.getColor(R.color.batterymeter_bolt_color));
@@ -328,7 +325,6 @@ public class BatteryMeterDrawable extends Drawable implements
         mIconTint = fillColor;
         mFramePaint.setColor(backgroundColor);
         mBoltPaint.setColor(fillColor);
-        mChargeColor = fillColor;
         invalidateSelf();
         mOldDarkIntensity = darkIntensity;
     }
@@ -382,7 +378,7 @@ public class BatteryMeterDrawable extends Drawable implements
         mFrame.bottom -= mSubpixelSmoothingRight;
 
         // set the battery charging color
-        mBatteryPaint.setColor(mPluggedIn ? mChargeColor : getColorForLevel(level));
+        mBatteryPaint.setColor(getColorForLevel(level));
 
         if (level >= FULL) {
             drawFrac = 1f;
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -121,8 +121,9 @@ import android.view.WindowManagerGlobal;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Interpolator;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-
+import android.widget.LinearLayout;
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.internal.statusbar.NotificationVisibility;
@@ -152,6 +153,7 @@ import com.android.systemui.keyguard.KeyguardViewMediator;
 import com.android.systemui.navigation.NavigationController;
 import com.android.systemui.navigation.Navigator;
 import com.android.systemui.qs.QSContainer;
+import com.android.systemui.omni.BatteryViewManager;
 import com.android.systemui.qs.QSPanel;
 import com.android.systemui.recents.ScreenPinningRequest;
 import com.android.systemui.recents.events.EventBus;
@@ -420,6 +422,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     int[] mAbsPos = new int[2];
     ArrayList<Runnable> mPostCollapseRunnables = new ArrayList<>();
+
+    // battery styles
+    private BatteryViewManager mBatteryViewManager;
 
     private boolean mAutomaticBrightness;
     private boolean mBrightnessControl;
@@ -1036,7 +1041,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         // set the initial view visibility
         setAreThereNotifications();
 
-        createIconController();
 
         // Background thread for any controllers that need it.
         mHandlerThread = new HandlerThread(TAG, Process.THREAD_PRIORITY_BACKGROUND);
@@ -1059,6 +1063,13 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 // noop
             }
         });
+
+        // must be before StatusBarIconController
+        LinearLayout batteryContainer = (LinearLayout) mStatusBarView.findViewById(R.id.battery_container);
+        mBatteryViewManager = new BatteryViewManager(mContext, batteryContainer, mStatusBarView.getBarTransitions(), null);
+
+        createIconController();
+
         mNetworkController = new NetworkControllerImpl(mContext, mHandlerThread.getLooper());
         mNetworkController.setUserSetupComplete(mUserSetup);
         mHotspotController = new HotspotControllerImpl(mContext);
@@ -1128,8 +1139,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mKeyguardStatusBar.setUserSwitcherController(mUserSwitcherController);
         mUserInfoController.reloadUserInfo();
 
-        ((BatteryMeterView) mStatusBarView.findViewById(R.id.battery)).setBatteryController(
-                mBatteryController);
+        mBatteryViewManager.setBatteryController(mBatteryController);
+        mHeader.setBatteryController(mBatteryController);
         mKeyguardStatusBar.setBatteryController(mBatteryController);
 
         mReportRejectedTouch = mStatusBarWindow.findViewById(R.id.report_rejected_touch);
@@ -3957,6 +3968,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mLockscreenWallpaper.setCurrentUser(newUserId);
         mScrimController.setCurrentUser(newUserId);
         updateMediaMetaData(true, false);
+        mBatteryViewManager.update();
     }
 
     private void setControllerUsers() {
@@ -5436,5 +5448,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 }
             }
         }
+    }
+
+    protected BatteryViewManager getBatteryViewManager() {
+        return mBatteryViewManager;
     }
 }
