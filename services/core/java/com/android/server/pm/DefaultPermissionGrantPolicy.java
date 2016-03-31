28a719101d63652c44d1eee975a645e799fd8aca
mydongistiny <jaysonedson@gmail.com>
2016-03-30 17:06:26 -0700

    Permissions: Fix permissons for ContactsProvider2
    Fix this error:
    
    W ActivityManager: Permission Denial: opening provider com.android.providers.contacts.ContactsProvider2 from ProcessRecord{3ca8b67 13435:com.google.android.apps.plus/u0a73} (pid=13435, uid=10073) requires android.permission.READ_CONTACTS or android.permission.WRITE_CONTACTS
    E GooglePlusContactsSync: Failed to clear out contacts
    E GooglePlusContactsSync: java.lang.SecurityException: Permission Denial: opening provider com.android.providers.contacts.ContactsProvider2 from ProcessRecord{3ca8b67 13435:com.google.android.apps.plus/u0a73} (pid=13435, uid=10073) requires android.permission.READ_CONTACTS or android.permission.WRITE_CONTACTS

--- services/core/java/com/android/server/pm/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/DefaultPermissionGrantPolicy.java
@@ -764,8 +764,6 @@ final class DefaultPermissionGrantPolicy {
                 grantRuntimePermissionsLPw(vendingPackage, SMS_PERMISSIONS, userId);
                 grantRuntimePermissionsLPw(vendingPackage, STORAGE_PERMISSIONS, userId);
             }
-<<<<<<< HEAD
-=======
 
             // Project Fi
             PackageParser.Package fiPackage = getDefaultProviderAuthorityPackageLPr(
@@ -777,7 +775,14 @@ final class DefaultPermissionGrantPolicy {
                 grantRuntimePermissionsLPw(fiPackage, LOCATION_PERMISSIONS, userId);
                 grantRuntimePermissionsLPw(fiPackage, SMS_PERMISSIONS, userId);
             }
->>>>>>> 4d871b1... Fix Fi permissions
+
+            // ContactsProvider2
+            PackageParser.Package conpro2Package = getDefaultProviderAuthorityPackageLPr(
+                    "com.android.providers.contacts.ContactsProvider2", userId);
+            if (conpro2Package != null) {
+                grantRuntimePermissionsLPw(conpro2Package, CONTACTS_PERMISSIONS, true, userId);
+                grantRuntimePermissionsLPw(conpro2Package, STORAGE_PERMISSIONS, true, userId);
+            }
             mService.mSettings.onDefaultRuntimePermissionsGrantedLPr(userId);
         }
     }
