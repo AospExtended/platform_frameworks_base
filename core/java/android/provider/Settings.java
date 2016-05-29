534621cb801e1fd93e71375b4c6a72d7650de831
Nico60 <ndhouailly@gmail.com>
2016-05-29 13:05:37 +0200

    Add interpolators to qs tiles animation [1/2]
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: Iaa1923e378e54ea452a2006dcade2bb1cd049d21

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4820,6 +4820,13 @@ public final class Settings {
         public static final String ANIM_TILE_DURATION = "anim_tile_duration";
 
         /**
+         * Change quick settings tiles interpolator
+         *
+         * @hide
+         */
+        public static final String ANIM_TILE_INTERPOLATOR = "anim_tile_interpolator";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
