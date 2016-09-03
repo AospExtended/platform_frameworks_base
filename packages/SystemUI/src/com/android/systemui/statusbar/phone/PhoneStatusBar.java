81ae6662deb540fefe4e5ec96f21b79382c1abca
Calin <nilac8991@gmail.com>
2016-09-03 23:34:47 +0200

    Blur: Color Filters implementation (1/2)
    - Since a part of the code was already available in the Blur base code, let's fix the remaining stuff and use it.
    
    How does it works?
    
    - It uses the lightness level of the background in use to determine which color to apply for the Blur bckground. So for example the background is a more lighter one a different color will be used and so on...
    - If this cannot be determined, a third type of color filter will be used which is like a sort of mixed combination of the other two  ones, which kinda good looking.
    
    There are three color filters:
    
    - Light color filter (Used for Light backgrounds)
    - Dark color filter (Used for Dark backgrounds)
    - Mixed color filter (Used for Dark/light backgrounds)
    
    - I made the colors to be applied simultaneously for both Blur backgrounds: StatusBar and RecentApps ones.
    - I took this decision because in my opinion there's no reason to make three more different ColorPicker preferences just for Recents, if there will be requests on that it will be implemented. But for now we stick to this.
    
    Change-Id: Ia358a62db891140eaf04c0085f373835e2a25bae
    Signed-off-by: Calin <nilac8991@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -57,6 +57,7 @@ import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.ColorFilter;
 import android.graphics.PixelFormat;
 import android.graphics.Point;
@@ -427,6 +428,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private boolean mBlurredRecents;
     private int mRadiusRecents;
     private int mScaleRecents;
+    private int mBlurDarkColorFilter;
+    private int mBlurMixedColorFilter;
+    private int mBlurLightColorFilter;
 
     // RemoteInputView to be activated after unlock
     private View mPendingRemoteInputView;
@@ -604,6 +608,15 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.RECENT_APPS_RADIUS_PREFERENCE_KEY), 
                     false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.BLUR_DARK_COLOR_PREFERENCE_KEY), 
+                    false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.BLUR_LIGHT_COLOR_PREFERENCE_KEY), 
+                    false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.BLUR_MIXED_COLOR_PREFERENCE_KEY), 
+                    false, this);
             updateSettings();
         }
 
@@ -684,7 +697,15 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mScaleRecents = Settings.System.getInt(mContext.getContentResolver(),
                     Settings.System.RECENT_APPS_SCALE_PREFERENCE_KEY, 6);
             mRadiusRecents = Settings.System.getInt(mContext.getContentResolver(),
-                    Settings.System.RECENT_APPS_RADIUS_PREFERENCE_KEY, 3);
+                    Settings.System.RECENT_APPS_RADIUS_PREFERENCE_KEY, 3);         
+            mBlurDarkColorFilter = Settings.System.getInt(mContext.getContentResolver(), 
+                    Settings.System.BLUR_DARK_COLOR_PREFERENCE_KEY, Color.LTGRAY);
+            mBlurMixedColorFilter = Settings.System.getInt(mContext.getContentResolver(), 
+                    Settings.System.BLUR_MIXED_COLOR_PREFERENCE_KEY, Color.GRAY);
+            mBlurLightColorFilter = Settings.System.getInt(mContext.getContentResolver(), 
+                    Settings.System.BLUR_LIGHT_COLOR_PREFERENCE_KEY, Color.DKGRAY);
+                    
+            RecentsActivity.updateBlurColors(mBlurDarkColorFilter,mBlurMixedColorFilter,mBlurLightColorFilter);
             RecentsActivity.updateRadiusScale(mScaleRecents,mRadiusRecents);
             mClockLocation = Settings.System.getIntForUser(
                 resolver, Settings.System.STATUSBAR_CLOCK_STYLE, 0,
