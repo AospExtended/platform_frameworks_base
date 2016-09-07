46ce338dd22e3a053d3b0287807e1d84e7f31141
Alex <mazdarider23@gmail.com>
2016-09-07 03:22:13 -0400

    Flashlight notification toggle [1/2]
    - Off by default
    
    Change-Id: Ic40da4aa7a67818725c971c1ee018fc8b7bd4535

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/FlashlightController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/FlashlightController.java
@@ -29,6 +29,7 @@ import android.hardware.camera2.CameraManager;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Process;
+import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -53,6 +54,8 @@ public class FlashlightController {
             "com.android.systemui.action.TURN_FLASHLIGHT_OFF";
 
     private Context mContext;
+
+
     private final CameraManager mCameraManager;
     /** Call {@link #ensureHandler()} before using */
     private Handler mHandler;
@@ -79,7 +82,12 @@ public class FlashlightController {
                     }
                 });
             } else if (Intent.ACTION_SCREEN_ON.equals(intent.getAction())) {
+                if (Settings.System.getInt(mContext.getContentResolver(),
+                      Settings.System.FLASHLIGHT_NOTIFICATION, 0) == 1) {
                 setNotificationShown(true);
+                } else {
+                setNotificationShown(false);
+                }
             }
         }
     };
@@ -142,6 +150,12 @@ public class FlashlightController {
             filter.addAction(Intent.ACTION_SCREEN_ON);
             mContext.registerReceiver(mReceiver, filter);
             mReceiverRegistered = true;
+            if (Settings.System.getInt(mContext.getContentResolver(),
+                  Settings.System.FLASHLIGHT_NOTIFICATION, 0) == 1) {
+            setNotificationShown(true);
+            } else {
+            setNotificationShown(false);
+            }
         } else if (!listen) {
             if (mReceiverRegistered) {
                 mContext.unregisterReceiver(mReceiver);
