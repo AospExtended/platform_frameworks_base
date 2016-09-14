489beb09239f7dd29660447009ffc7a85875dae8
Nico60 <ndhouailly@gmail.com>
2016-09-14 19:34:01 +0800

    Hide arrows in network traffic indicators [1/2]
    Change-Id: Iec9e5a111c871f2d6f0cf9f233df581066d2b460

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3754,6 +3754,12 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
 
         /**
+         * Whether to disable showing arrows in network traffic indicators
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_HIDEARROW = "network_traffic_hidearrow";
+
+        /**
          * Disable dashboard conditions in Settings
          * @hide
          */
