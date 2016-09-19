c0ebc297f76911cdb7c3b64f79002a18dafbf3db
B--B <dark.schneider.ds@gmail.com>
2016-09-19 20:59:17 +0200

    Substratum: updates to match latest official changes merged on TeamSubstratum git

--- packages/SystemUI/src/com/android/systemui/ViewInvertHelper.java
+++ packages/SystemUI/src/com/android/systemui/ViewInvertHelper.java
@@ -20,7 +20,6 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
 import android.content.Context;
-import android.content.res.Resources;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
@@ -91,8 +90,7 @@ public class ViewInvertHelper {
     }
 
     public void update(boolean invert) {
-        if (invert && Resources.getSystem().getBoolean(
-                com.android.internal.R.bool.config_invert_colors_on_doze)) {
+        if (invert) {
             updateInvertPaint(1f);
             for (int i = 0; i < mTargets.size(); i++) {
                 mTargets.get(i).setLayerType(View.LAYER_TYPE_HARDWARE, mDarkPaint);
