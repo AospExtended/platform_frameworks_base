088680183f92c629e32f3ca4f3927a8ac92906d7
camcory <camcory1@gmail.com>
2016-09-25 16:26:08 -0400

    Remove battery percentage SystemUI Tuner setting
    Change-Id: Ic37190e64ef56a4db9894a194c4fa82bd49b00bf
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -25,13 +25,11 @@ import android.widget.ImageView;
 
 import com.android.systemui.statusbar.phone.StatusBarIconController;
 import com.android.systemui.statusbar.policy.BatteryController;
-import com.android.systemui.tuner.TunerService;
 
 public class BatteryMeterView extends ImageView implements
-        BatteryController.BatteryStateChangeCallback, TunerService.Tunable {
+        BatteryController.BatteryStateChangeCallback {
 
     private final BatteryMeterDrawable mDrawable;
-    private final String mSlotBattery;
     private BatteryController mBatteryController;
 
     public BatteryMeterView(Context context) {
@@ -52,8 +50,6 @@ public class BatteryMeterView extends ImageView implements
         mDrawable = new BatteryMeterDrawable(context, new Handler(), frameColor);
         atts.recycle();
 
-        mSlotBattery = context.getString(
-                com.android.internal.R.string.status_bar_battery);
         setImageDrawable(mDrawable);
     }
 
@@ -63,19 +59,10 @@ public class BatteryMeterView extends ImageView implements
     }
 
     @Override
-    public void onTuningChanged(String key, String newValue) {
-        if (StatusBarIconController.ICON_BLACKLIST.equals(key)) {
-            ArraySet<String> icons = StatusBarIconController.getIconBlacklist(newValue);
-            setVisibility(icons.contains(mSlotBattery) ? View.GONE : View.VISIBLE);
-        }
-    }
-
-    @Override
     public void onAttachedToWindow() {
         super.onAttachedToWindow();
         mBatteryController.addStateChangedCallback(this);
         mDrawable.startListening();
-        TunerService.get(getContext()).addTunable(this, StatusBarIconController.ICON_BLACKLIST);
     }
 
     @Override
@@ -83,7 +70,6 @@ public class BatteryMeterView extends ImageView implements
         super.onDetachedFromWindow();
         mBatteryController.removeStateChangedCallback(this);
         mDrawable.stopListening();
-        TunerService.get(getContext()).removeTunable(this);
     }
 
     @Override
