cbede9a1b7424f1cc7f47cc5c20caa574d1826e2
SpiritCroc <spiritcroc@gmail.com>
2016-10-02 10:03:37 +0200

    Battery bar enhancements [1/2]
    Add color settings for charging & low battery
    Add reversed alignment setting
    
    Change-Id: I0ff3831d046689a30941718e4e9e4d301a65ff53
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryBarController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryBarController.java
@@ -42,6 +42,7 @@ public class BatteryBarController extends LinearLayout {
 
     public static final int STYLE_REGULAR = 0;
     public static final int STYLE_SYMMETRIC = 1;
+    public static final int STYLE_REVERSE = 2;
 
     int mStyle = STYLE_REGULAR;
     int mLocation = 0;
@@ -183,7 +184,11 @@ public class BatteryBarController extends LinearLayout {
                 addView(bar2, (new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT,
                         LayoutParams.MATCH_PARENT, 1)));
             }
-
+        } else if (mStyle == STYLE_REVERSE) {
+            BatteryBar bar = new BatteryBar(mContext, mBatteryCharging, mBatteryLevel, isVertical);
+            bar.setRotation(180);
+            addView(bar, new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT,
+                        LayoutParams.MATCH_PARENT, 1));
         }
     }
 
