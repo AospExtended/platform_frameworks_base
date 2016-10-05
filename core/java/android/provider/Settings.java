e1639493da03982ef3f8c14e95cebff1657bb23e
xyyx <xyyx@mail.ru>
2016-10-05 09:06:44 +0800

    Custom small QS tiles [1/2]
    - Fix NPE when fancy animation enabled and we want to change small tiles count
    
    10-05 10:43:05.818  1748  1748 E AndroidRuntime: FATAL EXCEPTION: main
    10-05 10:43:05.818  1748  1748 E AndroidRuntime: Process: com.android.systemui, PID: 1748
    10-05 10:43:05.818  1748  1748 E AndroidRuntime: java.lang.NullPointerException: Attempt to invoke virtual method 'com.android.systemui.qs.QSIconView com.android.systemui.qs.QSTileBaseView.getIcon()' on a null object reference
    10-05 10:43:05.818  1748  1748 E AndroidRuntime: 	at com.android.systemui.qs.QSAnimator.updateAnimators(QSAnimator.java:175)
    10-05 10:43:05.818  1748  1748 E AndroidRuntime: 	at com.android.systemui.qs.QSAnimator.onTuningChanged(QSAnimator.java:133)
    10-05 10:43:05.818  1748  1748 E AndroidRuntime: 	at com.android.systemui.tuner.TunerService.reloadSetting(TunerService.java:174)
    10-05 10:43:05.818  1748  1748 E AndroidRuntime: 	at com.android.systemui.tuner.TunerService$Observer.onChange(TunerService.java:283)
    10-05 10:43:05.818  1748  1748 E AndroidRuntime: 	at android.database.ContentObserver$NotificationRunnable.run(ContentObserver.java:216)
    10-05 10:43:05.818  1748  1748 E AndroidRuntime: 	at android.os.Handler.handleCallback(Handler.java:751)
    10-05 10:43:05.818  1748  1748 E AndroidRuntime: 	at android.os.Handler.dispatchMessage(Handler.java:95)
    10-05 10:43:05.818  1748  1748 E AndroidRuntime: 	at android.os.Looper.loop(Looper.java:154)
    10-05 10:43:05.818  1748  1748 E AndroidRuntime: 	at android.app.ActivityThread.main(ActivityThread.java:6137)
    10-05 10:43:05.818  1748  1748 E AndroidRuntime: 	at java.lang.reflect.Method.invoke(Native Method)
    10-05 10:43:05.818  1748  1748 E AndroidRuntime: 	at com.android.internal.os.ZygoteInit$MethodAndArgsCaller.run(ZygoteInit.java:867)
    10-05 10:43:05.818  1748  1748 E AndroidRuntime: 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:757)
    
    Change-Id: I0d5829643ab37ea76863c9efeeff64b9e2961f58
    Signed-off-by: ishubhamsingh <coolsks94@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7079,6 +7079,12 @@ public final class Settings {
         public static final String QS_DATA_ADVANCED = "qs_data_advanced";
 
         /**
+         * Count of small QS tiles.
+         * @hide
+         */
+        public static final String QQS_COUNT = "sysui_qqs_count";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
