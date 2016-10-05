0f6931778aa3784294cec104c45a05a4965d5d55
xyyx <xyyx@mail.ru>
2016-10-05 09:06:44 +0800

    Bring back 'custom small QS tiles' [1/2]
    Change-Id: I0d5829643ab37ea76863c9efeeff64b9e2961f58
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7979,6 +7979,12 @@ public final class Settings {
         public static final String ONE_HANDED_MODE_UI = "one_handed_mode_ui";
 
         /**
+         * Count of small QS tiles.
+         * @hide
+         */
+        public static final String QQS_COUNT = "sysui_qqs_count";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
