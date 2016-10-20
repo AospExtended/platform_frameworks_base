2cfed6afc0e0ac37d09d149d0b7925a9942e7c36
martincz <martincz@mokeedev.com>
2016-10-20 15:46:47 +0200

    Show carrier label / custom label [1/2]
    AICP brought up to O by AliB:
    - Adapt to O statusbar
    - Used Omni's SettingsObserver
    
    Change-Id: Ida3321c9176e4e786fad96766dab056d8614360d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4544,6 +4544,18 @@ public final class Settings {
         public static final String FORCE_AMBIENT_FOR_MEDIA = "force_ambient_for_media";
 
         /**
+         * show carrier in statusbar. The value is
+         * int (1: Show or 0: Hide).
+         */
+        public static final String STATUS_BAR_CARRIER = "status_bar_carrier";
+
+        /**
+         * custom carrier label. The value is
+         * String.
+         */
+        public static final String CUSTOM_CARRIER_LABEL = "custom_carrier_label";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
