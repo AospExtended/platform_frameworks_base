5d8894f247f1f496ef8b164ecce672d58f856eaf
yuanhuihui <yuanhuihui@xiaomi.com>
2016-10-20 21:43:44 +0800

    [ActivityManager] clean up pending broadcast receviers when force stop package
    when app was force-stop,  maybe exist broadcast receviers in pendind list.
    need clean up it completely.
    
    Change-Id: Ic62241b04863efb51448924c9387cee844e49739
    Signed-off-by: yuanhuihui <yuanhuihui@xiaomi.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- services/core/java/com/android/server/am/BroadcastQueue.java
+++ services/core/java/com/android/server/am/BroadcastQueue.java
@@ -1438,6 +1438,14 @@ public final class BroadcastQueue {
             }
         }
 
+        if(mPendingBroadcast != null){
+            didSomething |= mPendingBroadcast.cleanupDisabledPackageReceiversLocked(
+                    packageName, filterByClasses, userId, doit);
+            if (!doit && didSomething) {
+                return true;
+            }
+        }
+
         return didSomething;
     }
 
