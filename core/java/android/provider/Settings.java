3443a227bb39acf715d85d501dd85ca8d8734e8f
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-10-21 19:58:41 +0800

    SystemUI: Implement hidden and text battery styles
    Forward port the hidden and text battery styles from CM 13.0:
    2e0a11bb0bbf22c32f6e15d97ccac10414ec7b44 Forward port battery icon options
    
    Change-Id: I30d95edf9c6fe6afb343aff5effe132f94c49eeb

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7282,6 +7282,19 @@ public final class Settings {
          */
         public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
 
+
+        /**
+         * Display style of the status bar battery information
+         * 0: Display the battery an icon in portrait mode
+         * 2: Display the battery as a circle
+         * 4: Hide the battery status information
+         * 5: Display the battery an icon in landscape mode
+         * 6: Display the battery as plain text
+         * default: 0
+         * @hide
+         */
+        public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+
         /**
          * Whether the camera double twist gesture to flip between front and ba$
          * enabled.
@@ -7589,7 +7602,8 @@ public final class Settings {
             DOZE_ENABLED,
             DOZE_PULSE_ON_PICK_UP,
             DOZE_PULSE_ON_DOUBLE_TAP,
-            STATUS_BAR_SHOW_BATTERY_PERCENT
+            STATUS_BAR_SHOW_BATTERY_PERCENT,
+            STATUS_BAR_BATTERY_STYLE
         };
 
         /**
