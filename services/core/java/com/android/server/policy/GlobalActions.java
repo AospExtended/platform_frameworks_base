6e493abbd6dfe57dfa748caf70995c1ed91c2093
ishubhamsingh <coolsks94@gmail.com>
2016-11-09 03:51:31 +0000

    Fix derps

--- services/core/java/com/android/server/policy/GlobalActions.java
+++ services/core/java/com/android/server/policy/GlobalActions.java
@@ -448,14 +448,6 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         public boolean showDuringKeyguard() {
             return true;
         }
-            public void onPress() {
-                if (Settings.System.getInt(mContext.getContentResolver(),
-                      Settings.System.SCREENSHOT_TYPE, 0) == 1) {
-                   takeScreenshot(mScreenshotSelectedRegion);
-                } else {
-                   takeScreenshot(mScreenshotFullscreen);
-                }
-            }
 
         @Override
         public boolean showBeforeProvisioning() {
@@ -480,7 +472,12 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 R.string.global_action_screenshot) {
 
             public void onPress() {
-                takeScreenshot();
+                if (Settings.System.getInt(mContext.getContentResolver(),
+                      Settings.System.SCREENSHOT_TYPE, 0) == 1) {
+                   takeScreenshot(mScreenshotSelectedRegion);
+                } else {
+                   takeScreenshot(mScreenshotFullscreen);
+                }
             }
 
             public boolean showDuringKeyguard() {
