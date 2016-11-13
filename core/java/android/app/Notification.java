f782270c7ea2c943086ad5bd4454372caecc4dd5
ishubhamsingh <coolsks94@gmail.com>
2016-11-13 16:17:24 +0000

    Revert "Notifications: Expose a bool to disable dynamic colors"
    This reverts commit a5ba89fb9fc6be743f5890a05db4ea43786b3eba.

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -28,7 +28,6 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.ColorStateList;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -3720,7 +3719,7 @@ public class Notification implements Parcelable
         private void processSmallIconColor(Icon smallIcon, RemoteViews contentView) {
             boolean colorable = !isLegacy() || getColorUtil().isGrayscaleIcon(mContext, smallIcon);
             if (colorable) {
-                contentView.setDrawableParameters(R.id.icon, false, -1, resolveIconContrastColor(),
+                contentView.setDrawableParameters(R.id.icon, false, -1, resolveContrastColor(),
                         PorterDuff.Mode.SRC_ATOP, -1);
 
             }
@@ -3737,7 +3736,7 @@ public class Notification implements Parcelable
             if (largeIcon != null && isLegacy()
                     && getColorUtil().isGrayscaleIcon(mContext, largeIcon)) {
                 // resolve color will fall back to the default when legacy
-                contentView.setDrawableParameters(R.id.icon, false, -1, resolveIconContrastColor(),
+                contentView.setDrawableParameters(R.id.icon, false, -1, resolveContrastColor(),
                         PorterDuff.Mode.SRC_ATOP, -1);
             }
         }
@@ -3748,23 +3747,7 @@ public class Notification implements Parcelable
             }
         }
 
-        int getSenderTextColor() {
-            return mContext.getColor(R.color.sender_text_color);
-        }
-
-        int resolveIconContrastColor() {
-            if (!Resources.getSystem().getBoolean(R.bool.config_allowNotificationIconTextTinting)) {
-                return mContext.getColor(R.color.notification_icon_default_color);
-            } else {
-                return resolveContrastColor();
-            }
-        }
-
         int resolveContrastColor() {
-            if (!Resources.getSystem().getBoolean(R.bool.config_allowNotificationIconTextTinting)) {
-                return mContext.getColor(R.color.notification_text_default_color);
-            }
-
             if (mCachedContrastColorIsFor == mN.color && mCachedContrastColor != COLOR_INVALID) {
                 return mCachedContrastColor;
             }
@@ -4756,7 +4739,7 @@ public class Notification implements Parcelable
                         0 /* flags */);
             } else {
                 sb.append(bidi.unicodeWrap(m.mSender),
-                        makeFontColorSpan(mBuilder.getSenderTextColor()),
+                        makeFontColorSpan(Color.BLACK),
                         0 /* flags */);
             }
             CharSequence text = m.mText == null ? "" : m.mText;
