ff3f021a4b0d9a40ac578af3cd0396efe63caf40
ishubhamsingh <coolsks94@gmail.com>
2016-11-15 13:59:05 +0000

    Fix hell lot of mismerges
    Conflicts:
    	services/core/java/com/android/server/power/PowerManagerService.java

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -1160,7 +1160,7 @@ public final class PowerManagerService extends SystemService
         return -1;
     }
 
-    private void notifyWakeLockAcquiredLocked(WakeLock wakeLock) {
+    protected void notifyWakeLockAcquiredLocked(WakeLock wakeLock) {
         if (mSystemReady) {
             if (!wakeLock.isBlocked()){
                 mNotifier.onWakeLockAcquired(wakeLock.mFlags, wakeLock.mTag, wakeLock.mPackageName,
@@ -4053,9 +4053,6 @@ public final class PowerManagerService extends SystemService
             }
         }
 
-    }
-
-        @Override
         public String getSeenWakeLocks(){
             StringBuffer buffer = new StringBuffer();
             Iterator<String> nextWakeLock = mSeenWakeLocks.iterator();
