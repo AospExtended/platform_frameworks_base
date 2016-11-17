a8fed6704d54b2ee4b1654554da469ce00ba4262
d34d <clark@cyngn.com>
2016-11-17 15:53:03 -0600

    QS: Show # of clients connected to hotspot
    Since we no longer post a notification showing that the wifi hotspot
    is on and the # of connected clients, we now show the # of clients
    connected in the label of the hotspot QS tile.
    
    Change-Id: I9ed37b96db9b5b4320a7260524f69733ea70d030
    
    HotSpot: Store # of connected clients in receiver
    
    Get the number of connected clients in onReceive and cache the value
    to be used in handleUpdateState.
    
    Change-Id: I011a13e186c1fa9976df58a4ad1603e6c0cd6bbb
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -79,6 +79,7 @@ import android.net.metrics.DefaultNetworkEvent;
 import android.net.metrics.IpConnectivityLog;
 import android.net.metrics.NetworkEvent;
 import android.net.util.AvoidBadWifiTracker;
+import android.net.wifi.WifiDevice;
 import android.os.Binder;
 import android.os.Build;
 import android.os.Bundle;
@@ -3014,6 +3015,14 @@ public class ConnectivityService extends IConnectivityManager.Stub
                     break;
                 }
             }
+        }
+    }
+
+    public List<WifiDevice> getTetherConnectedSta() {
+        if (isTetheringSupported()) {
+            return mTethering.getTetherConnectedSta();
+        } else {
+             return null;
         }
     }
 
