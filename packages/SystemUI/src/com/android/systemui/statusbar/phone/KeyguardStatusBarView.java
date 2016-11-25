004574c7f393f1fbfe3a3616d4fd36f39ed3834e
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2016-11-25 09:08:12 -0500

    Battery icon: let disable force text outside icon when charging [1/2]
    When text outside the icon is disabled, remove the bolt and put the text
    inside the icon.
    
    Also cleanup the code a bit.
    
    Change-Id: Ib7b079c8a0a6efbea184943f4a7f1fb945272885

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
@@ -82,6 +82,7 @@ public class KeyguardStatusBarView extends RelativeLayout
     };
     private boolean mShowBatteryText;
     private boolean mForceBatteryText;
+    private boolean mForceChargeBatteryText;
 
     public KeyguardStatusBarView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -204,7 +205,7 @@ public class KeyguardStatusBarView extends RelativeLayout
             }
         }
         mBatteryLevel.setVisibility(
-                mBatteryCharging || mShowBatteryText || mForceBatteryText ? View.VISIBLE : View.GONE);
+                (mBatteryCharging && mForceChargeBatteryText) || mShowBatteryText || mForceBatteryText ? View.VISIBLE : View.GONE);
     }
 
     private void updateSystemIconsLayoutParams() {
@@ -380,6 +381,8 @@ public class KeyguardStatusBarView extends RelativeLayout
             mShowBatteryText = newValue == null ? false : Integer.parseInt(newValue) == 2;
             mForceBatteryText = Settings.Secure.getInt(getContext().getContentResolver(),
                     Settings.Secure.STATUS_BAR_BATTERY_STYLE, 0) == 6 ? true : false;
+            mForceChargeBatteryText = Settings.Secure.getInt(getContext().getContentResolver(),
+                    Settings.Secure.FORCE_CHARGE_BATTERY_TEXT, 1) == 1 ? true : false;
             updateVisibilities();
         }
     }
