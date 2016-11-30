a47f0746d088730a1bf65db42e0ca308530f9cab
Joe Maples <joe@frap129.org>
2016-11-30 21:13:15 +0000

    Edit Icon Toggle [1/2]
    aex edits(@hamza-badar):- * Adapt for oreo
                              * Cleanup
    
    Adds toggle for the quick tiles edit
    
    @nathanchance edit: Switched the logic so that enabling the setting disables the icon so that all switches are disabled by default. Also removed the Settings expanded option.
    
    Change-Id: I496ea08eed21eea9e852d0f4760d615d2067f9e4
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4771,6 +4771,12 @@ public final class Settings {
         public static final String QS_RUNNING_SERVICES_TOGGLE = "qs_running_services_toggle";
 
         /**
+         * Whether to show or hide the edit icon
+         * @hide
+         */
+        public static final String QS_EDIT_TOGGLE = "qs_edit_toggle";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
