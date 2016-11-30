003c30cf7d35552e8e7d801cfe054144be99d8b9
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-11-30 22:09:18 +0800

    [2/2] frameworks: show charging current & voltage on lockscreen Thanx to ezio84 for the switch ;)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3879,6 +3879,12 @@ public final class Settings {
          */
         public static final String IMMERSIVE_RECENTS = "immersive_recents";
 
+          /**
+          * Whether to show the negociated charger current in the lockscreen
+          * @hide
+          */
+         public static final String LOCKSCREEN_CHARGING_CURRENT = "lockscreen_charging_current";
+ 
         /**
          * Whether to allow fullscreen screenshots or let the user select which area to screenshot
          *
