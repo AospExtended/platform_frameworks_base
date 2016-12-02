6db5bed2f361c873bda8cafb0ae111ca8ddb8858
Danny Baumann <dannybaumann@web.de>
2016-12-02 12:41:55 +0100

    Battery styles: bring in latest upstream changes
    - fix battery bolt/text not fully transparent in keyguard and tile
    - minor code improvements
    
    Change-Id: I0172ac01b3e589bba3002a3256b351ddfae59adc

--- packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
@@ -201,14 +201,14 @@ public class BatteryMeterDrawable extends Drawable implements
         final int resId = getBatteryDrawableStyleResourceForStyle(style);
         PorterDuff.Mode xferMode = PorterDuff.Mode.XOR;
         if (resId != 0) {
-            TypedArray a = mContext.obtainStyledAttributes(
-                    getBatteryDrawableStyleResourceForStyle(style), attrs);
+            TypedArray a = mContext.obtainStyledAttributes(resId, attrs);
             mTextGravity = a.getInt(0, Gravity.CENTER);
             if (mBoltOverlay) {
                 xferMode = PorterDuff.Mode.OVERLAY;
             } else {
                 xferMode = PorterDuff.intToMode(a.getInt(1, PorterDuff.modeToInt(PorterDuff.Mode.XOR)));
             }
+            a.recycle();
         } else {
             mTextGravity = Gravity.CENTER;
         }
@@ -345,8 +345,8 @@ public class BatteryMeterDrawable extends Drawable implements
         }
         final float[] ptsF = new float[pts.length];
         for (int i = 0; i < pts.length; i += 2) {
-            ptsF[i] = (float)pts[i] / maxX;
-            ptsF[i + 1] = (float)pts[i + 1] / maxY;
+            ptsF[i] = (float) pts[i] / maxX;
+            ptsF[i + 1] = (float) pts[i + 1] / maxY;
         }
         return ptsF;
     }
@@ -406,16 +406,17 @@ public class BatteryMeterDrawable extends Drawable implements
         } else {
             // If we are in power save mode, always use the normal color.
             if (mPowerSaveEnabled) {
-                return mColors[mColors.length-1];
+                return mColors[mColors.length - 1];
             }
-            int thresh, color = 0;
-            for (int i=0; i<mColors.length; i+=2) {
+            int thresh = 0;
+            int color = 0;
+            for (int i = 0; i < mColors.length; i += 2) {
                 thresh = mColors[i];
                 color = mColors[i+1];
                 if (percent <= thresh) {
 
                     // Respect tinting for "normal" level
-                    if (i == mColors.length-2) {
+                    if (i == mColors.length - 2) {
                         return mIconTint;
                     } else {
                         return color;
