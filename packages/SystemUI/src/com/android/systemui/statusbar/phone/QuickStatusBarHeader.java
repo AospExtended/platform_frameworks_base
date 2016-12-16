75c945ca5a4653b314b2918b99d842ab143d329d
Joe Maples <joe@frap129.org>
2016-12-16 14:03:41 +0000

    Fix battery bug on Header DateTime Positions
    Signed-off-by: Joe Maples <joe@frap129.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickStatusBarHeader.java
@@ -85,6 +85,7 @@ public class QuickStatusBarHeader extends BaseStatusBarHeader implements
 
     private ViewGroup mDateTimeGroup;
     private ViewGroup mDateTimeAlarmGroup;
+    private ViewGroup mDateTimeAlarmCenterGroup;
     private TextView mEmergencyOnly;
 
     protected ExpandableIndicator mExpandIndicator;
@@ -129,6 +130,8 @@ public class QuickStatusBarHeader extends BaseStatusBarHeader implements
 
         mDateTimeAlarmGroup = (ViewGroup) findViewById(R.id.date_time_alarm_group);
         mDateTimeAlarmGroup.findViewById(R.id.empty_time_view).setVisibility(View.GONE);
+        mDateTimeAlarmCenterGroup = (ViewGroup) findViewById(R.id.date_time_alarm_center_group);
+        mDateTimeAlarmCenterGroup.setVisibility(View.GONE);
         mDateTimeGroup = (ViewGroup) findViewById(R.id.date_time_group);
         mDateTimeGroup.setPivotX(0);
         mDateTimeGroup.setPivotY(0);
@@ -224,13 +227,13 @@ public class QuickStatusBarHeader extends BaseStatusBarHeader implements
 
     private void updateDateTimeCenter() {
         mDateTimeGroupCenter = isDateTimeGroupCenter();
-        LayoutParams lp = (LayoutParams) mDateTimeAlarmGroup.getLayoutParams();
         if (mDateTimeGroupCenter && !(hasSettingsIcon && hasEdit && hasMultiUserSwitch && hasExpandIndicator)) {
-            lp.addRule(CENTER_HORIZONTAL);
+            mDateTimeAlarmGroup.setVisibility(View.GONE);
+            mDateTimeAlarmCenterGroup.setVisibility(View.VISIBLE);
         } else {
-            lp.addRule(CENTER_HORIZONTAL,0);
+            mDateTimeAlarmCenterGroup.setVisibility(View.GONE);
+            mDateTimeAlarmGroup.setVisibility(View.VISIBLE);
         }
-        mDateTimeAlarmGroup.setLayoutParams(lp);
     }
 
     @Override
