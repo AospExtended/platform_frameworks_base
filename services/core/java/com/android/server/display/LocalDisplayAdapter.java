41859040de438eca385beb1a94748d6fb253e8e8
thecrazyskull <anaskarbila@aospa.co>
2016-12-18 12:48:20 -0500

    LocalDisplayAdapter: fix crash with HWC2
    [@nathanchance: updated coding style, I hate single line ifs]
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- services/core/java/com/android/server/display/LocalDisplayAdapter.java
+++ services/core/java/com/android/server/display/LocalDisplayAdapter.java
@@ -290,6 +290,10 @@ final class LocalDisplayAdapter extends DisplayAdapter {
 
             List<Integer> pendingColorModes = new ArrayList<>();
 
+            if (colorModes == null) {
+                return false;
+            }
+
             // Build an updated list of all existing color modes.
             boolean colorModesAdded = false;
             for (int colorMode: colorModes) {
