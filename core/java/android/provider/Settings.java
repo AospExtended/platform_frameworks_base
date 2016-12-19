711157834e69c8cd472c859c1b45983653f89d64
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2016-12-19 09:10:35 -0500

    Merge the two night light options into one
    - Smaller code for the same achievement
    
    https://github.com/DirtyUnicorns/android_frameworks_base/commit/c8c717152afb35e80cabd12a1e2eb1d7ad7d7764
    
    https://github.com/DirtyUnicorns/android_frameworks_base/commit/75d2b44e85efeb1e12e723a14b139fa87d311dc3
    
    Change-Id: I773537d7c32dfeaf020ffd0079c379a775fa1343

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7266,12 +7266,6 @@ public final class Settings {
         public static final String DOZE_ENABLED = "doze_enabled";
 
         /**
-         * Wheter to automatically change brightness when enabling night mode tile
-         * @hide
-         */
-        public static final String QS_NIGHT_BRIGHTNESS_TOGGLE = "qs_night_brightness_toggle";
-
-        /**
          * Low brightness level for the advanced night mode tile
          * @hide
          */
