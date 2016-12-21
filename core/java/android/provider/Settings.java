285dcd62d9b18098b256dd612be49b0d1d21d239
AlmightyMegadeth00 <activethrasher00@gmail.com>
2016-12-21 14:09:43 +0100

    Add membar to recents options [1/2]
    Change-Id: Ieaf86a2360cc993bb20f971b25c2b8c49a746f64
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4350,6 +4350,12 @@ public final class Settings {
         public static final String STATUS_BAR_CARRIER_FONT_SIZE = "status_bar_carrier_font_size";
 
         /**
+          * show the membar in recents
+          *  @hide
+          */
+        public static final String SYSTEMUI_RECENTS_MEM_DISPLAY = "systemui_recents_mem_display";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
