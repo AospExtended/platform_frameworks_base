cb84789531c8ab7e97aef2e68c6f482657f02449
Marko Man <darkobas@gmail.com>
2016-12-29 01:07:25 +0100

    [1/2] base: configure lock screen media artwork blur level
    Change-Id: I68895ae6a471bbc99b538b85337e15e8793665ce
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5107,6 +5107,12 @@ public final class Settings {
          public static final String NETWORK_TRAFFIC_VIEW_LOCATION = "network_traffic_view_location";
 
         /**
+         * media artwork wallpaper blur level on lockscreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_MEDIA_BLUR = "lockscreen_media_blur";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
