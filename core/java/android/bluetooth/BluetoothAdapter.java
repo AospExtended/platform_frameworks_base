09f9db1c82b02b963149262172e63a6c7dd4bbf1
beanstown106 <nbenis106@gmail.com>
2016-12-28 19:15:24 -0500

    Turn off bluetooth by default
    Author: beanstown106 <nbenis106@gmail.com>
    Date:   Wed Dec 28 19:12:24 2016 -0500
    Revert "Enable Bluetooth by default"
    This reverts commit 8c18431be868e0960e80638c1a974d71a0df05b4.
    
    Author: beanstown106 <nbenis106@gmail.com>
    Date:   Wed Dec 28 19:12:13 2016 -0500
    Revert "Bluetooth: More logging of bluetooth service state"
    This reverts commit 9fa24918509151bcd30ac763a26c50c4f24d5e7a.

--- core/java/android/bluetooth/BluetoothAdapter.java
+++ core/java/android/bluetooth/BluetoothAdapter.java
@@ -2046,7 +2046,7 @@ public final class BluetoothAdapter {
     final private IBluetoothManagerCallback mManagerCallback =
         new IBluetoothManagerCallback.Stub() {
             public void onBluetoothServiceUp(IBluetooth bluetoothService) {
-                if (DBG) Log.d(TAG, "onBluetoothServiceUp: " + bluetoothService);
+                if (VDBG) Log.d(TAG, "onBluetoothServiceUp: " + bluetoothService);
 
                 mServiceLock.writeLock().lock();
                 mService = bluetoothService;
@@ -2068,7 +2068,7 @@ public final class BluetoothAdapter {
             }
 
             public void onBluetoothServiceDown() {
-                if (DBG) Log.d(TAG, "onBluetoothServiceDown: " + mService);
+                if (VDBG) Log.d(TAG, "onBluetoothServiceDown: " + mService);
 
                 try {
                     mServiceLock.writeLock().lock();
@@ -2097,7 +2097,7 @@ public final class BluetoothAdapter {
             }
 
             public void onBrEdrDown() {
-                if (DBG) Log.i(TAG, "onBrEdrDown:");
+                if (VDBG) Log.i(TAG, "on QBrEdrDown: ");
             }
     };
 
