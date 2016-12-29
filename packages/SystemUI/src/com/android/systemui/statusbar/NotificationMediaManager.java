078ca9a75cff08c88023528d8ec3dc67dc50e18c
Marko Man <darkobas@gmail.com>
2016-12-29 01:07:25 +0100

    [1/2] base: configure lock screen media artwork blur level
    Change-Id: I68895ae6a471bbc99b538b85337e15e8793665ce
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -40,6 +40,7 @@ import android.os.Trace;
 import android.os.UserHandle;
 import android.provider.DeviceConfig;
 import android.provider.DeviceConfig.Properties;
+import android.provider.Settings;
 import android.util.ArraySet;
 import android.util.Log;
 import android.view.View;
@@ -710,7 +711,14 @@ public class NotificationMediaManager implements Dumpable {
     };
 
     private Bitmap processArtwork(Bitmap artwork) {
-        return mMediaArtworkProcessor.processArtwork(mContext, artwork);
+        return mMediaArtworkProcessor.processArtwork(mContext, artwork, getLockScreenMediaBlurLevel());
+    }
+
+    private float getLockScreenMediaBlurLevel() {
+        float level = (float) Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_MEDIA_BLUR, 25,
+                UserHandle.USER_CURRENT) / 100;
+        return level;
     }
 
     @MainThread
