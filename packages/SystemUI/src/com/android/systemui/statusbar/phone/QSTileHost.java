8182eaaf272900f36f6688e7609c8f41b704300b
SpiritCroc <spiritcroc@gmail.com>
2017-01-01 13:41:11 +0100

    Language toggle QS
    Toggle the primary default language
    
    Change-Id: I3e336eddb360ea1a796727657652ad9301007380
    
    Conflicts:
    	packages/SystemUI/AndroidManifest.xml
    	packages/SystemUI/res/values/custom_config.xml
    	packages/SystemUI/res/values/custom_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -56,6 +56,7 @@ import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.ImeTile;
 import com.android.systemui.qs.tiles.IntentTile;
+import com.android.systemui.qs.tiles.LocaleTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
@@ -461,6 +462,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("pulse")) return new PulseTile(this);
         else if (tileSpec.equals("nfc")) return new NfcTile(this);
         else if (tileSpec.equals("ambient_display")) return new AmbientDisplayTile(this);
+        else if (tileSpec.equals("locale")) return new LocaleTile(this);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(this,tileSpec);
