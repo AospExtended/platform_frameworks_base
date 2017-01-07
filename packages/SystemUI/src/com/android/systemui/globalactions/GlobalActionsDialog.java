0c4173aaedbdfc486de85d7f74e6fd33ba2585e8
Meninblack007 <sanyam.53jain@gmail.com>
2017-01-07 16:32:10 +0530

    base: Disable Emergency affordance feature in Power Menu
    Updated for Oreo: @ishubhamsingh
    
    Change-Id: I56e707abdbf57fbe61e2633f4e96784f195e3799
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -440,7 +440,8 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
             addedKeys.add(actionKey);
         }
 
-        if (mEmergencyAffordanceManager.needsEmergencyAffordance()) {
+        if (mEmergencyAffordanceManager.needsEmergencyAffordance() && mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_showEmergencyButtonInPowerMenu)) {
             mItems.add(getEmergencyAction());
         }
 
