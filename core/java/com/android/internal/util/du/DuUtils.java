192eac76c918b313594e0c6e40876cf842519b42
maxwen <max.weninger@gmail.com>
2017-01-12 02:28:02 +0100

    Custom QS header images [1/2]
    Brought back up from MM days. Just now with fixed size (about 416x96dpi)
    
    - Brought in 'isAvailableApp' into our DuUtils
    
    Change-Id: Icb42295181684d093a686001ca4756d4b380261e

--- core/java/com/android/internal/util/du/DuUtils.java
+++ core/java/com/android/internal/util/du/DuUtils.java
@@ -65,6 +65,19 @@ public class DuUtils {
         return (cm.isNetworkSupported(ConnectivityManager.TYPE_MOBILE) == false);
     }
 
+    public static boolean isAvailableApp(String packageName, Context context) {
+        Context mContext = context;
+        final PackageManager pm = mContext.getPackageManager();
+        try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            int enabled = pm.getApplicationEnabledSetting(packageName);
+            return enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED &&
+                enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER;
+        } catch (NameNotFoundException e) {
+            return false;
+        }
+    }
+
     public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
         if (pkg != null) {
             try {
