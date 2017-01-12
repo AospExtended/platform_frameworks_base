6da5bab93f0c781b34a68028ad87faae951b9029
Ritayan Chakraborty <ritayanout@gmail.com>
2017-01-12 12:11:49 +0530

    Add Pulse Lava Lamp start and end colours (1/3)
    Change-Id: I43934d2870c868d08b1e15e5d3300ce0d9ce1773
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6126,6 +6126,14 @@ public final class Settings {
         public static final String FLING_PULSE_LAVALAMP_SPEED = "fling_pulse_lavalamp_speed";
 
         /**
+         * Fling pulse lavalamp start and end colors
+         *
+         * @hide
+         */
+        public static final String FLING_PULSE_LAVALAMP_COLOR_FROM = "fling_lavalamp_color_from";
+        public static final String FLING_PULSE_LAVALAMP_COLOR_TO = "fling_lavalamp_color_to";
+
+        /**
          * Pulse renderer implementation
          *
          * @hide
