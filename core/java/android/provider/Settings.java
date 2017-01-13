1db22569cd959074301a0ebcf5a235c0d6d79d5d
maxwen <max.weninger@gmail.com>
2017-01-13 01:56:56 +0100

    Custom QS header extensions
    - Add static image provider that allows using a specific image from a header pack
    
    Change-Id: I6918aba6d9d2c756002517c0339000af7433ea39

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4098,10 +4098,16 @@ public final class Settings {
         public static final String STATUS_BAR_DAYLIGHT_HEADER_PACK = "status_bar_daylight_header_pack";
 
         /**
-         * Current active provider
+         * Current active provider - available currently "static" "daylight"
          * @hide
          */
         public static final String STATUS_BAR_CUSTOM_HEADER_PROVIDER = "status_bar_custom_header_provider";
+
+        /**
+         * Manual override picture to use
+         * @hide
+         */
+        public static final String STATUS_BAR_CUSTOM_HEADER_IMAGE = "status_bar_custom_header_image";
 
         /**
          * Whether to show media art on lockscreen
