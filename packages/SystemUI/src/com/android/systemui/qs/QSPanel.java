6b50418385e18a4beb0ef1498f4ea34f894d715b
Arne Coucheron <arco68@gmail.com>
2017-01-16 20:36:32 +0100

    Revert "MSIM: Provide voice pref selection in qucksettings."
    This reverts commit d609fd174dd725cc4bb9168a0d61d18e851c1f5e.
    
    Change-Id: Ic7d86d64775dd45221187017455345bc77e2b536
    
    Conflicts:
    	packages/SystemUI/Android.mk

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -35,11 +35,9 @@ import com.android.systemui.qs.QSTile.Host.Callback;
 import com.android.systemui.qs.customize.QSCustomizer;
 import com.android.systemui.qs.external.CustomTile;
 import com.android.systemui.settings.BrightnessController;
-import com.android.systemui.settings.SimSwitchController;
 import com.android.systemui.settings.ToggleSlider;
 import com.android.systemui.statusbar.phone.QSTileHost;
 import com.android.systemui.statusbar.policy.BrightnessMirrorController;
-import com.android.systemui.statusbar.policy.MobileSignalController;
 import com.android.systemui.tuner.TunerService;
 import com.android.systemui.tuner.TunerService.Tunable;
 
@@ -54,7 +52,6 @@ public class QSPanel extends LinearLayout implements Tunable, Callback {
     protected final Context mContext;
     protected final ArrayList<TileRecord> mRecords = new ArrayList<TileRecord>();
     protected final View mBrightnessView;
-    protected View mSimSwitcherView = null;
     private final H mHandler = new H();
 
     private int mPanelPaddingBottom;
@@ -64,7 +61,6 @@ public class QSPanel extends LinearLayout implements Tunable, Callback {
 
     private Callback mCallback;
     private BrightnessController mBrightnessController;
-    private SimSwitchController mSimSwitchController;
     protected QSTileHost mHost;
 
     protected QSFooter mFooter;
@@ -86,13 +82,6 @@ public class QSPanel extends LinearLayout implements Tunable, Callback {
         mContext = context;
 
         setOrientation(VERTICAL);
-
-        if(MobileSignalController.isCarrierOneSupported()) {
-            mSimSwitcherView = LayoutInflater.from(context).inflate(
-                R.layout.sim_switcher, this, false);
-            addView(mSimSwitcherView);
-            mSimSwitchController = new SimSwitchController(getContext(), mSimSwitcherView, this);
-        }
 
         mBrightnessView = LayoutInflater.from(context).inflate(
                 R.layout.quick_settings_brightness_dialog, this, false);
