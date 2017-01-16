8b7b014eb74f6843efcb0cb83d703cd2d8dd3b67
Arne Coucheron <arco68@gmail.com>
2017-01-17 00:34:30 +0100

    Revert "SystemUi changes submit"
    This reverts commit a1b4f559a9b40833829b167b9f1ee576d5129331.
    
    Change-Id: Ic3837c3f4ef80b41d028f212f2d3f7e2431b6caa
    
    Conflicts:
    	packages/SystemUI/res/values/config.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java

--- packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
@@ -111,7 +111,6 @@ public class SignalClusterView
     private boolean mBlockMobile;
     private boolean mBlockWifi;
     private boolean mBlockEthernet;
-    private TelephonyManager mTelephonyManager;
 
     public SignalClusterView(Context context) {
         this(context, null);
@@ -138,7 +137,6 @@ public class SignalClusterView
         TypedValue typedValue = new TypedValue();
         res.getValue(R.dimen.status_bar_icon_scale_factor, typedValue, true);
         mIconScaleFactor = typedValue.getFloat();
-        mTelephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
     }
 
     @Override
@@ -741,7 +739,6 @@ public class SignalClusterView
 
             mMobileSingleGroup = (ViewGroup) root.findViewById(R.id.mobile_signal_single);
             mMobileStackedGroup = (ViewGroup) root.findViewById(R.id.mobile_signal_stacked);
-            mRoaming        = (ImageView) root.findViewById(R.id.mobile_roaming);
         }
 
         public boolean apply(boolean isSecondaryIcon) {
@@ -792,14 +789,6 @@ public class SignalClusterView
                     mMobileStackedGroup.setVisibility(View.GONE);
                 }
 
-                if (mContext.getResources().getBoolean(R.bool.show_roaming_and_network_icons)
-                        &&(mTelephonyManager != null
-                        && mTelephonyManager.isNetworkRoaming(mSubId))) {
-                    mRoaming.setImageDrawable(getContext().getResources().getDrawable(
-                            R.drawable.stat_sys_data_fully_connected_roam));
-                } else {
-                    mRoaming.setImageDrawable(null);
-                }
                 mMobileGroup.setContentDescription(mMobileTypeDescription
                         + " " + mMobileDescription);
                 mMobileGroup.setVisibility(View.VISIBLE);
