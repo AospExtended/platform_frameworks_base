76ab5d885d87c53c9c3b2ca568ebfebf1dd4bcd4
beanstown106 <nbenis106@gmail.com>
2017-01-16 22:27:17 -0500

    One handed mode toggle [1/3]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7891,6 +7891,12 @@ public final class Settings {
         public static final String QQS_COUNT = "sysui_qqs_count";
 
         /**
+         * One Handed mode toggle
+         * @hide
+         */
+        public static final String ONE_HANDED_MODE_UI = "one_handed_mode_ui";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
