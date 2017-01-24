90dde708aecd122cd13f6a8a31bfe71562472472
Badhri Jagan Sridharan <Badhri@google.com>
2017-01-23 16:28:38 -0800

    Check and send usb state broadcast when devices boots
    Broadcasts are not sent until BOOT_COMPLETED is received.
    When the device boots with usb plugged, the UPDATE_STATE
    for connted and configured would be received way before
    BOOT_COMPLETED is received. Since, devices can continue
    to stay in same usb configuration(set through persistent
    property trigger), check and send a usb state broadcast
    if needed.
    
    BUG: 34451258
    Test: Manually checked to verify the broadcasts sent.
    Change-Id: I8eca5f241ad758f5c4f1afa6c9807868dfd6195a

--- services/usb/java/com/android/server/usb/UsbDeviceManager.java
+++ services/usb/java/com/android/server/usb/UsbDeviceManager.java
@@ -152,6 +152,7 @@ public class UsbDeviceManager {
     private UsbDebuggingManager mDebuggingManager;
     private final UsbAlsaManager mUsbAlsaManager;
     private Intent mBroadcastedIntent;
+    private boolean mPendingBootBroadcast;
 
     private class AdbSettingsObserver extends ContentObserver {
         public AdbSettingsObserver() {
@@ -772,6 +773,8 @@ public class UsbDeviceManager {
                         }
                         updateUsbStateBroadcastIfNeeded();
                         updateUsbFunctions();
+                    } else {
+                        mPendingBootBroadcast = true;
                     }
                     break;
                 case MSG_UPDATE_HOST_STATE:
@@ -783,6 +786,8 @@ public class UsbDeviceManager {
                     updateUsbNotification();
                     if (mBootCompleted) {
                         updateUsbStateBroadcastIfNeeded();
+                    } else {
+                        mPendingBootBroadcast = true;
                     }
                     break;
                 case MSG_UPDATE_CHARGING_STATE:
@@ -807,6 +812,10 @@ public class UsbDeviceManager {
                     break;
                 case MSG_BOOT_COMPLETED:
                     mBootCompleted = true;
+                    if (mPendingBootBroadcast) {
+                        updateUsbStateBroadcastIfNeeded(false);
+                        mPendingBootBroadcast = false;
+                    }
                     if (mCurrentAccessory != null) {
                         getCurrentSettings().accessoryAttached(mCurrentAccessory);
                     }
