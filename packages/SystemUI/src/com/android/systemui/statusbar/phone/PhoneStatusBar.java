d6b5c6b72d06743abad222e596169abc954f5bb8
Josh Chasky <jchasky@gmail.com>
2017-02-14 12:59:13 -0500

    Remove custom ticker logic
    -- We're going to put a dialog/toast in Settings for this
    
    Change-Id: Ie6fbf6371388d4a19807e39caa8b0a861dcdbf19
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -685,7 +685,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     mTickerEnabled = Settings.System.getIntForUser(
                             mContext.getContentResolver(),
                             Settings.System.STATUS_BAR_SHOW_TICKER,
-                            1, UserHandle.USER_CURRENT) == 1;
+                            0, UserHandle.USER_CURRENT) == 1;
                     initTickerView();
             }
             updateSettings();
@@ -708,17 +708,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
             mClockLocation = Settings.System.getIntForUser(resolver,
                     Settings.System.STATUSBAR_CLOCK_STYLE, 0, UserHandle.USER_CURRENT);
-
-            boolean headsUpEnabled = Settings.System.getIntForUser(resolver,
-                    Settings.System.HEADS_UP_USER_ENABLED, 0, UserHandle.USER_CURRENT) == 1;
-            if (headsUpEnabled) {
-                mTickerEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
-                        Settings.System.STATUS_BAR_SHOW_TICKER,
-                        mContext.getResources().getBoolean(R.bool.enable_ticker)
-                        ? 1 : 1, UserHandle.USER_CURRENT) == 1;
-                initTickerView();
-            }
-
 
             mBlurScale = Settings.System.getInt(mContext.getContentResolver(),
                     Settings.System.BLUR_SCALE_PREFERENCE_KEY, 10);
@@ -1267,15 +1256,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         mCenterClock = (TextView) mStatusBarWindow.findViewById(R.id.center_clock);
 
-        boolean headsUpEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.HEADS_UP_USER_ENABLED, 0, UserHandle.USER_CURRENT) == 1;
-        if (headsUpEnabled) {
-            mTickerEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
-                    Settings.System.STATUS_BAR_SHOW_TICKER,
-                    mContext.getResources().getBoolean(R.bool.enable_ticker)
-                    ? 1 : 1, UserHandle.USER_CURRENT) == 1;
-            initTickerView();
-        }
+        mTickerEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_SHOW_TICKER,
+                mContext.getResources().getBoolean(R.bool.enable_ticker)
+                        ? 1 : 1, UserHandle.USER_CURRENT) == 1;
+        initTickerView();
 
         // set the initial view visibility
         setAreThereNotifications();
