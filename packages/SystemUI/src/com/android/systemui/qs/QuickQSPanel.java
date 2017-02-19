9cdf0872b5d15dbceef2f7bea3b8019ef94a67ef
maxwen <max.weninger@gmail.com>
2017-02-19 01:23:29 +0100

    Adjust quickbar tile gap
    Adjust tile gap if the number of tiles is below the maximum
    
    Change-Id: I0f7a1190d5d2fcd4cc76130c3a3d42b94cd36ced

--- packages/SystemUI/src/com/android/systemui/qs/QuickQSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickQSPanel.java
@@ -143,14 +143,14 @@ public class QuickQSPanel extends QSPanel {
             }
         }
         super.setTiles(quickTiles, true);
-        ((HeaderTileLayout) mTileLayout).updateTileGaps();
+        ((HeaderTileLayout) mTileLayout).updateTileGaps(mHost.getTiles().size());
     }
 
     private final Tunable mNumTiles = new Tunable() {
         @Override
         public void onTuningChanged(String key, String newValue) {
             NUM_QUICK_TILES_DEFAULT = getNumQuickTiles(mContext);
-            ((HeaderTileLayout) mTileLayout).updateTileGaps();
+            ((HeaderTileLayout) mTileLayout).updateTileGaps(mHost.getTiles().size());
             updateSettings();
         }
     };
@@ -167,7 +167,7 @@ public class QuickQSPanel extends QSPanel {
     protected void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         setMaxTiles(((HeaderTileLayout) mTileLayout).calcNumTiles());
-        ((HeaderTileLayout) mTileLayout).updateTileGaps();
+        ((HeaderTileLayout) mTileLayout).updateTileGaps(mHost.getTiles().size());
     }
 
     @Override
@@ -177,7 +177,7 @@ public class QuickQSPanel extends QSPanel {
                 Settings.System.QS_QUICKBAR_SCROLL_ENABLED, 0, UserHandle.USER_CURRENT) == 0 ?
                 NUM_QUICK_TILES_DEFAULT : NUM_QUICK_TILES_ALL) == NUM_QUICK_TILES_ALL;
         setMaxTiles(((HeaderTileLayout) mTileLayout).calcNumTiles());
-        ((HeaderTileLayout) mTileLayout).updateTileGaps();
+        ((HeaderTileLayout) mTileLayout).updateTileGaps(mHost.getTiles().size());
     }
 
     private static class HeaderTileLayout extends LinearLayout implements QSTileLayout {
@@ -301,14 +301,15 @@ public class QuickQSPanel extends QSPanel {
             return maxNumTiles;
         }
 
-        public void updateTileGaps() {
+        public void updateTileGaps(int numTiles) {
             int panelWidth = mContext.getResources().getDimensionPixelSize(R.dimen.notification_panel_width);
             if (panelWidth == -1) {
                 panelWidth = mScreenWidth;
             }
             panelWidth -= 2 * mStartMargin;
             int maxNumTiles = panelWidth / (mTileSize + 2 * mMinTileGap);
-            int tileGap = (panelWidth - mTileSize * maxNumTiles) / (maxNumTiles - 1);
+            int layoutNumTiles = Math.min(maxNumTiles, numTiles);
+            int tileGap = (panelWidth - mTileSize * layoutNumTiles) / (layoutNumTiles - 1);
             final int N = getChildCount();
             for (int i = 0; i < N; i++) {
                 if (getChildAt(i) instanceof Space) {
