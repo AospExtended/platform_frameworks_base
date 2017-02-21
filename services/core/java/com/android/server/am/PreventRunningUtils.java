86aaa7440c8b8bcb395ea09ac555e6b1beb34505
wuyi-ligux <wuyi@ligux.com>
2017-02-21 12:58:28 +0800

    Add brevent patch Thanks to liudongmiao. Source: https://github.com/SudaMod/android_frameworks_base/commit/0e2f26fd5b97e9d5cd0375de2ee6da387d60f91e

--- services/core/java/com/android/server/am/PreventRunningUtils.java
+++ services/core/java/com/android/server/am/PreventRunningUtils.java
@@ -2,105 +2,32 @@ package com.android.server.am;
 
 import android.app.IApplicationThread;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.ApplicationInfo;
 import android.net.Uri;
 import android.os.Binder;
+import android.os.Build;
 import android.os.IBinder;
-import android.util.ArraySet;
+import android.os.ServiceManager;
 
 import java.util.Set;
 
-import me.piebridge.PreventRunning;
-
 public class PreventRunningUtils {
 
-    private static ActivityManagerService mAm;
+    private static ActivityManagerService ams;
 
     private static PreventRunning mPreventRunning = new PreventRunning();
 
     private PreventRunningUtils() {
     }
 
-    public static void init(ActivityManagerService am) {
-        mAm = am;
-    }
-
-    public static void onUserLeavingActivity(ActivityRecord ar, boolean userLeaving) {
-        if (userLeaving) {
-            mPreventRunning.onUserLeavingActivity(ar);
+    private static ActivityManagerService getAms() {
+        if (ams == null) {
+            ams = (ActivityManagerService) ServiceManager.getService(Context.ACTIVITY_SERVICE);
         }
-    }
-
-    public static void onResumeActivity(ActivityRecord ar) {
-        mPreventRunning.onResumeActivity(ar);
-    }
-
-    public static void onDestroyActivity(ActivityRecord ar) {
-        mPreventRunning.onDestroyActivity(ar);
-    }
-
-    public static void onLaunchActivity(ActivityRecord ar) {
-        mPreventRunning.onLaunchActivity(ar);
-    }
-
-    private static void setSender(IApplicationThread caller) {
-        final ProcessRecord callerApp = mAm.getRecordForAppLocked(caller);
-        mPreventRunning.setSender(callerApp != null ? callerApp.info.packageName : String.valueOf(Binder.getCallingUid()));
-    }
-
-    public static void setSenderInStartService(IApplicationThread caller) {
-        setSender(caller);
-    }
-
-    public static ComponentName clearSenderInStartService(ComponentName res) {
-        mPreventRunning.setSender(null);
-        if (res != null && res.getPackageName().startsWith("!")) {
-            return null;
-        }
-        return res;
-    }
-
-    public static void setSenderInBindService(IApplicationThread caller) {
-        setSender(caller);
-    }
-
-    public static int clearSenderInBindService(int res) {
-        mPreventRunning.setSender(null);
-        return res;
-    }
-
-    public static void setSenderInBroadcastIntent(IApplicationThread caller) {
-        setSender(caller);
-    }
-
-    public static int onBroadcastIntent(int res, Intent intent) {
-        if (res >= 0) {
-            mPreventRunning.onBroadcastIntent(intent);
-        }
-        mPreventRunning.setSender(null);
-        return res;
-    }
-
-    public static void onAppDied(ProcessRecord app) {
-        mPreventRunning.onAppDied(app);
-    }
-
-    public static void onCleanUpRemovedTask(ComponentName component) {
-        mPreventRunning.onCleanUpRemovedTask(component.getPackageName());
-    }
-
-    public static boolean onMoveActivityTaskToBack(boolean res, IBinder token) {
-        if (res) {
-            ActivityRecord ar = forToken(token);
-            mPreventRunning.onMoveActivityTaskToBack(ar != null ? ar.packageName : null);
-        }
-        return res;
-    }
-
-    private static ActivityRecord forToken(IBinder token) {
-        return ActivityRecord.forTokenLocked(token);
+        return ams;
     }
 
     public static boolean isExcludingStopped(Intent intent) {
@@ -117,9 +44,13 @@ public class PreventRunningUtils {
         }
     }
 
-    public static int onStartActivity(int res, IApplicationThread caller, Intent intent) {
+    public static boolean hookStartProcessLocked(String processName, ApplicationInfo info, boolean knownToBeDead, int intentFlags, String hostingType, ComponentName hostingName) {
+        return mPreventRunning.hookStartProcessLocked(getAms().mContext, info, hostingType, hostingName);
+    }
+
+    public static int onStartActivity(int res, IApplicationThread caller, String callingPackage, Intent intent) {
         if (res >= 0 && intent != null && (intent.hasCategory(Intent.CATEGORY_HOME) || intent.hasCategory(Intent.CATEGORY_LAUNCHER))) {
-            ProcessRecord callerApp = mAm.getRecordForAppLocked(caller);
+            ProcessRecord callerApp = getAms().getRecordForAppLocked(caller);
             if (callerApp != null) {
                 mPreventRunning.onStartHomeActivity(callerApp.info.packageName);
             }
@@ -127,11 +58,70 @@ public class PreventRunningUtils {
         return res;
     }
 
-    public static boolean hookStartProcessLocked(ApplicationInfo info, boolean knownToBeDead, int intentFlags, String hostingType, ComponentName hostingName) {
-        return mPreventRunning.hookStartProcessLocked(mAm.mContext, info, hostingType, hostingName);
+    public static void onAppDied(ProcessRecord app) {
+        mPreventRunning.onAppDied(app);
     }
 
-    public static boolean isDep(ArraySet<String> pkgDeps) {
-        return false;
+    public static boolean returnFalse(boolean res) {
+        if (mPreventRunning.isActiviated()) {
+            return false;
+        } else {
+            return res;
+        }
     }
+
+    public static void onCleanUpRemovedTask(Intent intent) {
+        if (intent != null && intent.getComponent() != null) {
+            mPreventRunning.onCleanUpRemovedTask(intent.getComponent().getPackageName());
+        }
+    }
+
+    public static void onMoveActivityTaskToBack(IBinder token) {
+        ActivityRecord ar = forToken(token);
+        mPreventRunning.onMoveActivityTaskToBack(ar != null ? ar.packageName : null);
+    }
+
+    public static void setSender(IApplicationThread caller) {
+        final ProcessRecord callerApp = getAms().getRecordForAppLocked(caller);
+        mPreventRunning.setSender(callerApp != null ? callerApp.info.packageName : String.valueOf(Binder.getCallingUid()));
+    }
+
+    public static void clearSender() {
+        mPreventRunning.setSender(null);
+    }
+
+    public static boolean hookStartService(IApplicationThread caller, Intent service) {
+        return mPreventRunning.hookStartService(service);
+    }
+
+    public static boolean hookBindService(IApplicationThread caller, IBinder token, Intent service) {
+        return mPreventRunning.hookBindService(service);
+    }
+
+    public static void onBroadcastIntent(Intent intent) {
+        mPreventRunning.onBroadcastIntent(intent);
+    }
+
+    public static void onUserLeavingActivity(IBinder token, boolean finishing, boolean userLeaving) {
+        if (userLeaving) {
+            mPreventRunning.onUserLeavingActivity(forToken(token));
+        }
+    }
+
+    public static void onResumeActivity(IBinder token) {
+        mPreventRunning.onResumeActivity(forToken(token));
+    }
+
+    public static void onDestroyActivity(IBinder token) {
+        mPreventRunning.onDestroyActivity(forToken(token));
+    }
+
+    public static void onLaunchActivity(IBinder token) {
+        mPreventRunning.onLaunchActivity(forToken(token));
+    }
+
+    private static ActivityRecord forToken(IBinder token) {
+        return ActivityRecord.forTokenLocked(token);
+    }
+
 }
