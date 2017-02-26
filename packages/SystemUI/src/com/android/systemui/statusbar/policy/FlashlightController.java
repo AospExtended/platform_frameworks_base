40aa10fec3dc270b7af9137d068fd09f44bffe1d
Dominggoes Isakh <drjisakh@gmail.com>
2017-02-26 12:20:51 +0100

    Move wakelock option for flashlight to CameraManager.
    On some legacy devices, the flashlight can't stay on when screen is off.
    With this change, the flashlight can use wakelock. For obvious reasons,
    this is turned off by default and needs to be enabled using the overlay.
    See: https://review.lineageos.org/#/c/64929/
    
    The wakelock-code and overlay is moved to CameraManager to be also effective
    for the new long-press power torch feature.
    
    BUGBASH-179
    
    Change-Id: Iee905d4e5ca7a69219d9753167213fcb80d7c19e
    
    Conflicts:
    	core/res/res/values/config.xml
    	core/res/res/values/symbols.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/FlashlightController.java

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/FlashlightController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/FlashlightController.java
@@ -30,8 +30,6 @@ import android.hardware.camera2.CameraManager;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Process;
-import android.os.PowerManager;
-import android.os.PowerManager.WakeLock;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.Log;
@@ -68,9 +66,6 @@ public class FlashlightController {
     /** Lock on mListeners when accessing */
     private final ArrayList<WeakReference<FlashlightListener>> mListeners = new ArrayList<>(1);
 
-    private final boolean mUseWakeLock;
-    private final WakeLock mWakeLock;
-
     /** Lock on {@code this} when accessing */
     private boolean mFlashlightEnabled;
 
@@ -104,10 +99,6 @@ public class FlashlightController {
         this.mContext = mContext;
         mCameraManager = (CameraManager) mContext.getSystemService(Context.CAMERA_SERVICE);
 
-        PowerManager pm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
-        mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
-        mUseWakeLock = mContext.getResources().getBoolean(R.bool.config_useWakelockForFlashlight);
-
         tryInitCamera();
     }
 
@@ -132,24 +123,12 @@ public class FlashlightController {
             if (mFlashlightEnabled != enabled) {
                 mFlashlightEnabled = enabled;
 
-                if (mUseWakeLock) {
-                    if (enabled) {
-                        if (!mWakeLock.isHeld()) mWakeLock.acquire();
-                    } else {
-                        if (mWakeLock.isHeld()) mWakeLock.release();
-                    }
-                }
-
                 try {
                     mCameraManager.setTorchMode(mCameraId, enabled);
                 } catch (CameraAccessException e) {
                     Log.e(TAG, "Couldn't set torch mode", e);
                     mFlashlightEnabled = false;
                     pendingError = true;
-
-                    if (mUseWakeLock && mWakeLock.isHeld()) {
-                        mWakeLock.release();
-                    }
                 }
             }
         }
@@ -335,10 +314,6 @@ public class FlashlightController {
             synchronized (FlashlightController.this) {
                 changed = mTorchAvailable != available;
                 mTorchAvailable = available;
-
-                if (mUseWakeLock && !available && mWakeLock.isHeld()) {
-                    mWakeLock.release();
-                }
             }
             if (changed) {
                 if (DEBUG) Log.d(TAG, "dispatchAvailabilityChanged(" + available + ")");
@@ -351,10 +326,6 @@ public class FlashlightController {
             synchronized (FlashlightController.this) {
                 changed = mFlashlightEnabled != enabled;
                 mFlashlightEnabled = enabled;
-
-                if (mUseWakeLock && !enabled && mWakeLock.isHeld()) {
-                    mWakeLock.release();
-                }
             }
             if (changed) {
                 if (DEBUG) Log.d(TAG, "dispatchModeChanged(" + enabled + ")");
