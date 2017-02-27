b2bbc45e11458c4d7a81133f2f95acec7760f7da
Daniel Koman <dankoman30@gmail.com>
2017-02-27 16:04:11 +0100

    base: Enable/disable kill button on notification guts [1/2]
    * Add a setting by SpiritCroc
    
    Change-Id: Ic5a78e43a2212e5e8215c116a8f377b56bef6f8b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5027,6 +5027,14 @@ public final class Settings {
         public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
 
         /**
+         * Whether to show the kill app button in notification guts
+         *
+         * @hide
+         */
+        public static final String NOTIFICATION_GUTS_KILL_APP_BUTTON =
+                "notification_guts_kill_app_button";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
