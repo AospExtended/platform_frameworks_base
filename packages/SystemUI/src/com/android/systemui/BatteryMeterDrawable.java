ff3e710ba28d253e27a32da78a180d527e129914
gyupro5 <gyupro5@gmail.com>
2017-03-20 17:11:37 +0000

    PA solid circle battery style [1/2]
    This commit adds the
    infamous PA battery circle style from Marshmallow. The
    original implementation replaces CM's circle battery style.
    But, why not make it an option alongside? The code has been
    broken up and refractored to be a standalone battery style
    to be chosen in the Styles menu from Settings. Also a patch
    is included to disable Bolt checks, cherry-picked from the
    original styles commit here:
    AOSPA/android_frameworks_base/commit/c5ef709a286d92d78f8445c53c1a8c7a4a9456ca
    
    - Validus: Fixed a few NPE's along the way
    
    *DU Edits*
    - Removed unused imports
    - Simplify some of the code
    
    PureNexus Edit:
    *cleaned up even more
    *removed unused valueanimator

--- packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
@@ -78,7 +78,8 @@ public class BatteryMeterDrawable extends Drawable implements
     public static final int BATTERY_STYLE_HIDDEN    = 4;
     public static final int BATTERY_STYLE_LANDSCAPE = 5;
     public static final int BATTERY_STYLE_TEXT      = 6;
-    public static final int BATTERY_STYLE_BIGCIRCLE    = 7;
+    public static final int BATTERY_STYLE_BIGCIRCLE = 7;
+    public static final int BATTERY_STYLE_SOLID     = 8;
 
     private final int[] mColors;
     private final int mIntrinsicWidth;
@@ -622,6 +623,8 @@ public class BatteryMeterDrawable extends Drawable implements
                 return R.drawable.ic_battery_bigcircle;
             case BATTERY_STYLE_PORTRAIT:
                 return R.drawable.ic_battery_portrait;
+	    case BATTERY_STYLE_SOLID:
+		return R.drawable.ic_battery_solid;
             default:
                 return 0;
         }
@@ -636,6 +639,8 @@ public class BatteryMeterDrawable extends Drawable implements
                 return R.style.BatteryMeterViewDrawable_Circle;
             case BATTERY_STYLE_PORTRAIT:
                 return R.style.BatteryMeterViewDrawable_Portrait;
+	    case BATTERY_STYLE_SOLID:
+		return R.style.BatteryMeterViewDrawable_Solid;
             default:
                 return R.style.BatteryMeterViewDrawable;
         }
