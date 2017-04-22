d22140233258985d757c1243196effe7633d5f36
maxwen <max.weninger@gmail.com>
2017-04-23 01:42:08 +0200

    base: SystemUI: dont hide weather forecast data on errors
    only show error in header
    see https://gerrit.omnirom.org/23116 for details
    
    Change-Id: Iaa2fa7b6e0d6638b142b19149c5b647d64f3b2e0

--- packages/SystemUI/src/com/android/systemui/omni/DetailedWeatherView.java
+++ packages/SystemUI/src/com/android/systemui/omni/DetailedWeatherView.java
@@ -71,6 +71,7 @@ public class DetailedWeatherView extends LinearLayout {
     private TextView mCurrentText;
     private ImageView mRefresh;
     private View mConditionLine;
+    private View mProgressContainer;
 
     /** The background colors of the app, it changes thru out the day to mimic the sky. **/
     public static final String[] BACKGROUND_SPECTRUM = { "#212121", "#27232e", "#2d253a",
@@ -102,6 +103,7 @@ public class DetailedWeatherView extends LinearLayout {
     protected void onFinishInflate() {
         super.onFinishInflate();
         mConditionLine = findViewById(R.id.condition_line);
+        mProgressContainer = findViewById(R.id.progress_container);
         mWeatherCity  = (TextView) findViewById(R.id.current_weather_city);
         mWeatherTimestamp  = (TextView) findViewById(R.id.current_weather_timestamp);
         mWeatherData  = (TextView) findViewById(R.id.current_weather_data);
@@ -127,6 +129,8 @@ public class DetailedWeatherView extends LinearLayout {
         mRefresh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mProgressContainer.setVisibility(View.VISIBLE);
+                mConditionLine.setVisibility(View.GONE);
                 forceRefreshWeatherSettings();
             }
         });
@@ -143,17 +147,16 @@ public class DetailedWeatherView extends LinearLayout {
     }
 
     public void updateWeatherData(OmniJawsClient.WeatherInfo weatherData) {
+        mConditionLine.setVisibility(View.VISIBLE);
+        mProgressContainer.setVisibility(View.GONE);
+
         if (weatherData == null) {
-            mConditionLine.setVisibility(View.GONE);
-            mWeatherCity.setVisibility(View.GONE);
-            mWeatherTimestamp.setVisibility(View.GONE);
             mWeatherData.setVisibility(View.GONE);
             mNoWeatherNotice.setVisibility(View.VISIBLE);
             mNoWeatherNotice.setText(getResources().getString(R.string.omnijaws_service_unkown));
             return;
         }
         mNoWeatherNotice.setVisibility(View.GONE);
-        mConditionLine.setVisibility(View.VISIBLE);
         mWeatherCity.setVisibility(View.VISIBLE);
         mWeatherTimestamp.setVisibility(View.VISIBLE);
         mWeatherData.setVisibility(View.VISIBLE);
@@ -275,12 +278,13 @@ public class DetailedWeatherView extends LinearLayout {
         return Color.parseColor(BACKGROUND_SPECTRUM[hourOfDay]);
     }
 
-    public void weatherError() {
-        mConditionLine.setVisibility(View.GONE);
-        mWeatherCity.setVisibility(View.GONE);
-        mWeatherTimestamp.setVisibility(View.GONE);
+    public void weatherError(int errorReason) {
+        mConditionLine.setVisibility(View.VISIBLE);
+        mProgressContainer.setVisibility(View.GONE);
         mWeatherData.setVisibility(View.GONE);
         mNoWeatherNotice.setVisibility(View.VISIBLE);
-        mNoWeatherNotice.setText(getResources().getString(R.string.omnijaws_service_error_long));
+        mNoWeatherNotice.setText(errorReason == OmniJawsClient.EXTRA_ERROR_DISABLED ?
+            getResources().getString(R.string.omnijaws_service_disabled) :
+            getResources().getString(R.string.omnijaws_service_error_long));
     }
 }
