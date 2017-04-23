8e3fcdac56e74fd84a71a8515f0b91817d5bc3cc
Varun Date <date.varun123@gmail.com>
2017-04-23 21:56:38 +0800

    Keyguard Weather : Refactor for new API[1/2] --Use Omnijaws API thanks to omni rom and Max Winger --Fix Weather on Lockscreen not updating --Goodbye Cclock (AT LAST)
    Nitrogen:
    --Reworked and adapted for me)
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/java/com/android/internal/util/aospextended/WeatherController.java
    	core/java/com/android/internal/util/aospextended/WeatherControllerImpl.java
    	packages/Keyguard/src/com/android/keyguard/KeyguardStatusView.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4981,6 +4981,11 @@ public final class Settings {
         public static final String NAVBAR_BUTTON_COLOR= "navbar_button_color";
 
         /**
+         * @hide
+         */
+        public static final String OMNIJAWS_WEATHER_ICON_PACK = "omnijaws_weather_icon_pack";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
