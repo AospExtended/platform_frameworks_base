5af04cc97262cce0b68252bef8a4a4494c7a0922
Shubham Singh <coolsks94@gmail.com>
2017-04-27 03:33:55 +0530

    Revert Pin/Password extensions commits will add again correctly
    Revert "Keyguard: Pin Scramble [2/2]"
    
    This reverts commit bb92561992066389dc686bbcf849140e9ef7f8c6.
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/Keyguard/src/com/android/keyguard/KeyguardPINView.java
    
    Revert "Keyguard: Forward port lockscreen quick unlock (1/2)"
    
    This reverts commit dd89d6869bf337f2e41fea8179792feff31492ca.

--- packages/Keyguard/src/com/android/keyguard/KeyguardSimPukView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardSimPukView.java
@@ -188,7 +188,6 @@ public class KeyguardSimPukView extends KeyguardPinBasedInputView {
             ((EmergencyCarrierArea) mEcaView).setCarrierTextVisible(true);
         }
         mSimImageView = (ImageView) findViewById(R.id.keyguard_sim);
-        mPasswordEntry.setQuickUnlockListener(null);
     }
 
     @Override
