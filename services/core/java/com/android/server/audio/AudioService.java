f93542d066b4335af46c832ba78e8313a298e9f7
Sharad Sangle <assangle@codeaurora.org>
2017-05-09 16:05:40 +0530

    AudioService: Don't honour SCO ON request if SCO is not connected
    Check if SCO connection is establisted before honouring the
    setBluetoothScoOn request, if SCO is not connected then
    do not process SCO connection.
    
    CRs-Fixed: 2049708
    Change-Id: I1c004e9b6ec317d58a0126122b58d98e525b7c8f

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -2939,6 +2939,11 @@ public class AudioService extends IAudioService.Stub {
 
     public void setBluetoothScoOnInt(boolean on) {
         if (on) {
+            if ((mBluetoothHeadset != null) &&
+                (mBluetoothHeadset.getAudioState(mBluetoothHeadsetDevice)
+                             != BluetoothHeadset.STATE_AUDIO_CONNECTED)) {
+                return;
+            }
             mForcedUseForComm = AudioSystem.FORCE_BT_SCO;
             synchronized(mScoClients) {
                 if ((mBluetoothHeadset != null) &&
