3d0cff9300796f967c5c57965af6bc29257b150f
Arusha Goyal <arushag@codeaurora.org>
2017-05-28 09:04:07 +0800

    BoostFramework to enhance performance during critical scenarios Adding Boost APIs which can ensure the best possible resource availability during critical UX scenarios. These APIs can be either turned off or dynamically hooked to OEM specific performance optimizations
    Squashed with change: I90a733d7134d86efffadf6815ce3d2944dd09419
    
    Performance: Make Cached apps limit configurable.
    
    Set it via a target specific property
    file. Allows flexibility to set the limit
    based on device config
    
    Normally, setting a smaller value for
    low-end device helps with performance.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelView.java
@@ -43,6 +43,7 @@ import com.android.systemui.doze.DozeLog;
 import com.android.systemui.statusbar.FlingAnimationUtils;
 import com.android.systemui.statusbar.StatusBarState;
 import com.android.systemui.statusbar.policy.HeadsUpManager;
+import android.util.BoostFramework;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -105,6 +106,12 @@ public abstract class PanelView extends FrameLayout {
             }
         }
     };
+
+    /**
+     * For PanelView fling perflock call
+     */
+    private BoostFramework mPerf = null;
+    private int mBoostParamVal[];
 
     /**
      * Whether an instant expand request is currently pending and we are just waiting for layout.
@@ -206,6 +213,14 @@ public abstract class PanelView extends FrameLayout {
         mFlingAnimationUtils = new FlingAnimationUtils(context, 0.6f);
         mBounceInterpolator = new BounceInterpolator();
         mFalsingManager = FalsingManager.getInstance(context);
+
+        boolean lIsPerfBoostEnabled = context.getResources().getBoolean(
+                com.android.internal.R.bool.config_enableCpuBoostForPanelViewFling);
+        if (lIsPerfBoostEnabled) {
+            mBoostParamVal = context.getResources().getIntArray(
+                    com.android.internal.R.array.panelview_flingboost_param_value);
+            mPerf = new BoostFramework();
+        }
     }
 
     protected void loadDimens() {
@@ -690,6 +705,9 @@ public abstract class PanelView extends FrameLayout {
                                 / collapseSpeedUpFactor));
             }
         }
+        if (mPerf != null) {
+            mPerf.perfLockAcquire(0, mBoostParamVal);
+        }
         animator.addListener(new AnimatorListenerAdapter() {
             private boolean mCancelled;
 
@@ -700,12 +718,18 @@ public abstract class PanelView extends FrameLayout {
 
             @Override
             public void onAnimationCancel(Animator animation) {
+                if (mPerf != null) {
+                    mPerf.perfLockRelease();
+                }
                 mCancelled = true;
             }
 
             @Override
             public void onAnimationEnd(Animator animation) {
                 if (expand) PanelView.this.removeOnLayoutChangeListener(mLayoutChangeListener);
+                if (mPerf != null) {
+                    mPerf.perfLockRelease();
+                }
                 if (clearAllExpandHack && !mCancelled) {
                     setExpandedHeightInternal(getMaxPanelHeight());
                 }
--- services/core/java/com/android/server/am/ActivityStack.java
+++ services/core/java/com/android/server/am/ActivityStack.java
@@ -117,6 +117,7 @@ import android.os.Trace;
 import android.os.UserHandle;
 import android.service.voice.IVoiceInteractionSession;
 import android.util.ArraySet;
+import android.util.BoostFramework;
 import android.util.EventLog;
 import android.util.Log;
 import android.util.Slog;
@@ -237,6 +238,10 @@ final class ActivityStack {
     final WindowManagerService mWindowManager;
     private final RecentTasks mRecentTasks;
 
+    public BoostFramework mPerf = null;
+    public boolean mIsAnimationBoostEnabled = false;
+    public int aBoostTimeOut = 0;
+    public int aBoostParamVal[];
     /**
      * The back history of all previous (and possibly still
      * running) activities.  It contains #TaskRecord objects.
@@ -449,6 +454,14 @@ final class ActivityStack {
         mRecentTasks = recentTasks;
         mTaskPositioner = mStackId == FREEFORM_WORKSPACE_STACK_ID
                 ? new LaunchingTaskPositioner() : null;
+        mIsAnimationBoostEnabled = mService.mContext.getResources().getBoolean(
+                   com.android.internal.R.bool.config_enablePerfBoostForAnimation);
+        if (mIsAnimationBoostEnabled) {
+           aBoostTimeOut = mService.mContext.getResources().getInteger(
+                   com.android.internal.R.integer.animationboost_timeout_param);
+           aBoostParamVal = mService.mContext.getResources().getIntArray(
+                   com.android.internal.R.array.animationboost_param_value);
+        }
     }
 
     void attachDisplay(ActivityStackSupervisor.ActivityDisplay activityDisplay, boolean onTop) {
@@ -2358,6 +2371,9 @@ final class ActivityStack {
         // that the previous one will be hidden soon.  This way it can know
         // to ignore it when computing the desired screen orientation.
         boolean anim = true;
+        if (mIsAnimationBoostEnabled == true && mPerf == null) {
+            mPerf = new BoostFramework();
+        }
         if (prev != null) {
             if (prev.finishing) {
                 if (DEBUG_TRANSITION) Slog.v(TAG_TRANSITION,
@@ -2369,6 +2385,9 @@ final class ActivityStack {
                     mWindowManager.prepareAppTransition(prev.task == next.task
                             ? TRANSIT_ACTIVITY_CLOSE
                             : TRANSIT_TASK_CLOSE, false);
+                    if(prev.task != next.task && mPerf != null) {
+                       mPerf.perfLockAcquire(aBoostTimeOut, aBoostParamVal);
+                    }
                 }
                 mWindowManager.setAppVisibility(prev.appToken, false);
             } else {
@@ -2383,6 +2402,9 @@ final class ActivityStack {
                             : next.mLaunchTaskBehind
                                     ? TRANSIT_TASK_OPEN_BEHIND
                                     : TRANSIT_TASK_OPEN, false);
+                    if(prev.task != next.task && mPerf != null) {
+                        mPerf.perfLockAcquire(aBoostTimeOut, aBoostParamVal);
+                    }
                 }
             }
         } else {
