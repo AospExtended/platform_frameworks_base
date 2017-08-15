76b7bf093d195ffd3f83f649be41b62a700f65c5
Corinna Vinschen <xda@vinschen.de>
2017-08-15 19:11:16 +0800

    PowerManagerService: fix HW button illumination timeout Patch eb01128a6a868c11f648da667e900dd52cc8d1fc, introducing hardware button backlight on button keypress only, also introduced a bug:
    When touching a button and then performing display activity while
    the buttons are still on, the buttons would keep lightened up until
    the next user interaction, potentially only switched off at the next
    screen off timeout.  Also, the buttons were not illuminated on
    device wakeup.
    
    This patch fixes it, together with another, long-standing problem:
    
    When touching a hardware button, nextTimeout was set to
    now + mButtonTimeout, even if mButtonTimeout is longer than the timeout
    determined by the screen off timeout.  To wit, if screen timeout is set
    to 15 secs, but button timeout to values > 15 secs.
    
    Change-Id: I612ddf6f695f08d0a0fd77906f0ae9150563d9b7

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -227,6 +227,7 @@ public final class PowerManagerService extends SystemService
     private int mKeyboardBrightness;
     private int mKeyboardBrightnessSettingDefault;
     private boolean mButtonPressed = false;
+    private boolean mButtonOn = false;
 
     private final Object mLock = new Object();
 
@@ -1355,7 +1356,9 @@ public final class PowerManagerService extends SystemService
             } else {
                 if (eventTime > mLastUserActivityTime) {
                     mButtonPressed = event == PowerManager.USER_ACTIVITY_EVENT_BUTTON;
-                    if (mButtonBacklightOnTouchOnly && mButtonPressed) {
+                    if ((mButtonBacklightOnTouchOnly && mButtonPressed)
+                            || eventTime == mLastWakeTime) {
+                        mButtonPressed = true;
                         mLastButtonActivityTime = eventTime;
                     }
                     mLastUserActivityTime = eventTime;
@@ -1933,13 +1936,20 @@ public final class PowerManagerService extends SystemService
                             if (mButtonTimeout != 0
                                     && now > mLastButtonActivityTime + mButtonTimeout) {
                                 mButtonsLight.setBrightness(0);
+                                mButtonOn = false;
                             } else {
                                 if ((!mButtonBacklightOnTouchOnly || mButtonPressed) && !isDeviceInPocket ) {
                                     mButtonsLight.setBrightness(buttonBrightness);
                                     mButtonPressed = false;
                                     if (buttonBrightness != 0 && mButtonTimeout != 0) {
-                                        nextTimeout = now + mButtonTimeout;
+                                        mButtonOn = true;
+                                        if (now + mButtonTimeout < nextTimeout) {
+                                            nextTimeout = now + mButtonTimeout;
+                                        }
                                     }
+                                } else if (mButtonBacklightOnTouchOnly && mButtonOn &&
+                                        mLastButtonActivityTime + mButtonTimeout < nextTimeout) {
+                                    nextTimeout = mLastButtonActivityTime + mButtonTimeout;
                                 }
                             }
                         }
@@ -1950,6 +1960,7 @@ public final class PowerManagerService extends SystemService
                             if (mWakefulness == WAKEFULNESS_AWAKE) {
                                 mButtonsLight.setBrightness(0);
                                 mKeyboardLight.setBrightness(0);
+                                mButtonOn = false;
                             }
                         }
                     }
