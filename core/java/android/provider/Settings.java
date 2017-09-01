7fdf8095ee4e043a1676dc27aecbddd3c245111e
maxwen <max.weninger@gmail.com>
2017-09-01 15:54:53 +0200

    base: allow to swap volume buttons rotation based [1/2]
    Change-Id: Ia50d9d1d8a72a649dd86b99f9701b0de05252ebe
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4984,6 +4984,12 @@ public final class Settings {
         public static final String SHOW_VOLTE_ICON = "volte_icon";
 
         /**
+         * Change volume up and down handlign based on rotation
+         * @hide
+         */
+        public static final String SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
