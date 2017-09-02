e8055ba38ca788ad029c01d83ddb98c814dba570
faust93 <monumentum@gmail.com>
2017-09-02 14:32:42 +0800

    AlarmsBlocker [1/2] Made by @faust93. Based on WakelockBlocker by @maxwen
    Change-Id: I9be5d5179bd34b81248e2c6f684257123a9af377

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4058,6 +4058,18 @@ public final class Settings {
         public static final String WAKELOCK_BLOCKING_LIST = "wakelock_blocking_list";
 
         /**
+         * List of alarms blocks selected
+         * @hide
+         */
+        public static final String ALARM_BLOCKING_LIST = "alarm_blocking_list";
+
+        /**
+         * Enable blocking alarm
+         * @hide
+         */
+        public static final String ALARM_BLOCKING_ENABLED = "alarm_blocking_enabled";
+
+        /**
          * Whether to show network traffic indicator in statusbar
          * @hide
          */
