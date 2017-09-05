408e359c2860e67e9c1fafa82818d74a7da379d9
beanstown106 <nbenis106@gmail.com>
2017-09-05 14:34:10 +0800

    base: incall vibration options [1/3]
     *allow setting vibration when call is connected
     *allow setting vibration when call is disconnected
     *allow setting vibration for call waiting
     *this works with google and aosp dialer :)
    
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: I14a403189f37e45fa4b999ab015bde8f89ae3da4
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5185,6 +5185,24 @@ public final class Settings {
         public static final String PROXIMITY_ON_WAKE = "proximity_on_wake";
 
         /**
+         * Whether the phone vibrates on call connect
+         * @hide
+         */
+        public static final String VIBRATE_ON_CONNECT = "vibrate_on_connect";
+
+        /**
+         * Whether the phone vibrates on call waiting
+         * @hide
+         */
+        public static final String VIBRATE_ON_CALLWAITING = "vibrate_on_callwaiting";
+
+        /**
+         * Whether the phone vibrates on disconnect
+         * @hide
+         */
+        public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
