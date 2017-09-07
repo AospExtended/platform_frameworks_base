ccca71e74d942e950df44f7cbc05cbad1bf144d8
xyyx <xyyx@mail.ru>
2017-09-07 11:16:04 +0800

    Ability to disable dashboards conditions (1/2)
    Change-Id: I3e2ca14d83564cc0201cf0f1b9acddd6e75ceef2
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4706,6 +4706,12 @@ public final class Settings {
         public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
 
         /**
+         * Disable dashboard conditions in settings
+         * @hide
+         */
+        public static final String ENABLE_CONDITIONS = "enable_conditions";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
