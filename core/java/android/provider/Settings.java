08b82d8d93c35f1050629a064a7cbf3b573134e2
Konsta <konsta09@gmail.com>
2017-09-09 19:10:41 +0800

    Volume key cursor control (1/2)
    This feature is moved to framework so it also works with third
    party keyboards.
    
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: Ic1fc2e802e8df4a74a01828d0549e2b4340886b8
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5126,6 +5126,15 @@ public final class Settings {
         public static final String FP_SUCCESS_VIBRATE = "fingerprint_success_vib";
 
         /**
+          * Volume keys control cursor in text fields (default is 0)
+          * 0 - Disabled
+          * 1 - Volume up/down moves cursor left/right
+          * 2 - Volume up/down moves cursor right/left
+          * @hide
+          */
+        public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
