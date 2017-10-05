4c1cd776931b0d1a7bf2df335667b7fc5c9bf16d
xyyx <xyyx@mail.ru>
2017-10-05 21:23:11 +0800

    Disable dashboard suggestions (1/2)
    Change-Id: I0963f4d0c74311ad462fbb9f2bc73fc02ff3c8b1
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3967,6 +3967,12 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
 
         /**
+         * Disable dashboard suggestions in settings
+         * @hide
+         */
+        public static final String ENABLE_SUGGESTIONS = "enable_suggestions";
+
+        /**
          * show clear all recents button
          * @hide
          */
