6b91e7d2cecea5cb17775fe4199541694565b36c
Pranav Vashi <neobuddy89@gmail.com>
2017-10-13 00:43:01 +0530

    PhoneWindowManager: Add missing check for immersive mode
    Change-Id: Ifb3fd594e30c2e9b03417a474f26c6818c1419f2
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1465,7 +1465,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         boolean panic = mImmersiveModeConfirmation.onPowerKeyDown(interactive,
                 SystemClock.elapsedRealtime(), isImmersiveMode(mLastSystemUiFlags),
                 isNavBarEmpty(mLastSystemUiFlags));
-        if (panic) {
+        if (panic && !WindowManagerPolicyControl.isImmersiveFiltersActive()) {
             mHandler.post(mHiddenNavPanic);
         }
 
