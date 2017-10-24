6a8473743fb7d3c27a40c358f07699e67c2be34b
Shubham Singh <coolsks94@gmail.com>
2017-10-24 11:32:28 +0530

    PowerMenu: Add Soft reboot & SystemUI restart
      Add it under toggleable Advanced reboot menu introduced in https://github.com/AospExtended/platform_frameworks_base/commit/4f9aa80103dbc2582129f176b870d5cc0487c353
      Adapted SystemUI restart helper class from https://github.com/AospExtended/platform_frameworks_base/commit/f80ffdcc8144f7ebce78c013763756d57bedd118
      Used Soft reboot method from https://github.com/AospExtended/platform_frameworks_base/commit/2d290c18ca738a02a130b8e45f9b1a345ba8caf4#diff-3b54852bb3c1981a4c6a4ba90ad70fe7
    
    Change-Id: I8c83c791a6aa1854f59cca2eaa6306de1b971eae
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -49,6 +49,7 @@ import android.system.ErrnoException;
 import android.system.Os;
 
 import com.android.internal.telephony.ITelephony;
+import com.android.internal.util.aospextended.Helpers;
 import com.android.server.pm.PackageManagerService;
 
 import android.util.Log;
@@ -221,6 +222,38 @@ public final class ShutdownThread extends Thread {
         mReason = reason;
         shutdownInner(context, confirm);
     }
+
+    /**
+     * Request for Soft reboot or SystemUI restart.  Must be called from a Looper thread in which its UI
+     * is shown.
+     *
+     * @param context Context used by SystemUI restart helper method. This must be a context
+     *                suitable for displaying UI (aka Themable).
+     */
+    public static void advancedReboot(final Context context, String reason) {
+        mReason = reason;
+        if (mReason != null && mReason.equals(PowerManager.REBOOT_SOFT)) {
+            doSoftReboot();
+        } else if (mReason != null && mReason.equals(PowerManager.REBOOT_SYSTEMUI)) {
+            doSystemUIReboot(context);
+        }
+    }
+
+    private static void doSoftReboot() {
+        try {
+            final IActivityManager am =
+                  IActivityManager.Stub.asInterface(ServiceManager.checkService("activity"));
+            if (am != null) {
+                am.restart();
+            }
+        } catch (RemoteException e) {
+            Log.e(TAG, "failure trying to perform soft reboot", e);
+        }
+    }
+
+    private static void doSystemUIReboot(Context context) {
+        Helpers.restartSystemUI(context);
+      }
 
     /**
      * Request a reboot into safe mode.  Must be called from a Looper thread in which its UI
