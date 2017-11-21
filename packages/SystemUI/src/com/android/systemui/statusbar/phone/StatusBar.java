5c1e0eae8e43d4ce5103280dba73a48ca07fdb12
Shubham Singh <coolsks94@gmail.com>
2017-11-21 22:58:47 +0530

    Revert "SystemUI: Fix Screen Not On Issue"
    This reverts commit b13a73eaf5c83316258734dbc398e5677406235c.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -788,8 +788,6 @@ public class StatusBar extends SystemUI implements DemoMode,
     private KeyguardMonitorImpl mKeyguardMonitor;
     private BatteryController mBatteryController;
     private boolean mPanelExpanded;
-    private boolean mKeyguardRequested;
-    private boolean mIsKeyguard;
     private LogMaker mStatusBarStateLog;
     private LockscreenGestureLogger mLockscreenGestureLogger = new LockscreenGestureLogger();
     private NotificationIconAreaController mNotificationIconAreaController;
@@ -4517,30 +4515,6 @@ public class StatusBar extends SystemUI implements DemoMode,
     }
 
     public void showKeyguard() {
-        mKeyguardRequested = true;
-        updateIsKeyguard();
-    }
-
-    public boolean hideKeyguard() {
-        mKeyguardRequested = false;
-        return updateIsKeyguard();
-    }
-
-    private boolean updateIsKeyguard() {
-        // For dozing, keyguard needs to be shown whenever the device is non-interactive. Otherwise
-        // there's no surface we can show to the user.
-        boolean keyguardForDozing = mDozingRequested && !mDeviceInteractive;
-        boolean shouldBeKeyguard = mKeyguardRequested || keyguardForDozing;
-        if (shouldBeKeyguard && !mIsKeyguard) {
-            showKeyguardImpl();
-        } else if (!shouldBeKeyguard && mIsKeyguard) {
-            return hideKeyguardImpl();
-        }
-        return false;
-    }
-
-    public void showKeyguardImpl() {
-        mIsKeyguard = true;
         if (mLaunchTransitionFadingAway) {
             mNotificationPanel.animate().cancel();
             onLaunchTransitionFadingEnded();
@@ -4688,8 +4662,7 @@ public class StatusBar extends SystemUI implements DemoMode,
     /**
      * @return true if we would like to stay in the shade, false if it should go away entirely
      */
-    public boolean hideKeyguardImpl() {
-        mIsKeyguard = false;
+    public boolean hideKeyguard() {
         Trace.beginSection("StatusBar#hideKeyguard");
         boolean staying = mLeaveOpenOnKeyguardHide;
         setBarState(StatusBarState.SHADE);
@@ -5380,7 +5353,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                 }
             });
         }
-        updateIsKeyguard();
     }
 
     public void onStartedWakingUp() {
@@ -5389,7 +5361,6 @@ public class StatusBar extends SystemUI implements DemoMode,
         mVisualStabilityManager.setScreenOn(true);
         mNotificationPanel.setTouchDisabled(false);
         updateVisibleToUser();
-        updateIsKeyguard();
     }
 
     public void onScreenTurningOn() {
@@ -5511,7 +5482,7 @@ public class StatusBar extends SystemUI implements DemoMode,
     private void updateDozing() {
         Trace.beginSection("StatusBar#updateDozing");
         // When in wake-and-unlock while pulsing, keep dozing state until fully unlocked.
-        mDozing = mDozingRequested
+        mDozing = mDozingRequested && mState == StatusBarState.KEYGUARD
                 || mFingerprintUnlockController.getMode()
                         == FingerprintUnlockController.MODE_WAKE_AND_UNLOCK_PULSING;
         // When in wake-and-unlock we may not have received a change to mState
@@ -5521,7 +5492,6 @@ public class StatusBar extends SystemUI implements DemoMode,
             mDozing = false;
         }
         mStatusBarWindowManager.setDozing(mDozing);
-        mStatusBarKeyguardViewManager.setDozing(mDozing);
         updateDozingState();
         Trace.endSection();
     }
