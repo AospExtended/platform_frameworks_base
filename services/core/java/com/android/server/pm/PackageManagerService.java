21d4ec19a716393c6eb519668c151c172fa9fe24
Ethan Chen <intervigil@gmail.com>
2017-12-06 09:58:03 -0800

    PackageManager: Add configuration to specify vendor platform signatures
    Devices with split system/vendor images may want to use the OEM's vendor
    image. In that case, the OEM's platform signature is not actually the
    same as the platform signature used to sign the Lineage system image.
    Allow devices to specify an OEM platform signature which will also be
    recognized as the system's platform signature.
    
    Change-Id: Ida9bb25a32234af9d9507a214eae6a4672320d2b
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -1653,6 +1653,8 @@ public class PackageManagerService extends IPackageManager.Stub
     private final PackageUsage mPackageUsage = new PackageUsage();
     private final CompilerStats mCompilerStats = new CompilerStats();
 
+    private final Signature[] mVendorPlatformSignatures;
+
     class PackageHandler extends Handler {
 
         PackageHandler(Looper looper) {
@@ -2743,6 +2745,14 @@ public class PackageManagerService extends IPackageManager.Stub
         }
     }
 
+    private static Signature[] createSignatures(String[] hexBytes) {
+        Signature[] sigs = new Signature[hexBytes.length];
+        for (int i = 0; i < sigs.length; i++) {
+            sigs[i] = new Signature(hexBytes[i]);
+        }
+        return sigs;
+    }
+
     /**
      * A extremely minimal constructor designed to start up a PackageManagerService instance for
      * testing.
@@ -2763,6 +2773,9 @@ public class PackageManagerService extends IPackageManager.Stub
         mPermissionManager = injector.getPermissionManagerServiceInternal();
         mSettings = injector.getSettings();
         mUserManager = injector.getUserManagerService();
+
+        mVendorPlatformSignatures = createSignatures(mContext.getResources().getStringArray(
+                           com.android.internal.R.array.config_vendorPlatformSignatures));
 
         mApexManager = testParams.apexManager;
         mArtManagerService = testParams.artManagerService;
@@ -2845,6 +2858,10 @@ public class PackageManagerService extends IPackageManager.Stub
         }
 
         mContext = injector.getContext();
+
+        mVendorPlatformSignatures = createSignatures(mContext.getResources().getStringArray(
+                    com.android.internal.R.array.config_vendorPlatformSignatures));
+
         mFactoryTest = factoryTest;
         mOnlyCore = onlyCore;
         mMetrics = new DisplayMetrics();
@@ -9218,6 +9235,20 @@ public class PackageManagerService extends IPackageManager.Stub
             Trace.traceBegin(TRACE_TAG_PACKAGE_MANAGER, "collectCertificates");
             parsedPackage.setSigningDetails(
                     ParsingPackageUtils.getSigningDetails(parsedPackage, skipVerify));
+
+            if (compareSignatures(parsedPackage.getSigningDetails().signatures,
+                mVendorPlatformSignatures) == PackageManager.SIGNATURE_MATCH) {
+                // Overwrite package signature with our platform signature
+                // if the signature is the vendor's platform signature
+                if (mPlatformPackage != null) {
+                    parsedPackage.setSigningDetails(mPlatformPackage.getSigningDetails());
+                    parsedPackage.setSeInfo(SELinuxMMAC.getSeInfo(
+                        parsedPackage,
+                        parsedPackage.isPrivileged(),
+                        parsedPackage.getTargetSdkVersion()));
+                }
+           }
+
         } catch (PackageParserException e) {
             throw PackageManagerException.from(e);
         } finally {
@@ -9415,7 +9446,7 @@ public class PackageManagerService extends IPackageManager.Stub
                             null, disabledPkgSetting /* pkgSetting */,
                             null /* disabledPkgSetting */, null /* originalPkgSetting */,
                             null, parseFlags, scanFlags, isPlatformPackage, user, null);
-                    applyPolicy(parsedPackage, parseFlags, scanFlags, mPlatformPackage, true);
+                    applyPolicy(parsedPackage, parseFlags, scanFlags, mPlatformPackage, true, mVendorPlatformSignatures);
                     final ScanResult scanResult =
                             scanPackageOnlyLI(request, mInjector, mFactoryTest, -1L);
                     if (scanResult.existingSettingCopied && scanResult.request.pkgSetting != null) {
@@ -11054,7 +11085,7 @@ public class PackageManagerService extends IPackageManager.Stub
             } else {
                 isUpdatedSystemApp = disabledPkgSetting != null;
             }
-            applyPolicy(parsedPackage, parseFlags, scanFlags, mPlatformPackage, isUpdatedSystemApp);
+            applyPolicy(parsedPackage, parseFlags, scanFlags, mPlatformPackage, isUpdatedSystemApp, mVendorPlatformSignatures);
             assertPackageIsValid(parsedPackage, parseFlags, scanFlags);
 
             SharedUserSetting sharedUserSetting = null;
@@ -11815,7 +11846,7 @@ public class PackageManagerService extends IPackageManager.Stub
      */
     private static void applyPolicy(ParsedPackage parsedPackage, final @ParseFlags int parseFlags,
             final @ScanFlags int scanFlags, AndroidPackage platformPkg,
-            boolean isUpdatedSystemApp) {
+            boolean isUpdatedSystemApp, Signature[] vendorPlatformSignatures) {
         if ((scanFlags & SCAN_AS_SYSTEM) != 0) {
             parsedPackage.setSystem(true);
             // TODO(b/135203078): Can this be done in PackageParser? Or just inferred when the flag
@@ -11854,8 +11885,11 @@ public class PackageManagerService extends IPackageManager.Stub
         // Check if the package is signed with the same key as the platform package.
         parsedPackage.setSignedWithPlatformKey(
                 (PLATFORM_PACKAGE_NAME.equals(parsedPackage.getPackageName())
-                        || (platformPkg != null && compareSignatures(
+                        || (platformPkg != null && (compareSignatures(
                         platformPkg.getSigningDetails().signatures,
+                        parsedPackage.getSigningDetails().signatures
+                        ) == PackageManager.SIGNATURE_MATCH) ||
+                        compareSignatures(vendorPlatformSignatures,
                         parsedPackage.getSigningDetails().signatures
                 ) == PackageManager.SIGNATURE_MATCH))
         );
