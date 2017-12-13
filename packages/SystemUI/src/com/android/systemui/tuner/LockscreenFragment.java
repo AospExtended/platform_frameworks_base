daf47ded740e0b12217f178e81aa8c5df01c57b7
ezio84 <brabus84@gmail.com>
2017-12-13 11:17:55 +0100

    Fix custom lockscreen shortcut icons being white
    now 8.1 sets a tint to make icons black and white according to the
    wallpaper main color but this is not suitable for custom icons
    let's fix this setting the tint only for default lockscreen buttons
    
    Change-Id: I79c39fb39e308f9815afda0c85fcd3922c528c69
    Signed-off-by: xyyx <xyyx@mail.ru>

--- packages/SystemUI/src/com/android/systemui/tuner/LockscreenFragment.java
+++ packages/SystemUI/src/com/android/systemui/tuner/LockscreenFragment.java
@@ -343,6 +343,8 @@ public class LockscreenFragment extends PreferenceFragment {
         public IntentButton create(Map<String, String> settings) {
         Log.d("maxwen", "create " + mKey + " " + settings);
             String buttonStr = settings.get(mKey);
+            // if buttonStr is not empty it means it's not the Default button,
+            // so we'll create a custom one
             if (!TextUtils.isEmpty(buttonStr)) {
                 if (buttonStr.contains("::")) {
                     return new ShortcutButton(mContext, buttonStr);
@@ -375,6 +377,7 @@ public class LockscreenFragment extends PreferenceFragment {
             mIconState.drawable = new ScalingDrawableWrapper(mIconState.drawable,
                     mSize / (float) mIconState.drawable.getIntrinsicWidth());
             mIconState.tint = false;
+            mIconState.isDefaultButton = false;
             init();
         }
 
@@ -425,6 +428,7 @@ public class LockscreenFragment extends PreferenceFragment {
             mIconState.drawable = new ScalingDrawableWrapper(mIconState.drawable,
                     mSize / (float) mIconState.drawable.getIntrinsicWidth());
             mIconState.tint = false;
+            mIconState.isDefaultButton = false;
             init();
         }
 
@@ -464,6 +468,7 @@ public class LockscreenFragment extends PreferenceFragment {
         public HiddenButton() {
             mIconState = new IconState();
             mIconState.isVisible = false;
+            mIconState.isDefaultButton = false;
         }
 
         @Override
