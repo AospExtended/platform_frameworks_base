cb1fc8ef8d38522d3d436ed8f059e064fe0f11e2
Joe Maples <joe@frap129.org>
2017-12-18 21:49:25 -0500

    etc: Add GApp default permissions
    Change-Id: I9bbc297bfe59e03cd5d8e68c810f82ff526cd306
    Signed-off-by: Joe Maples <joe@frap129.org>

--- data/etc/Android.mk
+++ data/etc/Android.mk
@@ -48,3 +48,11 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
+
+########################
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp-permissions-google.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
