83b44e5bb909fbe7b1ad5c9f1058cfb7fbbe6bf2
ezio84 <brabus84@gmail.com>
2017-12-26 15:31:19 +0100

    Power menu advanced reboot toggle: new icons
    base from flaticon.com
    
    Change-Id: I411fcba542e6418dabbf0498a3b9fed48c041618

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -359,7 +359,7 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
 
         mRestartAdvancedAction = new ToggleRestartAdvancedAction(
                 com.android.systemui.R.drawable.ic_restart_advanced,
-                com.android.systemui.R.drawable.ic_restart_advanced,
+                com.android.systemui.R.drawable.ic_restart_bootloader,
                 com.android.systemui.R.string.global_action_restart_advanced,
                 com.android.systemui.R.string.global_action_restart_recovery,
                 com.android.systemui.R.string.global_action_restart_bootloader,
