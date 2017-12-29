a49ef3b9cf2dd58069d187514b1a6988ea808c95
Alex Cruz <du.alexcruz@gmail.com>
2017-12-29 00:35:37 -0500

    Comment out safe reboot on power menu item
    No reason to keep it on the reboot menu item and on the power one.
    
    Change-Id: I759326689afa6ec862e7ce06289b24b3900358c7

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -564,11 +564,11 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
 
         @Override
         public boolean onLongPress() {
-            UserManager um = (UserManager) mContext.getSystemService(Context.USER_SERVICE);
-            if (!um.hasUserRestriction(UserManager.DISALLOW_SAFE_BOOT)) {
-                mWindowManagerFuncs.reboot(true);
-                return true;
-            }
+            //UserManager um = (UserManager) mContext.getSystemService(Context.USER_SERVICE);
+            //if (!um.hasUserRestriction(UserManager.DISALLOW_SAFE_BOOT)) {
+            //    mWindowManagerFuncs.reboot(true);
+            //    return true;
+            //}
             return false;
         }
 
