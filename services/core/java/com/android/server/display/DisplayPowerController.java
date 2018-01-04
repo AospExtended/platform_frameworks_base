445115c2db2b75ba87b045c31cb6a88a6dcbb0bf
Joe Maples <joe@frap129.org>
2018-01-03 21:53:01 -0500

    SystemUI: Smart Pixels [1/2]
    Disables a percentage of pixels on screen to reduce power consumption.
    If enabled with battery saver, don't scale brightness at 0.5f for UX.
    
    Includes:
     - Option to enable on battery saver
     - User chosen grid
     - Burn-in protection
    
    Configurable via overlay and disabled by defualt:
      "config_enableSmartPixels"
    
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    
    Change-Id: Id3c78548cb090ab2da11f543da31c5a408fb9fe9
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- services/core/java/com/android/server/display/DisplayPowerController.java
+++ services/core/java/com/android/server/display/DisplayPowerController.java
@@ -21,6 +21,7 @@ import android.animation.ObjectAnimator;
 import android.annotation.Nullable;
 import android.annotation.UserIdInt;
 import android.app.ActivityManager;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.pm.ParceledListSlice;
 import android.content.res.Resources;
@@ -401,6 +402,8 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
     @Nullable
     private final BrightnessSynchronizer mBrightnessSynchronizer;
 
+    private ContentResolver mContentResolver;
+
     /**
      * Creates the display power controller.
      */
@@ -421,6 +424,8 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
 
         PowerManager pm =  context.getSystemService(PowerManager.class);
         DisplayDeviceConfig displayDeviceConfig = mDisplayDevice.getDisplayDeviceConfig();
+
+        mContentResolver = context.getContentResolver();
 
         final Resources resources = context.getResources();
 
@@ -1032,10 +1037,20 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
             slowChange = false;
             mAppliedDimming = false;
         }
-        // If low power mode is enabled, scale brightness by screenLowPowerBrightnessFactor
-        // as long as it is above the minimum threshold.
+
+        // If low power mode is enabled and Smart Pixels Service is stopped,
+        // scale brightness by screenLowPowerBrightnessFactor
+        // as long as it is above the minimum threshold
+        final int mSmartPixelsEnable = Settings.System.getIntForUser(
+                mContentResolver, Settings.System.SMART_PIXELS_ENABLE,
+                0, UserHandle.USER_CURRENT);
+        final int mSmartPixelsOnPowerSave = Settings.System.getIntForUser(
+                mContentResolver, Settings.System.SMART_PIXELS_ON_POWER_SAVE,
+                0, UserHandle.USER_CURRENT);
+
         if (mPowerRequest.lowPowerMode) {
-            if (brightnessState > mScreenBrightnessRangeMinimum) {
+            if ((brightnessState > mScreenBrightnessRangeMinimum) &&
+                  ((mSmartPixelsEnable == 0) || (mSmartPixelsOnPowerSave == 0))) {
                 final float brightnessFactor =
                         Math.min(mPowerRequest.screenLowPowerBrightnessFactor, 1);
                 final float lowPowerBrightnessFloat = (brightnessState * brightnessFactor);
