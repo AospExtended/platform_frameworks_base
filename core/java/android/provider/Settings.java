9b7f3fbbff188d50471334335392996fc20fa8b6
ezio84 <brabus84@gmail.com>
2018-01-04 10:26:17 +0100

    Allow to suppress notifications sound/vibration if screen is ON [1/2]
    Change-Id: Ia15ac6bf5a2f2a6437701607b540f41df707545b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5955,6 +5955,14 @@ public final class Settings {
 
 
         /**
+         * Wheter to play notification sound and vibration if screen is ON
+         * 0 - never
+         * 1 - always
+         * @hide
+         */
+        public static final String NOTIFICATION_SOUND_VIB_SCREEN_ON = "notification_sound_vib_screen_on";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
