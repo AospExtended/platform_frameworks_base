a28714e766fc2dbcc7e52b9b9f9548fc56793792
xyyx <xyyx@mail.ru>
2018-01-30 14:35:41 +0800

    Add toggle for face auto unlock (1/2)
    Change-Id: I2974a2b44ee977ccc973c083bef9777f94eed1af

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8228,6 +8228,13 @@ public final class Settings {
         public static final String ADB_PORT = "adb_port";
 
         /**
+         * Enable face auto unlock on secure lock screens
+         *
+         * @hide
+         */
+        public static final String FACE_AUTO_UNLOCK = "face_auto_unlock";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
