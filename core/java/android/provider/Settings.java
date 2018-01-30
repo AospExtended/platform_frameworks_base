c3fb7dcdc8922d3a1a8c9176bc408038f76aac7e
xyyx <xyyx@mail.ru>
2018-01-30 14:35:41 +0800

    Add toggle for face auto unlock (1/2)
    Change-Id: I4360f96924378f21517ef66d79a26f8104c5b113

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8122,6 +8122,13 @@ public final class Settings {
         public static final String QS_SHOW_BRIGHTNESS_SLIDER = "qs_show_brightness_slider";
 
         /**
+         * Enable face auto unlock on secure lock screens
+         *
+         * @hide
+         */
+        public static final String FACE_AUTO_UNLOCK = "face_auto_unlock";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
