7e22689ecb9294db4cdc7623235ee644d3f806db
maxwen <max.weninger@gmail.com>
2018-02-02 14:55:57 +0100

    base: store aex fingerprint for package data
    and use that to clear package cache instead of our
    recovery script solution
    
    Change-Id: I21f599a84f8ff28b53bc088f739374c037f81be0

--- core/java/android/os/Build.java
+++ core/java/android/os/Build.java
@@ -1014,6 +1014,10 @@ public class Build {
     /** A string that uniquely identifies this build.  Do not attempt to parse this value. */
     public static final String FINGERPRINT = deriveFingerprint();
 
+    /** @hide */
+    public static final String AEX_FINGERPRINT = SystemProperties.get("ro.aex.fingerprint",
+            deriveFingerprint());
+
     /**
      * Some devices split the fingerprint components between multiple
      * partitions, so we might derive the fingerprint at runtime.
