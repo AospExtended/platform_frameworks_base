b9be1d64b0b8ac53ad4116d7aaa334773bbcd728
xyyx <xyyx@mail.ru>
2018-02-12 09:57:28 +0800

    Themes: build as apps
    * nexus and pixel ready
    
    Change-Id: Ib97084a432951bbffc7d356c9421455ae9138be9

--- packages/overlays/SettingsBlackThemeOverlay/Android.mk
+++ packages/overlays/SettingsBlackThemeOverlay/Android.mk
@@ -1,13 +1,8 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
-
-LOCAL_RRO_THEME := SettingsBlackTheme
-LOCAL_CERTIFICATE := platform
-
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
-
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
-
+LOCAL_MODULE_TAGS := optional
 LOCAL_PACKAGE_NAME := SettingsBlackThemeOverlay
-
-include $(BUILD_RRO_PACKAGE)
+LOCAL_SDK_VERSION := current
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := false
+include $(BUILD_PACKAGE)
