dc01deb088872b2e3145c92d6b62159a22f43a14
ezio84 <brabus84@gmail.com>
2018-02-19 14:01:16 +0100

    Clock: show clock on left in qs panel if left clock is set
    but skip the date
    also hide the clock in the panel if the user disabled it
    for statusbar
    
    Change-Id: I190a638b73a2872c9e216f46b6c73422ec9ec952

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -107,6 +107,8 @@ public class Clock extends TextView implements DemoMode, CommandQueue.Callbacks,
     private boolean mShowSeconds;
     private Handler mSecondsHandler;
 
+    private boolean mQuickStatusBarHeader;
+
     public Clock(Context context) {
         this(context, null);
     }
@@ -127,6 +129,7 @@ public class Clock extends TextView implements DemoMode, CommandQueue.Callbacks,
         } finally {
             a.recycle();
         }
+        updateSettings();
     }
 
     @Override
@@ -340,7 +343,7 @@ public class Clock extends TextView implements DemoMode, CommandQueue.Callbacks,
         String timeResult = sdf.format(mCalendar.getTime());
         String dateResult = "";
 
-        if (mClockDateDisplay != CLOCK_DATE_DISPLAY_GONE) {
+        if (mClockDateDisplay != CLOCK_DATE_DISPLAY_GONE && !mQuickStatusBarHeader) {
             Date now = new Date();
 
             if (mClockDateFormat == null || mClockDateFormat.isEmpty()) {
@@ -371,7 +374,7 @@ public class Clock extends TextView implements DemoMode, CommandQueue.Callbacks,
                 int dateStringLen = dateString.length();
                 int timeStringOffset = (mClockDatePosition == STYLE_DATE_RIGHT)
                         ? timeResult.length() + 1 : 0;
-                if (mClockDateDisplay == CLOCK_DATE_DISPLAY_GONE) {
+                if (mClockDateDisplay == CLOCK_DATE_DISPLAY_GONE || mQuickStatusBarHeader) {
                     formatted.delete(0, dateStringLen);
                 } else {
                     if (mClockDateDisplay == CLOCK_DATE_DISPLAY_SMALL) {
@@ -511,4 +514,8 @@ public class Clock extends TextView implements DemoMode, CommandQueue.Callbacks,
             mSecondsHandler.postAtTime(this, SystemClock.uptimeMillis() / 1000 * 1000 + 1000);
         }
     };
+
+    public void setIsQshb(boolean qshb) {
+        mQuickStatusBarHeader = qshb;
+    }
 }
