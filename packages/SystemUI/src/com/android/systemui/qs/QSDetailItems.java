f512ed9b970d5c494f7d8e385ab61cea4a9aeeb1
bigrushdog <randall.rushing@gmail.com>
2018-02-21 16:16:21 -0800

    Introduce Theme tile
    Long press tile to change between Style (dark/light/auto ui)
    mode and Accent (#FruityPebbles) mode.
    Click to show detail view.
    
    aex edits:- Include support for black and extended UI themes
                Update for Pie (@ishubhamsingh)
    
    Change-Id: Ic9ca2a3305b4b79120d7ffca607477636fbbb5c5
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSDetailItems.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSDetailItems.java
@@ -266,6 +266,8 @@ public class QSDetailItems extends FrameLayout {
         public Object tag;
         public boolean canDisconnect;
         public int icon2 = -1;
+        public boolean doDisableTint;
+        public boolean doDisableFocus;
     }
 
     public interface Callback {
--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -45,6 +45,7 @@ import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
+import com.android.systemui.qs.tiles.ThemeTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.qs.tiles.WorkModeTile;
@@ -119,6 +120,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new UsbTetherTile(mHost);
             case "hwkeys":
                 return new HWKeysTile(mHost);
+            case "theme":
+                return new ThemeTile(mHost);
         }
 
         // Intent tiles.
