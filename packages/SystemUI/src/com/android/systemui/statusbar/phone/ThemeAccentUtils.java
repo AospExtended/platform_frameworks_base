025c56c86d7562955f28e010cc9e94cc806e09f0
ezio84 <brabus84@gmail.com>
2018-02-26 11:30:32 +0100

    Cleanup: move theme/accent manager stuff out of StatusBar class
    kanging @neobuddy89 and @SpiritCroc idea
    https://github.com/crdroidandroid/android_frameworks_base/commit/f9db9b5c1a48fceaf5c212d2970c9282ca625466
    
    aex edits:- Adadpt it for black and extended theme
    
    Change-Id: I3bed59662b4943a5bca12f07ff116854b24b2aed
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

---
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ThemeAccentUtils.java
@@ -0,0 +1,230 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.statusbar.phone;
+
+import android.content.om.IOverlayManager;
+import android.content.om.OverlayInfo;
+import android.os.RemoteException;
+import android.util.Log;
+
+public class ThemeAccentUtils {
+    public static final String TAG = "ThemeAccentUtils";
+
+    private static final String[] ACCENTS = {
+        "default_accent", // 0
+        "com.accents.red", // 1
+        "com.accents.pink", // 2
+        "com.accents.purple", // 3
+        "com.accents.deeppurple", // 4
+        "com.accents.indigo", // 5
+        "com.accents.blue", // 6
+        "com.accents.lightblue", // 7
+        "com.accents.cyan", // 8
+        "com.accents.teal", // 9
+        "com.accents.green", // 10
+        "com.accents.lightgreen", // 11
+        "com.accents.lime", // 12
+        "com.accents.yellow", // 13
+        "com.accents.amber", // 14
+        "com.accents.orange", // 15
+        "com.accents.deeporange", // 16
+        "com.accents.brown", // 17
+        "com.accents.grey", // 18
+        "com.accents.bluegrey", // 19
+        "com.accents.black", // 20
+        "com.accents.white", // 21
+    };
+
+    private static final String[] DARK_THEMES = {
+        "com.android.system.theme.dark", // 0
+        "com.android.settings.theme.dark", // 1
+        "com.android.dui.theme.dark", // 2
+    };
+
+    private static final String[] BLACK_THEMES = {
+        "com.android.system.theme.black", // 0
+        "com.android.settings.theme.black", // 1
+        "com.android.dui.theme.black", // 2
+    };
+
+    private static final String[] EXTENDED_THEMES = {
+        "com.android.system.theme.extended", // 0
+        "com.android.settings.theme.extended", // 1
+        "com.android.dui.theme.extended", // 2
+    };
+
+    private static final String STOCK_DARK_THEME = "com.android.systemui.theme.dark";
+
+    // Switches theme accent from to another or back to stock
+    public static void updateAccents(IOverlayManager om, int userId, int accentSetting) {
+        if (accentSetting == 0) {
+            unloadAccents(om, userId);
+        } else if (accentSetting < 20) {
+            try {
+                om.setEnabled(ACCENTS[accentSetting],
+                        true, userId);
+            } catch (RemoteException e) {
+                Log.w(TAG, "Can't change theme", e);
+            }
+        } else if (accentSetting == 20) {
+            try {
+                // If using a dark, black or extendedUI theme we use the white accent, otherwise use the black accent
+                if (isUsingDarkTheme(om, userId) || isUsingBlackTheme(om, userId) || isUsingExtendedTheme(om, userId)) {
+                    om.setEnabled(ACCENTS[21],
+                            true, userId);
+                } else {
+                    om.setEnabled(ACCENTS[20],
+                            true, userId);
+                }
+            } catch (RemoteException e) {
+                Log.w(TAG, "Can't change theme", e);
+            }
+        }
+    }
+
+    // Unload all the theme accents
+    public static void unloadAccents(IOverlayManager om, int userId) {
+        // skip index 0
+        for (int i = 1; i < ACCENTS.length; i++) {
+            String accent = ACCENTS[i];
+            try {
+                om.setEnabled(accent,
+                        false /*disable*/, userId);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // Check for the dark system theme
+    public static boolean isUsingDarkTheme(IOverlayManager om, int userId) {
+        OverlayInfo themeInfo = null;
+        try {
+            themeInfo = om.getOverlayInfo(DARK_THEMES[0],
+                    userId);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        return themeInfo != null && themeInfo.isEnabled();
+    }
+
+    // Check for the black system theme
+    public static boolean isUsingBlackTheme(IOverlayManager om, int userId) {
+        OverlayInfo themeInfo = null;
+        try {
+            themeInfo = om.getOverlayInfo(BLACK_THEMES[0],
+                    userId);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        return themeInfo != null && themeInfo.isEnabled();
+     }
+
+    // Check for the extended system theme
+    public static boolean isUsingExtendedTheme(IOverlayManager om, int userId) {
+        OverlayInfo themeInfo = null;
+        try {
+            themeInfo = om.getOverlayInfo(EXTENDED_THEMES[0],
+                    userId);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        return themeInfo != null && themeInfo.isEnabled();
+     }
+
+    public static void setLightDarkTheme(IOverlayManager om, int userId, boolean useDarkTheme) {
+        for (String theme : DARK_THEMES) {
+            try {
+                om.setEnabled(theme,
+                        useDarkTheme, userId);
+                unfuckBlackWhiteAccent(om, userId);
+                if (useDarkTheme) {
+                    unloadStockDarkTheme(om, userId);
+                }
+            } catch (RemoteException e) {
+                Log.w(TAG, "Can't change theme", e);
+            }
+        }
+    }
+
+    public static void setLightBlackTheme(IOverlayManager om, int userId, boolean useBlackTheme) {
+        for (String theme : BLACK_THEMES) {
+            try {
+                om.setEnabled(theme,
+                        useBlackTheme, userId);
+                unfuckBlackWhiteAccent(om, userId);
+            } catch (RemoteException e) {
+                Log.w(TAG, "Can't change theme", e);
+            }
+        }
+    }
+
+    public static void setLightExtendedTheme(IOverlayManager om, int userId, boolean useExtendedTheme) {
+        for (String theme : EXTENDED_THEMES) {
+            try {
+                om.setEnabled(theme,
+                        useExtendedTheme, userId);
+                unfuckBlackWhiteAccent(om, userId);
+            } catch (RemoteException e) {
+                Log.w(TAG, "Can't change theme", e);
+            }
+        }
+    }
+
+    // Check for black and white accent overlays
+    public static void unfuckBlackWhiteAccent(IOverlayManager om, int userId) {
+        OverlayInfo themeInfo = null;
+        try {
+            if (isUsingDarkTheme(om, userId) || isUsingBlackTheme(om, userId) || isUsingExtendedTheme(om, userId)) {
+                themeInfo = om.getOverlayInfo(ACCENTS[20],
+                        userId);
+                if (themeInfo != null && themeInfo.isEnabled()) {
+                    om.setEnabled(ACCENTS[20],
+                            false /*disable*/, userId);
+                    om.setEnabled(ACCENTS[21],
+                            true, userId);
+                }
+            } else {
+                themeInfo = om.getOverlayInfo(ACCENTS[21],
+                        userId);
+                if (themeInfo != null && themeInfo.isEnabled()) {
+                    om.setEnabled(ACCENTS[21],
+                            false /*disable*/, userId);
+                    om.setEnabled(ACCENTS[20],
+                            true, userId);
+                }
+            }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Unloads the stock dark theme
+    public static void unloadStockDarkTheme(IOverlayManager om, int userId) {
+        OverlayInfo themeInfo = null;
+        try {
+            themeInfo = om.getOverlayInfo(STOCK_DARK_THEME,
+                    userId);
+            if (themeInfo != null && themeInfo.isEnabled()) {
+                om.setEnabled(STOCK_DARK_THEME,
+                        false /*disable*/, userId);
+            }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+}
