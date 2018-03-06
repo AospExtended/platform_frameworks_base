CyanogenMod/android_frameworks_base

lockscreen: Add option to pass swipe-up-to-unlock (1/2)
* Option appears on PIN,pattern and password methods
* User should press back button to see notifications, clock etc.
* Instantly hides keyguard if Smart Lock has unlocked phone.

Change-Id: I2ceeed348d7e61632b8344f35431f30cba62ca4f
Signed-off-by: eray orçunus <erayorcunus@gmail.com>

services/core/java/com/android/server/LockSettingsService.java
@@ -424,6 +424,7 @@ public class LockSettingsService extends ILockSettings.Stub {
         Secure.LOCK_BIOMETRIC_WEAK_FLAGS,
         Secure.LOCK_PATTERN_VISIBLE,
         Secure.LOCK_PATTERN_TACTILE_FEEDBACK_ENABLED,
+        Secure.LOCK_PASS_TO_SECURITY_VIEW,
         Secure.LOCK_PATTERN_SIZE,
         Secure.LOCK_DOTS_VISIBLE,
         Secure.LOCK_SHOW_ERROR_PATH,

AospExtended/platform_frameworks_base

 * Option appears on PIN, pattern and password methods
 * User can press back button to see swipe-up-to-unlock screen
 * Instantly hide keyguard if Smart Lock has unlocked phone

services/core/java/com/android/server/locksettings/LockSettingsService.java
@@ -1980,7 +1980,8 @@ public class LockSettingsService extends ILockSettings.Stub {
             Secure.LOCK_PATTERN_ENABLED,
             Secure.LOCK_BIOMETRIC_WEAK_FLAGS,
             Secure.LOCK_PATTERN_VISIBLE,
-            Secure.LOCK_PATTERN_TACTILE_FEEDBACK_ENABLED
+            Secure.LOCK_PATTERN_TACTILE_FEEDBACK_ENABLED,
+            Secure.LOCK_PASS_TO_SECURITY_VIEW
     };
 
     // Reading these settings needs the contacts permission