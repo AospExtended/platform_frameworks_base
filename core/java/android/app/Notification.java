39c22f64e75a4784cb39a5df62dbd6a05146ae3f
Marko Man <darkobas@gmail.com>
2018-03-06 20:13:49 -0500

    Add support for theming notifications
    @maxwen Fix notification text when using dark themes
    
    Change-Id: Iea927eb3d95a9e9d07f07f61b2b420c95056dd5c

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -4832,7 +4832,8 @@ public class Notification implements Parcelable
 
         private CharSequence processLegacyText(CharSequence charSequence, boolean ambient) {
             boolean isAlreadyLightText = isLegacy() || textColorsNeedInversion();
-            boolean wantLightText = ambient;
+            boolean wantLightText = ambient || mContext.getResources().getBoolean(
+                    R.bool.config_useDarkBgNotificationIconTextTinting);
             if (isAlreadyLightText != wantLightText) {
                 return getColorUtil().invertCharSequenceColors(charSequence);
             } else {
@@ -6275,7 +6276,7 @@ public class Notification implements Parcelable
                 sb.append(bidi.unicodeWrap(m.mSender),
                         makeFontColorSpan(colorize
                                 ? builder.getPrimaryTextColor()
-                                : builder.getSenderTextColor()),
+                                : mBuilder.resolveContrastColor()),
                         0 /* flags */);
             }
             CharSequence text = m.mText == null ? "" : m.mText;
