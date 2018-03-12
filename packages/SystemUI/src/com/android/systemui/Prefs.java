e9727af1fd6ac42503bdce1c45466f2b92f5478f
Alex Cruz <du.alexcruz@gmail.com>
2018-03-12 18:42:20 -0400

    Add a one time QS dialog for the theme tile
    We're adding this for the same reason we added the one for the caffeine
    QS tile.
    
    https://github.com/DirtyUnicorns/android_frameworks_base/commit/1caa14eb31fdc9bb0316db530b7f0688fa58710b
    
    Change-Id: I9958323fdb4898e2083fac434ef2b9caa22c8f73

--- packages/SystemUI/src/com/android/systemui/Prefs.java
+++ packages/SystemUI/src/com/android/systemui/Prefs.java
@@ -46,6 +46,7 @@ public final class Prefs {
             Key.QS_DATA_SAVER_ADDED,
             Key.QS_DATA_SAVER_DIALOG_SHOWN,
             Key.QS_INVERT_COLORS_ADDED,
+            Key.QS_THEME_DIALOG_SHOWN,
             Key.QS_WORK_ADDED,
             Key.QS_NIGHTDISPLAY_ADDED,
             Key.QS_LONG_PRESS_TOOLTIP_SHOWN_COUNT,
@@ -80,6 +81,7 @@ public final class Prefs {
         String QS_DATA_SAVER_ADDED = "QsDataSaverAdded";
         @Deprecated
         String QS_INVERT_COLORS_ADDED = "QsInvertColorsAdded";
+        String QS_THEME_DIALOG_SHOWN = "QsThemeDialogShown";
         @Deprecated
         String QS_WORK_ADDED = "QsWorkAdded";
         @Deprecated
