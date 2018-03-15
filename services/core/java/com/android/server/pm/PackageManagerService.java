bc31f771f6f4d9292ca327ce27c9a8e1fafbe426
Ethan Chen <intervigil@gmail.com>
2018-03-14 22:35:21 -0700

    PackageManager: Don't dereference mPlatfromPackage until available
    * mPlatformPackage is not set until later in the package parsing
      process. Avoid the NPE instead of catching it, as it leaves
      packages unparsed.
    
    Change-Id: Ib88a7e4e3f723288ad435d79b1b68d6d3fcb48c2

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -9264,8 +9264,10 @@ public class PackageManagerService extends IPackageManager.Stub
                   mVendorPlatformSignatures) == PackageManager.SIGNATURE_MATCH) {
                 // Overwrite package signature with our platform signature
                 // if the signature is the vendor's platform signature
-                pkg.mSignatures = mPlatformPackage.mSignatures;
-                SELinuxMMAC.assignSeInfoValue(pkg);
+                if (mPlatformPackage != null) {
+                    pkg.mSignatures = mPlatformPackage.mSignatures;
+                    SELinuxMMAC.assignSeInfoValue(pkg);
+                }
             }
         } catch (PackageParserException e) {
             throw PackageManagerException.from(e);
