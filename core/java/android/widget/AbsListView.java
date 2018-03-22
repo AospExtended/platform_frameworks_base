6f8165f015b826bbd4073b44c0d243bf11594557
Dabug123 <clarkbrunswick55@gmail.com>
2018-03-22 18:34:29 +0100

    frameworks: Tweak some values for better transitions
    frameworks: Add more speed
    
    Signed-off-by: Dabug123 <clarkbrunswick55@gmail.com>
    
    frameworks: Edit more values
    
    Change-Id: Iae4098bdb096fdce1bfca4db12195396fa429939
    Signed-off-by: Dabug123 <clarkbrunswick55@gmail.com>

--- core/java/android/widget/AbsListView.java
+++ core/java/android/widget/AbsListView.java
@@ -4799,7 +4799,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
 
         FlingRunnable() {
             mScroller = new OverScroller(getContext());
-            mScroller.setFriction(0.004f);
+            mScroller.setFriction(0.002f);
         }
 
         void start(int initialVelocity) {
