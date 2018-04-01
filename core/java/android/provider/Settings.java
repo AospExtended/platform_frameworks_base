dda1b285c037cced0540a3e56d998270ebe17605
Pranav Vashi <neobuddy89@gmail.com>
2018-04-02 00:10:08 +0530

    Add toggle to enable Android P system animation [1/2]
    Change-Id: Iabef2773c54aee04bea34ed42b04b14cb6c65aa1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11730,6 +11730,12 @@ public final class Settings {
         public static final String RINGTONE_FOCUS_MODE = "ringtone_focus_mode";
 
         /**
+         * Default animation
+         * @hide
+         */
+        public static final String SYSTEM_DEFAULT_ANIMATION = "system_default_animation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
