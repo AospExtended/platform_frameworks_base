df328d7def4f5edf96d3228ed5c8f49ed5c6d165
maxwen <max.weninger@gmail.com>
2018-04-03 18:10:05 +0200

    base: SystemUI: add pref for qs brightness slider position (1/2)
    Change-Id: I5dfb7170b7d9f3328f9ab0a997135dc4db4cd3bf
    Signed-off-by: xyyx <xyyx@mail.ru>
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -87,6 +87,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         Dumpable {
 
     public static final String QS_SHOW_HEADER = "qs_show_header";
+    public static final String QS_BRIGHTNESS_POSITION_BOTTOM = "qs_brightness_position_bottom";
 
     private static final String TAG = "QSPanel";
 
@@ -212,6 +213,8 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         mSettingObserver.observe();
         mSettingObserver.update();
 
+        final TunerService tunerService = Dependency.get(TunerService.class);
+        tunerService.addTunable(this, QS_BRIGHTNESS_POSITION_BOTTOM);
         if (mHost != null) {
             setTiles(mHost.getTiles());
         }
@@ -244,7 +247,15 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
 
     @Override
     public void onTuningChanged(String key, String newValue) {
-        // No tunings for you.
+        if (QS_BRIGHTNESS_POSITION_BOTTOM.equals(key)) {
+            if (newValue == null || Integer.parseInt(newValue) == 0) {
+                removeView(mBrightnessView);
+                addView(mBrightnessView, 0);
+            } else {
+                removeView(mBrightnessView);
+                addView(mBrightnessView, 1);
+            }
+        }
     }
 
     private void updateViewVisibilityForTuningValue(View view, @Nullable String newValue) {
