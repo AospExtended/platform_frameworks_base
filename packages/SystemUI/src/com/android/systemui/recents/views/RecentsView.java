084b5799f1f5a6d369ba5fb09e200397fc5e708e
Alex Cruz <du.alexcruz@gmail.com>
2018-04-11 02:52:49 -0400

    Ensure Recents FAB respects RTL
    - When using Gravity you should use END and START opposed to RIGHT and LEFT
    to maintain RTL working as it should.
    
    Change-Id: Ic89f4a8e05c1ea3ead98fe27b4eb214ea1b63c6f

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -564,20 +564,20 @@ public class RecentsView extends FrameLayout {
 
             switch (clearRecentsLocation) {
                 case 0:
-                    params.gravity = Gravity.TOP | Gravity.RIGHT;
+                    params.gravity = Gravity.TOP | Gravity.END;
                     break;
                 case 1:
-                    params.gravity = Gravity.TOP | Gravity.LEFT;
+                    params.gravity = Gravity.TOP | Gravity.START;
                     break;
                 case 2:
                     params.gravity = Gravity.TOP | Gravity.CENTER;
                     break;
                 case 3:
                 default:
-                    params.gravity = Gravity.BOTTOM | Gravity.RIGHT;
+                    params.gravity = Gravity.BOTTOM | Gravity.END;
                     break;
                 case 4:
-                    params.gravity = Gravity.BOTTOM | Gravity.LEFT;
+                    params.gravity = Gravity.BOTTOM | Gravity.START;
                     break;
                 case 5:
                     params.gravity = Gravity.BOTTOM | Gravity.CENTER;
