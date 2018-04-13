f8bbf85892f3be67104efc4b15916f2cfae976f3
maxwen <max.weninger@gmail.com>
2018-04-13 03:32:51 +0200

    base: SystemUI: stop crashing qs tiles during boot
    this triggers my ocd
    continues a579c2430f1a0bf088023b4283dcec37776c070d
    
    Change-Id: I31719b595cb50a330f26e25074243e04c06f63c1
    Signed-off-by: xyyx <xyyx@mail.ru>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/SoundTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/SoundTile.java
@@ -61,6 +61,9 @@ public class SoundTile extends QSTileImpl<BooleanState> {
 
     @Override
     public void handleSetListening(boolean listening) {
+        if (mAudioManager == null) {
+            return;
+        }
         if (mListening == listening) return;
         mListening = listening;
         if (listening) {
@@ -116,6 +119,9 @@ public class SoundTile extends QSTileImpl<BooleanState> {
 
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
+        if (mAudioManager == null) {
+            return;
+        }
         switch (mAudioManager.getRingerModeInternal()) {
             case AudioManager.RINGER_MODE_NORMAL:
                 state.icon = ResourceIcon.get(R.drawable.ic_qs_ringer_audible);
