9a63252b92d5daab0ae4855b1dd4709e5d4ebb5f
maxwen <max.weninger@gmail.com>
2018-04-14 08:45:19 -0600

    base: featureize ro.recents.grid and Go Layout
    thanks google for coding like a 1st grader by adding public
    member variables and making this more work then needed
    
    Bootleggers (@eldainosor) edit: Let's apply to use Android Go layout too, and also allow to drag the windows on non-go builds
    
    Change-Id: I5ced8171be9f01fceb44be4a467265066ba8a884
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5123,6 +5123,12 @@ public final class Settings {
         public static final String LOCKSCREEN_MEDIA_METADATA = "lockscreen_media_metadata";
 
         /**
+         * show recents in grid style or android go style
+         * @hide
+         */
+        public static final String RECENTS_LAYOUT_STYLE = "recents_layout_style";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
