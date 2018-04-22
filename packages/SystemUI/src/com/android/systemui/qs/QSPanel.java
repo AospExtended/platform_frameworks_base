54829bdee555ee2fd6b434ac8c2ddf0fd726ef93
PMS22 <pratham220399@gmail.com>
2018-04-22 16:51:08 +0000

    Brightness slider: Vibrate after a long press
    - A small haptic feedback after long pressing feels good
    
    Change-Id: I1058bbcf24bbccaeeebb3f547905860552bd1131
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -37,6 +37,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.os.UserHandle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.Gravity;
@@ -158,6 +160,8 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
     private LinearLayout mHorizontalLinearLayout;
     private LinearLayout mHorizontalContentContainer;
 
+    private final Vibrator mVibrator;
+
     // Only used with media
     private QSTileLayout mHorizontalTileLayout;
     protected QSTileLayout mRegularTileLayout;
@@ -192,6 +196,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         mBroadcastDispatcher = broadcastDispatcher;
         mUiEventLogger = uiEventLogger;
 
+        mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
         setOrientation(VERTICAL);
 
         addViewsAboveTiles();
@@ -278,6 +283,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
             @Override
             public boolean onLongClick(View v) {
                 setBrightnessMin(context, isAdaptiveBrightness(context));
+                mVibrator.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE));
                 return false;
             }
         });
@@ -310,6 +316,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
             @Override
             public boolean onLongClick(View v) {
                 setBrightnessMax(context, isAdaptiveBrightness(context));
+                mVibrator.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE));
                 return false;
             }
         });
