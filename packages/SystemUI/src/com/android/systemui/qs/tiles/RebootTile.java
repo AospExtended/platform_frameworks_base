7c431610b5f66878b533124e75a40aec388c02cd
Pranav Vashi <neobuddy89@gmail.com>
2018-05-03 03:45:46 +0530

    SystemUI: Extend reboot tile to add power off
    SKULSHADY: Use MD2 icons
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: I329ce07cb69ee43cbe117a6c453faffa4555a08b
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/RebootTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/RebootTile.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 crDroid Android Project
+ * Copyright (C) 2013 Slimroms
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.PowerManager;
+import android.service.quicksettings.Tile;
 
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
@@ -32,7 +33,7 @@ import javax.inject.Inject;
 
 public class RebootTile extends QSTileImpl<BooleanState> {
 
-    private boolean mRebootToRecovery = false;
+    private int mRebootToRecovery = 0;
 
     @Inject
     public RebootTile(QSHost host) {
@@ -46,7 +47,13 @@ public class RebootTile extends QSTileImpl<BooleanState> {
 
     @Override
     public void handleClick() {
-        mRebootToRecovery = !mRebootToRecovery;
+        if (mRebootToRecovery == 0) {
+            mRebootToRecovery = 1;
+        } else if (mRebootToRecovery == 1) {
+            mRebootToRecovery = 2;
+        } else {
+            mRebootToRecovery = 0;
+        }
         refreshState();
     }
 
@@ -58,7 +65,13 @@ public class RebootTile extends QSTileImpl<BooleanState> {
             public void run() {
                 PowerManager pm =
                     (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
-                pm.reboot(mRebootToRecovery ? "recovery" : "");
+                if (mRebootToRecovery == 1) {
+                    pm.reboot(PowerManager.REBOOT_RECOVERY);
+                } else if (mRebootToRecovery == 2) {
+                    pm.shutdown(false, pm.SHUTDOWN_USER_REQUESTED, false);
+                } else {
+                    pm.reboot("");
+                }
             }
         }, 500);
     }
@@ -80,13 +93,17 @@ public class RebootTile extends QSTileImpl<BooleanState> {
 
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
-        if (mRebootToRecovery) {
+        if (mRebootToRecovery == 1) {
             state.label = mContext.getString(R.string.quick_settings_reboot_recovery_label);
             state.icon = ResourceIcon.get(R.drawable.ic_qs_reboot_recovery);
+        } else if (mRebootToRecovery == 2) {
+            state.label = mContext.getString(R.string.quick_settings_poweroff_label);
+            state.icon = ResourceIcon.get(R.drawable.ic_qs_poweroff);
         } else {
             state.label = mContext.getString(R.string.quick_settings_reboot_label);
             state.icon = ResourceIcon.get(R.drawable.ic_qs_reboot);
         }
+        state.state = Tile.STATE_INACTIVE;
     }
 
     @Override
