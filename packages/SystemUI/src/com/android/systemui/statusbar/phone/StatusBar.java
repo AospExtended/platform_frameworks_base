9f0e7a27b585816b4c549ddf1a83ffbe267fa2c6
ezio84 <brabus84@gmail.com>
2018-05-06 19:37:48 +0200

    SlimRecents: add fingerprint scanner gestures [2/2]
    Change-Id: I1d22817dd7be66c00fb37942c62bd75261be95f9

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -3378,9 +3378,17 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         if (KeyEvent.KEYCODE_SYSTEM_NAVIGATION_UP == key) {
             mMetricsLogger.action(MetricsEvent.ACTION_SYSTEM_NAVIGATION_KEY_UP);
+            if (mSlimRecents != null && mSlimRecents.isShowing()) {
+                mSlimRecents.scrollPanel(false);
+                return;
+            }
             mNotificationPanel.collapse(false /* delayed */, 1.0f /* speedUpFactor */);
         } else if (KeyEvent.KEYCODE_SYSTEM_NAVIGATION_DOWN == key) {
             mMetricsLogger.action(MetricsEvent.ACTION_SYSTEM_NAVIGATION_KEY_DOWN);
+            if (mSlimRecents != null && mSlimRecents.isShowing()) {
+                mSlimRecents.scrollPanel(true);
+                return;
+            }
             if (mNotificationPanel.isFullyCollapsed()) {
                 if (mFpQuickPulldownQs) {
                     mNotificationPanel.expandWithQs();
@@ -3395,6 +3403,10 @@ public class StatusBar extends SystemUI implements DemoMode,
             }
         } else if (mFpDismissNotifications && (KeyEvent.KEYCODE_SYSTEM_NAVIGATION_LEFT == key
                 || KeyEvent.KEYCODE_SYSTEM_NAVIGATION_RIGHT == key)) {
+            if (mSlimRecents != null && mSlimRecents.isShowing()) {
+                mSlimRecents.clearAllAppsFromSwipe();
+                return;
+            }
             if (!mNotificationPanel.isFullyCollapsed() && !mNotificationPanel.isExpanding()){
                 mMetricsLogger.action(MetricsEvent.ACTION_DISMISS_ALL_NOTES);
                 clearAllNotifications(KeyEvent.KEYCODE_SYSTEM_NAVIGATION_LEFT == key ? true : false);
