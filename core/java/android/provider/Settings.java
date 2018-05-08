b019987f896d91f1d39cb403268e27ef49897b93
Nathan Chancellor <natechancellor@gmail.com>
2018-05-08 05:33:21 +0000

    Running services icon toggle [1/2]
    aex edits(@ishubhamsingh):- * Adapt for oreo
                                * Clean up useless stuffs
                                * Disabled by default
    
    Change-Id: Ic92795891f907110cc40d539a34918d09e2af156
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4765,6 +4765,12 @@ public final class Settings {
         public static final String HEADS_UP_NOTIFICATION_SNOOZE = "heads_up_notification_snooze";
 
         /**
+         * Whether to show or hide the running services icon
+         * @hide
+         */
+        public static final String QS_RUNNING_SERVICES_TOGGLE = "qs_running_services_toggle";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
