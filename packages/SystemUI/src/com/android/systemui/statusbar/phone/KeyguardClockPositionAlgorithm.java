83a4b7101d314f8d2c357bccfa2fdbd316a284f7
Shubham Singh <coolsks94@gmail.com>
2018-06-06 22:52:58 +0530

    LS: Add spidey clock style [1/2]
     based on https://github.com/AospExtended/platform_frameworks_base/commit/1b4cad1d316df9e77e48e39c050100058b492b44
    
     Thanks @allstargaurav for the vectors
    
    Change-Id: I96d677a721c5f55af5fca9ae0693679a03fc6454
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardClockPositionAlgorithm.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardClockPositionAlgorithm.java
@@ -184,7 +184,7 @@ public class KeyguardClockPositionAlgorithm {
     }
 
     public void setClockSelection(int style) {
-        mIsBigClock = style == 3;
+        mIsBigClock = style == 3 || style == 7;
     }
 
     private float burnInPreventionOffsetY() {
