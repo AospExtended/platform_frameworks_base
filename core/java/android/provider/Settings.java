d4a6c92cce55015d2fd2434a1495751ad64239db
AgentFabulous <kshitijgm@gmail.com>
2018-06-07 10:54:11 +0530

    Allow using framework values for rounded corners [1/2]
    Aligned as per our fuckedup radius
    
    Change-Id: Iaccd9dc357f52ff83615f8b7f760b9943e7a270c
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: Anand Ramachandran <anand.nv1998@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4497,6 +4497,18 @@ public final class Settings {
         public static final String HEADS_UP_STOPLIST_VALUES = "heads_up_stoplist_values";
 
         /**
+         * Setting to allow setting rounded corner size and content padding
+         */
+        public static final String SYSUI_ROUNDED_SIZE = "sysui_rounded_size";
+        public static final String SYSUI_ROUNDED_CONTENT_PADDING = "sysui_rounded_content_padding";
+
+        /**
+          * Setting to disable rounded corner preferences and use frameworks values instead
+          * @hide
+          */
+         public static final String SYSUI_ROUNDED_FWVALS = "sysui_rounded_fwvals";
+
+	/**
          * Which applications to disable heads up notifications for
          *
          * @hide
@@ -6536,7 +6548,7 @@ public final class Settings {
         public static final String DEVELOPMENT_SETTINGS_ENABLED =
                 Global.DEVELOPMENT_SETTINGS_ENABLED;
 
-        /**
+	/**
          * When the user has enable the option to have a "bug report" command
          * in the power menu.
          * @deprecated Use {@link android.provider.Settings.Global#BUGREPORT_IN_POWER_MENU} instead
