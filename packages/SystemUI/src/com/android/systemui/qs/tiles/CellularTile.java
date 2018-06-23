0a9124c1cebaad8eab731d3973d7a0d7a9e0b0d6
ezio84 <brabus84@gmail.com>
2018-06-23 15:15:15 +0200

    Allow details view for Cellular tile when tile title is disabled
    Change-Id: I41d5b246507465fec8097b7555abc13040c86d3d

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
@@ -144,6 +144,11 @@ public class CellularTile extends QSTileImpl<SignalState> {
     }
 
     @Override
+    public boolean isDualTarget() {
+        return true;
+    }
+
+    @Override
     public CharSequence getTileLabel() {
         return mContext.getString(R.string.quick_settings_cellular_detail_title);
     }
