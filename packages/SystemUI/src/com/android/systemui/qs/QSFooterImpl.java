e70900f8be1ce39fe8beda830b14b08bf0fec8bc
Nick Doherty <nick@dirtyunicorns.com>
2018-06-23 02:37:11 -0400

    Add onLongClick functions to calendar/clock in qs footer
    With the split of calendar from clock in qs footer, lets add onLongClick functions
    to both date and alarm. This way we still can set an alarm quickly from qs and the
    same for calendar.
    
    Change-Id: I7655c559915bc4ad981328dbb21204cfa76792e5

--- packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
@@ -142,6 +142,7 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
         mAlarmStatusCollapsed = findViewById(R.id.alarm_status_collapsed);
         mAlarmStatus = findViewById(R.id.alarm_status);
         mDateTimeGroup.setOnClickListener(this);
+        mDateTimeGroup.setOnLongClickListener(this);
 
         mMultiUserSwitch = findViewById(R.id.multi_user_switch);
         mMultiUserAvatar = mMultiUserSwitch.findViewById(R.id.multi_user_avatar);
@@ -406,12 +407,19 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
     @Override
     public boolean onLongClick(View v) {
         if (v == mSettingsButton) {
+
             startExtensionsActivity();
-        } else if (v == mRunningServicesButton) {
-            MetricsLogger.action(mContext,
-                    mExpanded ? MetricsProto.MetricsEvent.ACTION_QS_EXPANDED_SETTINGS_LAUNCH
-                            : MetricsProto.MetricsEvent.ACTION_QS_COLLAPSED_SETTINGS_LAUNCH);
-            startRunningServicesActivity();
+        } else if (v == mDateTimeGroup) {
+            if (mAlarmShowing) {
+                Uri.Builder builder = CalendarContract.CONTENT_URI.buildUpon();
+                builder.appendPath("time");
+                builder.appendPath(Long.toString(System.currentTimeMillis()));
+                Intent todayIntent = new Intent(Intent.ACTION_VIEW, builder.build());
+                mActivityStarter.postStartActivityDismissingKeyguard(todayIntent, 0);
+            } else {
+                mActivityStarter.postStartActivityDismissingKeyguard(new Intent(
+                        AlarmClock.ACTION_SHOW_ALARMS), 0);
+            }
         }
         return false;
     }
