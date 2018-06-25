18972cc635829382a1421dbfcb1f0bd8a0936c9f
Gaurav <allstargaurav@gmail.com>
2018-06-25 12:33:10 +0530

    Revert "[1/2] base: featureize ro.recents.grid and Go Layout"
    This reverts commit ec793a94f7751ac84e34f803e5d87bddcb22015d.
    
    Change-Id: If7adfedee9cea352bf8accb6ad0111b49f8840c4

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1234,9 +1234,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.USE_EDGE_SERVICE_FOR_GESTURES), false, this,
                     UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.RECENTS_LAYOUT_STYLE), false, this,
-                    UserHandle.USER_ALL);
             updateSettings();
         }
 
