55fe3cf8add56aa7001302cd0b72b50e87241cd8
Ethan Chen <intervigil@gmail.com>
2018-08-07 09:34:45 -0400

    fwb: PackageManager: Add configuration to specify vendor platform signatures
    Devices with split system/vendor images may want to use the OEM's vendor
    image. In that case, the OEM's platform signature is not actually the
    same as the platform signature used to sign the Lineage system image.
    Allow devices to specify an OEM platform signature which will also be
    recognized as the system's platform signature.
    
    Ported to P by frap129
    
    Change-Id: Ida9bb25a32234af9d9507a214eae6a4672320d2b
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: xyyx <xyyx@mail.ru>

--- services/core/java/com/android/server/pm/permission/PermissionManagerService.java
+++ services/core/java/com/android/server/pm/permission/PermissionManagerService.java
@@ -36,6 +36,7 @@ import android.content.pm.PackageParser;
 import android.content.pm.PermissionGroupInfo;
 import android.content.pm.PermissionInfo;
 import android.content.pm.PackageParser.Package;
+import android.content.pm.Signature;
 import android.metrics.LogMaker;
 import android.os.Binder;
 import android.os.Build;
@@ -56,6 +57,7 @@ import android.util.Log;
 import android.util.Slog;
 import android.util.SparseArray;
 
+import com.android.internal.R;
 import com.android.internal.annotations.GuardedBy;
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
@@ -129,6 +131,8 @@ public class PermissionManagerService {
     private final Context mContext;
     private final MetricsLogger mMetricsLogger = new MetricsLogger();
 
+    private final Signature[] mVendorPlatformSignatures;
+
     /** Internal storage for permissions and related settings */
     @GuardedBy("mLock")
     private final PermissionSettings mSettings;
@@ -159,6 +163,9 @@ public class PermissionManagerService {
         SystemConfig systemConfig = SystemConfig.getInstance();
         mSystemPermissions = systemConfig.getSystemPermissions();
         mGlobalGids = systemConfig.getGlobalGids();
+
+        mVendorPlatformSignatures = PackageManagerServiceUtils.createSignatures(
+               context.getResources().getStringArray(R.array.config_vendorPlatformSignatures));
 
         // propagate permission configuration
         final ArrayMap<String, SystemConfig.PermissionEntry> permConfig =
@@ -1093,6 +1100,7 @@ public class PermissionManagerService {
         //     - or its signing certificate is a previous signing certificate of the defining
         //       package, and the defining package still trusts the old certificate for permissions
         //     - or it shares the above relationships with the system package
+        //     - or its signing certificate is from the original vendor
         boolean allowed =
                 pkg.mSigningDetails.hasAncestorOrSelf(
                         bp.getSourcePackageSetting().getSigningDetails())
@@ -1102,7 +1110,9 @@ public class PermissionManagerService {
                 || pkg.mSigningDetails.hasAncestorOrSelf(systemPackage.mSigningDetails)
                 || systemPackage.mSigningDetails.checkCapability(
                         pkg.mSigningDetails,
-                        PackageParser.SigningDetails.CertCapabilities.PERMISSION);
+                        PackageParser.SigningDetails.CertCapabilities.PERMISSION)
+                || (PackageManagerServiceUtils.compareSignatures(mVendorPlatformSignatures,
+                        pkg.mSigningDetails.signatures) == PackageManager.SIGNATURE_MATCH);
         if (!allowed && (privilegedPermission || oemPermission)) {
             if (pkg.isSystem()) {
                 // For updated system applications, a privileged/oem permission
