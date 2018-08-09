33b88204bc517441273b3c7014c4282709674926
Shubham Singh <coolsks94@gmail.com>
2018-08-09 20:11:06 +0530

    Revert Edge Gesture [2/2]
    Revert "Implement animations for back edge gestures"
    
    This reverts commit cab4aa923034c83a16ca54b428ce5d48e9914a59.
    
    Revert "Add NPE handling to the Edge Gesture Manager"
    
    This reverts commit 93180a4d5c6c915bdc669e5bf0c47fbb292b65a7.
    
    Revert "Add ability to use EdgeGestureService for system gestures"
    
    This reverts commit 864c101be90e46db70a90fee3670bb81384a40f1.
    
    Revert "Add EdgeGesture service"
    
    This reverts commit 69e53c47d21772631889f641347ae88a8df02a0c.
    
    Revert "Add edge gesture (1/2)"
    
    This reverts commit 8fff4802c7ae4890c4f40d27fad833331f206165.
    
    Change-Id: I64fea446eca252e638d2c844eadc4b1d51cfab63
    Signed-off-by: arghyac35 <arghyac35@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -276,7 +276,6 @@ import com.android.systemui.statusbar.policy.RemoteInputView;
 import com.android.systemui.statusbar.policy.UserInfoController;
 import com.android.systemui.statusbar.policy.UserInfoControllerImpl;
 import com.android.systemui.statusbar.policy.UserSwitcherController;
-import com.android.systemui.statusbar.screen_gestures.ScreenGesturesController;
 import com.android.systemui.statusbar.stack.NotificationStackScrollLayout;
 import com.android.systemui.statusbar.stack.NotificationStackScrollLayout
         .OnChildLocationsChangedListener;
@@ -557,9 +556,6 @@ public class StatusBar extends SystemUI implements DemoMode,
     private int mTickerAnimationMode;
 
     private int mAmbientMediaPlaying;
-
-    // Full Screen Gestures
-    protected ScreenGesturesController gesturesController;
 
     // Tracking finger for opening/closing.
     boolean mTracking;
@@ -1162,10 +1158,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                 com.android.internal.R.array.config_nonBlockableNotificationPackages));
         // end old BaseStatusBar.start().
 
-        mContext.getContentResolver().registerContentObserver(Settings.Secure.getUriFor(
-                Settings.Secure.EDGE_GESTURES_ENABLED), false,
-                mEdgeGesturesSettingsObserver);
-
         mMediaSessionManager
                 = (MediaSessionManager) mContext.getSystemService(Context.MEDIA_SESSION_SERVICE);
         // TODO: use MediaSessionManager.SessionListener to hook us up to future updates
@@ -1571,12 +1563,6 @@ public class StatusBar extends SystemUI implements DemoMode,
         mNotificationIconAreaController.onDensityOrFontScaleChanged(mContext);
 
         reevaluateStyles();
-
-        ContentResolver resolver = mContext.getContentResolver();
-
-        boolean edgeGesturesEnabled = Settings.Secure.getIntForUser(resolver,
-                Settings.Secure.EDGE_GESTURES_ENABLED, 0, UserHandle.USER_CURRENT) == 1;
-        updateEdgeGestures(edgeGesturesEnabled);
     }
 
     @Override
@@ -6722,17 +6708,6 @@ public class StatusBar extends SystemUI implements DemoMode,
         return mDeviceInteractive;
     }
 
-    private final ContentObserver mEdgeGesturesSettingsObserver = new ContentObserver(mHandler) {
-        @Override
-        public void onChange(boolean selfChange) {
-            ContentResolver resolver = mContext.getContentResolver();
-            boolean edgeGesturesEnabled = Settings.Secure.getIntForUser(resolver,
-                    Settings.Secure.EDGE_GESTURES_ENABLED, 0, UserHandle.USER_CURRENT) == 1;
-
-            updateEdgeGestures(edgeGesturesEnabled);
-        }
-    };
-
     @Override  // NotificationData.Environment
     public boolean isDeviceProvisioned() {
         return mDeviceProvisionedController.isDeviceProvisioned();
@@ -8991,17 +8966,4 @@ public class StatusBar extends SystemUI implements DemoMode,
             mNavigationBar.getBarTransitions().setAutoDim(true);
         }
     };
-
-    public void updateEdgeGestures(boolean enabled) {
-        Log.d(TAG, "updateEdgeGestures: Updating edge gestures");
-        if (enabled) {
-            if (gesturesController == null) {
-                gesturesController = new ScreenGesturesController(mContext, mWindowManager, this);
-            }
-            gesturesController.reorient();
-        } else if (!enabled && gesturesController != null) {
-            gesturesController.stop();
-            gesturesController = null;
-        }
-    }
 }
