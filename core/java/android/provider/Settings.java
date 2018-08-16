9df68bba3df1f810d493b3d232627f6a8971cac3
xyyx <xyyx@mail.ru>
2018-08-16 06:27:04 +0000

    LockscreenCharging: squashed (1/3) needed system/core patch based on: [2/2] frameworks: show charging current & voltage on lockscreen by yank555-lu [2/2]Base: show more battery info on lockscreen when charging by yank555-lu LockscreenCharging: Use double for mChargingVoltage and show as4.XV by xyyx Lockscreen charging: Formatting improvements by nathanchance
    Change-Id: I42f90b413fd1445ded00cb5769af0ce5faf15c49

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4408,6 +4408,12 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
 
         /**
+         * Whether to show the battery info on the lockscreen while charging
+         * @hide
+         */
+        public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
