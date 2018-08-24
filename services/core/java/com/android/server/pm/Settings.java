8fb5940697d24cdf5594d5546747b4821d3a8d96
Shubham Singh <coolsks94@gmail.com>
2018-08-24 19:40:54 +0000

    Revert "Core: Use ro.build.date to signal mIsUpgrade"
    This reverts commit 32671c9ee24df6823d542209e269c86a1d672b40.

--- services/core/java/com/android/server/pm/Settings.java
+++ services/core/java/com/android/server/pm/Settings.java
@@ -350,7 +350,7 @@ public final class Settings {
         public void forceCurrent() {
             sdkVersion = Build.VERSION.SDK_INT;
             databaseVersion = CURRENT_DATABASE_VERSION;
-            fingerprint = Build.DATE;
+            fingerprint = Build.FINGERPRINT;
         }
     }
 
@@ -3160,7 +3160,7 @@ public final class Settings {
         // on update drop the files before loading them.
         if (PackageManagerService.CLEAR_RUNTIME_PERMISSIONS_ON_UPGRADE) {
             final VersionInfo internal = getInternalVersion();
-            if (!Build.DATE.equals(internal.fingerprint)) {
+            if (!Build.FINGERPRINT.equals(internal.fingerprint)) {
                 for (UserInfo user : users) {
                     mRuntimePermissionsPersistence.deleteUserRuntimePermissionsFile(user.id);
                 }
@@ -5147,7 +5147,7 @@ public final class Settings {
         }
 
         public void onDefaultRuntimePermissionsGrantedLPr(int userId) {
-            mFingerprints.put(userId, Build.DATE);
+            mFingerprints.put(userId, Build.FINGERPRINT);
             writePermissionsForUserAsyncLPr(userId);
         }
 
@@ -5311,7 +5311,7 @@ public final class Settings {
                 serializer.endDocument();
                 destination.finishWrite(out);
 
-                if (Build.DATE.equals(fingerprint)) {
+                if (Build.FINGERPRINT.equals(fingerprint)) {
                     mDefaultPermissionsGranted.put(userId, true);
                 }
             // Any error while writing is fatal.
@@ -5423,7 +5423,7 @@ public final class Settings {
                     case TAG_RUNTIME_PERMISSIONS: {
                         String fingerprint = parser.getAttributeValue(null, ATTR_FINGERPRINT);
                         mFingerprints.put(userId, fingerprint);
-                        final boolean defaultsGranted = Build.DATE.equals(fingerprint);
+                        final boolean defaultsGranted = Build.FINGERPRINT.equals(fingerprint);
                         mDefaultPermissionsGranted.put(userId, defaultsGranted);
                     } break;
 
