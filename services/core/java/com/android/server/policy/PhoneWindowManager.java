818f93d77067b2a5af8926530bf4ac6bf42ccdf2
xyyx <xyyx@mail.ru>
2018-08-30 20:45:24 +0800

    AmbientDisplay: Disable hw keys Prevent stop dozing after hw keys tapping
    Change-Id: Icc1d577c2df52584f472d60b70803fe5abdf6102

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6168,6 +6168,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             return 0;
         }
 
+        // Disable hw keys in Ambient
+        if (isDozeMode() && (appSwitchKey || homeKey || menuKey || backKey)) {
+            return 0;
+        }
+
         // Basic policy based on interactive state.
         int result;
         boolean isWakeKey = (policyFlags & WindowManagerPolicy.FLAG_WAKE) != 0
