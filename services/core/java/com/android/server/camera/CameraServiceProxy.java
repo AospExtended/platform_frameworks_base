f33ce5113fccc8a30d14d09650cc278e931b1ee7
Danny Baumann <dannybaumann@web.de>
2018-08-31 13:29:12 +0200

    CameraServiceProxy: Loosen UID check conditionally
    Also allow media UID for camera-in-mediaserver devices.
    
    Change-Id: I34e9ddb49adc78ba0589e3d64918eca7d675ec98
    Signed-off-by: hsj51 <hrutvikjagtap51@gmail.com>

--- services/core/java/com/android/server/camera/CameraServiceProxy.java
+++ services/core/java/com/android/server/camera/CameraServiceProxy.java
@@ -124,6 +124,9 @@ public class CameraServiceProxy extends SystemService
     private final @NfcNotifyState int mNotifyNfc;
     private boolean mLastNfcPollState = true;
 
+    private final boolean mNotifyNfc;
+    private final boolean mAllowMediaUid;
+
     /**
      * Structure to track camera usage
      */
@@ -192,7 +195,8 @@ public class CameraServiceProxy extends SystemService
     private final ICameraServiceProxy.Stub mCameraServiceProxy = new ICameraServiceProxy.Stub() {
         @Override
         public void pingForUserUpdate() {
-            if (Binder.getCallingUid() != Process.CAMERASERVER_UID) {
+            if (Binder.getCallingUid() != Process.CAMERASERVER_UID
+                    && (!mAllowMediaUid || Binder.getCallingUid() != Process.MEDIA_UID)) {
                 Slog.e(TAG, "Calling UID: " + Binder.getCallingUid() + " doesn't match expected " +
                         " camera service UID!");
                 return;
@@ -203,7 +207,8 @@ public class CameraServiceProxy extends SystemService
         @Override
         public void notifyCameraState(String cameraId, int newCameraState, int facing,
                 String clientName, int apiLevel) {
-            if (Binder.getCallingUid() != Process.CAMERASERVER_UID) {
+            if (Binder.getCallingUid() != Process.CAMERASERVER_UID
+                    && (!mAllowMediaUid || Binder.getCallingUid() != Process.MEDIA_UID)) {
                 Slog.e(TAG, "Calling UID: " + Binder.getCallingUid() + " doesn't match expected " +
                         " camera service UID!");
                 return;
@@ -224,12 +229,19 @@ public class CameraServiceProxy extends SystemService
         mHandlerThread.start();
         mHandler = new Handler(mHandlerThread.getLooper(), this);
 
+<<<<<<< HEAD
         int notifyNfc = SystemProperties.getInt(NFC_NOTIFICATION_PROP, 0);
         if (notifyNfc < NFC_NOTIFY_NONE || notifyNfc > NFC_NOTIFY_FRONT) {
             notifyNfc = NFC_NOTIFY_NONE;
         }
         mNotifyNfc = notifyNfc;
         if (DEBUG) Slog.v(TAG, "Notify NFC state is " + nfcNotifyToString(mNotifyNfc));
+=======
+        mNotifyNfc = SystemProperties.getInt(NFC_NOTIFICATION_PROP, 0) > 0;
+        if (DEBUG) Slog.v(TAG, "Notify NFC behavior is " + (mNotifyNfc ? "active" : "disabled"));
+        mAllowMediaUid = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_allowMediaUidForCameraServiceProxy);
+>>>>>>> 8654b05059e... CameraServiceProxy: Loosen UID check conditionally
     }
 
     @Override
