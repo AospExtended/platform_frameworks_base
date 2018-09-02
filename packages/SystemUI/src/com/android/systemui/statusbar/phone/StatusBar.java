9f23e39cc0fae6b1a9fbf3be41bd7c9beb594a9c
Kshitij Gupta <kshitijgm@gmail.com>
2018-09-02 21:23:24 +0000

    themes: enable the separated SystemUItheme
    - We may later merge this back into the SystemDarkTheme if we don't have
      oreo-like qs customisations in pie.
    - For now however, we'll simply build and enable the overlay as is.
    
    Change-Id: I7077171a171cb11f207e58ff1cdde9c65a6bb510

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -3982,6 +3982,8 @@ public class StatusBar extends SystemUI implements DemoMode,
                 try {
                     mOverlayManager.setEnabled("com.android.system.theme.dark",
                             useDarkTheme, mLockscreenUserManager.getCurrentUserId());
+                    mOverlayManager.setEnabled("com.android.systemui.theme.dark",
+                            useDarkTheme, mLockscreenUserManager.getCurrentUserId());
                     mOverlayManager.setEnabled("com.android.settings.theme.dark",
                             useDarkTheme, mLockscreenUserManager.getCurrentUserId());
                 } catch (RemoteException e) {
