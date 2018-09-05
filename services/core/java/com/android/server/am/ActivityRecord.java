f60a792a7529fc70ceaffa5582d4c801a9ec3cb6
Bhargav Upperla <bhargavuln@codeaurora.org>
2018-09-05 16:09:17 -0700

    Add Game boost & modify launchboostv2 invocation.
    Trigger LBv2 from where LBv1 is triggered.
    Recent app behavior changes cause oringinal LBv2
    in startProcess to not trigger. This is fixed.
    
    Detect if app is a game & trigger game boost,
    for faster game launch latency.
    
    CRs-Fixed: 2309700
    Change-Id: I1b332639559043977e0581f537e62eccdaf55dd0
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- services/core/java/com/android/server/am/ActivityRecord.java
+++ services/core/java/com/android/server/am/ActivityRecord.java
@@ -2116,13 +2116,9 @@ final class ActivityRecord extends ConfigurationContainer implements AppWindowCo
             Log.i(TAG, sb.toString());
         }
         mStackSupervisor.reportActivityLaunchedLocked(false, this, thisTime, totalTime);
-        if (appInfo != null) {
-            int isGame = 0;
-            isGame = (appInfo.category == ApplicationInfo.CATEGORY_GAME ||
-                      (appInfo.flags & ApplicationInfo.FLAG_IS_GAME) == ApplicationInfo.FLAG_IS_GAME) ? 1 : 0;
-            if (mUxPerf !=  null) {
-                mUxPerf.perfUXEngine_events(BoostFramework.UXE_EVENT_GAME, 0, packageName, isGame);
-            }
+        int isGame = isAppInfoGame();
+        if (mUxPerf !=  null) {
+            mUxPerf.perfUXEngine_events(BoostFramework.UXE_EVENT_GAME, 0, packageName, isGame);
         }
         if (mPerfFirstDraw == null) {
             mPerfFirstDraw = new BoostFramework();
@@ -2139,6 +2135,15 @@ final class ActivityRecord extends ConfigurationContainer implements AppWindowCo
             mPerf.perfLockReleaseHandler(perfActivityBoostHandler);
             perfActivityBoostHandler = -1;
         }
+    }
+
+    public int isAppInfoGame() {
+        int isGame = 0;
+        if (appInfo != null) {
+            isGame = (appInfo.category == ApplicationInfo.CATEGORY_GAME ||
+                      (appInfo.flags & ApplicationInfo.FLAG_IS_GAME) == ApplicationInfo.FLAG_IS_GAME) ? 1 : 0;
+        }
+        return isGame;
     }
 
     @Override
