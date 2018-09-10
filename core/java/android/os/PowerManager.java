9cd93144d6a03cc4083ad502b93bde92ff90fb9e
Shubham Singh <coolsks94@gmail.com>
2018-09-10 12:27:41 +0000

    Revert button backlight commits
    Revert "services: button brightness should check proximity"
    
    This reverts commit f1754bf17c1f7eaf1337d4a6a165fed49a83d8f8.
    
    Revert "PowerManagerService: Fix updating of mUserActivitySummary"
    
    This reverts commit 0e643b64f7a90c52d2250ddd4dad9cbf1b4e864d.
    
    Revert "power: Disable keyboard/button lights while dozing/dreaming"
    
    This reverts commit 50332af6f2afb1b583b47b9f48549e7d48f51a35.
    
    Revert "Add PowerManager integration for button backlight."
    
    This reverts commit 482870d736b7450d4fb2eef1e198fe1fc3df8cd3.
    
    Revert "Reintroduce button-backlight (and respective inactivity timeout)"
    
    This reverts commit 68a63bbe889ae0226911bbf4775452ba26b0bb0b.
    
    Change-Id: I037b8954e6f1d203325670777aaf30d4b74ea60a

--- core/java/android/os/PowerManager.java
+++ core/java/android/os/PowerManager.java
@@ -1708,13 +1708,4 @@ public final class PowerManager {
             };
         }
     }
-
-    /**
-     * Gets the default button brightness value.
-     * @hide
-     */
-    public int getDefaultButtonBrightness() {
-        return mContext.getResources().getInteger(
-                com.android.internal.R.integer.config_buttonBrightnessSettingDefault);
-    }
 }
