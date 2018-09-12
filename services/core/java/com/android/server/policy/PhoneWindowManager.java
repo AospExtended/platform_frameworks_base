77a5380531d33c7ebc06e44dc2968be07b967cf6
xyyx <xyyx@mail.ru>
2018-09-12 09:36:21 +0800

    HWKeys: Fix haptic feedback on navbar when hwkeys is disabled
    Change-Id: I40caca585eac43931011f4bcd7f89c24094ba389

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6394,6 +6394,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         final boolean isInjected = (policyFlags & WindowManagerPolicy.FLAG_INJECTED) != 0;
 
+        final boolean virtualKey = event.getDeviceId() == KeyCharacterMap.VIRTUAL_KEYBOARD;
+
         // If screen is off then we treat the case where the keyguard is open but hidden
         // the same as if it were open and in front.
         // This will prevent any keys other than the power button from waking the screen
@@ -6471,9 +6473,13 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         boolean useHapticFeedback = down
                 && (policyFlags & WindowManagerPolicy.FLAG_VIRTUAL) != 0
                 && (!isNavBarVirtKey || mNavBarVirtualKeyHapticFeedbackEnabled)
-                && event.getRepeatCount() == 0
-                && !isHwKeysDisabled()
-                && !keyguardOn();
+                && event.getRepeatCount() == 0;
+
+        if (!virtualKey) {
+            if (isHwKeysDisabled() || keyguardOn()) {
+                useHapticFeedback = false;
+            }
+        }
 
         // Specific device key handling
         if (dispatchKeyToKeyHandlers(event)) {
