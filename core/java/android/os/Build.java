d209960273b6e1a5e41db7fd54c06cdac316a54b
Marko Man <darkobas@gmail.com>
2018-10-17 10:22:17 +0200

    [2/2] base: allow device to override *caugh*caugh* prop
    Change-Id: Ib51a5e5ae87b004053dcf1c01d8aa6cf3ef63b65

--- core/java/android/os/Build.java
+++ core/java/android/os/Build.java
@@ -254,6 +254,13 @@ public class Build {
                 "ro.build.version.security_patch", "");
 
         /**
+         * The user-visible aex security patch level.
+         * @hide
+         */
+        public static final String SECURITY_PATCH_CUSTOM = SystemProperties.get(
+                "ro.extended.version.security_patch", "");
+
+        /**
          * The user-visible SDK version of the framework in its raw String
          * representation; use {@link #SDK_INT} instead.
          *
