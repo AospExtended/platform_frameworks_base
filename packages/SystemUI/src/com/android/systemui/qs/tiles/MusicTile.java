173b8d06c0aaad2de7208aa2fad3d615c41d9a98
Pranav Vashi <neobuddy89@gmail.com>
2018-10-27 10:58:04 +0530

    MusicTile: Make tile state inactive if not playing music
    Change-Id: I3160e7b3efe98c0b0a9c9a7d1c756e5376fccf49
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/MusicTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/MusicTile.java
@@ -32,6 +32,7 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.ViewConfiguration;
 
+import android.service.quicksettings.Tile;
 import com.android.systemui.R;
 import com.android.systemui.plugins.qs.QSTile.BooleanState;
 import com.android.systemui.qs.QSHost;
@@ -120,6 +121,7 @@ public class MusicTile extends QSTileImpl<BooleanState> {
             state.icon = ResourceIcon.get(R.drawable.ic_qs_media_play);
             state.label = mContext.getString(R.string.quick_settings_music_play);
         }
+        state.state = mActive ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE;
     }
 
     private void playbackStateUpdate(int state) {
