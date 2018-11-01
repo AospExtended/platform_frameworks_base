4f15a0f931ec789039ffb1ec99d0f2224ff0396e
Alex Cruz <alex@dirtyunicorns.com>
2018-11-01 00:41:16 -0500

    Allow to check for QS tile styles overlays
    Change-Id: I4260120f49765039339093175ca76da0e0e6c280

--- core/java/com/android/internal/statusbar/ThemeAccentUtils.java
+++ core/java/com/android/internal/statusbar/ThemeAccentUtils.java
@@ -348,4 +348,16 @@ public class ThemeAccentUtils {
             }
         }
     }
+
+    // Check for any QS tile styles overlay
+    public static boolean isUsingQsTileStyles(IOverlayManager om, int userId, int qsstyle) {
+        OverlayInfo themeInfo = null;
+        try {
+            themeInfo = om.getOverlayInfo(QS_TILE_THEMES[qsstyle],
+                    userId);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        return themeInfo != null && themeInfo.isEnabled();
+    }
 }
