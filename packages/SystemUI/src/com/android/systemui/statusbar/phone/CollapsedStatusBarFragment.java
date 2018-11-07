a941b246c0757b02b06e93603b6f96272f804b4b
SpiritCroc <dev@spiritcroc.de>
2018-11-07 15:02:52 +0100

    Add back more battery bar locations [1/2]
    - Bring to Pie
    - Add bottom of statusbar location
    - Use extra preference for location so we can use our masterswitch
        preference in AE for en-/disabling
    - Leave away rotated navigation bar for now
        depending whether rotated to left or right, battery bar would show
        up on other edge, so would need extra logic
    
    Change-Id: I9b323e025d04a3a16b5557f96f0cd5ad08dce0dc
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -92,6 +92,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     private BatteryMeterView mBatteryMeterView;
     private StatusIconContainer mStatusIcons;
     private int mSignalClusterEndPadding = 0;
+    private View mBatteryBars[] = new View[2];
 
     private class SettingsObserver extends ContentObserver {
        SettingsObserver(Handler handler) {
@@ -184,6 +185,8 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         mAEXLogo = (ImageView)mStatusBar.findViewById(R.id.status_bar_logo);
         Dependency.get(DarkIconDispatcher.class).addDarkReceiver(mAEXLogo);
         mCustomCarrierLabel = mStatusBar.findViewById(R.id.statusbar_carrier_text);
+        mBatteryBars[0] = mStatusBar.findViewById(R.id.battery_bar);
+        mBatteryBars[1] = mStatusBar.findViewById(R.id.battery_bar_1);
         showSystemIconArea(false);
         initEmergencyCryptkeeperText();
         animateHide(mClockView, false, false);
@@ -333,6 +336,9 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
             animateHide(mRightClock, animate, true);
         }
         animateHide(mSystemIconArea, animate, true);
+        for (View batteryBar: mBatteryBars) {
+            animateHide(batteryBar, animate, true);
+        }
     }
 
     public void showSystemIconArea(boolean animate) {
@@ -341,6 +347,9 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
             animateShow(mRightClock, animate);
         }
         animateShow(mSystemIconArea, animate);
+        for (View batteryBar: mBatteryBars) {
+            animateShow(batteryBar, animate);
+        }
     }
 
 /*    public void hideClock(boolean animate) {
