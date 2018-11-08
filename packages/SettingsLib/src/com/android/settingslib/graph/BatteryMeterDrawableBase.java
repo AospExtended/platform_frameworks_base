c58cbd9199686f4f1e96a9d92b9532b733a11929
ezio84 <brabus84@gmail.com>
2018-11-08 01:02:56 -0600

    Inherit battery meter style in view [1/2]
    Change-Id: I9ce90477c1d327103b80c67cc67917476dc207ba

--- packages/SettingsLib/src/com/android/settingslib/graph/BatteryMeterDrawableBase.java
+++ packages/SettingsLib/src/com/android/settingslib/graph/BatteryMeterDrawableBase.java
@@ -180,6 +180,10 @@ public class BatteryMeterDrawableBase extends Drawable {
         mIntrinsicHeight = context.getResources().getDimensionPixelSize(R.dimen.battery_height);
     }
 
+    public void setDashEffect(float[] intervals, float phase) {
+        mPathEffect = new DashPathEffect(intervals, phase);
+    }
+
     @Override
     public int getIntrinsicHeight() {
         return mIntrinsicHeight;
