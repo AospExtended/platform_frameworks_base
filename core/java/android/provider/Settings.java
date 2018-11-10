5a13345901ff80914e053c3f1a3bd992652563e1
ezio84 <brabus84@gmail.com>
2018-11-10 20:43:31 +0100

    Stock Recents/Overview icon pack support [2/3]
    Change-Id: Ib3078fbeee5396501334f7a44b3ee3d58a9cb8ee
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4829,6 +4829,12 @@ public final class Settings {
 
 
         /**
+         * Whether to use icon pack for Recents
+         * @hide
+         */
+        public static final String RECENTS_ICON_PACK = "recents_icon_pack";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
