8cbcd59790e931af436b8d574db29787ad880077
Alex Cruz <alex@dirtyunicorns.com>
2018-11-15 23:16:45 -0600

    Multi-User fixes for brightness slider mod
    Previously this wasn't working in guest mode or in any multi-
    user accounts. While most don't care about this, this is just
    janky for our stuff not to work in these situations.
    
    Change-Id: Ic1ae93d2f4ccd023bd4ec4ed2b6edd9243f49e6a
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- packages/SystemUI/src/com/android/systemui/settings/BrightnessDialog.java
+++ packages/SystemUI/src/com/android/systemui/settings/BrightnessDialog.java
@@ -20,6 +20,7 @@ import static com.android.settingslib.display.BrightnessUtils.GAMMA_SPACE_MAX;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.view.Gravity;
@@ -81,12 +82,13 @@ public class BrightnessDialog extends Activity {
         minBrightness.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int currentValue = Settings.System.getInt(resolver,
-                        Settings.System.SCREEN_BRIGHTNESS, 0);
+                int currentValue = Settings.System.getIntForUser(resolver,
+                        Settings.System.SCREEN_BRIGHTNESS, 0, UserHandle.USER_CURRENT);
                 int brightness = currentValue - 10;
                 if (currentValue != 0) {
-                    Settings.System.putInt(resolver,
-                            Settings.System.SCREEN_BRIGHTNESS, Math.max(0, brightness));
+                    int math = Math.max(0, brightness);
+                    Settings.System.putIntForUser(resolver,
+                            Settings.System.SCREEN_BRIGHTNESS, math, UserHandle.USER_CURRENT);
                 }
             }
         });
@@ -104,12 +106,13 @@ public class BrightnessDialog extends Activity {
         maxBrightness.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int currentValue = Settings.System.getInt(resolver,
-                        Settings.System.SCREEN_BRIGHTNESS, 0);
+                int currentValue = Settings.System.getIntForUser(resolver,
+                        Settings.System.SCREEN_BRIGHTNESS, 0, UserHandle.USER_CURRENT);
                 int brightness = currentValue + 10;
                 if (currentValue != 255) {
-                    Settings.System.putInt(resolver,
-                            Settings.System.SCREEN_BRIGHTNESS, Math.min(255, brightness));
+                    int math = Math.min(255, brightness);
+                    Settings.System.putIntForUser(resolver,
+                            Settings.System.SCREEN_BRIGHTNESS, math, UserHandle.USER_CURRENT);
                 }
             }
         });
