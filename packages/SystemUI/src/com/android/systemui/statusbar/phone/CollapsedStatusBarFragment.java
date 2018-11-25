92e007aa07a6221eb7f65e8b1fb2c6fabe772750
Lars Greiss <kufikugel@googlemail.com>
2018-11-25 17:53:21 +0100

    Status bar clock/date customizations [1/2]
    @ezio84 & @nychitman1 edits:
    
    - Adapt to Pie
    - Strip down and drop features we don't need anymore
    - Cleanup/improve code
    - Make it play nice with Pie's expanded status bar date/clock view
    - Make it play nice with Heads Up
    
    Subfeatures:
    
    - Ability to hide clock
    - AM/PM
    - Date format
    - Date style
    
    *DU Edits*
    - Turn off by default
    
    Change-Id: I73d854ed5cfb57fa19e4b5554bb10cb2e1ce9b65

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -39,6 +39,7 @@ import com.android.systemui.R;
 import com.android.systemui.SysUiServiceProvider;
 import com.android.systemui.statusbar.CommandQueue;
 import com.android.systemui.statusbar.phone.StatusBarIconController.DarkIconManager;
+import com.android.systemui.statusbar.policy.Clock;
 import com.android.systemui.statusbar.policy.DarkIconDispatcher;
 import com.android.systemui.statusbar.policy.EncryptionHelper;
 import com.android.systemui.statusbar.policy.KeyguardMonitor;
@@ -307,6 +308,9 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
      * Animate a view to INVISIBLE or GONE
      */
     private void animateHiddenState(final View v, int state, boolean animate) {
+        if (v instanceof Clock && !((Clock)v).isClockVisible()) {
+            return;
+        }
         v.animate().cancel();
         if (!animate) {
             v.setAlpha(0f);
@@ -333,6 +337,9 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
      * Shows a view, and synchronizes the animation with Keyguard exit animations, if applicable.
      */
     private void animateShow(View v, boolean animate) {
+        if (v instanceof Clock && !((Clock)v).isClockVisible()) {
+            return;
+        }
         v.animate().cancel();
         v.setVisibility(View.VISIBLE);
         if (!animate) {
