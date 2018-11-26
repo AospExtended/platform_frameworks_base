741e58da8f9994432c5f2b9854d957e175c12049
ezio84 <brabus84@gmail.com>
2018-11-26 19:44:37 +0100

    Fix dnd tile longpress after no-title tiles commits
    Change-Id: Ice45b6a7e3c3d6aab0ef48a421996c05c6082696

--- packages/SystemUI/src/com/android/systemui/qs/tiles/DndTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/DndTile.java
@@ -98,11 +98,6 @@ public class DndTile extends QSTileImpl<BooleanState> {
         }
     }
 
-    @Override
-    public boolean isDualTarget() {
-        return true;
-    }
-
     public static void setVisible(Context context, boolean visible) {
         Prefs.putBoolean(context, Prefs.Key.DND_TILE_VISIBLE, visible);
     }
