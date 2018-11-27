c08da21940d39c6ea72ed9b9c2ad4c2d2f94c496
Daniel Koman <dankoman30@gmail.com>
2018-11-27 15:31:39 -0700

    Add kill button to notification guts [1/2]
    SDB:Pie Bringup
    
    [SpiritCroc]
    - Make it a setting
    - Don't allow killing apps when device is locked
    - Bring it to Oreo
    
    SagarMakhar: Convert TextView to ImageButton
    
    Change-Id: Id2c2f3259ae2ff45ab28288375bc14148a0e9f1e
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5779,6 +5779,14 @@ public final class Settings {
         public static final String LESS_BORING_HEADS_UP = "less_boring_heads_up";
 
         /**
+         * Whether to show the kill app button in notification guts
+         *
+         * @hide
+         */
+        public static final String NOTIFICATION_GUTS_KILL_APP_BUTTON =
+                "notification_guts_kill_app_button";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
