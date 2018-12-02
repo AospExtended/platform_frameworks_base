71676b3e3a16826e483f9ab2a104deaa742a967d
Nicolas Dhouailly <ndhouailly@gmail.com>
2018-12-02 12:12:39 +0100

    Battery charge warning [1/2]
    Change-Id: I7421cddd035eb198f8dc7a5ef395f927724567f6
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryControllerImpl.java
@@ -17,9 +17,13 @@
 package com.android.systemui.statusbar.policy;
 
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.BatteryManager;
 import android.os.BatteryStats;
 import android.os.Bundle;
@@ -27,6 +31,8 @@ import android.os.Handler;
 import android.os.PowerManager;
 import android.os.PowerSaveState;
 import android.os.SystemClock;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.Log;
 
 import com.android.internal.annotations.VisibleForTesting;
@@ -66,6 +72,7 @@ public class BatteryControllerImpl extends BroadcastReceiver implements BatteryC
     protected boolean mAodPowerSave;
     private boolean mTestmode = false;
     private boolean mHasReceivedBattery = false;
+    private int mLastLevel = 0;
 
     private Estimate mEstimate = null;
     private final EnhancedEstimates mEstimates;
@@ -147,6 +154,10 @@ public class BatteryControllerImpl extends BroadcastReceiver implements BatteryC
             mCharging = mCharged || status == BatteryManager.BATTERY_STATUS_CHARGING;
 
             fireBatteryLevelChanged();
+
+            if (mCharging) {
+                notificationBatteryChargeLevel();
+            }
         } else if (action.equals(PowerManager.ACTION_POWER_SAVE_MODE_CHANGED)) {
             updatePowerSave();
         } else if (action.equals(PowerManager.ACTION_POWER_SAVE_MODE_CHANGING)) {
@@ -228,6 +239,27 @@ public class BatteryControllerImpl extends BroadcastReceiver implements BatteryC
                 mChangeCallbacks.get(i).onPowerSaveChanged(mPowerSave);
             }
         }
+    }
+
+    private void notificationBatteryChargeLevel() {
+        boolean enabled = Settings.System.getIntForUser(mContext.getContentResolver(),
+                            Settings.System.BATTERY_LEVEL_CHARGE_ALARM_ENABLED, 0,
+                            UserHandle.USER_CURRENT) == 1;
+        if (!enabled) return;
+        int level = Settings.System.getIntForUser(mContext.getContentResolver(),
+                        Settings.System.SEEK_BAR_BATTERY_CHARGE_LEVEL_SOUND, 100,
+                        UserHandle.USER_CURRENT);
+        if (level == mLevel && mLevel != mLastLevel) {
+            String alarmSound = Settings.Global.getString(mContext.getContentResolver(),
+                                    Settings.Global.BATTERY_LEVEL_CHARGE_SOUND_ALARM);
+            if (alarmSound != null && !alarmSound.equals("silent")) {
+                Ringtone batteryChargeLevelAlarm = RingtoneManager.getRingtone(mContext, Uri.parse(alarmSound));
+                if (batteryChargeLevelAlarm != null) {
+                    batteryChargeLevelAlarm.play();
+                }
+            }
+        }
+        mLastLevel = mLevel;
     }
 
     private boolean mDemoMode;
