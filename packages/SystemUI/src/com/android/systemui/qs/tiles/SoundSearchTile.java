4d1b8cb9e7776ae679270d485024d852a97ce03a
ezio84 <brabus84@gmail.com>
2018-12-22 01:19:35 +0000

    SoundSearchTile: Update intent flags for the apps
    Taken from: https://github.com/ezio84/abc_frameworks_base/commit/9bafbf08390786966719f1b90b9f24b2de17d1c9#diff-5888d398e209da47a36960bdd9feeeb5R50
    
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: Iaa838df734f07eeda178dee8cb6b1120e9438806
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/SoundSearchTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/SoundSearchTile.java
@@ -54,17 +54,20 @@ public class SoundSearchTile extends QSTileImpl<BooleanState> {
         if (AEXUtils.isPackageInstalled(mContext, "com.shazam.android")
                     || AEXUtils.isPackageInstalled(mContext, "com.shazam.encore.android")) {
             Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             intent.setAction("com.shazam.android.intent.actions.START_TAGGING");
             mContext.startActivity(intent);
         // Soundhound
         } else if (AEXUtils.isPackageInstalled(mContext, "com.melodis.midomiMusicIdentifier.freemium")
                     || AEXUtils.isPackageInstalled(mContext, "com.melodis.midomiMusicIdentifier")) {
             Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             intent.setAction("com.soundhound.android.ID_NOW_EXTERNAL");
             mContext.startActivity(intent);
         // Google Search Music
         } else if (AEXUtils.isPackageInstalled(mContext, "com.google.android.googlequicksearchbox")) {
             Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             intent.setAction("com.google.android.googlequicksearchbox.MUSIC_SEARCH");
             mContext.startActivity(intent);
         } else {
