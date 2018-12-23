8b535b7e4e1d212f9287d60b733db63e0cebcf32
Pranav Vashi <neobuddy89@gmail.com>
2018-12-23 23:23:10 +0530

    BatteryMeterDrawableBase: Refresh only when value changed
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Change-Id: Ifbd3181fb770b49878f0d6061918eb9bcbabfb9c

--- packages/SettingsLib/src/com/android/settingslib/graph/BatteryMeterDrawableBase.java
+++ packages/SettingsLib/src/com/android/settingslib/graph/BatteryMeterDrawableBase.java
@@ -196,13 +196,17 @@ public class BatteryMeterDrawableBase extends Drawable {
     }
 
     public void setShowPercent(boolean show) {
-        mShowPercent = show;
-        postInvalidate();
+        if (mShowPercent != show) {
+            mShowPercent = show;
+            postInvalidate();
+        }
     }
 
     public void setCharging(boolean val) {
-        mCharging = val;
-        postInvalidate();
+        if (mCharging != val) {
+            mCharging = val;
+            postInvalidate();
+        }
     }
 
     public boolean getCharging() {
@@ -210,8 +214,10 @@ public class BatteryMeterDrawableBase extends Drawable {
     }
 
     public void setBatteryLevel(int val) {
-        mLevel = val;
-        postInvalidate();
+        if (mLevel != val) {
+            mLevel = val;
+            postInvalidate();
+        }
     }
 
     public int getBatteryLevel() {
@@ -219,8 +225,10 @@ public class BatteryMeterDrawableBase extends Drawable {
     }
 
     public void setPowerSave(boolean val) {
-        mPowerSaveEnabled = val;
-        postInvalidate();
+        if (mPowerSaveEnabled != val) {
+            mPowerSaveEnabled = val;
+            postInvalidate();
+        }
     }
 
     protected void setPowerSaveAsColorError(boolean asError) {
