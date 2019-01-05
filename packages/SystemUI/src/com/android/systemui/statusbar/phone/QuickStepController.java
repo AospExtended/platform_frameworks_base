07328ae122a92383ad2037ce5d9cf5c9cf7c941e
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2019-01-05 22:53:47 +0000

    Full gesture navbar: fix swipe to back glitches
    fixes jira_DU13-9
    
    some examples:
    - app window being resized because the finger did also a little swipe up within the swipe left, and not resetting its size after the back action
    
    - show a fullscreen landscape video on youtube, swipe back, yt window gets fu#cked up because of wrong navbar layout
    
    Change-Id: I3ae6d85cd8a3f4b7bac553cb6a9953266b862f8a

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickStepController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickStepController.java
@@ -395,6 +395,7 @@ public class QuickStepController implements GestureHelper {
                         mPreviousUpEventY = (int)event.getY();
                     }
                     if (mBackActionScheduled) {
+                        endQuickScrub(true /* animate */);
                         AEXUtils.sendKeycode(KeyEvent.KEYCODE_BACK, mHandler);
                         mNavigationBarView.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
                     } else {
@@ -407,11 +408,10 @@ public class QuickStepController implements GestureHelper {
                 break;
         }
 
-        // Proxy motion events to launcher if not handled by quick scrub or back action
+        // Proxy motion events to launcher if not handled by quick scrub
         // Proxy motion events up/cancel that would be sent after long press on any nav button
-        if (!mQuickScrubActive && !mBackActionScheduled
-                && (mAllowGestureDetection || action == MotionEvent.ACTION_CANCEL
-                || action == MotionEvent.ACTION_UP)) {
+        if (!mQuickScrubActive && (mAllowGestureDetection || mBackActionScheduled
+                || action == MotionEvent.ACTION_CANCEL || action == MotionEvent.ACTION_UP)) {
             proxyMotionEvents(event);
         }
         return mQuickScrubActive || mQuickStepStarted || deadZoneConsumed || mBackActionScheduled;
