e3d78914517f36fc21e19b5172278a8f83850cb9
Alex Cruz <du.alexcruz@gmail.com>
2019-01-13 11:10:54 -0600

    Method to detect a notch'd device
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: I4742eaeea94c4495361bdfe2b855ae0de0aa24c1
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/com/android/internal/util/aospextended/AEXUtils.java
+++ core/java/com/android/internal/util/aospextended/AEXUtils.java
@@ -39,6 +39,7 @@ import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkCapabilities;
 import android.net.NetworkInfo;
+import android.util.DisplayMetrics;
 import com.android.internal.R;
 
 import java.util.List;
@@ -230,5 +231,24 @@ public class AEXUtils {
 
     public static boolean isPackageInstalled(Context context, String pkg) {
         return isPackageInstalled(context, pkg, true);
+    }
+
+	    // Check if device has a notch
+    public static boolean hasNotch(Context context) {
+        int result = 0;
+        int resid;
+        int resourceId = context.getResources().getIdentifier(
+                "status_bar_height", "dimen", "android");
+        resid = context.getResources().getIdentifier("config_fillMainBuiltInDisplayCutout",
+                "bool", "android");
+        if (resid > 0) {
+            return context.getResources().getBoolean(resid);
+        }
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+        float px = 24 * (metrics.densityDpi / 160f);
+        return result > Math.round(px);
     }
 }
