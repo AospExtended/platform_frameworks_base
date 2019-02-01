d1c87437912703d068ffc4ddb4cbc622453c9948
Alex <du.alexcruz@gmail.com>
2019-02-01 10:38:43 -0600

    Add wellbeing dark theme
    Change-Id: I0a0d5582f5870cb040f724d0375c454797f937c9

--- core/java/com/android/internal/statusbar/ThemeAccentUtils.java
+++ core/java/com/android/internal/statusbar/ThemeAccentUtils.java
@@ -71,6 +71,7 @@ public class ThemeAccentUtils {
         "com.android.dialer.theme.dark", //3
         "com.android.contacts.theme.dark", //4
         "com.android.documentsui.theme.dark", //5
+        "com.google.android.apps.wellbeing.theme.dark", //6
     };
 
     private static final String[] BLACK_THEMES = {
@@ -80,6 +81,7 @@ public class ThemeAccentUtils {
         "com.android.dialer.theme.black", //3
         "com.android.contacts.theme.black", //4
         "com.android.documentsui.theme.black", //5
+        "com.google.android.apps.wellbeing.theme.black", //6
     };
 
     private static final String[] EXTENDED_THEMES = {
