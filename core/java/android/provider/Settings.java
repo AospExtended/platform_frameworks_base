699f7483b10c870c3badb5bdd4302108c1c7c482
maxwen <max.weninger@gmail.com>
2019-02-13 00:13:13 +0100

    LockscreenCharging: Keyguard show charging watt
    Change-Id: I80c0f6d0551cc6ae792e4b8238ca379fef697882

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14953,6 +14953,13 @@ public final class Settings {
          */
         public static final String SHOW_FPS_OVERLAY = "show_fps_overlay";
 
+
+        /**
+         * Whether to show the battery info on the lockscreen while charging
+         * @hide
+         */
+        public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
