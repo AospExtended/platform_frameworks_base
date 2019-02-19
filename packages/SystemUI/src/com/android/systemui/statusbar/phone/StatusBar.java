4007325e74a132e215de82689b5454c44d80605c
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-02-19 10:10:54 -0300

    SystemUI: Refresh themes on dirty flash
    Change-Id: I54263e6196387ce9f5eb914e76b8d999bed6b7a2

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -94,6 +94,7 @@ import android.media.session.PlaybackState;
 import android.metrics.LogMaker;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
@@ -4405,6 +4406,16 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
                 Settings.System.SYSTEM_DARK_THEME_STYLE, 0, mLockscreenUserManager.getCurrentUserId());
     }
 
+    private boolean themeNeedsRefresh(){
+        if (mContext.getSharedPreferences("systemui_theming", 0).getString(
+                "build_fingerprint", "").equals(Build.CUSTOM_FINGERPRINT)){
+            return false;
+        }
+        mContext.getSharedPreferences("systemui_theming", 0).edit().putString(
+                "build_fingerprint", Build.CUSTOM_FINGERPRINT).commit();
+        return true;
+    }
+
     /**
      * Switches theme from light to dark and vice-versa.
      */
@@ -4423,7 +4434,7 @@ public class StatusBar extends SystemUI implements DemoMode, TunerService.Tunabl
                 == Configuration.UI_MODE_NIGHT_YES;
         final boolean useDarkTheme = wallpaperWantsDarkTheme || nightModeWantsDarkTheme;
 
-        if(useDarkTheme) {
+        if(themeNeedsRefresh() || useDarkTheme) {
             switch (mCurrentDarkTheme) {
                 case DARK:
                     default:
