adce60670817232e5841105abbf600f0d1827df7
maxwen <max.weninger@gmail.com>
2019-03-02 18:33:27 +0530

    base: add vibrate util methods
    1) vibrate a pattern
    2) vibrate a resource id patterm
    e.g. like
        <integer-array name=config_defaultNotificationVibePattern>
            <item>0</item>
            <item>350</item>
            <item>250</item>
            <item>350</item>
        </integer-array>
    
    Change-Id: I92463ba823ac72920cd51bcf35c2f3bc51696045
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

--- core/java/com/android/internal/util/aospextended/AEXUtils.java
+++ core/java/com/android/internal/util/aospextended/AEXUtils.java
@@ -38,6 +38,7 @@ import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemClock;
+import android.os.Vibrator;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.view.IWindowManager;
@@ -359,4 +360,34 @@ public class AEXUtils {
             }
         }
     }
+
+        /* e.g.
+        <integer-array name="config_defaultNotificationVibePattern">
+        <item>0</item>
+        <item>350</item>
+        <item>250</item>
+        <item>350</item>
+        </integer-array>
+    */
+    public static void vibrateResourcePattern(Context context, int resId) {
+        if (DeviceUtils.deviceSupportsVibrator(context)) {
+            int[] pattern = context.getResources().getIntArray(resId);
+            if (pattern == null) {
+                return;
+            }
+            long[] out = new long[pattern.length];
+            for (int i=0; i<pattern.length; i++) {
+                out[i] = pattern[i];
+            }
+            ((Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE)).vibrate(out, -1);
+        }
+    }
+
+    public static void vibratePattern(Context context, long[] pattern) {
+        if (DeviceUtils.deviceSupportsVibrator(context)) {
+            ((Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE)).vibrate(pattern, -1);
+        }
+    }
 }
+
+
