e48439cdbede26222da15588bf87554d36897e8a
AmolAmrit <amol.amrit03@outlook.com>
2019-03-05 02:14:10 +0530

    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com> Change-Id: Ic9d310008ed2db455f74b8f3a09d4f5caf7f2744

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -50,7 +50,6 @@ import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
-import com.android.systemui.qs.tiles.SmartPixelsTile;
 import com.android.systemui.qs.tiles.SoundSearchTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
 import com.android.systemui.qs.tiles.ThemeTile;
