849fd81de32dd4217b8de127b814f4a24d10df12
Pranav Vashi <neobuddy89@gmail.com>
2019-03-04 16:25:50 +0530

    Revert "base: SystemUI: tuner: fix reorder of mobile slots"
    This reverts commit 3fb6ceabd036729a29b8e154040af636c86ddf27.
    
    Change-Id: I9124f661f26e929a931ac931f56e763f5a5fe2ae
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconList.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconList.java
@@ -252,21 +252,6 @@ public class StatusBarIconList {
             return holders;
         }
 
-        public List<StatusBarIconHolder> getHolderList() {
-            ArrayList<StatusBarIconHolder> holders = new ArrayList<>();
-            if (mHolder != null) {
-                holders.add(mHolder);
-            }
-
-            if (mSubSlots != null) {
-                for (int i = 0; i < mSubSlots.size(); i++) {
-                    holders.add(mSubSlots.get(i));
-                }
-            }
-
-            return holders;
-        }
-
         @Override
         public String toString() {
             return String.format("(%s) %s", mName, subSlotsString());
@@ -277,7 +262,7 @@ public class StatusBarIconList {
                 return "";
             }
 
-            return "" + mSubSlots.size() + " " + mSubSlots;
+            return "" + mSubSlots.size() + " subSlots";
         }
     }
 }
