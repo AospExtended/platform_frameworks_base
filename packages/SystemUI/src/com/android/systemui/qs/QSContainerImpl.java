d9ad5718db3924c34f340134abaa00f03b49c996
AmolAmrit <amol.amrit03@outlook.com>
2019-03-05 00:39:22 +0530

    Revert "base: QS panel opacity"
    This reverts commit 9e72eee94ea8689ae2fcd943bd51dcdcb593bd11.
    
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>
    Change-Id: I4d60694245393b929d00ea49473c4ad7cd161305

--- packages/SystemUI/src/com/android/systemui/qs/QSContainerImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSContainerImpl.java
@@ -20,12 +20,7 @@ import static android.app.StatusBarManager.DISABLE2_QUICK_SETTINGS;
 
 import android.content.Context;
 import android.content.res.Configuration;
-import android.database.ContentObserver;
 import android.graphics.Point;
-import android.graphics.drawable.Drawable;
-import android.os.Handler;
-import android.os.UserHandle;
-import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -57,13 +52,8 @@ public class QSContainerImpl extends FrameLayout {
     private int mSideMargins;
     private boolean mQsDisabled;
 
-    private Drawable mQsBackGround;
-
     public QSContainerImpl(Context context, AttributeSet attrs) {
         super(context, attrs);
-        Handler mHandler = new Handler();
-        SettingsObserver settingsObserver = new SettingsObserver(mHandler);
-        settingsObserver.observe();
     }
 
     @Override
@@ -72,14 +62,12 @@ public class QSContainerImpl extends FrameLayout {
         mQSPanel = findViewById(R.id.quick_settings_panel);
         mQSDetail = findViewById(R.id.qs_detail);
         mHeader = findViewById(R.id.header);
-        mQSCustomizer = (QSCustomizer) findViewById(R.id.qs_customize);
+        mQSCustomizer = findViewById(R.id.qs_customize);
         mQSFooter = findViewById(R.id.qs_footer);
         mBackground = findViewById(R.id.quick_settings_background);
         mStatusBarBackground = findViewById(R.id.quick_settings_status_bar_background);
         mBackgroundGradient = findViewById(R.id.quick_settings_gradient_view);
         mSideMargins = getResources().getDimensionPixelSize(R.dimen.notification_side_paddings);
-        mQsBackGround = getContext().getDrawable(R.drawable.qs_background_primary);
-        updateSettings();
 
         setClickable(true);
         setImportantForAccessibility(IMPORTANT_FOR_ACCESSIBILITY_NO);
@@ -102,39 +90,6 @@ public class QSContainerImpl extends FrameLayout {
         updateResources();
         updateSettings();
         mSizePoint.set(0, 0); // Will be retrieved on next measure pass.
-    }
-
-    private class SettingsObserver extends ContentObserver {
-        SettingsObserver(Handler handler) {
-            super(handler);
-        }
-
-        void observe() {
-            getContext().getContentResolver().registerContentObserver(Settings.System
-                            .getUriFor(Settings.System.OMNI_QS_PANEL_BG_ALPHA), false,
-                    this, UserHandle.USER_ALL);
-        }
-
-        @Override
-        public void onChange(boolean selfChange) {
-            updateSettings();
-        }
-    }
-
-    private void updateSettings() {
-        int mQsBackGroundAlpha = Settings.System.getIntForUser(getContext().getContentResolver(),
-                Settings.System.OMNI_QS_PANEL_BG_ALPHA, 255,
-                UserHandle.USER_CURRENT);
-
-        if (mQsBackGroundAlpha < 255 ) {
-            mBackground.setVisibility(View.INVISIBLE);
-            mBackgroundGradient.setVisibility(View.INVISIBLE);
-            mQsBackGround.setAlpha(mQsBackGroundAlpha);
-            setBackground(mQsBackGround);
-        } else {
-            mBackground.setVisibility(View.VISIBLE);
-            mBackgroundGradient.setVisibility(View.VISIBLE);
-        }
     }
 
     @Override
