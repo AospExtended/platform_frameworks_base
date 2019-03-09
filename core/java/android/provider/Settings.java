7bea97c0dd7cf733aff63dfa9ee3bacfaf77de7a
Arghya Chanda <arghyac35@gmail.com>
2019-03-09 19:12:47 +0530

    VolumeDialog: Remove Media stream
    * It is not working as expected
    
    Change-Id: I6d81178216296f88645c7f21b71fdc78f45ce9f0
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5209,9 +5209,6 @@ public final class Settings {
         private static final Validator ONE_HAND_MODE_ENABLED_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /** @hide */
-        public static final String AUDIO_PANEL_VIEW_MEDIA = "audio_panel_view_media";
-
-        /** @hide */
         public static final String AUDIO_PANEL_VIEW_NOTIFICATION = "audio_panel_view_notification";
 
         /** @hide */
