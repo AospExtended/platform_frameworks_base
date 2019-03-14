5b2885685edde766e0c9f8e513795bbeedb880a5
Nico60 <ndhouailly@gmail.com>
2019-03-14 07:04:00 -0400

    NetworkTraffic: Show arrows in statusbar [1/2]
    arrow-edits:
    adapt code to our dynamic arrows method
    
    SKULSHADY: Reverse logic of the toggle
    spezi77: Adapt for statusbar traffic indicator
    
    Change-Id: Ie50dbe3f54495d4b5c0c0a51969df595de7a1386
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5087,6 +5087,12 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_TYPE = "network_traffic_type";
 
         /**
+         * Whether to disable showing arrows in statusbar network traffic indicators
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_ARROW = "network_traffic_arrow";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
