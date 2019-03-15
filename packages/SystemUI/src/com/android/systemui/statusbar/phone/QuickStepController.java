mike2587/abc_frameworks_base

Full gestures navbar: add missing haptic feedback events

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickStepController.java
@@ -269,7 +269,10 @@ public class QuickStepController implements GestureHelper {
                     int deltaX = (int) mPreviousUpEventX - (int) event.getX();
                     int deltaY = (int) mPreviousUpEventY - (int) event.getY();
                     boolean isDoubleTapReally = deltaX * deltaX + deltaY * deltaY < sDoubleTapSquare;
-                    if (isDoubleTapReally) Utils.switchScreenOff(mContext);
+                    if (isDoubleTapReally) {
+                        mNavigationBarView.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+                        Utils.switchScreenOff(mContext);
+                    }
                 } else {
                     // this is the first tap, let's go further and schedule a
                     // mDoubleTapCancelTimeout call in the action up event so after the set time
@@ -398,8 +401,8 @@ public class QuickStepController implements GestureHelper {
                     }
                     if (mBackActionScheduled) {
                         endQuickScrub(true /* animate */);
-                        Utils.sendKeycode(KeyEvent.KEYCODE_BACK, mHandler);
                         mNavigationBarView.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+                        Utils.sendKeycode(KeyEvent.KEYCODE_BACK, mHandler);
                     } else {
                         endQuickScrub(true /* animate */);
                     }
@@ -426,6 +429,7 @@ public class QuickStepController implements GestureHelper {
             isDoubleTapPending = false;
             // it was a single tap, let's trigger the home button action
             mHandler.removeCallbacksAndMessages(null);
+            mNavigationBarView.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
             Utils.sendKeycode(KeyEvent.KEYCODE_HOME, mHandler);
         }
     };
@@ -441,6 +445,7 @@ public class QuickStepController implements GestureHelper {
 
         @Override
         public void run() {
+            mNavigationBarView.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
             moveKbCursor(isRight, true);
         }
     }

AospExtended/platform_frameworks_base

Signed-off-by: AmulyaX <amulya.b520@gmail.com>
Change-Id: Ib7c175b1d9833c3d44e66a36f8e28323d7c30879

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickStepController.java
@@ -267,7 +267,10 @@ public class QuickStepController implements GestureHelper {
                     int deltaX = (int) mPreviousUpEventX - (int) event.getX();
                     int deltaY = (int) mPreviousUpEventY - (int) event.getY();
                     boolean isDoubleTapReally = deltaX * deltaX + deltaY * deltaY < sDoubleTapSquare;
-                    if (isDoubleTapReally) AEXUtils.switchScreenOff(mContext);
+                    if (isDoubleTapReally) {
+                        mNavigationBarView.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+                        AEXUtils.switchScreenOff(mContext);
+                    }
                 } else {
                     // this is the first tap, let's go further and schedule a
                     // mDoubleTapCancelTimeout call in the action up event so after the set time
@@ -393,11 +396,11 @@ public class QuickStepController implements GestureHelper {
                         mNavigationBarView.isDt2s() ? sDoubleTapTimeout : 0);
                         mPreviousUpEventX = (int)event.getX();
                         mPreviousUpEventY = (int)event.getY();
-                    }
+	            }
                     if (mBackActionScheduled) {
                         endQuickScrub(true /* animate */);
-                        AEXUtils.sendKeycode(KeyEvent.KEYCODE_BACK, mHandler);
                         mNavigationBarView.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+                        AEXUtils.sendKeycode(KeyEvent.KEYCODE_BACK, mHandler);
                     } else {
                         endQuickScrub(true /* animate */);
                     }
@@ -424,6 +427,7 @@ public class QuickStepController implements GestureHelper {
             isDoubleTapPending = false;
             // it was a single tap, let's trigger the home button action
             mHandler.removeCallbacksAndMessages(null);
+            mNavigationBarView.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
             AEXUtils.sendKeycode(KeyEvent.KEYCODE_HOME, mHandler);
         }
     };
@@ -439,6 +443,7 @@ public class QuickStepController implements GestureHelper {
 
         @Override
         public void run() {
+            mNavigationBarView.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
             moveKbCursor(isRight, true);
         }
     }