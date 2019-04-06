mike2587/abc_frameworks_base

RecentsIconPack: Fallback to default icons on icon pack removal
This commit sets a BroadcastReceiver to Recents in order to handle this
scenario:

- Install IconPack;
- Set IconPack as the Recents default;
- Uninstall IconPack.

And everything seems to look okay, the system switches to the android
default IconPack (which is equal to empty string "").

PS9: ezio84: cleanups here and there.

Manual test:
- Set icon pack (for this test I chose Moonshine);
- Issue: adb shell settings get system recent_icon_pack
-- Returns: nexbit.icons.moonshine;
- Uninstall IconPack;
- Issue the previous command again
--Returns: "" (empty string);

Change-Id: Ia0dbb0744becca8d4b26d2b81d3662acc7dfa169

packages/SystemUI/src/com/android/systemui/recents/Recents.java
@@ -23,10 +23,12 @@ import static com.android.systemui.statusbar.phone.StatusBar.SYSTEM_DIALOG_REASO
 
 import android.app.ActivityManager;
 import android.app.trust.TrustManager;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
@@ -34,6 +36,7 @@ import android.content.res.Resources;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.hardware.display.DisplayManager;
+import android.net.Uri;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.RemoteException;
@@ -46,6 +49,7 @@ import android.widget.Toast;
 
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.du.Utils;
 import com.android.systemui.Dependency;
 import com.android.systemui.EventLogConstants;
 import com.android.systemui.EventLogTags;
@@ -175,6 +179,26 @@ public class Recents extends SystemUI
         }
     };
 
+    // Broadcast receiver for RecentsIconPack
+    private final BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if(action.equals(Intent.ACTION_PACKAGE_REMOVED)) {
+                // Get packageName from Uri
+                String packageName = intent.getData().getSchemeSpecificPart();
+                // iconPack currently set as default
+                String currentIconPack = Settings.System.getString/*ForUser*/(mContext.getContentResolver(),
+                Settings.System.RECENTS_ICON_PACK/*, UserHandle.myUserId()*/);
+                // If the uninstalled package equals to our currently set iconPack
+                if(packageName.equals(currentIconPack)) {
+                    // Assert the iconPack to be strongly sure that the package got uninstalled
+                    assertIconPack(packageName);
+                }
+            }
+        }
+    };
+
     /**
      * Returns the callbacks interface that non-system users can call.
      */
@@ -241,6 +265,13 @@ public class Recents extends SystemUI
         putComponent(Recents.class, this);
 
         mTrustManager = (TrustManager) mContext.getSystemService(Context.TRUST_SERVICE);
+
+        // Intent for applications that get uninstalled
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_PACKAGE_REMOVED);
+        filter.addDataScheme("package");
+        // Register our BroadcastReceiver
+        mContext.registerReceiver(mBroadcastReceiver, filter);
     }
 
     @Override
@@ -890,5 +921,19 @@ public class Recents extends SystemUI
     public void dump(FileDescriptor fd, PrintWriter pw, String[] args) {
         pw.println("Recents");
         pw.println("  currentUserId=" + SystemServicesProxy.getInstance(mContext).getCurrentUser());
+    }
+
+    /**
+     * This method checks whether a passed iconPack is still available, if it's not
+     * the system will fall back to the android default icon pack.
+     */
+    private void assertIconPack(String packageName) {
+        // We need to verify whether the iconPack is still installed
+        if(!Utils.isPackageInstalled(mContext, packageName)) {
+            // The icon pack is not available anymore, let's fallback to the default iconPack
+            Settings.System.putString(mContext.getContentResolver(),
+                    Settings.System.RECENTS_ICON_PACK, /* defaultIconPack */ "");
+            // Quickstep settings observer will now refresh the icon pack
+        }
     }
 }

AospExtended/platform_frameworks_base

packages/SystemUI/src/com/android/systemui/recents/Recents.java
@@ -23,10 +23,12 @@ import static com.android.systemui.statusbar.phone.StatusBar.SYSTEM_DIALOG_REASO
 
 import android.app.ActivityManager;
 import android.app.trust.TrustManager;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
@@ -34,6 +36,7 @@ import android.content.res.Resources;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.hardware.display.DisplayManager;
+import android.net.Uri;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.RemoteException;
@@ -46,6 +49,7 @@ import android.widget.Toast;
 
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.aospextended.AEXUtils;
 import com.android.systemui.Dependency;
 import com.android.systemui.EventLogConstants;
 import com.android.systemui.EventLogTags;
@@ -175,6 +179,26 @@ public class Recents extends SystemUI
         }
     };
 
+    // Broadcast receiver for RecentsIconPack
+    private final BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if(action.equals(Intent.ACTION_PACKAGE_REMOVED)) {
+                // Get packageName from Uri
+                String packageName = intent.getData().getSchemeSpecificPart();
+                // Get iconPack currently set as default
+                String currentIconPack = Settings.System.getString/*ForUser*/(mContext.getContentResolver(),
+                Settings.System.RECENTS_ICON_PACK/*, UserHandle.myUserId()*/);
+                // If the uninstalled package equals to our currently set iconPack
+                if(packageName.equals(currentIconPack)) {
+                    // Assert the iconPack to be strongly sure that the package got uninstalled
+                    assertIconPack(packageName);
+                }
+            }
+        }
+    };
+
     /**
      * Returns the callbacks interface that non-system users can call.
      */
@@ -241,6 +265,13 @@ public class Recents extends SystemUI
         putComponent(Recents.class, this);
 
         mTrustManager = (TrustManager) mContext.getSystemService(Context.TRUST_SERVICE);
+
+        // Intent for applications that get uninstalled
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_PACKAGE_REMOVED);
+        filter.addDataScheme("package");
+        // Register our BroadcastReceiver
+        mContext.registerReceiver(mBroadcastReceiver, filter);
     }
 
     @Override
@@ -890,5 +921,19 @@ public class Recents extends SystemUI
     public void dump(FileDescriptor fd, PrintWriter pw, String[] args) {
         pw.println("Recents");
         pw.println("  currentUserId=" + SystemServicesProxy.getInstance(mContext).getCurrentUser());
+    }
+
+    /**
+     * This method checks whether a passed iconPack is still available, if it's not
+     * the system will fall back to the android default icon pack.
+     */
+    private void assertIconPack(String packageName) {
+        // We need to verify whether the iconPack is still installed
+        if(!AEXUtils.isPackageInstalled(mContext, packageName)) {
+            // The icon pack is not available anymore, let's fallback to the default iconPack
+            Settings.System.putString(mContext.getContentResolver(),
+                    Settings.System.RECENTS_ICON_PACK, /* defaultIconPack */ "");
+            // Quickstep settings observer will now refresh the icon pack
+        }
     }
 }