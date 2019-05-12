662521a0827b29104be84b2da6fa36dfb07e9d31
paphonb <paphonb@gmail.com>
2019-05-12 16:25:36 +0000

    Q Style Clock: Make it fully translatable
     * reduce font size to allow for longer words from other languages
    
     * italian translations included for testing
    
    Change-Id: Ied91fc8f364d4fc8ed897a3c801e3b8576e11925

---
+++ packages/SystemUI/src/com/android/keyguard/clocks/TypographicClock.java
@@ -0,0 +1,136 @@
+package com.android.keyguard.clocks;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.text.Annotation;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.SpannedString;
+import android.text.TextUtils;
+import android.text.format.DateFormat;
+import android.text.style.ForegroundColorSpan;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import com.android.keyguard.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+public class TypographicClock extends TextView {
+
+    private Resources mResources;
+    private String[] mHours;
+    private String[] mMinutes;
+    private String mDescFormat;
+    private TimeZone mTimeZone;
+    private int mAccentColor;
+    private final Calendar mTime;
+
+    private final BroadcastReceiver mTimeZoneChangedReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_TIMEZONE_CHANGED)) {
+                String tz = intent.getStringExtra("time-zone");
+                onTimeZoneChanged(TimeZone.getTimeZone(tz));
+                onTimeChanged();
+            } else if (intent.getAction().equals(Intent.ACTION_LOCALE_CHANGED)) {
+                mResources = context.getResources();
+                mHours = mResources.getStringArray(R.array.text_clock_hours_array);
+                mMinutes = mResources.getStringArray(R.array.text_clock_minutes_array);
+                mDescFormat = ((SimpleDateFormat) DateFormat.getTimeFormat(context)).toLocalizedPattern();
+                onTimeChanged();
+            }
+        }
+    };
+
+    public TypographicClock(Context context) {
+        this(context, null);
+    }
+
+    public TypographicClock(Context context, AttributeSet attributeSet) {
+        this(context, attributeSet, 0);
+    }
+
+    public TypographicClock(Context context, AttributeSet attributeSet, int defStyleAttr) {
+        super(context, attributeSet, defStyleAttr);
+
+        mTime = Calendar.getInstance(TimeZone.getDefault());
+        mDescFormat = ((SimpleDateFormat) DateFormat.getTimeFormat(context)).toLocalizedPattern();
+        mResources = context.getResources();
+        mHours = mResources.getStringArray(R.array.text_clock_hours_array);
+        mMinutes = mResources.getStringArray(R.array.text_clock_minutes_array);
+        mAccentColor = mResources.getColor(R.color.custom_text_clock_top_color, null);
+    }
+
+    public void onTimeChanged() {
+        mTime.setTimeInMillis(System.currentTimeMillis());
+        setContentDescription(DateFormat.format(mDescFormat, mTime));
+        boolean h24 = DateFormat.is24HourFormat(getContext());
+        int hours = mTime.get(Calendar.HOUR_OF_DAY) % (h24 ? 24 : 12);
+        int minutes = mTime.get(Calendar.MINUTE) % 60;
+        String header = mResources.getQuantityString(R.plurals.text_clock_header, hours);
+
+        // 12 hours format correction
+        if(hours == 0 && !h24)
+            hours = 12;
+
+        SpannedString rawFormat = (SpannedString) mResources.getText(R.string.text_clock_string_format);
+        Annotation[] annotationArr = rawFormat.getSpans(0, rawFormat.length(), Annotation.class);
+        SpannableString stringFormat = new SpannableString(rawFormat);
+
+        // Set header color
+        for (Annotation annotation : annotationArr) {
+            if ("header_color".equals(annotation.getValue())) {
+                stringFormat.setSpan(new ForegroundColorSpan(mAccentColor),
+                        stringFormat.getSpanStart(annotation),
+                        stringFormat.getSpanEnd(annotation),
+                        Spanned.SPAN_POINT_POINT);
+            }
+        }
+
+        setText(TextUtils.expandTemplate(stringFormat, new CharSequence[]{header, mHours[hours], mMinutes[minutes]}));
+    }
+
+    public void onTimeZoneChanged(TimeZone timeZone) {
+        mTimeZone = timeZone;
+        mTime.setTimeZone(timeZone);
+    }
+
+    public void updateHeaderColor() {
+        mAccentColor = mResources.getColor(R.color.custom_text_clock_top_color, null);
+        onTimeChanged();
+    }
+
+    @Override
+    public boolean hasOverlappingRendering() {
+        return false;
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Calendar calendar = mTime;
+        TimeZone timeZone = mTimeZone;
+        if (timeZone == null) {
+            timeZone = TimeZone.getDefault();
+        }
+        calendar.setTimeZone(timeZone);
+        onTimeChanged();
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
+        filter.addAction(Intent.ACTION_LOCALE_CHANGED);
+        getContext().registerReceiver(mTimeZoneChangedReceiver, filter);
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        getContext().unregisterReceiver(mTimeZoneChangedReceiver);
+    }
+}
