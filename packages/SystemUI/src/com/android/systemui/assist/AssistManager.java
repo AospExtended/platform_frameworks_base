f3e2f1ce62e3d31119db6a7d388fca8e70facb3d
argraur <argraur@gmail.com>
2019-05-12 16:40:31 +0000

    SystemUI: assist: disable old Assistant animations
    * Disable Assistant Disclosure animation
    * Disable Assistant Orb
    
    Signed-off-by: argraur <argraur@gmail.com>
    Change-Id: Iedd354e056adff34646c02f8048717cf60b841a5
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- packages/SystemUI/src/com/android/systemui/assist/AssistManager.java
+++ packages/SystemUI/src/com/android/systemui/assist/AssistManager.java
@@ -307,10 +307,6 @@ public class AssistManager {
         intent.setComponent(assistComponent);
         intent.putExtras(args);
 
-        if (structureEnabled && AssistUtils.isDisclosureEnabled(mContext)) {
-            showDisclosure();
-        }
-
         try {
             final ActivityOptions opts = ActivityOptions.makeCustomAnimation(mContext,
                     R.anim.search_launch_enter, R.anim.search_launch_exit);
@@ -359,9 +355,7 @@ public class AssistManager {
         return getAssistInfoForUser(KeyguardUpdateMonitor.getCurrentUser());
     }
 
-    public void showDisclosure() {
-        mAssistDisclosure.postShow();
-    }
+    public void showDisclosure() {}
 
     public void onLockscreenShown() {
         AsyncTask.execute(new Runnable() {
