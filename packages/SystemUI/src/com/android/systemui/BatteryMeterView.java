147accbd04dc72622f8f4358e5c8529f2989d444
apophis9283 <steaven.android@gmail.com>
2019-05-14 22:08:36 -0400

    Improve Q battery icon and unify with portrait style
    Change-Id: I69c13464f2e9c69260a600d51f501c575b9b6c32
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -181,7 +181,11 @@ public class BatteryMeterView extends LinearLayout implements
      *                                    components
      */
     public void useWallpaperTextColor(boolean shouldUseWallpaperTextColor) {
-        if (shouldUseWallpaperTextColor == mUseWallpaperTextColors) {
+        useWallpaperTextColor(shouldUseWallpaperTextColor, false);
+    }
+
+    public void useWallpaperTextColor(boolean shouldUseWallpaperTextColor, boolean force) {
+        if (!force && shouldUseWallpaperTextColor == mUseWallpaperTextColors) {
             return;
         }
 
@@ -468,7 +472,10 @@ public class BatteryMeterView extends LinearLayout implements
 
         final int style = styleStr == null ?
                 BatteryMeterDrawableBase.BATTERY_STYLE_PORTRAIT : Integer.parseInt(styleStr);
+
         mStyle = style;
+        mDrawable.setMeterStyle(style);
+        useWallpaperTextColor(mUseWallpaperTextColors, true);
 
         switch (style) {
             case BatteryMeterDrawableBase.BATTERY_STYLE_TEXT:
