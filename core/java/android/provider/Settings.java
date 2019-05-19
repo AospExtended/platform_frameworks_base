d584404ff6a356343256e4937a1fdec2d884040f
Rabih Salamey <somcom3@gmail.com>
2019-05-19 03:40:03 -0400

    Battery light: Add customization [1/2]
    abc ezio84: allow to
     disable battery light when Do Not Disturb is enabled use our margaritov color
     picker for the settings part
    
    -----
    
    https://review.lineageos.org/#/c/68217/
    
    Change-Id: I15cc7de12816535008227a6dd3f210ada54ffa46

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4718,6 +4718,49 @@ public final class Settings {
         public static final String HOME_WAKE_BUTTON = "home_wake_button";
 
         /**
+         * Whether the battery light should be enabled (if hardware supports it)
+         * The value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_ENABLED = "battery_light_enabled";
+
+        /**
+        * Whether to show battery light when DND mode is active
+        * @hide
+        */
+       public static final String BATTERY_LIGHT_ALLOW_ON_DND = "battery_light_allow_on_dnd";
+
+        /**
+        * Whether to show blinking light when battery is low
+        * @hide
+        */
+       public static final String BATTERY_LIGHT_LOW_BLINKING = "battery_light_low_blinking";
+
+        /**
+        * Low battery charging color
+        * @hide
+        */
+       public static final String BATTERY_LIGHT_LOW_COLOR = "battery_light_low_color";
+
+        /**
+        * Medium battery charging color
+        * @hide
+        */
+       public static final String BATTERY_LIGHT_MEDIUM_COLOR = "battery_light_medium_color";
+
+        /**
+        * Full battery charging color
+        * @hide
+        */
+       public static final String BATTERY_LIGHT_FULL_COLOR = "battery_light_full_color";
+
+        /**
+        * Really full 100 battery charging color
+        * @hide
+        */
+       public static final String BATTERY_LIGHT_REALLYFULL_COLOR = "battery_light_reallyfull_color";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
