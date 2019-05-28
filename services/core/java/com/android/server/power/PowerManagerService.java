c330bdc0e5218d36e78ccb69c3c9626fa30ba882
Anushek Prasal <anushekprasal@gmail.com>
2019-05-29 01:26:35 +0530

    base: Use wireless charging animation for wired charging too
    Change-Id: I67d76a1a02d83017bb7d01d34138a7e7a2242108
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -2091,7 +2091,7 @@ public final class PowerManagerService extends SystemService
                 if (mBootCompleted) {
                     if (mIsPowered && !BatteryManager.isPlugWired(oldPlugType)
                             && BatteryManager.isPlugWired(mPlugType)) {
-                        mNotifier.onWiredChargingStarted(mUserId);
+                        mNotifier.onWiredChargingStarted(mUserId, mBatteryLevel);
                     } else if (dockedOnWirelessCharger) {
                         mNotifier.onWirelessChargingStarted(mBatteryLevel, mUserId);
                     }
