b5fdda1c55ade9a1c834d654523070684f24819b
Anushek Prasal <anushekprasal@gmail.com>
2019-06-06 14:18:24 +0530

    base: Add toggle to disable wired charging animation [1/2]
    Change-Id: I8982623ce5d231d1a29dfa934179214a6745faae
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5870,6 +5870,12 @@ public final class Settings {
         public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
 
         /**
+         * Whether to show charging animation
+         * @hide
+         */
+        public static final String CHARGING_ANIMATION = "charging_animation";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
