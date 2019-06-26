1c98b9ad6dc455d06b6de392119eaa40ae2cb01a
DennySPB <dennyspb@gmail.com>
2019-06-26 10:37:44 +0300

    SmartCharging: add reset battery stats option
    When charging level reach user defined level allow to reset battery stats
    
    Change-Id: I5ee7ac164d7db2cb39544b671ce167fff378c647

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5787,6 +5787,11 @@ public final class Settings {
         /**
          * @hide
          */
+        public static final String SMART_CHARGING_RESET_STATS = "smart_charging_reset_stats";
+
+        /**
+         * @hide
+         */
         public static final String SMART_CHARGING_LEVEL = "smart_charging_level";
 
         /**
