99e1148da5ca8cb677227f263728f4797ebc4cce
Gaurav Singh <allstargaurav@gmail.com>
2019-07-10 16:01:02 +0200

    Revert "SystemUI: Add LiveDisplay tile"
    This reverts commit 2d9c3c9143d1dfce6027e577e6bddc624e58bc3d.
    
    Change-Id: Idbe9331a298d16f2a4f6dc3d43329a844247c980

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -45,7 +45,6 @@ import com.android.systemui.qs.tiles.FontTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.HWKeysTile;
 import com.android.systemui.qs.tiles.IntentTile;
-import com.android.systemui.qs.tiles.LiveDisplayTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.LteTile;
 import com.android.systemui.qs.tiles.OneHandTile;
@@ -165,8 +164,6 @@ public class QSFactoryImpl implements QSFactory {
                 return new AmbientDisplayTile(mHost);
             case "volume_panel":
                 return new VolumeTile(mHost);
-            case "livedisplay":
-                return new LiveDisplayTile(mHost);
             case "reading_mode":
                 return new ReadingModeTile(mHost);
             case "font":
