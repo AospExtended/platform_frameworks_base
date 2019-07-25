286ad437f7ded682832f9eadaac525daa702530b
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-07-26 01:23:52 +0200

    Settings: Don't show system overlays on apps list
    Change-Id: Ibfc55e3d201948b2854187fa30e7059f044d37ef
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

---
+++ core/java/com/android/internal/util/custom/OverlayUtils.java
@@ -0,0 +1,95 @@
+/*
+* Copyright (C) 2017 The Pixel Experience Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.android.internal.util.custom;
+
+public class OverlayUtils {
+    public static String[] AllPackages = {"com.accents.red",
+        "com.accents.pink",
+        "com.accents.purple",
+        "com.accents.deeppurple",
+        "com.accents.indigo",
+        "com.accents.blue",
+        "com.accents.lightblue",
+        "com.accents.cyan",
+        "com.accents.teal",
+        "com.accents.green",
+        "com.accents.lightgreen",
+        "com.accents.lime",
+        "com.accents.yellow",
+        "com.accents.amber",
+        "com.accents.orange",
+        "com.accents.deeporange",
+        "com.accents.brown",
+        "com.accents.grey",
+        "com.accents.bluegrey",
+        "com.accents.candyred",
+        "com.accents.palered",
+        "com.accents.extendedgreen",
+        "com.accents.paleblue",
+        "com.accents.jadegreen",
+        "com.accents.elegantgreen",
+        "com.android.system.theme.dark",
+        "com.android.settings.theme.dark",
+        "com.android.systemui.custom.theme.dark",
+        "com.android.dialer.theme.dark",
+        "com.android.contacts.theme.dark",
+        "com.android.documentsui.theme.dark",
+        "com.google.android.apps.wellbeing.theme.dark",
+        "com.aospextended.ota.theme.dark",
+        "com.android.system.theme.black",
+        "com.android.settings.theme.black",
+        "com.android.systemui.theme.black",
+        "com.android.dialer.theme.black",
+        "com.android.contacts.theme.black",
+        "com.android.documentsui.theme.black",
+        "com.google.android.apps.wellbeing.theme.black",
+        "com.aospextended.ota.theme.black",
+        "com.android.system.theme.extended",
+        "com.android.settings.theme.extended",
+        "com.android.systemui.theme.extended",
+        "com.accents.extendedgreen",
+        "com.android.dialer.theme.extended",
+        "com.android.contacts.theme.extended",
+        "com.android.documentsui.theme.extended",
+        "com.google.android.apps.wellbeing.theme.extended",
+        "com.aospextended.ota.theme.extended",
+        "com.android.system.theme.chocolate",
+        "com.android.settings.theme.chocolate",
+        "com.android.systemui.theme.chocolate",
+        "com.accents.candyred",
+        "com.android.dialer.theme.chocolate",
+        "com.android.contacts.theme.chocolate",
+        "com.android.documentsui.theme.chocolate",
+        "com.google.android.apps.wellbeing.theme.chocolate",
+        "com.aospextended.ota.theme.chocolate",
+        "com.android.system.theme.elegant",
+        "com.android.settings.theme.elegant",
+        "com.android.systemui.theme.elegant",
+        "com.accents.elegantgreen",
+        "com.android.dialer.theme.elegant",
+        "com.android.contacts.theme.elegant",
+        "com.android.documentsui.theme.elegant",
+        "com.google.android.apps.wellbeing.theme.elegant",
+	"com.google.android.apps.gms.theme.elegant",
+        "com.aospextended.ota.theme.elegant",
+        "com.android.systemui.qstile.square",
+        "com.android.systemui.qstile.roundedsquare",
+        "com.android.systemui.qstile.squircle",
+        "com.android.systemui.qstile.teardrop",
+        "com.android.systemui.qstile.circlegradient",
+        "com.android.systemui.qstile.circleoutline",
+        "com.android.systemui.qstile.justicons"};
+}
--- packages/SettingsLib/src/com/android/settingslib/applications/ApplicationsState.java
+++ packages/SettingsLib/src/com/android/settingslib/applications/ApplicationsState.java
@@ -67,6 +67,7 @@ import java.lang.ref.WeakReference;
 import java.text.Collator;
 import java.text.Normalizer;
 import java.text.Normalizer.Form;
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -75,6 +76,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 import java.util.regex.Pattern;
+import com.android.internal.util.custom.OverlayUtils;
 
 /**
  * Keeps track of information about all installed applications, lazy-loading
@@ -1671,7 +1673,7 @@ public class ApplicationsState {
 
         @Override
         public boolean filterApp(AppEntry entry) {
-            return true;
+            return !Arrays.asList(OverlayUtils.AllPackages).contains(entry.info.packageName);
         }
     };
 
