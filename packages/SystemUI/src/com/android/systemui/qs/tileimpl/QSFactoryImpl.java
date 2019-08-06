6b7e14f7e9e0b7a7f8f797d73f5d90c720ae0a83
Gaurav Singh <allstargaurav@gmail.com>
2019-08-06 16:15:55 +0200

    Revert "Add Music QS Tile"
    This reverts commit 82f73d09ccde06dffd15215f0b6fa8b66f51e524.
    
    Change-Id: I9ba5ae6a4c88af1a23889e9510d7ea0d842a4fe7

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -50,7 +50,6 @@ import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.LteTile;
 import com.android.systemui.qs.tiles.OneHandTile;
 import com.android.systemui.qs.tiles.NfcTile;
-import com.android.systemui.qs.tiles.MusicTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.ReadingModeTile;
@@ -158,8 +157,6 @@ public class QSFactoryImpl implements QSFactory {
                 return new SoundSearchTile(mHost);
             case "always_on_display":
                 return new AlwaysOnDisplayTile(mHost);
-            case "music":
-                return new MusicTile(mHost);
             case "sync":
                 return new SyncTile(mHost);
             case "ambient_display":
