487529323182e222052232e4f9ec5008d7e4c7c9
LuK1337 <priv.luk@gmail.com>
2019-09-10 22:25:06 +0200

    biometrics: Allow posting reset runnable for all clients
     * After commit df755c8, some devices fail to enumerate for
       unknown reason (likely a HAL issue). Add an overlay to
       restore old behavior and thus workaround this issue.
    
    [@sb6596: This also reverts commit e30727e39e2ca8c73b687e4afaace236c42937b1]
    
    Change-Id: Ib0d00d5987aa7f68a5c7efa785859e8eb208651d
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- services/core/java/com/android/server/biometrics/BiometricServiceBase.java
+++ services/core/java/com/android/server/biometrics/BiometricServiceBase.java
@@ -88,6 +88,7 @@ public abstract class BiometricServiceBase extends SystemService
     private final PowerManager mPowerManager;
     private final UserManager mUserManager;
     private final MetricsLogger mMetricsLogger;
+    private final boolean mPostResetRunnableForAllClients;
     private final BiometricTaskStackListener mTaskStackListener = new BiometricTaskStackListener();
     private final ResetClientStateRunnable mResetClientState = new ResetClientStateRunnable();
     private final ArrayList<LockoutResetMonitor> mLockoutMonitors = new ArrayList<>();
@@ -667,6 +668,8 @@ public abstract class BiometricServiceBase extends SystemService
                 com.android.internal.R.bool.config_notifyClientOnFingerprintCancelSuccess);
         mCleanupUnusedFingerprints = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_cleanupUnusedFingerprints);
+        mPostResetRunnableForAllClients = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_fingerprintPostResetRunnableForAllClients);
     }
 
     @Override
@@ -1108,6 +1111,10 @@ public abstract class BiometricServiceBase extends SystemService
                             + newClient.getClass().getSuperclass().getSimpleName()
                             + "(" + newClient.getOwnerString() + ")"
                             + ", initiatedByClient = " + initiatedByClient);
+                }
+                if (mPostResetRunnableForAllClients) {
+                    mHandler.removeCallbacks(mResetClientState);
+                    mHandler.postDelayed(mResetClientState, CANCEL_TIMEOUT_LIMIT);
                 }
             } else {
                 currentClient.stop(initiatedByClient);
