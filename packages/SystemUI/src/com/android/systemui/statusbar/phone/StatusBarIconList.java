e38fecf92acb2160baab46d613636f5dede7c381
Pranav Vashi <neobuddy89@gmail.com>
2019-10-13 20:10:38 +0530

    Fix status bar for network traffic icon holder
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: blinoff82 <blinov.in@gmail.com>
    Change-Id: I55ed1cdc2a41a11c552e21cc7977fb3f88af819e
    Signed-off-by: blinoff82 <blinov.in@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconList.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconList.java
@@ -34,7 +34,7 @@ public class StatusBarIconList {
 
     public StatusBarIconList(String[] slots) {
         final int N = slots.length;
-        for (int i=0; i < N; i++) {
+        for (int i=1; i < N; i++) {
             mSlots.add(new Slot(slots[i], null));
         }
         // Network traffic slot
@@ -49,8 +49,8 @@ public class StatusBarIconList {
                 return i;
             }
         }
-        // Auto insert new items behind network traffic
-        mSlots.add(1, new Slot(slot, null));
+        // Auto insert new items at the beginning.
+        mSlots.add(0, new Slot(slot, null));
         return 0;
     }
 
