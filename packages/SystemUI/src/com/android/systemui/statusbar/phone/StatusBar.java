4a5049a60882e7d3988ef96cc3ba86eff32ced51
Dil3mm4 <dil3mm4.dev@gmail.com>
2019-10-19 03:55:08 -0400

    Keyguard: Lock icon improvements
    There was a ~300ms delay (I think) between the actual dismissal of the lock icon from keyguard locked to unlocked
    (only with no security method activated). So, mKeyguardIndicationController.setVisible(false) has been moved to
    KeyguardgoingAway() to anticipate the icon dismissal with a small setAlpha hack in KeyguardIndicationController.
    
    Pulse: Don't show the icon here, it's stupid.
    
    Change-Id: Ia926cd27e96bb450ed8f5ed9c686556fd647fa45
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -3409,6 +3409,7 @@ public class StatusBar extends SystemUI implements DemoMode,
     public void keyguardGoingAway() {
         // Treat Keyguard exit animation as an app transition to achieve nice transition for status
         // bar.
+        mKeyguardIndicationController.setVisible(false);
         mKeyguardStateController.notifyKeyguardGoingAway(true);
         mCommandQueue.appTransitionPending(mDisplayId, true /* forced */);
     }
@@ -3587,7 +3588,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                 mAmbientIndicationContainer.setVisibility(View.VISIBLE);
             }
         } else {
-            mKeyguardIndicationController.setVisible(false);
             if (mKeyguardUserSwitcher != null) {
                 mKeyguardUserSwitcher.setKeyguard(false,
                         mStatusBarStateController.goingToFullShade() ||
