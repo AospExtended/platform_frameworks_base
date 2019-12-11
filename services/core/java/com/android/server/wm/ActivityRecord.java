ed6ed0cf1fe8ea705328575fa343e093583c9529
DennySPB <dennyspb@gmail.com>
2019-12-11 16:43:26 +0300

    ActivityRecord: add some missing bits after fwboost merge
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: Ia6ae2ed54cbb4ca084dd2deecbbd0badb51ab3f4
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -235,8 +235,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 
-import android.os.AsyncTask;
-
 /**
  * An entry in the history stack, representing an activity.
  */
@@ -2222,8 +2220,12 @@ public final class ActivityRecord extends ConfigurationContainer {
 
         if (isActivityTypeHome()) {
             mStackSupervisor.updateHomeProcess(task.mActivities.get(0).app);
+            try {
+                mStackSupervisor.new PreferredAppsTask().execute();
+            } catch (Exception e) {
+                Slog.v (TAG, "Exception: " + e);
+            }
         }
-
         if (nowVisible) {
             mStackSupervisor.stopWaitingForActivityVisible(this);
         }
