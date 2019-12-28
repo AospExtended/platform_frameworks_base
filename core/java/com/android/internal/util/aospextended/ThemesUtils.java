cc3f4726b7aec97949ffb5efafbb006c5763baea
Alex Cruz <du.alexcruz@gmail.com>
2019-12-28 17:12:25 -0500

    Fruity Pebbles 3.0
    - Update to Android 10, made to work with new accents (without SettingsProvider)
    - Update accent drawable
    - Give styles its own icon
    - Give styles options their own icon as well
    - Simpify some of the code while we're at it
    - Change strings, old ones didn't make much sense
    
    Change-Id: Ide8b95ee139b71f897cb56c0ebaaffeafc36b073

---
+++ core/java/com/android/internal/util/aospextended/ThemesUtils.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.aospextended;
+
+public class ThemesUtils {
+
+    public static final String[] ACCENTS = {
+            // AOSP colors
+            "com.android.theme.color.space", // Space
+            "com.android.theme.color.purple", // Purple
+            "com.android.theme.color.orchid", // Orchid
+            "com.android.theme.color.ocean", // Ocean
+            "com.android.theme.color.green", // Green
+            "com.android.theme.color.cinnamon", // Cinnamon
+            // Custom colors
+            "com.android.theme.color.amber", // Amber
+            "com.android.theme.color.blue", // Blue
+            "com.android.theme.color.bluegrey", // Blue Grey
+            "com.android.theme.color.brown", // Brown
+            "com.android.theme.color.cyan", // Cyan
+            "com.android.theme.color.deeporange", // Deep Orange
+            "com.android.theme.color.deeppurple", // Deep Purple
+            "com.android.theme.color.grey", // Grey
+            "com.android.theme.color.indigo", // Indigo
+            "com.android.theme.color.lightblue", // Light Blue
+            "com.android.theme.color.lightgreen", // Light Green
+            "com.android.theme.color.lime", // Lime
+            "com.android.theme.color.orange", // Orange
+            "com.android.theme.color.pink", // Pink
+            "com.android.theme.color.red", // Red
+            "com.android.theme.color.teal", // Teal
+            "com.android.theme.color.yellow", // Yellow
+            "com.android.theme.color.extendedgreen", // Extended Green
+            "com.android.theme.color.elegantgreen", // Elegant Green
+    };
+}
