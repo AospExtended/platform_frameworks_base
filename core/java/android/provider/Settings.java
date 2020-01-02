0c996890c227e149a3549eea406d82566961e2be
Anushek Prasal <anushekprasal@gmail.com>
2020-01-03 00:18:59 +0530

    base: Add Touch HIDL support
    All credits to LineageOS and related authors on:
    https://github.com/LineageOS/android_lineage-sdk
    
    Ported to AOSP from lineage-sdk by SKULSHADY
    
    [ anirudhgupta109 | AOSiP ]
     - Minimal import of LineageHardwareManager and HIDLHelper
     - We don't want LiveDisplay stuff
     - Nuke Legacy touchscreen support
    
    Change-Id: Ic8ca90663582c06f35562286f5ce5b2d84971ec4
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: ShubhamB <shubhamprince111@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5171,6 +5171,12 @@ public final class Settings {
         public static final String INCREASING_RING_RAMP_UP_TIME = "increasing_ring_ramp_up_time";
 
         /**
+         * Whether or not to vibrate when a touchscreen gesture is detected
+         * @hide
+         */
+        public static final String TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK = "touchscreen_gesture_haptic_feedback";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
