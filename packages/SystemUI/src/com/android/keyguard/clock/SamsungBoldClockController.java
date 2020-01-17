319ca7757b2a697828ce63ad4b5a59a770ff3b4d
spezi77 <spezi7713@gmx.net>
2020-01-17 09:17:55 +0100

    fwb: cLock: User toggle to show/hide status area [1/2]
    Other part is in DU-Themes
    
    Some custom clocks look cleaner w/o additional status line for weather information etc.
    So give the user the choice.
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I4901d998ced8d53df0f0144c1d77ad8aa082cb65

--- packages/SystemUI/src/com/android/keyguard/clock/SamsungBoldClockController.java
+++ packages/SystemUI/src/com/android/keyguard/clock/SamsungBoldClockController.java
@@ -16,12 +16,14 @@
 package com.android.keyguard.clock;
 
 import android.app.WallpaperManager;
+import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.Paint.Style;
 import android.text.Html;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextClock;
@@ -71,6 +73,8 @@ public class SamsungBoldClockController implements ClockPlugin {
      */
     private TextClock mClock;
 
+    private final Context mContext;
+
     /**
      * Create a DefaultClockController instance.
      *
@@ -80,9 +84,23 @@ public class SamsungBoldClockController implements ClockPlugin {
      */
     public SamsungBoldClockController(Resources res, LayoutInflater inflater,
             SysuiColorExtractor colorExtractor) {
+        this(res, inflater, colorExtractor, null);
+    }
+
+    /**
+     * Create a DefaultClockController instance.
+     *
+     * @param res Resources contains title and thumbnail.
+     * @param inflater Inflater used to inflate custom clock views.
+     * @param colorExtractor Extracts accent color from wallpaper.
+     * @param context A context.
+     */
+    public SamsungBoldClockController(Resources res, LayoutInflater inflater,
+            SysuiColorExtractor colorExtractor, Context context) {
         mResources = res;
         mLayoutInflater = inflater;
         mColorExtractor = colorExtractor;
+        mContext = context;
     }
 
     private void createViews() {
@@ -181,6 +199,7 @@ public class SamsungBoldClockController implements ClockPlugin {
 
     @Override
     public boolean shouldShowStatusArea() {
-        return true;
+        if (mContext == null) return true;
+        return Settings.System.getInt(mContext.getContentResolver(), Settings.System.CLOCK_SHOW_STATUS_AREA, 1) == 1;
     }
 }
