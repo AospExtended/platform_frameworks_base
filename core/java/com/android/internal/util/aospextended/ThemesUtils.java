9e3b28323c81c6a851fafca8f8d0f8932bd946d4
Mazda-- <alex@dirtyunicorns.com>
2020-01-20 21:24:30 +0530

    Themes: Add More Systme Themes [1/3]
    Thanks to bigrushdog <randall.rushing@gmail.com> for helping me figure out
    how to apply different actions based on the item selected, really HUGE help!
    
    Change-Id: I8a5cd9692d4ff33d5ebe6cd726eebdff2b2d217b

--- core/java/com/android/internal/util/aospextended/ThemesUtils.java
+++ core/java/com/android/internal/util/aospextended/ThemesUtils.java
@@ -45,4 +45,14 @@ public class ThemesUtils {
             "com.android.theme.color.teal", // Teal
             "com.android.theme.color.yellow", // Yellow
     };
+
+    public static final String[] PITCH_BLACK = {
+            "com.android.theme.pitchblack.system",
+            "com.android.theme.pitchblack.systemui",
+    };
+
+    public static final String[] EXTENDED_UI = {
+            "com.android.theme.extendedui.system",
+            "com.android.theme.extendedui.systemui",
+    };
 }
