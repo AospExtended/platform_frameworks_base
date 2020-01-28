bc8f6513abe2d2de5642baf931c4bfa48ddd59c1
Marko Man <darkobas@gmail.com>
2020-01-29 00:24:33 +0100

    base: Pulse light accent color option
    Change-Id: If402e96d7549aff7f758236e6f54a6034d08f9c2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5843,6 +5843,12 @@ public final class Settings {
         public static final String AMBIENT_NOTIFICATION_LIGHT = "ambient_notification_light";
 
         /**
+         * Whether to use accent color for pulse
+         * @hide
+         */
+        public static final String AMBIENT_NOTIFICATION_LIGHT_ACCENT = "ambient_notification_light_accent";
+
+        /**
          * Whether Ambient Edge lights are activated
          * @hide
          */
