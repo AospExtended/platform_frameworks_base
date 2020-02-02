f7914e7afb615fd9b0f1a6fd025c579854ac0d95
ezio84 <brabus84@gmail.com>
2020-02-02 22:32:16 +0530

    Less boring heads up option: always show alarm clock headsup
    Change-Id: Iea1f6c7a131689eed3d6bee8b28bebd91a56c412
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/NotificationInterruptionStateProvider.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/NotificationInterruptionStateProvider.java
@@ -366,7 +366,8 @@ public class NotificationInterruptionStateProvider {
         boolean isImportantHeadsUp = false;
         String notificationPackageName = sbn.getPackageName().toLowerCase();
         isImportantHeadsUp = notificationPackageName.contains("dialer") ||
-                notificationPackageName.contains("messaging");
+                notificationPackageName.contains("messaging") ||
+                notificationPackageName.contains("clock");
         return !Dependency.get(ShadeController.class).isDozing() && mLessBoringHeadsUp && !isImportantHeadsUp;
     }
 
