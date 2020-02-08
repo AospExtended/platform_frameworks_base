85187b6064817d744d11698a5b135c3de5071664
Joey Huab <joey@evolution-x.org>
2020-02-08 15:40:28 +0000

    SystemUI: Recognizing FP animation effects [1/2]
    * Add the rest of the icons from MIUI
    * Includes the following animations:
    - DNA
    - Future
    - Halo Ring
    - Molecular
    - Cosmos
    - McLaren
    - Stripe
    - Wave
    
        FODCircleView: don't require systemui restart to toggle recognizing animations
    
    Co-authored-by: 00day0 <therandomuser11@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Change-Id: I86cbb0b2c4f48d76bcfb0d876d3399ea68e97eb9
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4666,6 +4666,12 @@ public final class Settings {
         public static final String FOD_RECOGNIZING_ANIMATION = "fod_recognizing_animation";
 
         /**
+         * FOD recognizing animation picker
+         * @hide
+         */
+        public static final String FOD_ANIM = "fod_recognizing_animation_list";
+
+        /**
          * Show pointer location on screen?
          * 0 = no
          * 1 = yes
