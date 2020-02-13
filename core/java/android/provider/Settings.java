379ddf1663352bc882ebead0c0ed4886ec9a1dab
maxwen <max.weninger@gmail.com>
2020-02-13 02:47:09 +0100

    base: Support using notification color for pulse light
    optional use the color information of the notification
    as color for notification pulse light
    
    Change-Id: Icf7c24f8c695f29485123c2c941b08bbdabbbbdf

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5855,6 +5855,11 @@ public final class Settings {
         public static final String NOTIFICATION_PULSE_COLOR = "ambient_notification_light_color";
 
         /**
+         * @hide
+         */
+        public static final String NOTIFICATION_PULSE_COLOR_AUTOMATIC = "ambient_notification_light_color_automatic";
+
+        /**
          * Whether Ambient Edge lights are activated
          * @hide
          */
