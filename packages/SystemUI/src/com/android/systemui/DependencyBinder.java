e592d194198a09af112636bff292c46b18e4eb3f
maxwen <max.weninger@gmail.com>
2020-02-20 13:35:52 -0500

    Navbar: Add CustomSettingsService with dagger injection
    easy way to add observer to custom settings - in Settings.System ONLY
    like TunerService
    
    no more need to create a dedicated ContentObserver simply use
    
     Dependency.get(CustomSettingsService.class).addIntObserver(this, Settings.System.WHATEVER_YOU_WANT);
    
    and add CustomSettingsObserver interface
    as POC change the old style mobile data icon to use it
    
    Change-Id: I2ad9c8dc70913319e405ae71bae30049c6c2a649

--- packages/SystemUI/src/com/android/systemui/DependencyBinder.java
+++ packages/SystemUI/src/com/android/systemui/DependencyBinder.java
@@ -16,6 +16,8 @@
 
 package com.android.systemui;
 
+import com.android.systemui.CustomSettingsService;
+import com.android.systemui.CustomSettingsServiceImpl;
 import com.android.systemui.appops.AppOpsController;
 import com.android.systemui.appops.AppOpsControllerImpl;
 import com.android.systemui.classifier.FalsingManagerProxy;
@@ -241,4 +243,9 @@ public abstract class DependencyBinder {
      */
     @Binds
     public abstract FalsingManager provideFalsingmanager(FalsingManagerProxy falsingManagerImpl);
+
+    /**
+     */
+    @Binds
+    public abstract CustomSettingsService provideCustomSettingsService(CustomSettingsServiceImpl controllerImpl);
 }
