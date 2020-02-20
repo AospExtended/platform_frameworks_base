96e9ef15911e498f3a3fb60db411dad1298af79d
Bruno Martins <bgcngm@gmail.com>
2020-02-19 23:39:16 -0500

    Navbar: Add support for runtime toggle
    This is a refactor of the following changes, so to make use of tuner API.
    
    Author: Timo Wendt <timo@tjwendt.de>
    Date:   Thu Aug 30 12:18:41 2012 +0300
    
        Runtime toggle of navbar
    
        This adds the framework support for enabling the Navigation bar
        on devices with hardware keys. It is toggled from Settings, and
        depends on device-specific support for the KeyDisabler hardware
        control
    
        Change-Id: I88fecb2ca1e8613591c327a93f53909b00239cd8
    
        wm: Nullify hardkey function assignments if enabling the navbar
    
        This caused erroneous (and sometimes duplicate) events being generated
        due to the regular key function assignments. The navbar does its
        own action management, so don't try to derive from the actions
        usually present in hard keys.
    
        Change-Id: I82866e24547f8145cac4f07820ae90aacce09281
    
        Update DEV_FORCE_SHOW_NAVBAR constant.
    
        Change-Id: Ie5b4317162c514d22276956f81007e064a3d0f32
    
        Settings: Move DEV_FORCE_SHOW_NAVBAR load to loadSecureSettings.
    
        Change-Id: I6ac53b8c9f7fce6f9ca6b4ad7bf31a1c1e896863
    
    Author: Paul Keith <javelinanddart@gmail.com>
    Date:   Tue Jan 16 15:47:07 2018 +0100
    
        PhoneWindowManager: Make sure KeyDisabler is always called on boot
    
        * Otherwise, some KeyDisabler classes are left in a weird state
        * Because we don't keep track of whether an initial state was ever set,
          we never call KeyDisabler on boot if the setting is set to 0
        * To remedy this, keep track of whether an initial state was set
    
        Change-Id: Ib432ed3278dd8f4f4cba3ba488879b3c1cd9c8f4
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Sat Sep 29 20:42:04 2018 +0200
    
        PhoneWindowManager: Fix issues introduced with runtime navbar
    
        * With system settings we need to pass UserHandle.USER_CURRENT
          to make sure we are getting proper value, otherwise we always
          end up getting '0'. Also we need to make sure to set valid
          mHasNavigationBar in setInitialDisplaySize().
    
        Change-Id: I3efd614e735f9a602f13263a742ce858a9d14769
    
    Author: dianlujitao <dianlujitao@lineageos.org>
    Date:   Thu Nov 15 11:33:20 2018 +0800
    
        TunerServiceImpl: Blacklist Lineage settings from tuner reset
    
        [Partial pick for navbar]
    
        * They use the tunable infrastructure but are real user settings and
          shouldn't be reset with tuner settings. Moreover, setting them to
          null would cause a IllegalArgumentException.
    
        Change-Id: Ie05910efaee867632a3bd2e84625d7735259130e
    
    Lucchetto edits:
    
     - Make it lineage-sdk independent
    
    Change-Id: I4a6d3f89bc171c3921875b24c077cb78c03517ad

--- services/core/java/com/android/server/wm/DisplayPolicy.java
+++ services/core/java/com/android/server/wm/DisplayPolicy.java
@@ -119,11 +119,13 @@ import android.app.ActivityThread;
 import android.app.LoadedApk;
 import android.app.ResourcesManager;
 import android.app.StatusBarManager;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.content.pm.ApplicationInfo;
+import android.database.ContentObserver;
 import android.graphics.Insets;
 import android.graphics.PixelFormat;
 import android.graphics.Rect;
@@ -136,6 +138,7 @@ import android.os.Message;
 import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.BoostFramework;
 import android.util.ArraySet;
 import android.util.Pair;
@@ -268,6 +271,7 @@ public class DisplayPolicy {
 
     private volatile boolean mHasStatusBar;
     private volatile boolean mHasNavigationBar;
+    private volatile int mForceNavbar = -1;
     // Can the navigation bar ever move to the side?
     private volatile boolean mNavigationBarCanMove;
     private volatile boolean mNavigationBarLetsThroughTaps;
@@ -388,6 +392,8 @@ public class DisplayPolicy {
 
     private PointerLocationView mPointerLocationView;
 
+    private SettingsObserver mSettingsObserver;
+
     /**
      * The area covered by system windows which belong to another display. Forwarded insets is set
      * in case this is a virtual display, this is displayed on another display that has insets, and
@@ -472,6 +478,24 @@ public class DisplayPolicy {
                       currentPackage) == BoostFramework.WorkloadType.GAME);
         }
         return isGame;
+    }
+
+    private class SettingsObserver extends ContentObserver {
+        public SettingsObserver(Handler handler) {
+            super(handler);
+
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.FORCE_SHOW_NAVBAR), false, this,
+                    UserHandle.USER_ALL);
+
+            updateSettings();
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            updateSettings();
+        }
     }
 
     DisplayPolicy(WindowManagerService service, DisplayContent displayContent) {
@@ -751,6 +775,8 @@ public class DisplayPolicy {
         mRefreshRatePolicy = new RefreshRatePolicy(mService,
                 mDisplayContent.getDisplayInfo(),
                 mService.mHighRefreshRateBlacklist);
+
+        mSettingsObserver = new SettingsObserver(mHandler);
     }
 
     void systemReady() {
@@ -758,6 +784,14 @@ public class DisplayPolicy {
         if (mService.mPointerLocationEnabled) {
             setPointerLocationEnabled(true);
         }
+    }
+
+    public void updateSettings() {
+        ContentResolver resolver = mContext.getContentResolver();
+
+        mForceNavbar = Settings.System.getIntForUser(resolver,
+                Settings.System.FORCE_SHOW_NAVBAR, 0,
+                UserHandle.USER_CURRENT);
     }
 
     private int getDisplayId() {
@@ -815,7 +849,7 @@ public class DisplayPolicy {
     }
 
     public boolean hasNavigationBar() {
-        return mHasNavigationBar;
+        return mHasNavigationBar || mForceNavbar == 1;
     }
 
     public boolean hasStatusBar() {
