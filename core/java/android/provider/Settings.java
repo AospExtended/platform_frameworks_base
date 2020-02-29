bb85ccbd136251cd75c8c83c4fb99fadc5e75843
Joe Maples <joe@maples.dev>
2020-02-29 09:54:58 -0500

    QS: Add and use Bluetooth Panel instead of full Settings
    Change-Id: I9d65965ab56c2636b040554940e111cde3afb292

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -16283,11 +16283,25 @@ public final class Settings {
                 "android.settings.panel.action.VOLUME";
 
         /**
+         * Activity Action: Show a settings dialog containing controls for Bluetooth.
+         * <p>
+         * Input: Nothing.
+         * <p>
+         * Output: Nothing.
+         *
+         * @hide
+         */
+        @SdkConstant(SdkConstant.SdkConstantType.ACTIVITY_INTENT_ACTION)
+        public static final String ACTION_BLUETOOTH =
+                "android.settings.panel.action.BLUETOOTH";
+
+        /**
          * Activity Action: Show a settings dialog containing controls for Mobile Data.
          * <p>
          * Input: Nothing.
          * <p>
          * Output: Nothing.
+         *
          * @hide
          */
         @SdkConstant(SdkConstant.SdkConstantType.ACTIVITY_INTENT_ACTION)
