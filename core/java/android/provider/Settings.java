b918674d32094d297521b5ab7f9a3490f65be094
SagarMakhar <sagarmakhar@gmail.com>
2020-03-20 19:27:25 +0000

    Bring back themes [1/3]
    Change-Id: I8a5cd9692d4ff33d5ebe6cd726eebdff2b2d217b
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4522,6 +4522,12 @@ public final class Settings {
                 ANY_INTEGER_VALIDATOR;
 
         /**
+         * Current UI mode
+         * @hide
+         */
+        public static final String SYSTEM_THEME_STYLE = "system_theme_style";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
