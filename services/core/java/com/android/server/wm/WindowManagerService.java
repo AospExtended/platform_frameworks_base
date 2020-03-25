1ce7dfd0f82efb7773517391dcaeea6838397a75
// S∆UR∆V // <57617226+ugly-kid-af@users.noreply.github.com>
2020-03-25 20:14:47 +0530

    fwb: Animation : Set default animation scale to 0.5x

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -792,9 +792,9 @@ public class WindowManagerService extends IWindowManager.Stub
     PowerManager mPowerManager;
     PowerManagerInternal mPowerManagerInternal;
 
-    private float mWindowAnimationScaleSetting = 1.0f;
-    private float mTransitionAnimationScaleSetting = 1.0f;
-    private float mAnimatorDurationScaleSetting = 1.0f;
+    private float mWindowAnimationScaleSetting = 0.5f;
+    private float mTransitionAnimationScaleSetting = 0.5f;
+    private float mAnimatorDurationScaleSetting = 0.5f;
     private boolean mAnimationsDisabled = false;
     boolean mPointerLocationEnabled = false;
 
