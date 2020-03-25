f057b9651521f8f4707985f4a43804a390d2e475
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-24 22:04:47 -0300

    DisplayPolicy: Fix watchdog when adding new display
    * Fixes system crashing after connected to Android Auto or started screen record
    
    That was introduced on toggle navbar commit
    
    Only register content observer if default display
    
    Change-Id: Ia43a922251803be94de8618eb442dcf132e479e9
    Signed-off-by: MASTERGUY <murtipipo@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- services/core/java/com/android/server/wm/DisplayPolicy.java
+++ services/core/java/com/android/server/wm/DisplayPolicy.java
@@ -651,6 +651,9 @@ public class DisplayPolicy {
             } else if ("0".equals(navBarOverride)) {
                 mHasNavigationBar = true;
             }
+
+            // Register content observer only for main display
+            mSettingsObserver = new SettingsObserver(mHandler);
         } else {
             mHasStatusBar = false;
             mHasNavigationBar = mDisplayContent.supportsSystemDecorations();
@@ -669,8 +672,6 @@ public class DisplayPolicy {
             }
         });
         mHandler.post(mGestureNavigationSettingsObserver::register);
-
-        mSettingsObserver = new SettingsObserver(mHandler);
     }
 
     void systemReady() {
