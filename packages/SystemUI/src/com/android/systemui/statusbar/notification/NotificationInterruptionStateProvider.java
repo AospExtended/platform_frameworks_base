c44e95d5d905f6c6feca5f689a2f174a114eded0
ezio84 <brabus84@gmail.com>
2020-03-30 14:09:20 +0200

    Less boring headsup: allow any sms/dialer app set as default by user
    thanks @GeoZac for the suggestion!
    
    Change-Id: I72b8c459bc844d14760cfe1e62efe44af4b64c85

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/NotificationInterruptionStateProvider.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/NotificationInterruptionStateProvider.java
@@ -28,9 +28,11 @@ import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.UserHandle;
 import android.provider.Settings;
+import android.provider.Telephony.Sms;
 import android.service.dreams.DreamService;
 import android.service.dreams.IDreamManager;
 import android.service.notification.StatusBarNotification;
+import android.telecom.TelecomManager;
 import android.util.Log;
 
 import com.android.internal.annotations.VisibleForTesting;
@@ -77,6 +79,7 @@ public class NotificationInterruptionStateProvider {
     private boolean mDisableNotificationAlerts;
 
     private boolean mLessBoringHeadsUp;
+    private TelecomManager mTm;
     private boolean mPartialScreenshot;
 
     @Inject
@@ -89,7 +92,8 @@ public class NotificationInterruptionStateProvider {
                 new AmbientDisplayConfiguration(context),
                 filter,
                 batteryController,
-                stateController);
+                stateController,
+                (TelecomManager) context.getSystemService(Context.TELECOM_SERVICE));
     }
 
     @VisibleForTesting
@@ -100,7 +104,8 @@ public class NotificationInterruptionStateProvider {
             AmbientDisplayConfiguration ambientDisplayConfiguration,
             NotificationFilter notificationFilter,
             BatteryController batteryController,
-            StatusBarStateController statusBarStateController) {
+            StatusBarStateController statusBarStateController,
+            TelecomManager telecomManager) {
         mContext = context;
         mPowerManager = powerManager;
         mDreamManager = dreamManager;
@@ -108,6 +113,7 @@ public class NotificationInterruptionStateProvider {
         mAmbientDisplayConfiguration = ambientDisplayConfiguration;
         mNotificationFilter = notificationFilter;
         mStatusBarStateController = statusBarStateController;
+        mTm = telecomManager;
     }
 
     /** Sets up late-binding dependencies for this component. */
@@ -369,11 +375,20 @@ public class NotificationInterruptionStateProvider {
 
     public boolean shouldSkipHeadsUp(StatusBarNotification sbn) {
         boolean isImportantHeadsUp = false;
-        String notificationPackageName = sbn.getPackageName().toLowerCase();
-        isImportantHeadsUp = notificationPackageName.contains("dialer") ||
-                notificationPackageName.contains("messaging") ||
-                notificationPackageName.contains("clock");
-        return !Dependency.get(ShadeController.class).isDozing() && mLessBoringHeadsUp && !isImportantHeadsUp;
+        String notificationPackageName = sbn.getPackageName();
+        isImportantHeadsUp = notificationPackageName.equals(getDefaultDialerPackage(mTm))
+                || notificationPackageName.equals(getDefaultSmsPackage(mContext))
+                || notificationPackageName.contains("clock");
+        return !mStatusBarStateController.isDozing() && mLessBoringHeadsUp && !isImportantHeadsUp;
+    }
+
+    private static String getDefaultSmsPackage(Context ctx) {
+        return Sms.getDefaultSmsPackage(ctx);
+        // for reference, there's also a new RoleManager api with getDefaultSmsPackage(context, userid) 
+    }
+
+    private static String getDefaultDialerPackage(TelecomManager tm) {
+        return tm != null ? tm.getDefaultDialerPackage() : "";
     }
 
     /**
