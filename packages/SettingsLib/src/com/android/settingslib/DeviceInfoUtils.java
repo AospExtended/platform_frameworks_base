omnirom/android_frameworks_base_old

[2/2] base: allow device to override *caugh*caugh* prop
Change-Id: Ib51a5e5ae87b004053dcf1c01d8aa6cf3ef63b65

packages/SettingsLib/src/com/android/settingslib/DeviceInfoUtils.java
@@ -153,9 +153,13 @@ public class DeviceInfoUtils {
     }
 
     public static String getSecurityPatch() {
+        String patchOmni = Build.VERSION.SECURITY_PATCH_OMNI;
         String patch = Build.VERSION.SECURITY_PATCH;
         if (!"".equals(patch)) {
             try {
+                if (!"".equals(patchOmni)){
+                    patch = patchOmni;
+                }
                 SimpleDateFormat template = new SimpleDateFormat("yyyy-MM-dd");
                 Date patchDate = template.parse(patch);
                 String format = DateFormat.getBestDateTimePattern(Locale.getDefault(), "dMMMMyyyy");

AospExtended/platform_frameworks_base

packages/SettingsLib/src/com/android/settingslib/DeviceInfoUtils.java
@@ -154,9 +154,13 @@ public class DeviceInfoUtils {
     }
 
     public static String getSecurityPatch() {
+        String patchCustom = Build.VERSION.SECURITY_PATCH_CUSTOM;
         String patch = Build.VERSION.SECURITY_PATCH;
         if (!"".equals(patch)) {
             try {
+                if (!"".equals(patchCustom)){
+                    patch = patchCustom;
+                }
                 SimpleDateFormat template = new SimpleDateFormat("yyyy-MM-dd");
                 Date patchDate = template.parse(patch);
                 String format = DateFormat.getBestDateTimePattern(Locale.getDefault(), "dMMMMyyyy");