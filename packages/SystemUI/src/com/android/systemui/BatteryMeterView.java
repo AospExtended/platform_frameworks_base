c44b3dfc34d0bc06a61770ed839763a8c3e26d6a
Nicolas Dhouailly <ndhouailly%2540gmail.com>
2020-04-27 18:55:56 +0200

    Battery Styles: Remove unneeded function parameter in BatteryMeterView
    - Previously, we added a boolean parameter in the updateShowPercent
    function that was used for the battery percentage in the quick status bar header.
    With the new battery style code, this boolean is no longer needed.
    Let's remove all the code that was affected by this change.
    
    Change-Id: I29a1b894253a67812dcd8e4c0399a0ebe837ff8a
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -307,7 +307,7 @@ public class BatteryMeterView extends LinearLayout implements
             mThemedDrawable.setCharging(mCharging);
             mCircleDrawable.setCharging(mCharging);
             mFullCircleDrawable.setCharging(mCharging);
-            updateShowPercent(false);
+            updateShowPercent();
         } else {
             updatePercentText();
         }
@@ -318,7 +318,7 @@ public class BatteryMeterView extends LinearLayout implements
         mThemedDrawable.setPowerSaveEnabled(isPowerSave);
         mCircleDrawable.setPowerSaveEnabled(isPowerSave);
         mFullCircleDrawable.setPowerSaveEnabled(isPowerSave);
-        updateShowPercent(false);
+        updateShowPercent();
     }
 
     private TextView loadPercentView() {
@@ -400,7 +400,6 @@ public class BatteryMeterView extends LinearLayout implements
                     mBatteryPercentView.setTextAppearance(mPercentageStyleId);
                 }
                 if (mTextColor != 0) mBatteryPercentView.setTextColor(mTextColor);
-                updatePercentText();
                 addView(mBatteryPercentView,
                         new ViewGroup.LayoutParams(
                                 LayoutParams.WRAP_CONTENT,
@@ -442,7 +441,7 @@ public class BatteryMeterView extends LinearLayout implements
 
     @Override
     public void onOverlayChanged() {
-        updateShowPercent(false);
+        updateShowPercent();
         updateSettings();
     }
 
@@ -544,11 +543,11 @@ public class BatteryMeterView extends LinearLayout implements
             super.onChange(selfChange, uri);
             updateShowPercent();
             updateSettings();
-            if (TextUtils.equals(uri.getLastPathSegment(),
+            /*if (TextUtils.equals(uri.getLastPathSegment(),
                     Settings.Global.BATTERY_ESTIMATES_LAST_UPDATE_TIME)) {
                 // update the text for sure if the estimate in the cache was updated
                 updatePercentText();
-            }
+            }*/ //updatePercentText gets called always by updateShowPercent
         }
     }
 }
