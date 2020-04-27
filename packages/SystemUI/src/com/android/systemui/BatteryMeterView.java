3f024e0c144aa33c994a09fc0215c78e09bd4d4a
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2020-04-27 18:00:52 +0200

    Battery options cleanup [1/2]
    previously there was some logic in the Settings part that was setting text percentage style
    even for the hidden style option to load the needed values in BatteryMeterView.
    That made the code really hard to read and understand, and needed an additional system setting value.
    Now we move the proper logic to BatteryMeterView so there's no confusion and possible glitches due to settings preferences loading,
    This commit also fixes at least one glitch: while charging, set hidden style, then exit from battery options menu, then enter it again.
    Before the commit it was setting the text style back.
    
    Change-Id: Ic56196870427d11895e2d9e79413d0fcd673b7d3
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -85,7 +85,12 @@ public class BatteryMeterView extends LinearLayout implements
     private static final int BATTERY_STYLE_CIRCLE = 1;
     private static final int BATTERY_STYLE_DOTTED_CIRCLE = 2;
     private static final int BATTERY_STYLE_FULL_CIRCLE = 3;
-    private static final int BATTERY_STYLE_HIDDEN = 4;
+    private static final int BATTERY_STYLE_TEXT = 4; /*hidden icon*/
+    private static final int BATTERY_STYLE_HIDDEN = 5;
+
+    private static final int BATTERY_PERCENT_HIDDEN = 0;
+    private static final int BATTERY_PERCENT_SHOW_INSIDE = 1;
+    private static final int BATTERY_PERCENT_SHOW_OUTSIDE = 2;
 
     private final CircleBatteryDrawable mCircleDrawable;
     private final FullCircleBatteryDrawable mFullCircleDrawable;
@@ -112,6 +117,8 @@ public class BatteryMeterView extends LinearLayout implements
 
     private DualToneHandler mDualToneHandler;
     private int mUser;
+
+    private boolean mIsQsHeader;
 
     private int mNonAdaptedSingleToneColor;
     private int mNonAdaptedForegroundColor;
@@ -232,9 +239,21 @@ public class BatteryMeterView extends LinearLayout implements
     }
 
     private void updateSbShowBatteryPercent() {
-        mShowBatteryPercent = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, 0);
-        updatePercentView();
+        //updateSbBatteryStyle already called
+        switch (mBatteryStyle) {
+            case BATTERY_STYLE_TEXT:
+                mShowBatteryPercent = BATTERY_PERCENT_SHOW_OUTSIDE;
+                updatePercentView();
+                return;
+            case BATTERY_STYLE_HIDDEN:
+                mShowBatteryPercent = BATTERY_PERCENT_HIDDEN;
+                updatePercentView();
+                return;
+            default:
+                mShowBatteryPercent = Settings.System.getInt(mContext.getContentResolver(),
+                        Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, BATTERY_PERCENT_HIDDEN);
+                updatePercentView();
+        }
     }
 
     @Override
@@ -341,7 +360,7 @@ public class BatteryMeterView extends LinearLayout implements
             // Use the high voltage symbol ⚡ (u26A1 unicode) but prevent the system
             // to load its emoji colored variant with the uFE0E flag
             String bolt = "\u26A1\uFE0E";
-            CharSequence mChargeIndicator = mCharging && (mBatteryStyle == BATTERY_STYLE_HIDDEN)
+            CharSequence mChargeIndicator = mCharging && (mBatteryStyle == BATTERY_STYLE_TEXT)
                 ? (bolt + " ") : "";
             mBatteryPercentView.setText(mChargeIndicator +
                 NumberFormat.getPercentInstance().format(mLevel / 100f));
@@ -361,10 +380,11 @@ public class BatteryMeterView extends LinearLayout implements
     private void updateShowPercent() {
         final boolean showing = mBatteryPercentView != null;
         final boolean drawPercentInside = mShowPercentMode == MODE_DEFAULT &&
-                mShowBatteryPercent == 1;
+                mShowBatteryPercent == BATTERY_PERCENT_SHOW_INSIDE;
         final boolean drawPercentOnly = mShowPercentMode == MODE_ESTIMATE ||
-                mShowPercentMode == MODE_ON || mShowBatteryPercent == 2;
-        if (drawPercentOnly && (!drawPercentInside || mCharging)) {
+                mShowPercentMode == MODE_ON || mShowBatteryPercent == BATTERY_PERCENT_SHOW_OUTSIDE;
+        if (!(!mIsQsHeader && mBatteryStyle == BATTERY_STYLE_HIDDEN)
+                && drawPercentOnly && (!drawPercentInside || mCharging)) {
             mThemedDrawable.setShowPercent(false);
             mCircleDrawable.setShowPercent(false);
             mFullCircleDrawable.setShowPercent(false);
@@ -379,7 +399,7 @@ public class BatteryMeterView extends LinearLayout implements
                                 LayoutParams.WRAP_CONTENT,
                                 LayoutParams.MATCH_PARENT));
             }
-            if (mBatteryStyle == BATTERY_STYLE_HIDDEN) {
+            if (mBatteryStyle == BATTERY_STYLE_TEXT) {
                 mBatteryPercentView.setPaddingRelative(0, 0, 0, 0);
             } else {
                 Resources res = getContext().getResources();
@@ -395,8 +415,12 @@ public class BatteryMeterView extends LinearLayout implements
         updatePercentText();
     }
 
+    public void setIsQsHeader(boolean isQs) {
+        mIsQsHeader = isQs;
+    }
+
     public void updateVisibility() {
-        if (mBatteryStyle == BATTERY_STYLE_HIDDEN) {
+        if (mBatteryStyle == BATTERY_STYLE_TEXT || mBatteryStyle == BATTERY_STYLE_HIDDEN) {
             mBatteryIconView.setVisibility(View.GONE);
             mBatteryIconView.setImageDrawable(null);
             //setVisibility(View.GONE);
@@ -474,8 +498,9 @@ public class BatteryMeterView extends LinearLayout implements
 
     public void updateBatteryStyle() {
         switch (mBatteryStyle) {
+            case BATTERY_STYLE_TEXT:
             case BATTERY_STYLE_HIDDEN:
-            return;
+            break;
             case BATTERY_STYLE_PORTRAIT:
             mBatteryIconView.setImageDrawable(mThemedDrawable);
             break;
