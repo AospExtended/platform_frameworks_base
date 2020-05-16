4631e6db5f99cc937e412c7d13854abf69bbb5d7
Ali B <abittin@gmail.com>
2020-05-16 17:05:07 +0300

    NotificationLights: add reverse animation direction [1/2]
    When notifications are repeated more than once, every
    other notification light starts where the previous one
    ends.
    
    Change-Id: I56ec562badcc7dc92e63bd212974763557b83182
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: Alexandru Scurtu <sasha.scurtu@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5205,6 +5205,14 @@ public final class Settings {
         public static final String AMBIENT_LIGHT_PULSE_FOR_ALL = "ambient_light_pulse_for_all";
 
         /**
+         * Direction of repeat animations of Ambient edge light
+         * 0 is restart
+         * 1 is reverse
+         * @hide
+         */
+        public static final String AMBIENT_LIGHT_REPEAT_DIRECTION = "ambient_light_repeat_direction";
+
+        /**
          * Network traffic inactivity threshold (default is 1 kBs)
          * @hide
          */
