7b47d916201b32851173a2b25315c8debdd7bccc
ezio84 <brabus84@gmail.com>
2020-05-30 15:27:04 +0200

    Battery icon: fix requestLayout() calls spam when in lockscreen
    if percentage text is set outside the icon
    
    KeyguardStatusBarView.updateVisibilities calls setForceShowPercent,
    ending up calling updatePercentText
    updatePercentText method gets called in many cases in BatteryMeterView
    Actually i'm really busy so as a quick fix let's just skip all useless
    mBatteryPercentView.setText calls
    
    Change-Id: I82ded105248e5f7d8bce196f0a87cc51960b06cd
    Signed-off-by: Anirudh Gupta <anirudhgupta109@aosip.dev>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/battery/BatteryMeterView.java
@@ -269,7 +269,7 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
                     }
                     if (estimate != null) {
                         if (mBatteryPercentView != null) {
-                            mBatteryPercentView.setText(estimate);
+                            batteryPercentViewSetText(estimate);
                         }
                         setContentDescription(getContext().getString(
                                 R.string.accessibility_battery_level_with_estimate,
@@ -295,7 +295,7 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
             String bolt = "\u26A1\uFE0E";
             CharSequence mChargeIndicator = mCharging && (mBatteryStyle == BATTERY_STYLE_TEXT)
                 ? (bolt + " ") : "";
-            mBatteryPercentView.setText(mChargeIndicator +
+            batteryPercentViewSetText(mChargeIndicator +
                 NumberFormat.getPercentInstance().format(mLevel / 100f));
             setContentDescription(
                     getContext().getString(mCharging ? R.string.accessibility_battery_level_charging
@@ -361,6 +361,13 @@ public class BatteryMeterView extends LinearLayout implements DarkReceiver {
             mBatteryIconView.setVisibility(View.VISIBLE);
             //setVisibility(View.VISIBLE);
             scaleBatteryMeterViews();
+        }
+    }
+
+    private void batteryPercentViewSetText(CharSequence text) {
+        CharSequence currentText = mBatteryPercentView.getText();
+        if (!currentText.toString().equals(text.toString())) {
+            mBatteryPercentView.setText(text);
         }
     }
 
