f2f92b5add87577a993b8da179207ced8c201063
Kshitij Gupta <kshitijgm@gmail.com>
2020-07-07 15:56:11 +0530

    base: notch-city: Add 3 mode display cutout handler
    [@AgentFabulous - POSP]
    
    * About the port
    - This includes the latest patches applied to A10, combined to a single
      change. This includes quite a few changes authored by me, and a ton
      has been heavily changed and rewritten, so I won't even bother listing
      them here.
    
    * The change
    Modes:
     - Normal
     - Immerse
     - Hide
    
    Normal:
      You know this one ¯\_(ツ)_/¯
    
    Immerse:
      This change does not force layout parameters globally. Instead, sets
      statusbar colors and changes layout parameters for statusbar. It also
      forces corners to be drawn with LAYOUT_IN_DISPLAY_CUTOUT_MODE_NEVER so
      they are always below the statusbar. This however looks weird in
      landscape, thus the feature disables itself in landscape mode. This
      way, we can black out our cutout while being able to see our status
      icons still and have adjustable virtual corners, while not breaking
      any apps.
    
    Hide:
      We can't really register a content observer for overriding the
      resource bool in our java code (causes SystemUI to FC with a null
      pointer ref), so use overlays here. Works like most other "Hide"
    
    Change-Id: I2aa5e08b2f8c5b561873069354063633a2a9f1cc

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/BarTransitions.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/BarTransitions.java
@@ -90,6 +90,10 @@ public class BarTransitions {
         return mMode;
     }
 
+    public BarBackgroundDrawable getBackground() {
+        return mBarBackground;
+    }
+
     public void setAutoDim(boolean autoDim) {
         // Default is don't care.
     }
@@ -172,6 +176,7 @@ public class BarTransitions {
         private int mGradientAlphaStart;
         private int mColorStart;
         private Rect mFrame;
+        private Integer mColorOverride;
 
 
         public BarBackgroundDrawable(Context context, int gradientResourceId) {
@@ -230,6 +235,12 @@ public class BarTransitions {
             mGradient.setBounds(bounds);
         }
 
+        public void setColorOverride(Integer color) {
+            if (mColorOverride == color) return;
+            mColorOverride = color;
+            invalidateSelf();
+        }
+
         public void applyModeBackground(int oldMode, int newMode, boolean animate) {
             if (mMode == newMode) return;
             mMode = newMode;
@@ -270,6 +281,9 @@ public class BarTransitions {
             } else {
                 targetColor = mOpaque;
             }
+
+            if (mColorOverride != null)
+                targetColor = mColorOverride;
 
             if (!mAnimating) {
                 mColor = targetColor;
