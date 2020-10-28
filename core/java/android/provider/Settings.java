ae403d7915232f47e7b529f490992a0b05379598
Ido Ben-Hur <idoybh2@gmail.com>
2020-10-28 03:58:04 +0200

    base: Allow disabling SmartSpace [1/2]
    Change-Id: Ia0a4130c8f35426debaccf3a20acc5a06019c803

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5958,6 +5958,13 @@ public final class Settings {
         public static final String NOTIFICATION_SOUND_VIB_SCREEN_ON = "notification_sound_vib_screen_on";
 
         /**
+         * Whether to enable SmartSpace
+         * @hide
+         */
+        @Readable
+        public static final String SMART_SPACE = "smart_space";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
