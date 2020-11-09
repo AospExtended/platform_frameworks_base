a4a5f8038f8f578b29ce5722bc2580c4fd51a24f
Ido Ben-Hur <idoybh2@gmail.com>
2020-11-09 02:36:47 +0200

    base: Reorginize light pulse color options in one setting
    Slightly more human readable
    
    Change-Id: I24db7e166bec2e5eb9fd2997233c962564284ad3

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -88,9 +88,8 @@ public class SystemSettings {
         Settings.System.NOTIFICATION_PULSE,
         Settings.System.AOD_NOTIFICATION_PULSE,
         Settings.System.AOD_NOTIFICATION_PULSE_CLEAR,
-        Settings.System.NOTIFICATION_PULSE_ACCENT,
+        Settings.System.NOTIFICATION_PULSE_COLOR_MODE,
         Settings.System.NOTIFICATION_PULSE_COLOR,
-        Settings.System.NOTIFICATION_PULSE_COLOR_AUTOMATIC,
         Settings.System.AOD_NOTIFICATION_PULSE_TIMEOUT,
     };
 }
