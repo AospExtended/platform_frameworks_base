5c5ca4578875d907142e9c1d09caa529f6c75965
Ali B <abittin@gmail.com>
2020-11-09 20:24:56 +0300

    SystemUI: Fix crash in dark mode
    SystemUI force closes on dark themes when brightness slider is toggled.
    Remove restrictions check
    
    11-09 20:08:13.847  2764  2906 E AndroidRuntime: FATAL EXCEPTION: SysUiBg
    11-09 20:08:13.847  2764  2906 E AndroidRuntime: Process: com.android.systemui, PID: 2764
    11-09 20:08:13.847  2764  2906 E AndroidRuntime: android.view.ViewRootImpl$CalledFromWrongThreadException: Only the original thread that created a view hierarchy can touch its views.
    11-09 20:08:13.847  2764  2906 E AndroidRuntime: 	at android.view.ViewRootImpl.checkThread(ViewRootImpl.java:8803)
    11-09 20:08:13.847  2764  2906 E AndroidRuntime: 	at android.view.ViewRootImpl.focusableViewAvailable(ViewRootImpl.java:4607)
    11-09 20:08:13.847  2764  2906 E AndroidRuntime: 	at android.view.ViewGroup.focusableViewAvailable(ViewGroup.java:962)
    11-09 20:08:13.847  2764  2906 E AndroidRuntime: 	at android.view.ViewGroup.focusableViewAvailable(ViewGroup.java:962)
    11-09 20:08:13.847  2764  2906 E AndroidRuntime: 	at android.view.ViewGroup.focusableViewAvailable(ViewGroup.java:962)
    11-09 20:08:13.847  2764  2906 E AndroidRuntime: 	at android.view.ViewGroup.focusableViewAvailable(ViewGroup.java:962)
    11-09 20:08:13.847  2764  2906 E AndroidRuntime: 	at android.view.ViewGroup.focusableViewAvailable(ViewGroup.java:962)
    11-09 20:08:13.847  2764  2906 E AndroidRuntime: 	at android.view.ViewGroup.focusableViewAvailable(ViewGroup.java:962)
    11-09 20:08:13.847  2764  2906 E AndroidRuntime: 	at android.view.ViewGroup.focusableViewAvailable(ViewGroup.java:962)
    11-09 20:08:13.847  2764  2906 E AndroidRuntime: 	at android.view.ViewGroup.focusableViewAvailable(ViewGroup.java:962)
    11-09 20:08:13.847  2764  2906 E AndroidRuntime: 	at android.view.ViewGroup.focusableViewAvailable(ViewGroup.java:962)
    11-09 20:08:13.847  2764  2906 E AndroidRuntime: 	at android.view.View.setFlags(View.java:16122)
    11-09 20:08:13.847  2764  2906 E AndroidRuntime: 	at android.view.View.setEnabled(View.java:11744)
    11-09 20:08:13.847  2764  2906 E AndroidRuntime: 	at com.android.systemui.settings.ToggleSliderView.setEnforcedAdmin(ToggleSliderView.java:101)
    11-09 20:08:13.847  2764  2906 E AndroidRuntime: 	at com.android.systemui.settings.BrightnessController$9.run(BrightnessController.java:418)
    11-09 20:08:13.847  2764  2906 E AndroidRuntime: 	at android.os.Handler.handleCallback(Handler.java:938)
    11-09 20:08:13.847  2764  2906 E AndroidRuntime: 	at android.os.Handler.dispatchMessage(Handler.java:99)
    11-09 20:08:13.847  2764  2906 E AndroidRuntime: 	at android.os.Looper.loop(Looper.java:223)
    11-09 20:08:13.847  2764  2906 E AndroidRuntime: 	at android.os.HandlerThread.run(HandlerThread.java:67)
    
    Change-Id: Ibff2c40f33bf0f60d6021e850168bd82b912bd99

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -873,9 +873,9 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
     }
 
     public void refreshAllTiles() {
-        if (mBrightnessController != null) {
+        /*if (mBrightnessController != null) {
             mBrightnessController.checkRestrictionAndSetEnabled();
-        }
+        }*/
         for (TileRecord r : mRecords) {
             r.tile.refreshState();
         }
