921031cdc0156efbe290c5ade7bf296462946947
Ido Ben-Hur <idoybh2@gmail.com>
2020-11-10 18:45:10 +0200

    base: Only allow edge lights on AOD when AOD is enabled [1/2]
    Change-Id: Id34f98dc6362049c87ad215a959d6edb3eca2a24

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -1351,7 +1351,9 @@ public final class PowerManagerService extends SystemService
         boolean mAmbientLights = Settings.System.getIntForUser(
                 mContext.getContentResolver(), Settings.System.AOD_NOTIFICATION_PULSE,
                 0, UserHandle.USER_CURRENT) != 0;
-        if (mAmbientLights) {
+        boolean aodEnabled = Settings.Secure.getIntForUser(resolver,
+                Settings.Secure.DOZE_ALWAYS_ON, 0, UserHandle.USER_CURRENT) == 1;
+        if (mAmbientLights && aodEnabled) {
             boolean dozeOnNotification = Settings.System.getIntForUser(resolver,
                     Settings.System.AOD_NOTIFICATION_PULSE_TRIGGER, 0, UserHandle.USER_CURRENT) != 0;
             Settings.System.putIntForUser(resolver,
