fc36b95e2ae9ae13f6de8e693d55826124fdc9a8
Stallix <joey@evolution-x.org>
2020-12-26 16:35:02 +0300

    SystemUI: FOD pressed colors [1/2]
    Includes the following:
    * https://github.com/Evolution-X/frameworks_base/commit/b70ac04e293c7a2eea2710a1a689630a74e70364
    * https://github.com/Evolution-X/frameworks_base/commit/6ecbadff3550c01682a15331f3ccb0abf614d845
    
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Change-Id: I7bce07b196a15d60674a000552e2cbb64ea083eb
    
    FOD icon for oneplus 8 series
    
    	*needed for Oneplus 8/8pro/8t and Nord
    	*can be set by using this overlay:
    	<integer name="config_fod_pressed_color">4</integer>
    
    Rename file name for op8 icon
    
     * ffffe6 looks stupid in settings
    
    Change-Id: Ia18fb7a314066f0c8128e2162930f8f9eaf250cf
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6004,6 +6004,12 @@ public final class Settings {
         public static final String STATUSBAR_ICONS_STYLE = "statusbar_icons_style";
 
         /**
+         * FOD pressed color
+         * @hide
+         */
+        public static final String FOD_COLOR = "fod_color";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
