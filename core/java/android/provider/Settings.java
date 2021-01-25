0e43599b9d8ddce9736adbe26a1beabb2c737954
idoybh2 <idoybh2@gmail.com>
2021-01-25 17:04:06 +0000

    base: Allow toggling QS media player [1/2]
    Change-Id: Id80823c7ccab12f8cf3bb77a20ebcd2e9769b741

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6053,6 +6053,12 @@ public final class Settings {
         public static final String LOCKSCREEN_CHARGING_ANIMATION_STYLE = "lockscreen_charging_animation_style";
 
         /**
+         * Whether to show media player in QS
+         * @hide
+         */
+        public static final String QS_MEDIA_PLAYER = "qs_media_player";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
