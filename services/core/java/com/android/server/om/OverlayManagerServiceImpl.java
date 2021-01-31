76e93d690f92ad3b65952e3791123cce6fd7e492
Kshitij Gupta <kshitijgm@gmail.com>
2021-01-31 14:46:11 +0100

    ThemeOverlayController: Reload homeApp on cutout mode change
    Change-Id: I6ce604142bb674742981eebc9c4d8a5f8772d699
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- services/core/java/com/android/server/om/OverlayManagerServiceImpl.java
+++ services/core/java/com/android/server/om/OverlayManagerServiceImpl.java
@@ -797,6 +797,10 @@ final class OverlayManagerServiceImpl {
         mIdmapManager.removeIdmap(oi, oi.userId);
     }
 
+    public void reloadAssets(final String packageName, final int userId) {
+        mListener.onOverlaysChanged(packageName, userId);
+    }
+
     interface OverlayChangeListener {
 
         /**
