4f162a9370e8aafc52bbf0860a6880388959ccb8
00day0 <therandomuser11@gmail.com>
2021-02-10 20:24:45 +0700

    Add MIUI volume style plugin [1/3]
    Signed-off-by: Ziauddin Sameer <ziauddinsameer@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Change-Id: I69463fb3306af5a8b23f0d597e836e29c41f9cce

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/VolumePluginManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/VolumePluginManager.java
@@ -76,6 +76,7 @@ public class VolumePluginManager extends BroadcastReceiver {
         "co.potatoproject.plugin.volume.compact",
         "co.potatoproject.plugin.volume.oreo",
         "co.potatoproject.plugin.volume.tiled",
+        "co.potatoproject.plugin.volume.miui",
     };
 
     private PluginPrefs mPluginPrefs;
