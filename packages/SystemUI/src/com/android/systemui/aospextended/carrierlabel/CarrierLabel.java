Havoc-OS/android_frameworks_base

Notch friendly carrier label [1/2]
Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

packages/SystemUI/src/com/android/systemui/havoc/carrierlabel/CarrierLabel.java
@@ -65,6 +65,13 @@ public class CarrierLabel extends TextView implements DarkReceiver {
         super(context, attrs, defStyle);
         mContext = context;
         updateNetworkName(true, null, false, null);
+        /* Force carrier label to the lockscreen. This helps us avoid
+        the carrier label on the statusbar if for whatever reason
+        the user changes notch overlays */
+        if (Utils.hasNotch(mContext)) {
+            Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.STATUS_BAR_SHOW_CARRIER, 1);
+        }
     }
 
     @Override

AospExtended/platform_frameworks_base

Change-Id: I78d2eb84ff8c12b295aa7f59673bbcd81a0925e8
Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

packages/SystemUI/src/com/android/systemui/aospextended/carrierlabel/CarrierLabel.java
@@ -65,6 +65,13 @@ public class CarrierLabel extends TextView implements DarkReceiver {
         super(context, attrs, defStyle);
         mContext = context;
         updateNetworkName(true, null, false, null);
+        /* Force carrier label to the lockscreen. This helps us avoid
+        the carrier label on the statusbar if for whatever reason
+        the user changes notch overlays */
+        if (AEXUtils.hasNotch(mContext)) {
+            Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.STATUS_BAR_SHOW_CARRIER, 1);
+        }
     }
 
     @Override