zl1-aicp/frameworks_base

Less boring heads up option [1/2]
allow to show heads up only for dialer and messaging
app when they are not in foreground

AICPfied: disabled by default

Less annoying heads up: code fixes and improvements

fix Ambient Display not working when disabling heads up
remove the foreground app check, it's good to show heads up
when Dialer is in foreground instead of incallui

Change-Id: I08b035f5ba17a7964dc0d13385afb7d52fbfb395
Signed-off-by: Ali B <abittin@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -640,6 +640,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.Secure.getUriFor(
                     Settings.Secure.LOCK_QS_DISABLED),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LESS_BORING_HEADS_UP),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -657,6 +660,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             if (mQuickQSPanel != null) {
                 mQuickQSPanel.updateSettings();
             }
+            setUseLessBoringHeadsUp();
         }
     }
 
@@ -4634,4 +4638,10 @@ public class StatusBar extends SystemUI implements DemoMode,
         return mStatusBarMode;
     }
 
+    private void setUseLessBoringHeadsUp() {
+        boolean lessBoringHeadsUp = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.LESS_BORING_HEADS_UP, 0,
+                UserHandle.USER_CURRENT) == 1;
+        mNotificationInterruptionStateProvider.setUseLessBoringHeadsUp(lessBoringHeadsUp);
+    }
 }

AospExtended/platform_frameworks_base

Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4376,6 +4376,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUS_BAR_BRIGHTNESS_CONTROL),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LESS_BORING_HEADS_UP),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -4412,6 +4415,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             setLockScreenMediaArt();
             setQsPanelOptions();
             setScreenBrightnessMode();
+            setUseLessBoringHeadsUp();
         }
     }
 
@@ -4465,6 +4469,13 @@ public class StatusBar extends SystemUI implements DemoMode,
         mBrightnessControl = Settings.System.getIntForUser(
             mContext.getContentResolver(), Settings.System.STATUS_BAR_BRIGHTNESS_CONTROL, 0,
             UserHandle.USER_CURRENT) == 1;
+    }
+
+    private void setUseLessBoringHeadsUp() {
+        boolean lessBoringHeadsUp = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.LESS_BORING_HEADS_UP, 0,
+                UserHandle.USER_CURRENT) == 1;
+        mNotificationInterruptStateProvider.setUseLessBoringHeadsUp(lessBoringHeadsUp);
     }
 
     private final BroadcastReceiver mBannerActionBroadcastReceiver = new BroadcastReceiver() {