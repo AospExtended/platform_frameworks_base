7ed4bde8aa8422fa91ab346c56c11bc09af4b75c
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-02-23 18:11:53 +0530

    SystemUI: Fix issues with VoWiFi icon
     * some devices don't set data network type to IWLAN although
       vowifi works fine, besides the real check for vowifi is
       directly using TelephonyManager's isWifiCallingAvailable()
       method just like we check for volte and vilte status
    
     * cleanup some dead code while at it
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Change-Id: I29d913dfe33ced08b0591700a0637239f69d585a

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -111,6 +111,8 @@ public class MobileSignalController extends SignalController<
     private boolean mDataDisabledIcon;
     private boolean mRoamingIconAllowed;
 
+    private boolean mIsVowifiAvailable;
+
     // TODO: Reduce number of vars passed in, if we have the NetworkController, probably don't
     // need listener lists anymore.
     public MobileSignalController(Context context, Config config, boolean hasMobileData,
@@ -496,11 +498,13 @@ public class MobileSignalController extends SignalController<
         mCurrentState.voiceCapable = tm.isVolteAvailable();
         mCurrentState.videoCapable = tm.isVideoTelephonyAvailable();
         mCurrentState.imsRegistered = mPhone.isImsRegistered(mSubscriptionInfo.getSubscriptionId());
+        mIsVowifiAvailable = tm.isWifiCallingAvailable();
         if (DEBUG) {
             Log.d(mTag, "queryImsState tm=" + tm + " phone=" + mPhone
                     + " voiceCapable=" + mCurrentState.voiceCapable
                     + " videoCapable=" + mCurrentState.videoCapable
-                    + " imsRegistered=" + mCurrentState.imsRegistered);
+                    + " imsRegistered=" + mCurrentState.imsRegistered
+                    + " mIsVowifiAvailable=" + mIsVowifiAvailable);
         }
         notifyListenersIfNecessary();
     }
@@ -801,17 +805,8 @@ public class MobileSignalController extends SignalController<
 
     public boolean isVowifiAvailable() {
         return mCurrentState.voiceCapable && mCurrentState.imsRegistered
-                && getDataNetworkType() == TelephonyManager.NETWORK_TYPE_IWLAN;
-    }
-
-    private MobileIconGroup getVowifiIconGroup() {
-        if ( isVowifiAvailable() && !isCallIdle() ) {
-            return TelephonyIcons.VOWIFI_CALLING;
-        } else if (isVowifiAvailable()) {
-            return TelephonyIcons.VOWIFI;
-        } else {
-            return null;
-        }
+                && (getDataNetworkType() == TelephonyManager.NETWORK_TYPE_IWLAN
+                || mIsVowifiAvailable);
     }
 
     @Override
