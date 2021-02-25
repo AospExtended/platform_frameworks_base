b44d1c579cd69e937c227a0df39d1bcbeb38046b
Oliver Scott <olivercscott@gmail.com>
2021-02-25 11:29:35 -0500

    Wi-Fi timeout feature (1/2)
    Issue: calyxos#228
    Change-Id: I1ac6b19eb35af656afd38eac24bb37fe618859f1

--- packages/SettingsLib/src/com/android/settingslib/wifi/WifiStatusTracker.java
+++ packages/SettingsLib/src/com/android/settingslib/wifi/WifiStatusTracker.java
@@ -17,6 +17,7 @@ import static android.net.NetworkCapabilities.TRANSPORT_CELLULAR;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.ContentObserver;
 import android.net.ConnectivityManager;
 import android.net.ConnectivityManager.NetworkCallback;
 import android.net.Network;
@@ -115,6 +116,20 @@ public class WifiStatusTracker {
         mNetworkScoreManager = networkScoreManager;
         mConnectivityManager = connectivityManager;
         mCallback = callback;
+
+        mContext.getContentResolver().registerContentObserver(
+                Settings.Global.getUriFor(Settings.Global.WIFI_OFF_TIMEOUT),
+                false,
+                new ContentObserver(mHandler) {
+                    @Override
+                    public void onChange(boolean selfChange) {
+                        super.onChange(selfChange);
+                        WifiTimeoutReceiver.setTimeoutAlarm(context,
+                                Settings.Global.getLong(context.getContentResolver(),
+                                        Settings.Global.WIFI_OFF_TIMEOUT, 0));
+                    }
+                }
+        );
     }
 
     public void setListening(boolean listening) {
@@ -159,6 +174,10 @@ public class WifiStatusTracker {
                 updateRssi(mWifiInfo.getRssi());
                 maybeRequestNetworkScore();
             }
+        } else {
+            WifiTimeoutReceiver.setTimeoutAlarm(mContext,
+                    Settings.Global.getLong(mContext.getContentResolver(),
+                            Settings.Global.WIFI_OFF_TIMEOUT, 0));
         }
         updateStatusLabel();
     }
@@ -170,6 +189,11 @@ public class WifiStatusTracker {
         String action = intent.getAction();
         if (action.equals(WifiManager.WIFI_STATE_CHANGED_ACTION)) {
             updateWifiState();
+            if (intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE, WifiManager.WIFI_STATE_UNKNOWN) == WifiManager.WIFI_STATE_ENABLED) {
+                WifiTimeoutReceiver.setTimeoutAlarm(mContext,
+                        Settings.Global.getLong(mContext.getContentResolver(),
+                                Settings.Global.WIFI_OFF_TIMEOUT, 0));
+            }
         } else if (action.equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)) {
             updateWifiState();
             final NetworkInfo networkInfo =
@@ -188,6 +212,10 @@ public class WifiStatusTracker {
                     updateRssi(mWifiInfo.getRssi());
                     maybeRequestNetworkScore();
                 }
+            } else {
+                WifiTimeoutReceiver.setTimeoutAlarm(mContext,
+                        Settings.Global.getLong(mContext.getContentResolver(),
+                                Settings.Global.WIFI_OFF_TIMEOUT, 0));
             }
             updateStatusLabel();
             mCallback.run();
