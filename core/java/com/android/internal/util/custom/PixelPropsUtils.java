dce90c2683d4f4f16450a22670bace96e140cfba
palaych <palaychm@yandex.ru>
2021-03-17 20:32:33 +0000

    base: PixelPropsUtils: Cleanup
    * Remove override fingerprint for SettingsIntelligence, we use AOSP package.
    
    Change-Id: Icd58304f0205de74388a9366cfef454a32d556c8

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -159,10 +159,6 @@ public class PixelPropsUtils {
                 setPropValue(key, value);
             }
         }
-        // Set proper indexing fingerprint
-        if (packageName.equals("com.google.android.settings.intelligence")){
-            setPropValue("FINGERPRINT", Build.DATE);
-        }
     }
 
     private static void setPropValue(String key, Object value){
