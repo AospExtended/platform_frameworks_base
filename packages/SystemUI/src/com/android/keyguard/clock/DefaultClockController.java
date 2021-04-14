80ded7ac2d2116d00f50ee5fb246a3f03153fce4
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2021-04-14 19:10:59 -0400

    fixup! SystemUI: Add time refresh capabilities for added clock controllers
    merged prematurely, Default clock doesnt need this as it updates fine and fix Sfuny
    
    Change-Id: I6f31a400cd76ae525feef0bebebaa9d7bf68e495

--- packages/SystemUI/src/com/android/keyguard/clock/DefaultClockController.java
+++ packages/SystemUI/src/com/android/keyguard/clock/DefaultClockController.java
@@ -163,8 +163,6 @@ public class DefaultClockController implements ClockPlugin {
 
     @Override
     public void onTimeTick() {
-        mView.onTimeChanged();
-        mClock.refreshTime();
     }
 
     @Override
