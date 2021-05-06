868edfda71b3a32e57d2a8835d88fefcb02b47d4
Laker <lakerman1987@gmail.com>
2021-05-06 17:17:37 +0300

    Add lockscreen background blur [1/2]
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I5178538e81c4b78ef35a2737122a68051c67feb8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5821,6 +5821,12 @@ public final class Settings {
         public static final String QS_TILE_TITLE_VISIBILITY = "qs_tile_title_visibility";
         
         /**
+         * Whether to enable or disable lockscreen background blur
+         * @hide
+         */
+        public static final String LOCKSCREEN_BLUR = "lockscreen_blur";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
