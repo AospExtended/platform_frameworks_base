0aee7ee7056e09e856745399f3290834bcbcca91
George Zacharia <george.zcharia@gmail.com>
2021-05-25 22:36:56 +0530

    SystemUI: Make clock layouts aware of the media playing state
    Since media notifications are handled a bit different in Android 11,
    media notification on lockscreen messed up the clock layouts. Avoid this
    by adding it to notifications count.
    
    Change-Id: I774f01ae2c648c32b6cbc20eff3da445bda0ffe1

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayout.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayout.java
@@ -1330,6 +1330,7 @@ public class NotificationStackScrollLayout extends ViewGroup implements ScrollAd
                 count++;
             }
         }
+        if (mKeyguardMediaController.getView().getVisibility() == VISIBLE) count++;
         return count;
     }
 
