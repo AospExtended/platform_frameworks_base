afdfcb5d0091491dda349900daba53092e5aae92
Kshitij Gupta <kshitijgm@gmail.com>
2021-05-30 21:41:59 +0530

    base: Allow configuring minimum RefreshRate
    Change-Id: I510ca8be7a611e33005000501900dd6ce75c5433

--- services/core/java/com/android/server/display/DisplayModeDirector.java
+++ services/core/java/com/android/server/display/DisplayModeDirector.java
@@ -868,6 +868,7 @@ public class DisplayModeDirector {
         private final Context mContext;
         private float mDefaultPeakRefreshRate;
         private float mDefaultRefreshRate;
+        private float mDefaultMinRefreshRate;
 
         SettingsObserver(@NonNull Context context, @NonNull Handler handler) {
             super(handler);
@@ -876,6 +877,8 @@ public class DisplayModeDirector {
                     R.integer.config_defaultPeakRefreshRate);
             mDefaultRefreshRate =
                     (float) context.getResources().getInteger(R.integer.config_defaultRefreshRate);
+            mDefaultMinRefreshRate =
+                    (float) context.getResources().getInteger(R.integer.config_defaultMinimumRefreshRate);
         }
 
         public void observe() {
@@ -951,7 +954,7 @@ public class DisplayModeDirector {
 
         private void updateRefreshRateSettingLocked() {
             float minRefreshRate = Settings.System.getFloat(mContext.getContentResolver(),
-                    Settings.System.MIN_REFRESH_RATE, 0f);
+                    Settings.System.MIN_REFRESH_RATE, mDefaultMinRefreshRate);
             float peakRefreshRate = Settings.System.getFloat(mContext.getContentResolver(),
                     Settings.System.PEAK_REFRESH_RATE, mDefaultPeakRefreshRate);
             updateRefreshRateSettingLocked(minRefreshRate, peakRefreshRate, mDefaultRefreshRate);
