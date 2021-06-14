BootleggersROM/frameworks_base

[1/2] base: Introduce UI Styles
This overlay method is similar to the UI icons section but allows to specify a Custom style for apps like Android, Settings and SystemUI.
It'll preview the themes according to if they have a dark/light mode, and some UI elements.

Change-Id: Iafde71eea7e9f94c440283fcdac06d920eb5ff62

packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayManager.java
@@ -48,6 +48,12 @@ class ThemeOverlayManager {
     @VisibleForTesting
     static final String OVERLAY_CATEGORY_COLOR = "android.theme.customization.accent_color";
     @VisibleForTesting
+    static final String OVERLAY_CATEGORY_STYLE_ANDROID = "android.theme.customization.style.android";
+    @VisibleForTesting
+    static final String OVERLAY_CATEGORY_STYLE_SETTINGS = "android.theme.customization.style.settings";
+    @VisibleForTesting
+    static final String OVERLAY_CATEGORY_STYLE_SYSUI = "android.theme.customization.style.systemui";
+    @VisibleForTesting
     static final String OVERLAY_CATEGORY_FONT = "android.theme.customization.font";
     @VisibleForTesting
     static final String OVERLAY_CATEGORY_SHAPE =
@@ -77,6 +83,9 @@ class ThemeOverlayManager {
             OVERLAY_CATEGORY_SHAPE,
             OVERLAY_CATEGORY_FONT,
             OVERLAY_CATEGORY_COLOR,
+            OVERLAY_CATEGORY_STYLE_ANDROID,
+            OVERLAY_CATEGORY_STYLE_SETTINGS,
+            OVERLAY_CATEGORY_STYLE_SYSUI,
             OVERLAY_CATEGORY_ICON_ANDROID,
             OVERLAY_CATEGORY_ICON_SYSUI,
             OVERLAY_CATEGORY_ICON_SETTINGS,
@@ -88,6 +97,8 @@ class ThemeOverlayManager {
             OVERLAY_CATEGORY_COLOR,
             OVERLAY_CATEGORY_FONT,
             OVERLAY_CATEGORY_SHAPE,
+            OVERLAY_CATEGORY_STYLE_ANDROID,
+            OVERLAY_CATEGORY_STYLE_SYSUI,
             OVERLAY_CATEGORY_ICON_ANDROID,
             OVERLAY_CATEGORY_ICON_SYSUI);
 
@@ -108,11 +119,14 @@ class ThemeOverlayManager {
         mThemePickerPackage = themePickerPackage;
         mTargetPackageToCategories.put(ANDROID_PACKAGE, Sets.newHashSet(
                 OVERLAY_CATEGORY_COLOR, OVERLAY_CATEGORY_FONT,
-                OVERLAY_CATEGORY_SHAPE, OVERLAY_CATEGORY_ICON_ANDROID));
+                OVERLAY_CATEGORY_SHAPE, OVERLAY_CATEGORY_ICON_ANDROID,
+                OVERLAY_CATEGORY_STYLE_ANDROID));
         mTargetPackageToCategories.put(SYSUI_PACKAGE,
-                Sets.newHashSet(OVERLAY_CATEGORY_ICON_SYSUI));
+                Sets.newHashSet(OVERLAY_CATEGORY_ICON_SYSUI,
+                OVERLAY_CATEGORY_STYLE_SYSUI));
         mTargetPackageToCategories.put(SETTINGS_PACKAGE,
-                Sets.newHashSet(OVERLAY_CATEGORY_ICON_SETTINGS));
+                Sets.newHashSet(OVERLAY_CATEGORY_ICON_SETTINGS,
+                OVERLAY_CATEGORY_STYLE_SETTINGS));
         mTargetPackageToCategories.put(mLauncherPackage,
                 Sets.newHashSet(OVERLAY_CATEGORY_ICON_LAUNCHER));
         mTargetPackageToCategories.put(mThemePickerPackage,
@@ -120,6 +134,9 @@ class ThemeOverlayManager {
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_COLOR, ANDROID_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_FONT, ANDROID_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_SHAPE, ANDROID_PACKAGE);
+        mCategoryToTargetPackage.put(OVERLAY_CATEGORY_STYLE_ANDROID, ANDROID_PACKAGE);
+        mCategoryToTargetPackage.put(OVERLAY_CATEGORY_STYLE_SYSUI, SYSUI_PACKAGE);
+        mCategoryToTargetPackage.put(OVERLAY_CATEGORY_STYLE_SETTINGS, SETTINGS_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_ANDROID, ANDROID_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_SYSUI, SYSUI_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_SETTINGS, SETTINGS_PACKAGE);

AospExtended/platform_frameworks_base

packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayManager.java
@@ -48,6 +48,12 @@ class ThemeOverlayManager {
     @VisibleForTesting
     static final String OVERLAY_CATEGORY_COLOR = "android.theme.customization.accent_color";
     @VisibleForTesting
+    static final String OVERLAY_CATEGORY_STYLE_ANDROID = "android.theme.customization.theme_style.android";
+    @VisibleForTesting
+    static final String OVERLAY_CATEGORY_STYLE_SETTINGS = "android.theme.customization.theme_style.settings";
+    @VisibleForTesting
+    static final String OVERLAY_CATEGORY_STYLE_SYSUI = "android.theme.customization.theme_style.systemui";
+    @VisibleForTesting
     static final String OVERLAY_CATEGORY_FONT = "android.theme.customization.font";
     @VisibleForTesting
     static final String OVERLAY_CATEGORY_SHAPE =
@@ -77,6 +83,9 @@ class ThemeOverlayManager {
             OVERLAY_CATEGORY_SHAPE,
             OVERLAY_CATEGORY_FONT,
             OVERLAY_CATEGORY_COLOR,
+            OVERLAY_CATEGORY_STYLE_ANDROID,
+            OVERLAY_CATEGORY_STYLE_SETTINGS,
+            OVERLAY_CATEGORY_STYLE_SYSUI,
             OVERLAY_CATEGORY_ICON_ANDROID,
             OVERLAY_CATEGORY_ICON_SYSUI,
             OVERLAY_CATEGORY_ICON_SETTINGS,
@@ -88,6 +97,8 @@ class ThemeOverlayManager {
             OVERLAY_CATEGORY_COLOR,
             OVERLAY_CATEGORY_FONT,
             OVERLAY_CATEGORY_SHAPE,
+            OVERLAY_CATEGORY_STYLE_ANDROID,
+            OVERLAY_CATEGORY_STYLE_SYSUI,
             OVERLAY_CATEGORY_ICON_ANDROID,
             OVERLAY_CATEGORY_ICON_SYSUI);
 
@@ -108,11 +119,14 @@ class ThemeOverlayManager {
         mThemePickerPackage = themePickerPackage;
         mTargetPackageToCategories.put(ANDROID_PACKAGE, Sets.newHashSet(
                 OVERLAY_CATEGORY_COLOR, OVERLAY_CATEGORY_FONT,
-                OVERLAY_CATEGORY_SHAPE, OVERLAY_CATEGORY_ICON_ANDROID));
+                OVERLAY_CATEGORY_SHAPE, OVERLAY_CATEGORY_ICON_ANDROID,
+                OVERLAY_CATEGORY_STYLE_ANDROID));
         mTargetPackageToCategories.put(SYSUI_PACKAGE,
-                Sets.newHashSet(OVERLAY_CATEGORY_ICON_SYSUI));
+                Sets.newHashSet(OVERLAY_CATEGORY_ICON_SYSUI,
+                OVERLAY_CATEGORY_STYLE_SYSUI));
         mTargetPackageToCategories.put(SETTINGS_PACKAGE,
-                Sets.newHashSet(OVERLAY_CATEGORY_ICON_SETTINGS));
+                Sets.newHashSet(OVERLAY_CATEGORY_ICON_SETTINGS,
+                OVERLAY_CATEGORY_STYLE_SETTINGS));
         mTargetPackageToCategories.put(mLauncherPackage,
                 Sets.newHashSet(OVERLAY_CATEGORY_ICON_LAUNCHER));
         mTargetPackageToCategories.put(mThemePickerPackage,
@@ -120,6 +134,9 @@ class ThemeOverlayManager {
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_COLOR, ANDROID_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_FONT, ANDROID_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_SHAPE, ANDROID_PACKAGE);
+        mCategoryToTargetPackage.put(OVERLAY_CATEGORY_STYLE_ANDROID, ANDROID_PACKAGE);
+        mCategoryToTargetPackage.put(OVERLAY_CATEGORY_STYLE_SYSUI, SYSUI_PACKAGE);
+        mCategoryToTargetPackage.put(OVERLAY_CATEGORY_STYLE_SETTINGS, SETTINGS_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_ANDROID, ANDROID_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_SYSUI, SYSUI_PACKAGE);
         mCategoryToTargetPackage.put(OVERLAY_CATEGORY_ICON_SETTINGS, SETTINGS_PACKAGE);