bb0c93c1387b85a322169cca40545f3be2ffd4e5
jhenrique09 <jhsv09@gmail.com>
2021-06-15 19:37:18 -0300

    AppLock: Update tasks snapshot when needed
    * After 15 seconds timeout or when device is locked
    
    Change-Id: I29eb4bc3912ceda9958439c4fc79bded2b124ae8

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -4398,7 +4398,7 @@ final class ActivityRecord extends WindowToken implements WindowManagerService.A
      */
     boolean shouldUseAppThemeSnapshot() {
         return mDisablePreviewScreenshots || forAllWindows(WindowState::isSecureLocked,
-                true /* topToBottom */) || getIsAppLocked();
+                true /* topToBottom */);
     }
 
     /**
