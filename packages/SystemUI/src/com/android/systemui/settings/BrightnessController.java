0a70007ba09f82dbb311ceddc2e0d96e1c781f52
Rituj Beniwal <ritujbeniwal@gmail.com>
2021-06-20 12:21:44 +0530

    SystemUI: BrightnessSlider: Scale animation duration with defined BrightnessRampRate
    Signed-off-by: Rituj Beniwal <ritujbeniwal@gmail.com>
    Change-Id: I8e27c2bc90a1aca9d32512c219b70dac2e079ee6

--- packages/SystemUI/src/com/android/systemui/settings/BrightnessController.java
+++ packages/SystemUI/src/com/android/systemui/settings/BrightnessController.java
@@ -102,6 +102,7 @@ public class BrightnessController implements ToggleSlider.Listener {
     private boolean mControlValueInitialized;
 
     private ValueAnimator mSliderAnimator;
+    private float mBrightnessRampRate;
 
     public interface BrightnessStateChangeCallback {
         public void onBrightnessLevelChanged();
@@ -339,6 +340,8 @@ public class BrightnessController implements ToggleSlider.Listener {
         mDisplayManager = context.getSystemService(DisplayManager.class);
         mVrManager = IVrManager.Stub.asInterface(ServiceManager.getService(
                 Context.VR_SERVICE));
+        mBrightnessRampRate = mContext.getResources().getFloat(
+                com.android.internal.R.dimen.config_brightnessRampRateFastFloat);
 
        if (mIcon != null) {
             if (mAutomaticAvailable) {
@@ -514,8 +517,8 @@ public class BrightnessController implements ToggleSlider.Listener {
             mControl.setValue((int) animation.getAnimatedValue());
             mExternalChange = false;
         });
-        final long animationDuration = SLIDER_ANIMATION_DURATION * Math.abs(
-                mControl.getValue() - target) / GAMMA_SPACE_MAX;
+        final long animationDuration = Math.round(SLIDER_ANIMATION_DURATION * Math.abs(
+                mControl.getValue() - target)/mBrightnessRampRate) / GAMMA_SPACE_MAX;
         mSliderAnimator.setDuration(animationDuration);
         mSliderAnimator.start();
     }
