c224bd6695238844a13bc5489ce2d04195e6ce03
SagarMakhar <sagarmakhar@gmail.com>
2021-07-23 14:03:51 +0000

    SystemUI: Add aex fod icon [1/2]
    Change-Id: I17639d02f5acc84417091743019c76c8ce018c9b
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -136,6 +136,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener, T
     private int mSelectedIcon = 0;
     private final int[] ICON_STYLES = {
         R.drawable.fod_icon_default,
+        R.drawable.fod_icon_aex,
         R.drawable.fod_icon_default_1,
         R.drawable.fod_icon_default_2,
         R.drawable.fod_icon_default_3,
@@ -145,7 +146,6 @@ public class FODCircleView extends ImageView implements ConfigurationListener, T
         R.drawable.fod_icon_cpt_america_flat,
         R.drawable.fod_icon_cpt_america_flat_gray,
         R.drawable.fod_icon_dragon_black_flat,
-        R.drawable.fod_icon_evo1,
         R.drawable.fod_icon_glow_circle,
         R.drawable.fod_icon_neon_arc,
         R.drawable.fod_icon_neon_arc_gray,
