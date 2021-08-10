8f73501e47464c5290580d8299db6e316ccbeac3
Akash Srivastava <akashniki@gmail.com>
2021-08-10 17:12:14 +0530

    AndroidSClocks: Beautify it
    * Make both S and DP3 clock WallpaperColor based
    * Remove shadows from elements like clock, charging etc on lockscreen. It doesn't fit well and makes clock font look distorted.
    * Fix time being stuck on AOD/ambient
    
    Co-authored-by: hpnightowl <iamhp2k@gmail.com>
    Change-Id: I63288d2161a55af47264c7a35b13fcea4a297d0b

--- packages/SystemUI/src/com/android/keyguard/clock/AndroidSDP3ClockController.java
+++ packages/SystemUI/src/com/android/keyguard/clock/AndroidSDP3ClockController.java
@@ -115,6 +115,11 @@ public class AndroidSDP3ClockController implements ClockPlugin {
     private final ViewPreviewer mRenderer = new ViewPreviewer();
 
     /**
+     * Helper to extract colors from wallpaper palette for clock face.
+     */
+    private final ClockPalette mPalette = new ClockPalette();
+
+    /**
      * Root view of clock.
      */
     private ClockLayout mView;
@@ -214,6 +219,11 @@ public class AndroidSDP3ClockController implements ClockPlugin {
         TextClock previewClock = mView.findViewById(R.id.clock);
         previewClock.setFormat12Hour("hh\nmm");
         previewClock.setFormat24Hour("kk\nmm");
+        onTimeTick();
+        previewClock.setTextColor(Color.WHITE);
+        ColorExtractor.GradientColors colors = mColorExtractor.getColors(
+                WallpaperManager.FLAG_LOCK);
+        setColorPalette(colors.supportsDarkText(), colors.getColorPalette());
 
         return mRenderer.createPreview(previewView, width, height);
     }
@@ -241,11 +251,14 @@ public class AndroidSDP3ClockController implements ClockPlugin {
 
     @Override
     public void setTextColor(int color) {
-        mClock.setTextColor(color);
+        updateTextColors();
     }
 
     @Override
-    public void setColorPalette(boolean supportsDarkText, int[] colorPalette) {}
+    public void setColorPalette(boolean supportsDarkText, int[] colorPalette) {
+        mPalette.setColorPalette(supportsDarkText, colorPalette);
+        updateTextColors();
+    }
 
     @Override
     public void setSlice(Slice slice) {
@@ -399,6 +412,7 @@ public class AndroidSDP3ClockController implements ClockPlugin {
 
     private void updateTextColors() {
         final int blendedColor = getTextColor();
+        final int secondary = mPalette.getSecondaryColor();
         mTitle.setTextColor(blendedColor);
         int childCount = mRow.getChildCount();
         for (int i = 0; i < childCount; i++) {
@@ -407,6 +421,7 @@ public class AndroidSDP3ClockController implements ClockPlugin {
                 ((TextView) v).setTextColor(blendedColor);
             }
         }
+        mClock.setTextColor(secondary);
     }
 
     int getTextColor() {
