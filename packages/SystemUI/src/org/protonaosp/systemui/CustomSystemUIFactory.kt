20d5aa1ef2800c31b7a075a4cb949ee5d2fb3052
Danny Lin <danny@kdrag0n.dev>
2021-10-05 21:00:57 -0700

    SystemUI: Use colorkt-based dynamic theme engine for Monet
    Dynamic wallpaper-based theming ("Monet") is a prominent aspect of
    Android 12's Material You design system. Implement the system-wide
    wallpaper-based theming using my colorkt-based [1] dynamic theme engine [2],
    which many users prefer over Google's Pixel implementation in empirical
    tests as it typically keeps colors closer to the wallpaper and doesn't
    suffer from CAM16's blue-purple hue shift issues. It also provides
    better perceived contrast than Google's implementation in most cases
    because it uses ZCAM's modern lightness correlate as compared to CIELAB
    L* [3].
    
    NB: Unlike Google's implementation, we overlay the framework's default
    modulated surface colors for performance (no need to modulate colors
    when inflating views at runtime) and consistency (reduced dependency on
    framework-side color code, which may not respect user preferences).
    
    Demo screenshots: https://twitter.com/kdrag0n/status/1445584174790832134
    
    [1] https://github.com/kdrag0n/colorkt
    [2] https://github.com/kdrag0n/android12-extensions
    [3] https://twitter.com/MyndexResearch/status/1435491919702888448
    
    Change-Id: I48cbbacc32b2a8095eb07729aca1734f162d2391

--- packages/SystemUI/src/org/protonaosp/systemui/CustomSystemUIFactory.kt
+++ packages/SystemUI/src/org/protonaosp/systemui/CustomSystemUIFactory.kt
@@ -17,9 +17,24 @@
 package org.protonaosp.systemui
 
 import android.content.res.AssetManager
+import android.content.res.Resources
 import com.android.systemui.SystemUIFactory
+import com.android.systemui.theme.ThemeOverlayController
+import org.protonaosp.systemui.theme.CustomThemeOverlayController
 
 class CustomSystemUIFactory : SystemUIFactory() {
+    // Override services without having to copy the entire array
+    override fun getSystemUIServiceComponents(resources: Resources): Array<String> {
+        val services = super.getSystemUIServiceComponents(resources)
+        return services.map { CUSTOM_SERVICES[it] ?: it }.toTypedArray()
+    }
+
+    companion object {
+        private val CUSTOM_SERVICES = mapOf(
+            ThemeOverlayController::class to CustomThemeOverlayController::class,
+        ).map { it.key.java.name to it.value.java.name }.toMap()
+    }
+
     // ML back gesture provider
     override fun createBackGestureTfClassifierProvider(am: AssetManager, modelName: String) =
         CustomBackGestureTfClassifierProvider(am, modelName)
