66f5df8cade149c53da8b126a0ecd338f6f2c6dc
Matt Mower <mowerm@gmail.com>
2021-10-13 09:22:28 +0000

    Keyguard: Forward port lockscreen quick unlock (1/2)
    Aex Edits:
    port to S
    
    commit 142719299d1bea5db21d17b9cc7687efe553a1a3
    Author: Veeti Paananen <veeti.paananen@rojekti.fi>
    Date:   Tue Jun 9 12:44:59 2015 +0300
    
        Disable PIN entry after successful quick unlock
    
        Small polish: I still often press the enter button out of muscle memory
        when entering my PIN code, which leads to an "invalid PIN" message being
        shown for a tiny moment as the screen unlocks. Disable the entry view
        after a quick unlock to fix this.
    
        Change-Id: Icceb8311ed55564e9b1d92934cbca814a1cc4b29
        Signed-off-by: mydongistiny <jaysonedson@gmail.com>
        Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
        Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    
    commit 83278c5e9fde3d1dd6a4107d2483f5cd8fc450fa
    Author: Matt Mower <mowerm@gmail.com>
    Date:   Wed Dec 31 15:02:58 2014 -0600
    
        Keyguard: Forward port lockscreen quick unlock (1/2)
    
        * PasswordTextView QuickUnlock listener functions and method thanks to
          ParanoidAndroid project:
            Keyguard: Implement quick unlock feature [1/2]
            BigBrother1984 <carlosavignano@aospa.co>
            I0b3d92b91f72020d327a7f2f184c2fbc5519a5df
    
        @mydongistiny: ported to oreo/android10
        Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
        Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
        Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
        Change-Id: Iaa140723ffb037062d565127257a7cb60526f25e
        Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: I2ece844f9dab714a0e77a1206a3cf82aac24be83

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5202,6 +5202,12 @@ public final class Settings {
         public static final String LOCKSCREEN_PIN_SCRAMBLE_LAYOUT = "lockscreen_scramble_pin_layout";
 
         /**
+         * Whether to use the custom quick unlock screen control
+         * @hide
+         */
+        public static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL = "lockscreen_quick_unlock_control";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
