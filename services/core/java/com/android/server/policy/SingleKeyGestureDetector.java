2873e059ae9201b40dc7a458a35fb554b6e6e7d0
ezio84 <brabus84@gmail.com>
2021-10-21 18:07:40 +0000

    Allow doubletap/longpress power to toggle torch [1/2]
    AEX Edits:
    - Port to S
    - handle camera gesture here instead of extensions
    - simplify code
    
    Thanks to beanstown106 for the initial longpress action calls in
    PhoneWindowManager (improved by lineage guys)
    
    If the user enables double tap for camera action, torch action will
    automatically switch to long press mode.
    If the user enables double tap for torch, double tap for camera
    will be disabled accordingly.
    
    Remove the settings pref is the device doesn't have flashlight
    
    A 170ms delay for double tap action is not annoying on single tap
    to wake up the device.
    
    Vibrate when toggling torch to let the user know of unwanted toggling
    in the pocket (even if vibration is disabled by user)
    
    Allow torch action also on ambient display
    
    mydongistiny: Fixed for android10
    Change-Id: I12da044f86c7b625872607529cf8524615cf576b
    Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- services/core/java/com/android/server/policy/SingleKeyGestureDetector.java
+++ services/core/java/com/android/server/policy/SingleKeyGestureDetector.java
@@ -21,6 +21,8 @@ import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.ViewConfiguration;
@@ -58,6 +60,10 @@ public final class SingleKeyGestureDetector {
     private volatile boolean mHandledByLongPress = false;
     private final Handler mHandler;
     private static final long MULTI_PRESS_TIMEOUT = ViewConfiguration.getMultiPressTimeout();
+
+    static final int TORCH_DOUBLE_TAP_DELAY = 170;
+
+    private Context mContext;
 
     /** Supported gesture flags */
     public static final int KEY_LONGPRESS = 1 << 1;
@@ -152,6 +158,7 @@ public final class SingleKeyGestureDetector {
     }
 
     public SingleKeyGestureDetector(Context context) {
+        mContext = context;
         mLongPressTimeout = ViewConfiguration.get(context).getDeviceGlobalActionKeyTimeout();
         mVeryLongPressTimeout = context.getResources().getInteger(
                 com.android.internal.R.integer.config_veryLongPressTimeout);
@@ -284,7 +291,11 @@ public final class SingleKeyGestureDetector {
             Message msg = mHandler.obtainMessage(MSG_KEY_DELAYED_PRESS, mActiveRule.mKeyCode,
                     mKeyPressCounter, downTime);
             msg.setAsynchronous(true);
-            mHandler.sendMessageDelayed(msg, MULTI_PRESS_TIMEOUT);
+            mHandler.sendMessageDelayed(msg, Settings.System.getIntForUser(
+                    mContext.getContentResolver(),
+                    Settings.System.TORCH_POWER_BUTTON_GESTURE,
+                    0, UserHandle.USER_CURRENT) == 1 ? TORCH_DOUBLE_TAP_DELAY
+                    : MULTI_PRESS_TIMEOUT);
             return true;
         }
         reset();
