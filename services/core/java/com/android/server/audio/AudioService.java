3ddb0b3eb0a0b48d5f3f4369c64a2de3dc0e847f
AmolAmrit <amol.amrit03@outlook.com>
2021-11-18 23:08:09 +0530

    Revert "Audio: Do not disconnect profiles till Bluetooth Off"
    This reverts commit f02c86864470ba4777b0c3ce22b6d58231d78c77.
    
    Change-Id: I85352a1a2b9b2e6e5f535f8bb74c99e2449c291f

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -6876,7 +6876,8 @@ public class AudioService extends IAudioService.Stub
                         UserManager.DISALLOW_RECORD_AUDIO, false, userId);
             } else if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
                 state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, -1);
-                if (state == BluetoothAdapter.STATE_OFF) {
+                if (state == BluetoothAdapter.STATE_OFF ||
+                        state == BluetoothAdapter.STATE_TURNING_OFF) {
                     mDeviceBroker.disconnectAllBluetoothProfiles();
                 }
             } else if (action.equals(AudioEffect.ACTION_OPEN_AUDIO_EFFECT_CONTROL_SESSION) ||
