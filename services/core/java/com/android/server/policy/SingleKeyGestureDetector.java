430532b8f052dad24193c3222086c5eeef472aaf
palaych <palaych@arrowos.net>
2021-12-18 07:04:26 +0900

    base: Fixup! Allow doubletap/longpress power to toggle torch
     * Add "context", missed after merge r19 tag
    
    Change-Id: I331a8478967f286b1127daa08ee55fae601b6428

--- services/core/java/com/android/server/policy/SingleKeyGestureDetector.java
+++ services/core/java/com/android/server/policy/SingleKeyGestureDetector.java
@@ -179,7 +179,8 @@ public final class SingleKeyGestureDetector {
         }
     }
 
-    public SingleKeyGestureDetector() {
+    public SingleKeyGestureDetector(Context context) {
+        mContext = context;
         mHandler = new KeyHandler();
     }
 
