43a3610c0b1051267a6de0296b7b25ceb696c19a
jhenrique09 <jhsv09@gmail.com>
2021-12-17 10:40:44 +0900

    SystemUI: Port columbus (quick tap) from SQ1D
    * Import Google assist library/bindings and related resources as well
    
    Change-Id: I2ead6e10655937e659aa8afc8a8adc53696d51ce
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/6449
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>
    Tested-by: Anay Wadhera <anay1018@gmail.com>
    
    Squashed:
    
    SystemUI: import missing dimens
    
    Change-Id: I123f9b4e88fd851c63509795ee3ea3155dc47af2
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/6904
    Reviewed-by: Sourajit Karmakar <sourajit@live.com>
    Tested-by: Sourajit Karmakar <sourajit@live.com>
    
    SystemUI: Add basic assets for Pixel home button
    
    Change-Id: I47aba96b17a90ac12bc4c6f4cc6d55021b474a78
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/6900
    Reviewed-by: Sourajit Karmakar <sourajit@live.com>
    Tested-by: Sourajit Karmakar <sourajit@live.com>
    
    Co-authored-by: Anay Wadhera <anay1018@gmail.com>
    Co-authored-by: Sourajit Karmakar <sourajit.karmakar@gmail.com>
    [ghostrider-reborn: adapt to aospa]
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I2797175cce4b39c0817bd7f23c2717e81ab3e4d0
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/SystemUI/src/org/aospextended/android/systemui/dagger/SysUIComponentAEX.java
+++ packages/SystemUI/src/org/aospextended/android/systemui/dagger/SysUIComponentAEX.java
@@ -7,6 +7,7 @@ import com.android.systemui.dagger.SystemUIBinder;
 import com.android.systemui.dagger.SysUIComponent;
 import com.android.systemui.dagger.SystemUIModule;
 
+import org.aospextended.android.systemui.columbus.ColumbusModule;
 import org.aospextended.android.systemui.keyguard.KeyguardSliceProviderAEX;
 import org.aospextended.android.systemui.smartspace.KeyguardSmartspaceController;
 
@@ -14,9 +15,10 @@ import dagger.Subcomponent;
 
 @SysUISingleton
 @Subcomponent(modules = {
+        ColumbusModule.class,
         DefaultComponentBinder.class,
         DependencyProvider.class,
-        SystemUIBinder.class,
+        AEXSystemUIBinder.class,
         SystemUIModule.class,
         SystemUIAEXModule.class})
 public interface SysUIComponentAEX extends SysUIComponent {
