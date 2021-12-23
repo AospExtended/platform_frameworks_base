0f3d17932f7a890c067647b32619f6db109c6db0
Pranav Vashi <neobuddy89@gmail.com>
2021-12-24 02:54:12 +0900

    SystemUI: NetworkTraffic: Remove screen on/off triggers
    * ANR points here.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: Ie8b2e6c8fd6a0540915b0ba0fc352e20f3dbf2f4

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTrafficSB.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTrafficSB.java
@@ -96,7 +96,6 @@ public class NetworkTrafficSB extends TextView implements StatusIconDisplayable 
     private boolean mSystemIconVisible = true;
     private boolean mShowArrow;
 
-    private boolean mScreenOn = true;
     private boolean iBytes;
     private boolean oBytes;
 
@@ -311,8 +310,6 @@ public class NetworkTrafficSB extends TextView implements StatusIconDisplayable 
             mAttached = true;
             IntentFilter filter = new IntentFilter();
             filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
-            filter.addAction(Intent.ACTION_SCREEN_OFF);
-            filter.addAction(Intent.ACTION_SCREEN_ON);
             mContext.registerReceiver(mIntentReceiver, filter, null, getHandler());
         }
         updateSettings();
@@ -321,6 +318,7 @@ public class NetworkTrafficSB extends TextView implements StatusIconDisplayable 
     @Override
     protected void onDetachedFromWindow() {
         super.onDetachedFromWindow();
+        clearHandlerCallbacks();
         if (mAttached) {
             mContext.unregisterReceiver(mIntentReceiver);
             mAttached = false;
@@ -333,14 +331,8 @@ public class NetworkTrafficSB extends TextView implements StatusIconDisplayable 
             String action = intent.getAction();
             if (action == null) return;
 
-            if (action.equals(ConnectivityManager.CONNECTIVITY_ACTION) && mScreenOn) {
+            if (action.equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
                 updateSettings();
-            } else if (action.equals(Intent.ACTION_SCREEN_ON)) {
-                mScreenOn = true;
-                updateSettings();
-            } else if (action.equals(Intent.ACTION_SCREEN_OFF)) {
-                mScreenOn = false;
-                clearHandlerCallbacks();
             }
         }
     };
