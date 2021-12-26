6a30b306c4b311a44e8628d861f5ef714e25953d
Joker-V2 <nipinna0@gmail.com>
2021-12-26 01:44:42 +0000

    fixup! SystemUI: Port Statusbar brightness control [1/2]
    Change-Id: Ia49c2a35c261932a8b07ecfaf01aaad6e6777040

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4942,6 +4942,10 @@ public class StatusBar extends SystemUI implements DemoMode,
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.HEADS_UP_BLACKLIST_VALUES))) {
                 setHeadsUpBlacklist();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.STATUS_BAR_BRIGHTNESS_CONTROL)) ||
+                uri.equals(Settings.System.getUriFor(Settings.System.SCREEN_BRIGHTNESS_MODE))) {
+                setScreenBrightnessMode();
             }
         }
 
