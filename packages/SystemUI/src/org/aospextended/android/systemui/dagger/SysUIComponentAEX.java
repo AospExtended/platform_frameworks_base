bb20034128a4d0914fab22280ba2ed21662570ac
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 08:31:51 +0530

    SystemUI: integrate smartspace and backgesture provider
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: If46fe528418daca5bcd7ce2cde221c9b185947d6

--- packages/SystemUI/src/org/aospextended/android/systemui/dagger/SysUIComponentAEX.java
+++ packages/SystemUI/src/org/aospextended/android/systemui/dagger/SysUIComponentAEX.java
@@ -7,6 +7,9 @@ import com.android.systemui.dagger.SystemUIBinder;
 import com.android.systemui.dagger.SysUIComponent;
 import com.android.systemui.dagger.SystemUIModule;
 
+import org.aospextended.android.systemui.keyguard.KeyguardSliceProviderAEX;
+import org.aospextended.android.systemui.smartspace.KeyguardSmartspaceController;
+
 import dagger.Subcomponent;
 
 @SysUISingleton
@@ -22,4 +25,12 @@ public interface SysUIComponentAEX extends SysUIComponent {
     interface Builder extends SysUIComponent.Builder {
         SysUIComponentAEX build();
     }
+
+    /**
+     * Member injection into the supplied argument.
+     */
+    void inject(KeyguardSliceProviderAEX keyguardSliceProviderAEX);
+
+    @SysUISingleton
+    KeyguardSmartspaceController createKeyguardSmartspaceController();
 }
