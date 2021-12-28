efaa5e08f0f1c5468b52fc8c58a87a3f2a6066a0
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 08:31:51 +0530

    SystemUI: integrate smartspace and backgesture provider
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: If46fe528418daca5bcd7ce2cde221c9b185947d6

--- packages/SystemUI/src/org/aospextended/android/systemui/SystemUIAEXFactory.java
+++ packages/SystemUI/src/org/aospextended/android/systemui/SystemUIAEXFactory.java
@@ -1,12 +1,22 @@
 package org.aospextended.android.systemui;
 
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.os.Handler;
+
+import com.google.android.systemui.gesture.BackGestureTfClassifierProviderGoogle;
 
 import org.aospextended.android.systemui.dagger.DaggerGlobalRootComponentAEX;
 import org.aospextended.android.systemui.dagger.GlobalRootComponentAEX;
+import org.aospextended.android.systemui.dagger.SysUIComponentAEX;
 
 import com.android.systemui.SystemUIFactory;
 import com.android.systemui.dagger.GlobalRootComponent;
+import com.android.systemui.navigationbar.gestural.BackGestureTfClassifierProvider;
+import com.android.systemui.screenshot.ScreenshotNotificationSmartActionsProvider;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
 public class SystemUIAEXFactory extends SystemUIFactory {
     @Override
@@ -14,5 +24,18 @@ public class SystemUIAEXFactory extends SystemUIFactory {
         return DaggerGlobalRootComponentAEX.builder()
                 .context(context)
                 .build();
+    }
+
+    @Override
+    public BackGestureTfClassifierProvider createBackGestureTfClassifierProvider(AssetManager am, String modelName) {
+        return new BackGestureTfClassifierProviderGoogle(am, modelName);
+    }
+
+    @Override
+    public void init(Context context, boolean fromTest) throws ExecutionException, InterruptedException {
+        super.init(context, fromTest);
+        if (shouldInitializeComponents()) {
+            ((SysUIComponentAEX) getSysUIComponent()).createKeyguardSmartspaceController();
+        }
     }
 }
