44b169e274880618179c020f64d9c387e86f4549
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 05:49:21 +0530

    SystemUI: Initial SystemUI stub
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: I7adcb852dba99b67a92afc9fcff016a9f3c9f131

---
+++ packages/SystemUI/src/org/aospextended/android/systemui/dagger/SysUISubcomponentModuleAEX.java
@@ -0,0 +1,10 @@
+package org.aospextended.android.systemui.dagger;
+
+import dagger.Module;
+
+/**
+ * Dagger module for including the WMComponent.
+ */
+@Module(subcomponents = {SysUIComponentAEX.class})
+public abstract class SysUISubcomponentModuleAEX {
+}
