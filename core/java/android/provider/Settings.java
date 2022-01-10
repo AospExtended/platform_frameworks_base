af4bda59d715bbe273189f50740ea05e44a9ab1f
Rishawn <70256146+Stealth1226@users.noreply.github.com>
2022-01-10 09:47:15 +0530

    base: allow disabling ripple effect on unlock [1/2]
    @neobuddy89:
    * Reverse logic. Enabled animation by default.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Idecaa59547159d0a2b0ecdb55a207377bd466710

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6112,6 +6112,12 @@ public final class Settings {
         public static final String FULLSCREEN_GESTURES = "fullscreen_gestures";
 
         /**
+         * Whether to enable the ripple animation on fingerprint unlock
+         * @hide
+         */
+        public static final String ENABLE_RIPPLE_EFFECT = "enable_ripple_effect";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
