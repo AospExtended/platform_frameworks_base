fa1d024c53aa01ba16588ccc299b3e1674a762e5
sb6596 <shubhamprince111@gmail.com>
2022-01-24 05:22:19 +0000

    [SQUASH] runtime toggle for location privacy indicator [1/2]
    Revert "SystemUI: Use new privacy indicators for location"
    
    This reverts commit 0eb8ea9f01df8c77ad5533a6fa3eb4c3e17310d1.
    
    base: allow toggling location privacy indicator [1/2]
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: Ia45a568cdc55d285339d134a6adab90de5b28eac
    
    base: improve location privacy indicator toggle
    
    * We don't use device config flag as default anymore. Fixes indicator showing up even if toggle is off / vice versa.
    * Added a ContentObserver to observe settings change and update ui accordingly. Systemui restart is no longer needed.
    
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: Ia144bc8d5d92cb55f61668568100c1503058510b
    
    base: Fix location privacy indicator getting stuck
    
    * Previously, when there is ongoing location indicator visible and if it is
      toggled off, it get stucks permanently in statusbar and quick statusbar.
      This patch fixes it.
    * Bonus: Also fix setting setChipVisibility correctly for QuickStatusbarHeader view.
    
    @jhonboy121: let's keep the setter for micCameraAvailable private
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: I4610c4df667d8cace81fa40a261041f78b49442d
    Signed-off-by: sb6596 <shubhamprince111@gmail.com>

--- core/java/android/permission/PermissionUsageHelper.java
+++ core/java/android/permission/PermissionUsageHelper.java
@@ -48,6 +48,7 @@ import android.media.AudioManager;
 import android.os.Process;
 import android.os.UserHandle;
 import android.provider.DeviceConfig;
+import android.provider.Settings;
 import android.telephony.TelephonyManager;
 import android.util.ArrayMap;
 import android.util.ArraySet;
@@ -70,10 +71,6 @@ public class PermissionUsageHelper implements AppOpsManager.OnOpActiveChangedLis
 
     /** Whether to show the mic and camera icons.  */
     private static final String PROPERTY_CAMERA_MIC_ICONS_ENABLED = "camera_mic_icons_enabled";
-
-    /** Whether to show the location indicators. */
-    private static final String PROPERTY_LOCATION_INDICATORS_ENABLED =
-            "location_indicators_enabled";
 
     /** Whether to show the Permissions Hub.  */
     private static final String PROPERTY_PERMISSIONS_HUB_2_ENABLED = "permissions_hub_2_enabled";
@@ -99,9 +96,10 @@ public class PermissionUsageHelper implements AppOpsManager.OnOpActiveChangedLis
                 PROPERTY_CAMERA_MIC_ICONS_ENABLED, true) || shouldShowPermissionsHub();
     }
 
-    private static boolean shouldShowLocationIndicator() {
-        return DeviceConfig.getBoolean(DeviceConfig.NAMESPACE_PRIVACY,
-                PROPERTY_LOCATION_INDICATORS_ENABLED, true);
+    private boolean shouldShowLocationIndicator() {
+        return Settings.Secure.getIntForUser(mContext.getContentResolver(),
+            Settings.Secure.ENABLE_LOCATION_PRIVACY_INDICATOR, 0,
+            UserHandle.USER_CURRENT) == 1;
     }
 
     private static long getRecentThreshold(Long now) {
