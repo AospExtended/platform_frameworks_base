502e0f220aabe8976efd8cc71fe55eeac24bdb09
jhenrique09 <jhsv09@gmail.com>
2022-02-11 22:34:49 -0300

    SystemUI: Tell user when face unlock detection is running
    Google removed the animation on Android 12, let's use a text instead
    
    Change-Id: I93c4121c57137764edb0b31678d103fd5f7d4dcc

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -2433,6 +2433,12 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
             if (mFaceAuthOnlyOnSecurityView){
                 updateFaceListeningState();
             }
+            for (int i = 0; i < mCallbacks.size(); i++) {
+                KeyguardUpdateMonitorCallback cb = mCallbacks.get(i).get();
+                if (cb != null) {
+                    cb.onKeyguardBouncerFullyShown(mBouncerFullyShown);
+                }
+            }
         }
     }
 
