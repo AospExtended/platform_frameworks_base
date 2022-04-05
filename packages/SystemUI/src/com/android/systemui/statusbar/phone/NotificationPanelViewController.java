a622e0eb48d5d4c85cbe8e79423086d3be027b5c
Pranav Vashi <neobuddy89@gmail.com>
2022-04-05 14:04:57 +0530

    Properly update view for reTicker
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -4944,6 +4944,7 @@ public class NotificationPanelViewController extends PanelViewController {
             PendingIntent reTickerIntent = row.getEntry().getSbn().getNotification().contentIntent;
             String mergedContentText = reTickerAppName + " " + reTickerContent;
             mReTickerComebackIcon.setImageDrawable(icon);
+            Drawable dw = mView.getContext().getDrawable(R.drawable.reticker_background);
             boolean reTickerColored = Settings.System.getIntForUser(mView.getContext().getContentResolver(),
                     Settings.System.RETICKER_COLORED, 0, UserHandle.USER_CURRENT) != 0;
             if (reTickerColored) {
@@ -4956,11 +4957,13 @@ public class NotificationPanelViewController extends PanelViewController {
                         col = Color.parseColor(mAppExceptions[i+=1]);
                     }
                 }
-                Drawable dw = mView.getContext().getDrawable(R.drawable.reticker_background);
                 dw.setTint(col);
-                mReTickerComeback.setBackground(dw);
+            } else {
+                dw.setTintList(null);
             }
+            mReTickerComeback.setBackground(dw);
             mReTickerContentTV.setText(mergedContentText);
+            mReTickerContentTV.setTextAppearance(mView.getContext(), R.style.TextAppearance_Notifications_reTicker);
             mReTickerContentTV.setSelected(true);
             RetickerAnimations.doBounceAnimationIn(mReTickerComeback);
             mReTickerComeback.setOnClickListener(v -> {
@@ -4993,14 +4996,6 @@ public class NotificationPanelViewController extends PanelViewController {
         } else {
             mReTickerComeback.getViewTreeObserver().removeOnComputeInternalInsetsListener(mInsetsListener);
         }
-    }
-
-    public void reTickerViewUpdate() {
-        boolean reTickerStatus = Settings.System.getIntForUser(mView.getContext().getContentResolver(),
-                Settings.System.RETICKER_STATUS, 0, UserHandle.USER_CURRENT) != 0;
-        if (reTickerStatus) return;
-        if (mReTickerComeback != null) mReTickerComeback.setBackground(mView.getContext().getDrawable(R.drawable.reticker_background));
-        if (mReTickerContentTV != null) mReTickerContentTV.setTextAppearance(mView.getContext(), R.style.TextAppearance_Notifications_reTicker);
     }
 
     public void reTickerDismissal() {
