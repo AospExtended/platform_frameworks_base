498a2e40789a7e0855ef345409e3c9a7c1e2992b
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-04-09 18:50:36 +0530

    fixup! SystemUI: Allow Wi-Fi/cell tiles to co-exist with provider model
    Revert and re-pick from LineageOS[1] to fix broken internet tile.
    
    Squashed:
    
    Revert "SystemUI: Allow Wi-Fi/cell tiles to co-exist with provider model"
    
    This reverts commit 0469eb5d50b19a1a1ae25b4e6ac2dc256a31bf60.
    
    SystemUI: Allow Wi-Fi/cell tiles to co-exist with provider model
    
    The dedicated Wi-Fi/cellular data QS tiles are partially working now
    that we've exposed them in the list of tile options, but they're very
    buggy and somewhat broken when the provider model (unified internet) is
    enabled.
    
    Allow the tiles to co-exist with InternetTile and the provider model
    (including both settings_provider_model and combined signal icons) by
    always including QS icons in Wi-Fi/data indicator callbacks, and add
    a new flag to indicate whether they should be shown in the internet
    tile.
    
    (cdesai, mkbestas: Port to 12L)
    
    Change-Id: I10af134b35dfabdb9275f1aca8ca8512e1db6d27
    
    [1]: LineageOS/android_frameworks_base@05841fd
    Change-Id: I321ad6507914f9776cd105e4ccae5ba695e1f4f0
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
@@ -446,9 +446,11 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
         IconState qsIcon = null;
         CharSequence qsDescription = null;
 
-        boolean pm = mProviderModelSetting || mProviderModelBehavior;
         if (mCurrentState.dataSim) {
-            // If using provider model behavior, only show QS icons if the state is also default
+            if (mProviderModelBehavior) {
+                return new QsInfo(qsTypeIcon, qsIcon, qsDescription);
+            }
+
             if (mCurrentState.showQuickSettingsRatIcon() || mConfig.alwaysShowDataRatIcon) {
                 qsTypeIcon = dataTypeIcon;
             }
@@ -472,7 +474,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
 
         if (mProviderModelBehavior) {
             boolean showDataIconStatusBar = (mCurrentState.dataConnected || dataDisabled)
-                    && mCurrentState.dataSim;
+                    && (mCurrentState.dataSim && mCurrentState.isDefault);
             typeIcon =
                     (showDataIconStatusBar || mConfig.alwaysShowDataRatIcon) ? dataTypeIcon : 0;
             showDataIconStatusBar |= mCurrentState.roaming;
